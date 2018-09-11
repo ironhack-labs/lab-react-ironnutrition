6c20f367c42627b4f19968e76be8542136832520
Tina Reuther <tina.reuther@freenet.de>
2018-09-11 19:32:07 +0200

    Third iteration done

--- src/js/Application.jsx
+++ src/js/Application.jsx
@@ -1,29 +1,59 @@
-import React from 'react';
-import 'bulma/css/bulma.css';
-import foods from '../../foods.json';
+import React from "react";
+import "bulma/css/bulma.css";
+import foods from "../../foods.json";
 import FoodBox from "./FoodBox";
 
 class Application extends React.Component {
-    render() {
-        console.log(foods)
+  constructor(props) {
+    super(props);
 
-        const foodList = foods.map(food => {
-            return (
-                <FoodBox
-                name={food.name}
-                calories={food.calories}
-                image={food.image}
-                quantity={food.quantity}
-                />
-            )
-        })
-        return (
-            <div className="container">
-                <h1>IronNutrition</h1>
-                {foodList}
-            </div>
-        )
-    }
+    this.state = {
+      search: "",
+      foods: foods
+    };
+
+    this._updateSearchChange = this._updateSearchChange.bind(this);
+  }
+
+  render() {
+    const foodList = this.state.foods.map((food, index) => {
+      return (
+        <FoodBox
+          name={food.name}
+          calories={food.calories}
+          image={food.image}
+          quantity={food.quantity}
+          key={"food_" + index}
+        />
+      );
+    });
+    return (
+      <div className="container">
+        <h1>IronNutrition</h1>
+        <input
+          onChange={this._updateSearchChange}
+          className="input"
+          type="text"
+        />
+        {foodList}
+      </div>
+    );
+  }
+
+  _searchFood(filter) {
+    this.setState({
+      foods: foods.filter(el => {
+        return el.name.match(new RegExp(`.*${filter}.*`, "i"));
+      })
+    });
+  }
+
+  _updateSearchChange(event) {
+    this.setState({
+      search: event.target.value
+    });
+    return this._searchFood(event.target.value);
+  }
 }
 
-export default Application
+export default Application;

63821d285e24c8eea49f304aba1d87a188b5c49a
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-25 12:29:09 -0400

    Added foodlist component

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 import 'bulma/css/bulma.css';
@@ -8,15 +8,7 @@ import foods from './foods.json';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
     );
   }
 }

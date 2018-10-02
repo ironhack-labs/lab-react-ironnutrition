74b1529efd9e5619a32a5fdea9cff140c40ca72b
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-10-02 19:10:44 +0200

    Iteration 2. Food Menu done

---
+++ starter-code/src/components/foodmenu.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { FoodBox } from "./foodbox";
+import foods from "../foods.json";
+//name, calories, image, quantity
+
+export class FoodMenu extends Component {
+
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+
+      foods.map((e) => {
+        return (
+          <FoodBox key={e.name} name={e.name} calories={e.calories} image={e.image} quantity="" />
+        )
+      })
+    )
+  }
+}

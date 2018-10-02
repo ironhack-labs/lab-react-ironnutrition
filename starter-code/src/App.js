1164c500b2ac94cb30b74d4b8ce05a0f5bb391aa
Matt Ch. Rocha <mattchrocha@gmail.com>
2018-10-02 21:12:01 +0200

    done without bonus

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,51 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import 'bulma/css/bulma.css';
-import './App.css';
-import foods from './foods.json';
-import FoodBox from './FoodBox';
-import FoodForm from './FoodForm';
-
+import "bulma/css/bulma.css";
+import "./App.css";
+import foods from "./foods.json";
+import FoodBox from "./FoodBox";
+import FoodForm from "./FoodForm";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      foods: foods,
-      // search: "",
-      foodFormVisible: false
-    }
+      displayedFoods: foods,
+      allFoods: foods,
+      foodFormVisible: false,
+      totalCalories: 0,
+      today: []
+    };
   }
 
   showFoodForm = () => {
-    this.setState({foodFormVisible: !this.state.foodFormVisible});
-  }
+    this.setState({ foodFormVisible: !this.state.foodFormVisible });
+  };
 
-  addNewFood = (food) => {
-    this.state.foods.unshift(food)
-    this.setState({foods: this.state.foods, foodFormVisible: !this.state.foodFormVisible})
-  }
+  addNewFood = food => {
+    this.state.allFoods.unshift(food);
+    this.setState({
+      displayedFoods: this.state.allFoods,
+      allFoods: this.state.allFoods,
+      foodFormVisible: !this.state.foodFormVisible
+    });
+  };
 
-  searchFood = (input) => {
-    // if (input == "") return 
-    console.log(input)
-    let filtered = foods.filter(e => e.name.includes(input));
-    console.log(filtered)
-    this.setState({foods: filtered})
+  searchFood = input => {
+    let { allFoods } = this.state;
+    let filtered = allFoods.filter(e => e.name.toLowerCase().includes(input));
+    this.setState({ displayedFoods: filtered });
+  };
+
+  addToday = meal => {
+    
+    this.state.today.push(meal);
+    this.setState({today: this.state.today, totalCalories: this.state.totalCalories + meal.cals})
   }
 
   render() {
     return (
       <main className="section">
         <div className="container">
-        <h1 className="title">IronNutrition</h1>
-        {!this.state.foodFormVisible ?
-          <button className="button" onClick={this.showFoodForm}>Add new food</button>
-          :
-          <FoodForm newFood={food => this.addNewFood(food)}/>
-        }
-        <input className="input" type="text" placeholder="Search 🍖" onChange={(input) => this.searchFood(input.target.value)} />
-          {this.state.foods.map(element => <FoodBox key={element.name} {...element}/> )}
+          <div className="level">
+            <div className="level-left">
+              <h1 className="level-item title">IronNutrition</h1>
+            </div>
+            <div className="level-right">
+              {!this.state.foodFormVisible &&
+                <button className="button is-link" onClick={this.showFoodForm}>
+                  Add new food
+                </button>
+              }
+            </div>
+          </div>
+          <input
+            className="input level"
+            type="text"
+            placeholder="Search 🍖"
+            onChange={input =>
+              this.searchFood(input.target.value.toLowerCase())
+            }
+          />
+          {this.state.foodFormVisible &&
+            <FoodForm newFood={food => this.addNewFood(food)} />
+          }
+          <div className="columns">
+            <div className="column">
+             
+                {this.state.displayedFoods.map(element => (
+                  <FoodBox key={element.name} {...element} addToday={meal => this.addToday(meal)}/>
+                ))}
+              
+            </div>
+            <div className="column">
+                <h3 className="title">Today's foods</h3>
+                <ul>
+                  {this.state.today.map((e,i) => <li key={i}>{e.qty} {e.name} = {e.cals} cal</li> )}
+                </ul>
+                <p>Total: {this.state.totalCalories} cal</p>
+            </div>
+          </div>
         </div>
       </main>
     );
--- starter-code/src/FoodBox.js
+++ starter-code/src/FoodBox.js
@@ -1,35 +1,91 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Foodbox = ({image, name, calories}) => {
-  return (
-    <div className="box">
-      <article className="media">
-        <div className="media-left">
-          <figure className="image is-64x64">
-            <img src={image} alt={name}/>
-          </figure>
-        </div>
-        <div className="media-content">
-          <div className="content">
-            <p>
-              <strong>{name}</strong> <br />
-              <small>{calories}</small>
-            </p>
-          </div>
-        </div>
-        <div className="media-right">
-          <div className="field has-addons">
-            <div className="control">
-              <input className="input" type="number" value="1" />
-            </div>
-            <div className="control">
-              <button className="button is-info">+</button>
-            </div>
-          </div>
-        </div>
-      </article>
-    </div>
-  );
-};
+export default class FoodBox extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: props.name,
+      qty: 1,
+      cals: props.calories,
+      image: props.image
+    }
+  }
 
-export default Foodbox;
+  handleSubmit(){
+    console.log(this)
+    let {name, qty, cals} = this.state;
+    cals = cals * qty
+    this.props.addToday({name, qty, cals})
+  }
+
+  render (){
+    return (
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={this.state.image} alt={this.state.name}/>
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{this.state.name}</strong> <br />
+                <small>{this.state.calories}</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" type="number" value={this.state.qty} onChange={e => this.setState({qty: e.target.value})} />
+              </div>
+              <div className="control">
+                <button className="button is-info" onClick={this.handleSubmit.bind(this)}>+</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    );
+  };
+
+}
+
+
+
+
+
+// const Foodbox = ({image, name, calories}) => {
+//   return (
+//     <div className="box">
+//       <article className="media">
+//         <div className="media-left">
+//           <figure className="image is-64x64">
+//             <img src={image} alt={name}/>
+//           </figure>
+//         </div>
+//         <div className="media-content">
+//           <div className="content">
+//             <p>
+//               <strong>{name}</strong> <br />
+//               <small>{calories}</small>
+//             </p>
+//           </div>
+//         </div>
+//         <div className="media-right">
+//           <div className="field has-addons">
+//             <div className="control">
+//               <input className="input" type="number" value="1" />
+//             </div>
+//             <div className="control">
+//               <button className="button is-info">+</button>
+//             </div>
+//           </div>
+//         </div>
+//       </article>
+//     </div>
+//   );
+// };
+
+// export default FoodBox;

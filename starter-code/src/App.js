b2f410d12f9cc053a006857cbe0ab1401406556b
wergilia <weronika.zdrojewska@gmail.com>
2018-10-02 19:22:07 +0200

    Interation 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import FoodBox from './FoodBox.js'
 
 class App extends Component {
   render() {
@@ -10,9 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          < FoodBox />
       </div>
     );
   }

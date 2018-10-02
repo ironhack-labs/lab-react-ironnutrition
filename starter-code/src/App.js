5d38d4677a9cde0ee60d059c4d187dfe2dcfed34
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-10-02 19:09:53 +0200

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import FoodBox from './components/FoodBox';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      foods: foods
+    };
+  }
   render() {
+    console.log(this.state.foods);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div id="allFoods">
+          {this.state.foods.map((e,i)=>{
+            return(
+            <FoodBox
+            key = {e.name}
+            name = {e.name}
+            calories = {e.calories}
+            image = {e.image}
+            />)
+          })}
+        </div>
+        
       </div>
     );
   }

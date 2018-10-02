bf3e350c81a2466d69a853f09a052595a2e774c3
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-10-02 20:58:33 +0200

    Add new food

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,19 +3,33 @@ import './App.css';
 import FoodBox from "./FoodBox";
 import "bulma/css/bulma.css"
 import foods from "./foods.json"
+import Form from "./Form"
 class App extends Component {
   constructor(){
     super()
     this.state={foods:foods.map(e=>{
       e.quantity=1
+      e.value=""
       return e
     })}
   }
-  
+  addFood=(food)=>{
+    this.state.foods.unshift(food)
+    this.setState({foods:this.state.foods})
+  }
   render() {
     return (
-      <div>
-        {this.state.foods.map((f)=> <FoodBox key={f.name} {...f}/>)}
+      <div className="container">
+        <h1 className="title">IronNutrition</h1> 
+        <div>
+          <input type="text" className="input search-bar" name="search" placeholder="Search"/>
+        </div>
+        <div className="columns">
+          <div className="column">
+          <Form onAddFood={food=>this.addFood(food)}></Form>
+          {this.state.foods.map((f)=> <FoodBox key={f.name} {...f}/>)}
+          </div>
+        </div>
       </div>
     );
   }

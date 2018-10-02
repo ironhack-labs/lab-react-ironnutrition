ed3d0240f5834d80edbb3d1dfc2dafbd7bc784b3
Evenstarw1 <lauracorrallopez@gmail.com>
2018-10-03 01:15:36 +0200

    Tercera ite

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,20 +4,49 @@ import 'bulma/css/bulma.css';
 import './App.css';
 import foods from './foods.json'
 import FoodBox from './FoodBox.js'
+import Form from './Form.js'
+
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Food: foods,
+      showingForm: false
+
+    };
+  }
+
+  showForm = () => {
+    this.setState({
+      showingForm: !this.state.showingForm
+    })
+  };
+
+  addFood = (food) => {
+    this.state.FoodBox.unshift(food);
+    this.setState({Food: this.state.Food, showingForm: !this.state.showingForm
+    });
+  };
+
   render() {
     return (
       <div>
-      
         {foods.map((e) => {
           return <FoodBox class="column content" key={e.name} {...e} />
         })}
-        {/* <button onClick={()=> ()}>Add new food</button> */}
-    </div>
-    );    
-  }
-}
+      <div>
+        {!this.state.showingForm && (
+        <button onClick={this.showForm}> Add food!! </button>)} 
+        </div>   
+        {this.state.showingForm && (
+          <Form newFoods = {food =>this.addFood(food)}/>
+        )}
+    </div>  
+  );
+}}
+
+
 
 export default App;
---
+++ starter-code/src/Form.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            calories: '',
+            image: '',
+        }
+    }
+
+    handleSubmit = () => {
+        let {name, calories, image, error} = this.state;
+          if (name === '') return this.setState({error: 'Empty name'});
+          if (calories === '') return this.setState({error: 'Empty colories'});
+          if (image === '') return this.setState({error: 'Empty image'});
+        this.props.onSubmit(this.state)
+        this.setState({
+            name: '',
+            calories: '',
+            image: '',
+        })
+    }
+render() {
+        let {name, calories, image, error} = this.state;
+        return (
+            <form>
+                <input value={name} type="text" placeholder="Name" onChange={e => this.setState({ name: e.target.value })} />
+                <input value={calories} type="text" placeholder="Calories" onChange={e => this.setState({ calories: e.target.value })} />
+                <input value={image} type="text" placeholder="Image" onChange={e => this.setState({ image: e.target.value })} />
+                <button onClick={this.handleSubmit.bind(this)}>Submit Food</button>
+            </form>
+
+        )
+    }
+}
+
+export default Form

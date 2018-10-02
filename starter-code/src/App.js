2dddecffcf65cb43bc6d678c7d9e262322c48517
Alfonso Mateo Riego <fonriego@gmail.com>
2018-10-02 20:58:23 +0200

    Iteration 2 Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import foods from './foods.json'
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import {SearchBar} from './components/SearchBar';
+import {FoodBox} from './components/FoodBox';
 
 class App extends Component {
   render() {
@@ -10,9 +14,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="container">
+          <h1 class="title">IronNutrition</h1>
+          {/* <SearchBar /> */}
+          <div class="columns">
+            <div class="column">  
+              {foods.map((e) => <FoodBox key={e.name} food={e}/>)}          
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

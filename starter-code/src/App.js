b6a098e13a4ca9fb4c239af5220a8c690e6b635e
Mapno <mariorvinas@gmail.com>
2018-10-03 09:37:23 +0200

    added button

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import foods from './foods.json';
 import FoodBox from './FoodBox';
+import Button from './Button';
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      foodList: foods,
+    }
+  }
+
+  
+
   render() {
-    let foodList = foods;
     return (
       <div className="App">
         <div className="container">
           <h1>Ironnutrition</h1>
           <div className="columns">
             <div className="column">
-              {foodList.map(e => <FoodBox name={e.name} calories={e.calories} image={e.image}></FoodBox>)}
+              {this.state.foodList.map(e => <FoodBox name={e.name} calories={e.calories} image={e.image}></FoodBox>)}
             </div>
             <div className="column">
+              <Button onClick={this.openForm}>Add new food</Button>
               <h2>Today's foods</h2>
             </div>
           </div>

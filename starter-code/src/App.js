de7cccc42d9bb7079f64460a887e714dc231b3ba
pablotaboada <pablo.tabu@hotmail.com>
2018-10-03 09:41:00 +0200

    itineration 3 and bonus 6

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import {FoodBox} from './component/FoodBox';
+import FoodForm from "./component/FoodForm";
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(){
+    super();
+    this.state = {
+      foods: foods,
+      foodFormState: false
+    }
   }
+  showFoodForm = () => {
+    this.setState({foodFormVisible: !this.state.foodFormVisible});
+  }//para que muestre el formulario de foodForm
+
+
+  addFood = (food) =>{
+    this.state.foods.unshift(food);
+    this.setState({foods:this.state.foods})
+  }
+
+
+deleteFood = (i) => {
+  let copy = [...this.state.foods]
+  copy.splice(i, 1);
+  this.setState({
+    foods: copy
+  })
+};
+ 
+render(){
+  return (
+      <div style={{padding:'50px 0'}}>
+        <div>
+          <button onClick={ () => this.addFood(foods)}>Add new food</button>
+          <FoodForm allFood={food => this.addFood(food)}/>      
+           {this.state.foods.map((e,i) => {
+                return (
+                  <div>
+                    <FoodBox key={i} {...e}/>
+                    <button onClick={ () => {
+                      console.log(i)
+                      this.deleteFood(i)}}>Delete</button>
+                  </div>
+                )
+              })
+           }
+        </div>
+      </div>
+  )
+ } 
+
+
+
 }
 
 export default App;

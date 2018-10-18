1ad647e68f8624c7e5a2be40fcadb77eb531e07f
HananeDUP <hanane.moualil@gmail.com>
2018-10-18 21:49:32 +0200

    iteration 3 in progress

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json'
+
+class FoodTag extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64"> <img src={this.props.image} /></figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{this.props.name}</strong> <br />
+                <small>{this.props.calories}</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" type="number" value="1" />
+              </div>
+              <div className="control">
+                <button className="button is-info">
+                  +
+      </button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    )
+  }
+}
+
+
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={foods, displayForm: false}
+  }
+
+  toggleTitle() {
+    this.setState({ displayForm: !this.state.displayForm })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +60,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+        {this.state.displayForm ? (<form><input type="text" name="food" value="name"></input></form>): ''}
+        <button onClick={this.toggleTitle.bind(this)}>{this.state.displayForm ? 'Submit new food' : 'Add new food'}</button>
+        </div>
+        {
+          this.state.foods.map(food => {
+            return (
+              <FoodTag name={food.name} calories={food.calories} image={food.image}/>
+            )
+          }
+
+          )
+        }
+        <FoodTag name/>
       </div>
     );
   }
--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bulma/css/bulma.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'));
 registerServiceWorker();

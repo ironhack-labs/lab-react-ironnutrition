1b5d638ccc11ba5426d140e616c3f24382b7188d
PomPom248 <sofiroggero@gmail.com>
2018-12-04 20:09:17 +0100

    hide/show form

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,23 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import FoodBox from "./FoodBox/food.js"
 import Food from "./foods.json"
-// import Form from "./Form/Form.js"
+import Form from "./Form/Form.js"
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      show: false,
       foods: Food.map(foods => {
         return <FoodBox {...foods} />
       })
     }
   }
+  toggle = () => {
+    let show = !this.state.show 
+    this.setState({ show: show })
+  }
   render() {
     return (
       <div>
         <h1>IronNutrition</h1>
-        {/* <Form></Form> */}
-        {/* <FoodBox></FoodBox> */}
+        <button onClick={this.toggle}>Add Food</button>
+        {this.state.show && <Form />}
         {this.state.foods}
       </div>
     );
--- starter-code/src/Form/Form.js
+++ starter-code/src/Form/Form.js
@@ -1,26 +1,40 @@
 import React from 'react';
+import "./Form.css"
 
 class Form extends React.Component {
     constructor() {
         super()
-        // this.state = {
-        //     name,
-        //     calories,
-        //     image
-        // }
+        this.state = {
+            name: "",
+            calories: "",
+            image: ""
+        }
     }
+    handleNameInput = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+    handleCaloriesInput = (e) => {
+        this.setState({
+            calories: e.target.value
+        })
+    }
+
+
     render() {
         return (
             <div>
+
                 <form>
                     <label>Name:</label>
-                    <input type="text" name="title" value={this.state.name} /> <br /><br />
+                    <input type="text" name="name" value={this.state.name} onChange={(e) => this.handleNameInput(e)} /> <br /><br />
 
-                    <label>Number of Caloriess:</label>
-                    <input type="text" name="calories" value={this.state.calories} /><br /><br />
+                    <label>Number of Calories:</label>
+                    <input type="text" name="calories" value={this.state.calories} onChange={(e) => this.handleCaloriesInput(e)} /><br /><br />
 
                     <label>Image:</label>
-                    <input type="file" name="image" value={this.state.image} /><br /><br />
+                    <input type="file" name="image" value={this.state.image} onChange={(e) => this.handleImageInput(e)} /><br /><br />
 
                     <input type="submit" value="Submit" /><br /><br />
                 </form>

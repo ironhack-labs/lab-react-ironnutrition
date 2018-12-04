ac76b65b978c031e6b9d5d55b580dcdbc4b36194
Sonia Ruiz <yune.vk@gmail.com>
2018-12-04 16:39:25 +0100

    Iteration 2 | Display food

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -7,11 +7,24 @@ import foods from './foods.json'
 import FoodBox from './components/FoodBox/FoodBox'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foods: foods
+    }
+  }
+
   render() {
+    console.log(this.state.foods);
+
+    const foodsHTML = this.state.foods.map(food => <FoodBox {...food} /> );
+
+
     return (
       <div className="App container">
         <h1 className="title">IronNutrition</h1>
-        <FoodBox />
+        {foodsHTML}
       </div>
     );
   }

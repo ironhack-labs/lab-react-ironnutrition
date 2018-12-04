ca92559a35f8550e22ef552ee8f31a6128022d38
SandraLuengo <luchosandra@gmail.com>
2018-12-04 17:49:56 +0100

    Iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import Foodbox from './components/FoodBox/foodBox'
 
 class App extends Component {
   render() {
@@ -13,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Foodbox/>
       </div>
     );
   }

9f5675d5db256f456cca54be8ffb3f871c8d791d
cocobilbao <cocobilbao@gmail.com>
2018-12-04 19:11:08 +0100

    iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import FoodBox from './components/FoodBox/FoodBox.js'
+
+
+
 class App extends Component {
   render() {
     return (
@@ -10,9 +14,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+       <FoodBox/>
+
       </div>
     );
   }

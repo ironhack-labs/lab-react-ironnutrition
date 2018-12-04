e832a9cccc96a2b6a4936ae26a9d581df7d29e12
Yared Santiago <yaredmyers@gmail.com>
2018-12-04 18:38:56 +0100

    iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import Foodbox from './components/Foodbox/Foodbox.js'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      foods: foods
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -12,7 +21,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
+        {this.state.foods.map((food, index)=> <Foodbox key={index} {...food} />)}
       </div>
     );
   }

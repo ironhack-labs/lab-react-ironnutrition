934a6640d376ca8b57ada9284a9cc72bcb89d852
jrecio <recio.juan@gmail.com>
2018-12-04 19:10:08 +0100

    Iteration 1 & 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox/FoodBox'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      foods: foods
+    }
+  }
   render() {
+
+    let foodboxs=this.state.foods.map((food,index)=>{
+      return (<FoodBox name={food.name} quantity={food.quantity} img={food.image} cal={food.calories} id={index}/>)
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {foodboxs}
       </div>
     );
   }
---
+++ starter-code/src/components/FoodBox/FoodBox.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+export default class FoodBox extends Component {
+  render() {
+    return (
+      <div className="box" key={this.props.id}>
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={this.props.img} />
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{this.props.name}</strong> <br />
+                <small>{this.props.cal} cal</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input
+                  className="input"
+                  type="number"
+                  value="1"
+                />
+              </div>
+              <div className="control">
+                <button className="button is-info">
+                  +
+          </button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    )
+  }
+}

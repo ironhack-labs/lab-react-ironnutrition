092bbd871e692ff2290d9aab74bd9d9b6965363f
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-12-05 00:03:59 +0100

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -15,12 +15,33 @@ class App extends Component {
       }
     }
 
+    handleSubmit(event) {
+      event.preventDefault();
+    }
+  
 
 
   render() {
 
     return (
       <div className="foodBoxContainer">
+
+        <div className="box">
+          
+          <form onSubmit={this.handleFormSubmit}>
+              <label>Name:</label>
+              <input type="text" name="name" value={this.state.title} />
+
+              <label>Calories:</label>
+              <input type="text" name="cal" value={this.state.director} />
+
+              <label>Image:</label>
+              <input type="checkbox" name="img" checked={this.state.hasOscars} />
+              
+              <input type="submit" value="Submit" />
+          </form>
+        </div>
+
 
         {this.state.foods.map(plate => <FoodBox name={plate.name} cal={plate.calories} img={plate.image} quant={plate.quantity}/>)}
       
---
+++ starter-code/src/components/Form/form.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      name: '',
+      cal: '',
+      img: '',
+    }
+  }
+
+  
+render () {
+  return (
+    
+  )
+  }
+}
+
+
+export default FoodBox;

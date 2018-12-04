0694c189c1364c10241b1bd1742142aec44941c6
Jorge Devesa <jrgdevesa@gmail.com>
2018-12-04 18:44:04 +0100

    Iteration 1 done

---
+++ starter-code/src/Component/FoodBox/FoodBox.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import 'bulma/css/bulma.css';
+// import foods from './foods.json'
+
+class FoodBox extends Component{
+    constructor(){
+        super()
+    }
+    
+    render(){
+        return (
+            <div className="box">
+  <article className="media">
+    <div className="media-left">
+      <figure className="image is-64x64">
+        <img src="https://i.imgur.com/eTmWoAN.png" />
+      </figure>
+    </div>
+    <div className="media-content">
+      <div className="content">
+        <p>
+          <strong>Pizza</strong> <br />
+          <small>400 cal</small>
+        </p>
+      </div>
+    </div>
+    <div className="media-right">
+      <div className="field has-addons">
+        <div className="control">
+          <input
+            className="input"
+            type="number" 
+            value="1"
+          />
+        </div>
+        <div className="control">
+          <button className="button is-info">
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  </article>
+</div>
+        )
+    }
+}
+
+export default FoodBox

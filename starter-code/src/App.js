579fe5c4328ac1bbc4fc91cb73b9a1be59c3f152
Xiaomi Notebook Pro <xiaomi@gmailcom>
2018-12-04 23:25:35 +0100

    Iteration 5 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,7 +6,7 @@ import foods from "./foods.json";
 
 import FoodBox from "./components/FoodBox";
 import AddFood from "./components/AddFood";
-import SearchBar from './components/SearchBar';
+import SearchBar from "./components/SearchBar";
 
 class App extends Component {
   constructor() {
@@ -14,37 +14,49 @@ class App extends Component {
     this.state = {
       foods: foods,
       foodsFiltered: [],
+      ingredientsList: [],
       formShowed: false
     };
   }
 
-  componentWillMount =() =>{
-    this.setState({foodsFiltered: this.state.foods})
-  }
+  componentWillMount = () => {
+    this.setState({ foodsFiltered: this.state.foods });
+  };
 
   toggleForm = () => {
     let show = !this.state.formShowed;
     this.setState({ ...this.state, formShowed: show });
   };
 
-  addFoodHandler = (theFood) => {
+  addFoodHandler = theFood => {
     const foodCopy = [...this.state.foods];
     foodCopy.push(theFood);
     this.setState({
       foods: foodCopy,
       formShowed: false
-    })
-  }
-
-  filterList = (event) => {
-    console.log(event.target.value.toLowerCase())
-    var updatedList = this.state.foods;
-    updatedList = updatedList.filter(function(food){
-      return food.name.toLowerCase().search(
-        event.target.value.toLowerCase()) !== -1;
     });
-    this.setState({foodsFiltered: updatedList});
-  }
+  };
+
+  addIngredientList = theIngredient => {
+    if (theIngredient.quantity > 0) {
+      const ingredientCopy = [...this.state.ingredientsList];
+      ingredientCopy.push(theIngredient);
+      this.setState({
+        ingredientsList: ingredientCopy
+      });
+    }
+  };
+
+  filterList = event => {
+    console.log(event.target.value.toLowerCase());
+    var updatedList = this.state.foods;
+    updatedList = updatedList.filter(function(food) {
+      return (
+        food.name.toLowerCase().search(event.target.value.toLowerCase()) !== -1
+      );
+    });
+    this.setState({ foodsFiltered: updatedList });
+  };
 
   render() {
     console.log();
@@ -53,24 +65,47 @@ class App extends Component {
       <React.Fragment>
         <div className="App">
           <h1 className="title">IronNutrition</h1>
-          <SearchBar filterList={this.filterList}></SearchBar>
-          <div className='buttonAdd'>
+          <SearchBar filterList={this.filterList} />
+          <div className="buttonAdd">
             <a onClick={this.toggleForm} className="button is-primary">
               Add new food
             </a>
           </div>
 
-          {this.state.formShowed && <AddFood addFoodHandler={this.addFoodHandler} />}
+          {this.state.formShowed && (
+            <AddFood addFoodHandler={this.addFoodHandler} />
+          )}
 
-          {this.state.foodsFiltered.map((food, i) => (
-            <FoodBox
-              key={i}
-              name={food.name}
-              img={food.image}
-              calories={food.calories}
-              quantity={food.quantity}
-            />
-          ))}
+          <div className="mainSection">
+            <div className="foodSection">
+              {this.state.foodsFiltered.map((food, i) => (
+                <FoodBox
+                  key={i}
+                  name={food.name}
+                  img={food.image}
+                  calories={food.calories}
+                  quantity={food.quantity}
+                  addIngredientList={this.addIngredientList}
+                />
+              ))}
+            </div>
+            <div className="listSection">
+              <h1>Today's foods</h1>
+              <ul>
+              {this.state.ingredientsList.map((ingredient, i) => (
+                <li key={i}>
+                  {ingredient.name} = {ingredient.calories} cal
+                </li>
+              ))}
+              </ul>
+              <h1>
+                Total:{" "}
+                {this.state.ingredientsList.reduce((acum, current) => {
+                  return acum + current.calories;
+                }, 0)}
+              </h1>
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
--- starter-code/src/components/FoodBox/FoodBox.js
+++ starter-code/src/components/FoodBox/FoodBox.js
@@ -1,43 +1,61 @@
 import React, { Component } from "react";
-import './FoodBox.css'
+import "./FoodBox.css";
 
 export default class FoodBox extends Component {
 
-    constructor(props) {
-        super(props);
-        this.props=props 
-    }
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      quantity: 0
+    };
+  }
+
+  handleAddIngredient = (ingredient) => {
+    this.props.addIngredientList(ingredient);
+  }
+
+  handleChange(event) {
+    let { name, value } = event.target;
+    this.setState({[name]: value});
+  }
 
   render() {
     return (
       <React.Fragment>
         <div className="box">
-            <article className="media">
-              <div className="media-left">
-                <figure className="image is-64x64">
-                  <img src={this.props.img}/>
-                </figure>
+          <article className="media">
+            <div className="media-left">
+              <figure className="image is-64x64">
+                <img src={this.props.img} />
+              </figure>
+            </div>
+            <div className="media-content">
+              <div className="content">
+                <p>
+                  <strong>{this.props.name}</strong> <br />
+                  <small>{this.props.calories} cal</small>
+                </p>
               </div>
-              <div className="media-content">
-                <div className="content">
-                  <p>
-                    <strong>{this.props.name}</strong> <br/>
-                    <small>{this.props.calories} cal</small>
-                  </p>
+            </div>
+            <div className="media-right">
+              <div className="field has-addons">
+                <div className="control">
+                  <input
+                    type="number"
+                    className="input"
+                    name = 'quantity'
+                    value={this.state.quantity}
+                    onChange={e => this.handleChange(e)}
+                  />
+                </div>
+                <div className="control">
+                  <button onClick={()=>this.handleAddIngredient({...this.props, calories: this.props.calories * this.state.quantity ,quantity: +this.state.quantity})} className="button is-info">+</button>
                 </div>
               </div>
-              <div className="media-right">
-                <div className="field has-addons">
-                  <div className="control">
-                    <input type="number" className="input" value={this.props.quantity}/>
-                  </div>
-                  <div className="control">
-                    <button className="button is-info">+</button>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </div>
+            </div>
+          </article>
+        </div>
       </React.Fragment>
     );
   }

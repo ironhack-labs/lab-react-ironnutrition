125aaed13e599cb8992a82a999bd80235b139c5b
David <davidmunozvi@icloud.com>
2018-12-04 19:42:58 +0100

    Iteration 2 Display Food

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,15 +4,24 @@ import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json'
 import FoodBox from './components/FoodBox/FoodBox.js'
-
+let foodsCreated = foods;
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        foods: foodsCreated
+    }
+  }
   render() {
     return (
       <div className="App">
-        <FoodBox />
+       
+        { this.state.foods.map((food,index) => 
+         <FoodBox key={index} image={food.image}  name={food.name} calories={food.calories} quantity = {food.quantity}  />) 
+      }
       </div>
     );
   }
-}
 
+}
 export default App;

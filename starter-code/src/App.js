92b6dd36b48c7e6503635fdd848db4fb12c5b484
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-12-04 18:53:42 +0100

    Iteration I done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json'
+
+
 
 class App extends Component {
+
+    constructor() {
+      super()
+    }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <div className="box">
+  <article className="media">
+    <div className="media-left">
+      <figure className="image is-64x64">
+        <img src="https://i.imgur.com/eTmWoAN.png" />
+      </figure>
+    </div>
+    <div className="media-content">
+      <div className="content">
+        <p>
+          <strong>Pizza</strong> <br />
+          <small>400 cal</small>
         </p>
+      </div>
+    </div>
+    <div className="media-right">
+      <div className="field has-addons">
+        <div className="control">
+          <input
+            className="input"
+            type="number" 
+            value="1"
+          />
+        </div>
+        <div className="control">
+          <button className="button is-info">
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  </article>
+</div>
       </div>
     );
   }

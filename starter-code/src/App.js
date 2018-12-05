f2d8bd8bb61fb7a69161d5eaac666c4659137dc1
mattviy <mrmhamers@gmail.com>
2018-12-05 16:09:21 +0100

    finalized funcionality of excercise without styleing

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import Foodbox from './FoodBox';
+import foods from './foods.json'
+
 
 class App extends Component {
+  state = {
+    search: "",   
+    foods: foods,
+
+  }
+
+  handleAdd = (event) =>{
+    var newFoodAtt = {}
+    var keyName = event.target.name
+    var inputValue = event.target.value
+    newFoodAtt[keyName] =  inputValue
+
+    this.setState(newFoodAtt)
+  }
+
+  addFoodItem = () => {
+    var currentFoods = this.state.foods; 
+    var newFoodItem = {
+      name: this.state.name,
+      calories: this.state.calories,
+      image: this.state.image,
+      quantity: 0
+    }
+    currentFoods.push(newFoodItem);
+    this.setState({foods: currentFoods})
+  }
+
+  handleChange = (event) => {
+    this.setState({search: event.target.value}) 
+  }
+  
   render() {
+    
+    var foodBoxes = this.state.foods
+    .filter((food)=> 
+      food.name.toLowerCase().includes(this.state.search.toLowerCase())
+    )
+    .map((food)=> <Foodbox calories={food.calories} name={food.name} image={food.image}/>)
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={this.handleChange} name="search" placeholder={this.state.search} placeholder="search"/>
+        <div>
+          <input type="text" name="name" onChange={this.handleAdd} placeholder="name"/>
+          <input type="text" name="image" onChange={this.handleAdd} placeholder="image"/>
+          <input type="text" name="calories" onChange={this.handleAdd} placeholder="calories"/>
+        </div>
+        <button onClick={this.addFoodItem}>Add Food</button>
+        {foodBoxes}
       </div>
     );
   }
---
+++ starter-code/src/addFoodForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+import 'bulma/css/bulma.css';
+
+class AddFoodForm extends Component {
+    toggleModal() {
+        
+    }
+    
+    
+    render (){
+
+    }
+}

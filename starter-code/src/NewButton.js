fcc6e931fa569ee149f835d847f5fc9c3aa224bc
Ross Gravitte <Rbgravitte@gmail.com>
2018-12-05 11:05:50 -0500

    the form is toggling but i cant input anything into add new food text-area

--- starter-code/src/NewButton.js
+++ starter-code/src/NewButton.js
@@ -6,7 +6,8 @@ class NewButton extends Component{
   state = {
     //add array of foods to state
     // foods: foods,
-    addFood: false,
+    addFood: true,
+    nameInput: '',
   }
    addNewFood = ()=>{
     this.setState({
@@ -14,14 +15,47 @@ class NewButton extends Component{
       //make copy of food array
     })
     console.log(this.state.addFood);
+  }  
+        
+        // <form>
+        // <input type='text' placeholder='add a food' />
+        // <input type='text' placeholder='how many calories' />
+        // <input type='text' placeholder='add an image URL' />
+        // </form>
+        
       
-    }
-
+    // addForm = ()=>{
+      // if(this.state.addFood = true){
+      // return(
+      //   <form>
+      //   <input type='text' placeholder='add a food' />
+      //   <input type='text' placeholder='how many calories' />
+      //   <input type='text' placeholder='add an image URL' />
+      //   </form>
+      
+      // )
+      // }
+    
+      functionForAddingFood = (theEvent)=>{
+        this.setState({nameInput: theEvent.target.value})
+      }
+    
+      
+    
+  
   
 render(){
 
-
-  
+if(this.state.addFood === false){
+  return(
+  <form>
+        <input type='text' placeholder='add a food' value={this.state.nameInput} onChange={(nameInput)=>this.functionForAddingFood()} />
+        <input type='text' placeholder='how many calories' />
+        <input type='text' placeholder='add an image URL' />
+        <input type='submit' placeholder='button' />
+  </form>)
+}
+  // this is where i will add the form -- does it have to be equal to a variable?
   
  const btn = <button onClick={()=>this.addNewFood()}>Add new food</button>
   
@@ -29,9 +63,12 @@ render(){
 
 
   return(
+
+   
     <div>{btn}</div>
     
-    
+    //i have to add an if statement that checks the state of addFood if it is true then add the form
+
 
   )
  

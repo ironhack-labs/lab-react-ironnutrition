a87951c5243b18f135a15c6b79e5bf320a4b34a7
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-12-05 01:07:25 +0100

    Iteration 3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import foods from './foods.json'
 import FoodBox from './components/FoodBox/FoodBox'
+import Form from './components/Form/Form'
 
 
-class App extends Component {
+export default class App extends Component {
 
     constructor() {
       super()
@@ -15,34 +15,23 @@ class App extends Component {
       }
     }
 
-    handleSubmit(event) {
-      event.preventDefault();
+    addFoodHandler = (theFood) => {
+      const foodCopy = [...this.state.foods];
+      foodCopy.push(theFood);
+      this.setState({
+        foods: foodCopy
+      })
     }
-  
-
 
   render() {
 
     return (
-      <div className="foodBoxContainer">
+      <div className="container">
 
-        <div className="box">
-          
-          <form onSubmit={this.handleFormSubmit}>
-              <label>Name:</label>
-              <input type="text" name="name" value={this.state.title} />
-
-              <label>Calories:</label>
-              <input type="text" name="cal" value={this.state.director} />
-
-              <label>Image:</label>
-              <input type="checkbox" name="img" checked={this.state.hasOscars} />
-              
-              <input type="submit" value="Submit" />
-          </form>
-        </div>
-
-
+        {/* <button onClick={() => this.toggle() }>
+            {isForm ? hide : show }
+        </button> */}
+        <Form addTheFood={this.addMovieHandler} />
         {this.state.foods.map(plate => <FoodBox name={plate.name} cal={plate.calories} img={plate.image} quant={plate.quantity}/>)}
       
       </div>
@@ -50,4 +39,4 @@ class App extends Component {
   }
 }
 
-export default App;
+
--- starter-code/src/components/Form/form.js
+++ starter-code/src/components/Form/form.js
@@ -11,13 +11,53 @@ class Form extends Component {
     }
   }
 
+
+handleFormSubmit = (event) => {
+  event.preventDefault();
+  this.props.addTheFood(this.state);
+  this.setState({     
+    name: '',
+    cal: '',
+    img: ''
+  })     
+}
+
+  handleChange = (e) => {
+    let { name, cal, img} = event.target;
+    if()
+  }
+
+  // handleCalInput = (e) => {
+  //   this.setState({
+  //     cal: e.target.value
+  //   })
+  // }
+  
+  // handleImgInput = (e) => {
+  //   this.setState({
+  //     img: e.target.value
+  //   })
+  // }
   
 render () {
   return (
-    
-  )
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+            <label>Name:</label>
+            <input type="text" name="title" value={this.state.name} onChange={(e) => this.handleChange(e)}/>
+
+            <label>Calories:</label>
+            <input type="text" name="cal" value={this.state.cal} onChange={(e) => this.handleChange(e)}/>
+
+            <label>Image:</label>
+            <input type="checkbox" name="img" checked={this.state.img} onChange={(e) => this.handleChange(e)} />
+
+            <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
   }
 }
 
 
-export default FoodBox;
+export default Form;

5df423cb75ae7883fa42e1b10a92040e90e6fc7c
Fernando Hernandez <fernandohernandez@MacBook-Pro-de-Fernando.local>
2018-12-11 17:58:24 -0600

    iteracion 1 y 2

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'antd/dist/antd.css'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

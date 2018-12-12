29396e76495cebe14d31ce976c7989cc50d2c279
MefitHp <mefitDev@gmail.com>
2018-12-11 19:53:13 -0600

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import './App.css'
+import foods from './foods.json'
+import FoodBox from './components/FoodBox'
+import AddFoodForm from './components/AddFoodForm';
 
 class App extends Component {
+  state = {
+    foods: [],
+    newFood: {},
+    todaysFood: []
+  }
+
+  componentDidMount = () => {
+    this.setState({ foods })
+  }
+
+  onAdd = (e) => {
+    const { foods, todaysFood } = this.state
+    const toList = {
+      name: foods[e.target.name].name,
+      qty: foods[e.target.name].quantity,
+      calories: foods[e.target.name].quantity * foods[e.target.name].calories
+    }
+
+    todaysFood.push(toList)
+    this.setState({ todaysFood })
+  }
+
+  onChangeQuantity = (e) => {
+    const { foods } = this.state
+    foods[e.target.name].quantity = e.target.value
+    this.setState({ foods })
+  }
+
+  drawFoodBox = () => {
+    const { foods } = this.state
+    return foods.map((box, index) => <FoodBox key={index} arrayPos={index} {...box} onChangeQuantity={this.onChangeQuantity} onAdd={this.onAdd} />)
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    const { foods, newFood } = this.state
+    newFood.quantity = 0
+    foods.push(newFood)
+
+    this.setState({ foods })
+  }
+
+  onChange = (e) => {
+    const { newFood } = this.state
+    newFood[e.target.name] = e.target.value
+    this.setState({ newFood })
+  }
+  onSearch = (e) => {
+    let { foods } = this.state
+    const searchQuery = e.target.value.toUpperCase()
+    foods = foods.filter((food) => {
+      return food.name.toUpperCase().includes(searchQuery);
+    })
+    this.setState({ foods })
+  }
+
+
+
   render() {
+    const { drawFoodBox, onSubmit, onChange, onSearch } = this
+    const { newFood, todaysFood } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <h1>Iron nutrition</h1>
+        <input type="text" onChange={onSearch} name="searchBar" />
+        <AddFoodForm onSubmit={onSubmit} onChange={onChange}  {...newFood} />
+        <div className="columns">
+          <div className="column">
+            {drawFoodBox()}
+          </div>
+          <div className="column">
+            <h3>Today's food</h3>
+            <ul>
+              {todaysFood.map((item, aux) => <li key={aux}> {item.qty} - {item.name} Calories: {item.calories}</li>)}
+              {
+                '0' || todaysFood.reduce((a, b) => ({ calories: a.calories + b.calories }))
+              }
+            </ul>
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }

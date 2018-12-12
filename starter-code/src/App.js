73aabdc6ae60893ac88b91ac632d2b3da0dde4e6
Omar Avelar <omar.avelar.f@gmail.com>
2018-12-12 09:28:55 -0600

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,15 +2,41 @@ import React, { Component } from 'react';
  import logo from './logo.svg';
  import './App.css';
  import foods from './foods.json';
- 
- class App extends Component {
-   constructor() {
-     super()
-     this.state = {
-       foods: foods,
-       showForm: false
-     }
-   }
+
+
+class App extends Component {
+  
+  state = { }
+  
+componentWillMount(){
+let list = [];
+for (var i = 0; i < 5; i++) {
+  list.push(foods[i])
+}
+this.setState({foods: list})
+}
+
+
+  render() {
+    console.table(this.state.foods)
+    return (
+      <div className="App">
+          <h1>Ironfood</h1>
+         <table>
+           <thead>
+             <tr>
+            
+             </tr>
+           </thead>
+           <tbody>
+   
+          </tbody>
+         </table>
+      </div>
+    );
+  }
+}
+
 
    addNewFood = function (name,cal,imgPath){
        let foodsCopia=[...this.state.foods]
@@ -27,6 +53,6 @@ import React, { Component } from 'react';
   
 
    
- }
+ 
  
  export default App;

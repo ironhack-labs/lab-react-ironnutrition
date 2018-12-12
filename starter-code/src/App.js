f7841752f0369e292bc96aeed1052f604b68ccf3
sabasm <smma1992@gmail.com>
2018-12-12 10:38:52 -0600

    Bliss code added, need to make it work

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,75 @@
-import React, { Component } from 'react';
-import './App.css';
-import Food from './components/food'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import foods from "./foods.json";
+import FoodBox from "./FoodBox";
+
 class App extends Component {
+  state = {
+    total: 0,
+    foods,
+    selected: []
+  };
+
+  searchFood = e => {
+    const text = e.target.value;
+    const regEx = RegExp(text, "i");
+    const filtered = foods.filter(f => regEx.test(f.name));
+    this.setState({ foods: filtered });
+  };
+
+  selectFood = (name, calories, quantity) => {
+    let { selected, total } = this.state;
+    const s = {
+      name,
+      calories: calories * Number(quantity),
+      quantity
+    };
+    //revisar que ya exista
+    const item = selected.find(f => f.name === name);
+    if (item) selected.splice(selected.indexOf(item), 1);
+    selected.unshift(s);
+    //sumo
+    total = selected.reduce((acc, item) => acc + item.calories, 0);
+    this.setState({ selected, total });
+  };
+
+  deleteFood = item => {
+    let { selected, total } = this.state;
+    selected = selected.filter(f => f.name !== item.name);
+    total = selected.reduce((acc, item) => acc + item.calories, 0);
+    this.setState({ selected, total });
+  };
+
   render() {
+    const { foods, selected, total } = this.state;
     return (
-      <div className="App">
-      <Food/>
-      
+      <div className="container">
+        <input
+          onChange={this.searchFood}
+          style={{
+            margin: "20px",
+            width: "50%",
+            height: 60,
+            fontSize: "2rem"
+          }}
+          placeholder="Buscale"
+        />
+        {foods.map((food, index) => {
+          return <FoodBox selectFood={this.selectFood} key={index} {...food} />;
+        })}
+
+        <div style={{ position: "absolute", right: 0, top: 100 }}>
+          {selected.map((item, index) => {
+            return (
+              <p className="selected-item">
+                {item.quantity} - {item.name} = {item.calories} cal
+                <button onClick={() => this.deleteFood(item)}>X</button>
+              </p>
+            );
+          })}
+          {selected.length > 0 && `Total: ${total} cal`}
+        </div>
       </div>
     );
   }
--- starter-code/src/components/foodbox.js
+++ starter-code/src/components/foodbox.js
@@ -1,40 +1,51 @@
-import React from 'react'
-const Foodbox = ({name,calories,image,quantity})=>{
-    return(
-        <div className="box">
-  <article className="media">
-    <div className="media-left">
-      <figure className="image is-64x64">
-        <img src={image} />
-      </figure>
-    </div>
-    <div className="media-content">
-      <div className="content">
-        <p>
-          <strong>{name}</strong> <br />
-          <small>{calories} cal</small>
-        </p>
-      </div>
-    </div>
-    <div className="media-right">
-      <div className="field has-addons">
-        <div className="control">
-          <input
-            className="input"
-            type="number" 
-            value={quantity}
-          />
-        </div>
-        <div className="control">
-          <button className="button is-info">
-            +
-          </button>
-        </div>
-      </div>
-    </div>
-  </article>
-</div>
-    )
-}
-export default Foodbox
+import React from "react";
 
+let quantity;
+
+function onChange(e) {
+  quantity = e.target.value;
+}
+
+const FoodBox = ({ name, image, calories, selectFood }) => {
+  return (
+    <div className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={image || "https://i.imgur.com/eTmWoAN.png"} />
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{name}</strong> <br />
+              <small>{calories} cal</small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input
+                onChange={onChange}
+                placeholder="1"
+                className="input"
+                type="number"
+              />
+            </div>
+            <div className="control">
+              <button
+                onClick={() => selectFood(name, calories, quantity)}
+                className="button is-info"
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default FoodBox;

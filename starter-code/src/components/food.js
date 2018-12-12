e822c9d3c62b32b5651514a841f5515a762b2fe2
sabasm <smma1992@gmail.com>
2018-12-11 18:24:36 -0600

    fixed first 2 iteration, thing was horrible

--- starter-code/src/components/food.js
+++ starter-code/src/components/food.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import foods from '../foods.json'
 import Foodbox from './foodbox'
 
 class Food extends Component {
+  state={
+    foodList : foods
+  }
   render() {
+    const {foodList}=this.state
     return (
       <div className="food">
-      <Foodbox/>
+      {foodList.map(foodList=><Foodbox key ={foodList.name}{...foodList}/> )}
       </div>
     );
   }
 }
 
 export default Food;
+
+

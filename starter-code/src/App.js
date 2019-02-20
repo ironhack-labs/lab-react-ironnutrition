4912948034d7c1e3b10f75c137d66b529f2d85ea
Goldziher <nhirschfeld@gmail.com>
2019-02-20 15:28:19 +0100

    finished iteration 5

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -7,16 +7,21 @@ class App extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			foods: foods.map(v => (
+      food: foods,
+      allfoods: foods.map(v => (
 				<FoodBox
 					foodName={v.name}
 					foodCalories={v.calories}
 					foodImage={v.image}
 					foodQuantity={v.quantity}
+          increaseQuantity={(name, quantity) => this.updateQuantity(name, quantity)}
+          addToMenu={(name) => this.addToMenu(name)}
 				/>
 			)),
       addFoodCLick: false,
       displayFoods: "",
+      menu: [],
+      menuDisplay: "",
 		};
     this.handleClick = this.handleClick.bind(this);
 	}
@@ -30,6 +35,24 @@ class App extends Component {
     this.setState(prevState => ({
       foods: [...prevState.foods, newFood]
     }));
+    this.setState(prevState => ({
+      allfoods: [...prevState.foods].map(v => (
+				<FoodBox
+					foodName={v.name}
+					foodCalories={v.calories}
+					foodImage={v.image}
+					foodQuantity={v.quantity}
+          increaseQuantity={(name, quantity) => this.updateQuantity(name, quantity)}
+          addToMenu={(name) => this.addToMenu(name)}
+				/>
+			)),
+    }))
+  }
+
+  clearSearch() {
+    this.setState({
+      displayFoods: ""
+    });
   }
 
   handleSearch(query) {
@@ -39,12 +62,52 @@ class App extends Component {
         foodCalories={v.calories}
         foodImage={v.image}
         foodQuantity={v.quantity}
+        increaseQuantity={(name, quantity) => this.updateQuantity(name, quantity)}
+        addToMenu={(name) => this.addToMenu(name)}
       />
     ));
     this.setState({
       displayFoods: [...displayFoods]
     });
+    this.setState(prevState => ({
+      allfoods: [...prevState.foods].map(v => (
+				<FoodBox
+					foodName={v.name}
+					foodCalories={v.calories}
+					foodImage={v.image}
+					foodQuantity={v.quantity}
+          increaseQuantity={(name, quantity) => this.updateQuantity(name, quantity)}
+          addToMenu={(name) => this.addToMenu(name)}
+				/>
+      )),
+  }))};
+
+  updateQuantity(name,quantity) {
+    let item = this.state.foods.filter(v => v.name === name)
+    item.quantity = quantity
+    this.setState(prevState => ({
+      foods: prevState.foods.map(v => (v.name === item.name) ? v = item : v )
+    }))
+  };
+
+  addToMenu(name) {
+    if (!this.state.menu.includes(name)) {
+      this.setState(prevState => ({
+        menu: [...prevState.menu, name]
+      }))
+      let foods = this.state.food.filter(v => this.state.menu.includes(v.name))
+      this.setState({
+        displayMenu: [...foods].map(v => (
+          <div>
+            <img src={v.image} alt="a food pic"/>
+            <span>{v.name}</span>
+            <span>{v.calories}</span>
+          </div>
+        )),
+      })
+    }
   }
+
 	render() {
 		let footer;
 		if (this.state.addFoodCLick) {
@@ -60,8 +123,9 @@ class App extends Component {
 		}
 		return (
       <div className="App">
-				<SearchForm submitHandler={(query) => this.handleSearch(query)}/>
-        {this.state.displayFoods !== "" ? this.state.displayFoods : this.state.foods}
+				<SearchForm submitHandler={(query) => this.handleSearch(query)} clearQuery={() => this.clearSearch}/>
+        {this.state.displayFoods !== "" ? this.state.displayFoods : this.state.allfoods}
+        {this.state.displayMenu}
 				{footer}
 			</div>
 		);
@@ -93,6 +157,10 @@ class SearchForm extends Component {
 		});
 	}
   
+  handleClick() {
+    this.props.clearQuery()
+  }
+
   render() {
     return(
       <form onSubmit={(e) => this.handleSubmit(e)}>
@@ -104,6 +172,9 @@ class SearchForm extends Component {
           placeholder="Search for food by name"
           onChange={e => this.handleChange(e)}
         />
+        <button className="button is-warning" onClick={() => this.handleClick}>
+					Clear Search
+				</button>
       </form>
     )
   }
@@ -121,14 +192,11 @@ class AddFoodForm extends Component {
   handleSubmit(e) {
     e.preventDefault()
 
-    let newFood = (
-			<FoodBox
-				foodName={this.state.name}
-				foodCalories={this.state.calories}
-				foodImage={this.state.image}
-				foodQuantity="0"
-			/>
-		);
+    let newFood = {
+      name: this.state.name,
+      calories: this.state.calories,
+      image: this.state.images
+    }
 		this.props.submitHandler(newFood)
   }
   handleChange(e) {
@@ -180,12 +248,17 @@ class FoodBox extends Component {
 		super(props);
 		this.state = {
 			quantity: this.props.quantity,
-		};
-	}
-	handleClick() {
+    };
+    this.addToMenu = this.addToMenu.bind(this)
+  }
+  addToMenu() {
+    this.props.addToMenu(this.props.name)
+  }
+	increaseQuantity() {
 		this.setState(prevState => ({
 			quantity: prevState.quantity + 1,
-		}));
+    }));
+    this.props.increaseQuantity(this.props.name, this.state.quantity)
 	}
 	render() {
 		return (
@@ -210,13 +283,14 @@ class FoodBox extends Component {
 								<input
 									className="input"
 									type="number"
-									value="1"
+									value={this.state.quantity}
+                  onClick={() => this.increaseQuantity}
 								/>
 							</div>
 							<div className="control">
 								<button
 									className="button is-info"
-									onClick={(e) => this.handleClick(e)}
+									onClick={this.addToMenu}
 								>
 									+
 								</button>

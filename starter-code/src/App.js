831012111c0f47f2a73e658df80938a3e77a7575
annearbor <betting.anne@gmail.com>
2019-02-21 17:14:31 +0100

    Berlin_Anne_Iteration 3 & search bar

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,15 +4,23 @@ import "bulma/css/bulma.css";
 import FoodBox from "./FoodBox";
 import foods from "./foods.json";
 import AddFood from "./AddFood";
+import Search from "./Search";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      foods: foods
+      foods: foods,
+      isHidden: true
     };
   }
+
+  toggleHidden = () => {
+    this.setState({
+      isHidden: !this.state.isHidden
+    });
+  };
 
   addFoodHandler = food => {
     const newFoodList = [...this.state.foods];
@@ -26,12 +34,21 @@ class App extends Component {
     return (
       <div className="App">
         <h1>IronNutrition</h1>
-        <AddFood addTheFood={this.addFoodHandler} />
-        <ul>
-          {this.state.foods.map((food, i) => (
-            <FoodBox key={i} food={food} />
-          ))}
-        </ul>
+        <button
+          className="button is-primary is-small"
+          onClick={this.toggleHidden.bind(this)}
+        >
+          Add Food
+        </button>
+        {!this.state.isHidden && <AddFood addTheFood={this.addFoodHandler} />}
+        <Search />
+        <div className="container">
+          <ul>
+            {this.state.foods.map((food, i) => (
+              <FoodBox key={i} food={food} />
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }

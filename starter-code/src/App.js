24e330735fbf083a09e9c4b0ee423e57da7a9416
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2019-02-25 21:13:10 +0100

    Iteration 2 completed

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import foods from './datasets/foods';
+import FoodBox from './components/FoodBox';
+import Header from './components/misc/Header';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      foods: [...foods]
+    }
+  }
+
   render() {
+
+    const foodBoxes = this.state.foods
+    .map(food => (
+      <FoodBox food={food} key={food.name} />
+    ));
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <div className="columns">
+          <div className="column">
+            {foodBoxes}
+          </div>
+        </div>
       </div>
     );
   }
---
+++ starter-code/src/components/misc/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => (
+  <header className="Header">
+    <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item has-text-weight-bold" href="/">
+          Iron Nutrition
+        </a>
+      </div>
+    </nav>
+  </header>
+);

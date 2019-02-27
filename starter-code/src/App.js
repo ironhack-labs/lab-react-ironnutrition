bc4fe3d72c4c7fd39f5ccb3fe88372d7cf6447cb
CGuillotG <c.guillot@outlook.com>
2019-02-26 18:01:46 -0600

    Iteration1 & 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import foods from './foods.json'
+import FoodBox from './components/FoodBox'
 
 class App extends Component {
+
+  state = {
+    food:[],
+    today:[],
+    total:0
+  }
+  componentDidMount = () => {
+    this.setState({food:foods})
+  }
+
+  drawFoodBox = (elem, index) => {
+    return <FoodBox key={index} image={elem.image} name={elem.name} calories={elem.calories} quantity={elem.quantity} />
+  }
+
+  addToday = (e) => {
+      let {today} = this.state
+      // today.push({
+      //   name,calories,image,quantity
+      // })
+      // this.setState({today})
+      console.log(today)
+  
+  }
+
+  drawToday = (elem, index) => {
+    return <li>{elem.quantity} {elem.name} = {elem.calories} cal</li>
+  }
+
   render() {
+    let {food, today, total} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1 className="title">IronNutrition</h1>
+        <div>
+          <input type="text" className="input search-bar" name="search" placeholder="Search" value="" />
+        </div>
+        <div className="columns">
+          <div className="column">
+            {food.map(this.drawFoodBox)}
+          </div>
+          <div className="column content">
+            <h2 className="subtitle">Today's foods</h2>
+            <ul>
+              {today.map(this.drawToday)}
+            </ul>
+            <strong>Total: {total} cal</strong>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ starter-code/src/components/FoodBox.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default ({key,image,name,calories,quantity}) => {
+  return (
+    <div key={key} className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={image} alt="Food Pic" />
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{name}</strong> <br />
+              <small>{calories} cal</small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input className="input" type="number" value={quantity} />
+            </div>
+            <div className="control">
+              <button onClick={this.addToday} className="button is-info">+</button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  )
+}

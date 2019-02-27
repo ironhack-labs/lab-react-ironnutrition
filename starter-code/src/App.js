86861d8deac43c65d2c3e1b83be6a97be240f020
Daniela Lopez <danielalopez@MacBook-Air-de-Daniela.local>
2019-02-26 22:00:38 -0600

    casi terminado

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import './App.css'
+import foods from './foods.json'
+import FoodBox from './components/FoodBox'
+import AddFoodForm from './components/AddFoodForm';
 
 class App extends Component {
+  state = {
+    foods: [],
+    newFood: {},
+    todaysFood: []
+  }
+
+  componentDidMount = () => {
+    this.setState({ foods })
+  }
+
+  onAdd = (e) => {
+    let { foods, todaysFood } = this.state
+    let toList = {
+      name: foods[e.target.name].name,
+      quaty: foods[e.target.name].quantity,
+      calories: foods[e.target.name].quantity * foods[e.target.name].calories
+    }
+
+    todaysFood.push(toList)
+    this.setState({ todaysFood })
+  }
+  onChangeQuantity = (e) => {
+    let { foods } = this.state
+    foods[e.target.name].quantity = e.target.value
+    this.setState({ foods })
+  }
+
+  onChange = (e) => {
+    let { newFood } = this.state
+    newFood[e.target.name] = e.target.value
+    this.setState({ newFood })
+  }
+  onSearch = (e) => {
+    let { foods } = this.state
+    let searchQuery = e.target.value.toUpperCase()
+    foods = foods.filter((food) => {
+      return food.name.toUpperCase().includes(searchQuery);
+    })
+    this.setState({ foods })
+  }
+
+  drawFoodBox = () => {
+    let { foods } = this.state
+    return foods.map((box, index) => <FoodBox key={index} arrayPos={index} {...box} onChangeQuantity={this.onChangeQuantity} onAdd={this.onAdd} />)
+  }
+
   render() {
+    let { drawFoodBox, onSubmit, onChange, onSearch } = this
+    let { newFood, todaysFood } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <h1>Iron nutrition</h1>
+        <input type="text" onChange={onSearch} name="searchBar" />
+        <AddFoodForm onSubmit={onSubmit} onChange={onChange}  {...newFood} />
+        <div className="columns">
+          <div className="column">
+            {drawFoodBox()}
+          </div>
+          <div className="column">
+            <h3>Today's food</h3>
+            <ul>
+              {todaysFood.map((item, aux) => <li key={aux}> {item.quaty} - {item.name} Calories: {item.calories}</li>)}
+              {
+                '0' || todaysFood.reduce((a, b) => ({ calories: a.calories + b.calories }))
+              }
+            </ul>
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
+
 
 export default App;
---
+++ starter-code/src/components/AddFoodForm.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AddFoodForm = ({ onSubmit, onChange, name, calories, image }) => (
+    <form onSubmit={onSubmit}>
+        <p>
+            Name:
+            <input type="text" name="name" value={name} onChange={onChange} />
+        </p>
+        <p>
+            Calories:
+            <input type="text" name="calories" value={calories} onChange={onChange} />
+        </p>
+        <p>
+            Image:
+            <input type="text" name="image" value={image} onChange={onChange} />
+        </p>
+        <button type="sumbit" >Add food</button>
+    </form>
+)
+
+export default AddFoodForm

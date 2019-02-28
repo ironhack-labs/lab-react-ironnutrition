dab81b75ecc5113b49708d131266c7f97f1a740f
Carlos Ortiz <ca.ortiz.pacheco@gmail.com>
2019-02-27 18:47:24 -0600

    DE CarlosAlberto

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,20 +1,77 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FoodBox from "./components/FoodBox"
+import foods from './foods.json'
 
 class App extends Component {
+state = {
+  food:{},
+  foods:[],
+  cart:[],
+  total:0
+}
+
+componentWillMount(){
+  this.setState({foods:foods})
+}
+
+onChangeSearch = event => {
+  let search = event.target.value.toLowerCase()
+  if (search === "") return this.setState({ foods: foods })
+  let array = this.state.foods.filter(food =>
+    food.name.toLowerCase().includes(search)
+  )
+  this.setState({ foods: array })
+}
+
+addCart = (index, quantity) => {
+  let { total, foods, cart } = this.state
+  let itemAdded = { ...foods[index] }
+  itemAdded.calories = quantity * itemAdded.calories
+  total += itemAdded.calories
+  cart.push(itemAdded)
+
+  // Total
+
+  this.setState({ cart: cart, total: total })
+}
+
+saveAmount = (event, i) => {
+  console.log(event.target.value)
+  let { foods } = this.state
+  foods[i].quantity = event.target.value
+  this.setState({ foods })
+}
+
+deleteCart = index => {
+  console.log(index)
+}
+
+
   render() {
+    let { foods, cart } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>IronNutrition</h1>
+        <input onChange={this.onChangeSearch} type="text" name="search" className="input search-bar"/>
+        {foods.map((food, index) => (
+          <FoodBox food={food} addCart={this.addCart} saveAmount={this.saveAmount} key={index} index={index}/>
+        ))}
+        <div className="column content">
+          <h2 className="subtitle">Today's foods</h2>
+          <ul>
+            {cart.map((cart, index) => (
+              <li key={index}>
+                {cart.quantity} {cart.name} = {cart.calories} cal{" "}
+              </li>
+            ))}
+          </ul>
+          <strong>Total: {this.state.total} cal</strong>
+        </div>
       </div>
     );
+    
   }
 }
 
---
+++ starter-code/src/components/FoodBox.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+
+
+export default function ({food,addCart,saveAmount,index}) {
+  return(
+    <div className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={food.image} alt={food.name} />
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{food.name}</strong> <br />
+              <small>{food.calories}</small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input onChange={event => saveAmount(event, index)} className="input" type="number" value={food.quantity}/>
+            </div>
+            <div className="control">
+              <button onClick={() => addCart(index, food.quantity)} className="button is-info">
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  )
+}

a457e8e4f209cf9597209877ca196550fb21fc25
daniloxxv <danilosoares@gmail.com>
2019-03-02 20:36:57 -0600

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import Foodbox from './components/Foodbox'
+import foods from './foods.json'
+
+const foodList = [...foods]
 
 class App extends Component {
+  state = {
+    foodList: foodList,
+    displayForm: false,
+    newFood: ["Undefined food item","0","https://i.imgur.com/eTmWoAN.png"],
+    search: ""
+  }
+
+  showForm = () => {
+    let newState = !this.state.displayForm;
+    this.setState({
+      displayForm: newState,
+    })
+  }
+
+  newFoodHandler = (e) =>{
+    let field = e.target.name
+    let value = e.target.value
+    let temp = this.state.newFood.slice()
+    if (field === "name") temp[0] = value 
+    if (field === "calories") temp[1] = value 
+    if (field === "image") temp[2] = value 
+    this.setState({
+      newFood: temp
+    })
+  }
+
+  addFood = (name, calories, image) => {
+    let newFood = {
+      name: name,
+      calories: calories,
+      image: image,
+    }
+    let newList = this.state.foodList.slice()
+    newList.unshift(newFood)
+    this.setState({
+      foodList: newList
+    })
+  }
+
+  searchHandler = (e) =>{
+    let searchTerm = e.target.value;
+    console.log(e.target.value)
+    let newList = searchTerm.length > 0 ? foods.slice().filter(el=>el.name.includes(searchTerm)) : foods.slice()
+    this.setState({
+      foodList: newList
+    })
+  }
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="search">
+          <label htmlFor="search">Search</label>
+          <input className="input" onChange={(e)=>this.searchHandler(e)}type="text" name="search" placeholder="Type your search here..." />
+        </div>
+        <section className="display">
+        <div className="foodList">
+          {this.state.foodList.map((food,i)=><Foodbox key={i} {...food}/>)}
+        </div>
+        <button onClick={this.showForm} className="button is-primary">Add new food</button>
+        <div className = "form">
+          {this.state.displayForm ? <div className="control"><input className="input" value={this.state.foodName} onChange={(e) =>this.newFoodHandler(e)}  type="text" name = "name" placeholder="Name" /></div> : <br />}
+          {this.state.displayForm ? <div className="control"><input className="input" value={this.state.foodCalories} onChange={(e) =>this.newFoodHandler(e)}  type="text" name="calories" placeholder="Calories" /></div> : <br />}
+          {this.state.displayForm ? <div className="control"><input className="input" value={this.state.foodImage} onChange={(e) =>this.newFoodHandler(e)}  type="text" name= "image" placeholder="Image url" /></div> : <br />}
+          {this.state.displayForm ? <button onClick={()=>this.addFood(...this.state.newFood)} className="button is-primary">Upload food data</button> : <br/>}
+        </div>
+        </section>
       </div>
     );
   }

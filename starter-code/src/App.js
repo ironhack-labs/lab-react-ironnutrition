2304838c1c9c5fb8bd79e97e2b169f747cd7fcdf
Mike <troianello@protonmail.com>
2019-03-05 11:00:41 -0500

    Mike and Monroe, iterarions 1 and 2 finished

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import foods from './foods.json'
+import 'bulma/css/bulma.css';
 import './App.css';
 
+
+
 class App extends Component {
+  state = {
+    foods: foods
+  }
+  showFood = () => {
+    console.log(foods)
+    let list = this.state.foods.map((food, i) => {
+    return(
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={food.image} />
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{food.name}</strong> <br />
+                <small>{food.calories}</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input
+                  className="input"
+                  type="number" 
+                  value="1"
+                />
+              </div>
+              <div className="control">
+                <button className="button is-info">
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+
+    )
+    })
+    return list;
+  }
+  
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <div>
+      {this.showFood()}
+    
+        <button onClick={this.addFood}>Add Food</button>
+
+    </div>
     );
   }
 }
 
+
 export default App;
+

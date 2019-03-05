d556a912730703f5c87fec9c3af582a83aea3488
Manuel Garaboa <mannygaraboa@gmail.com>
2019-03-05 10:56:37 -0500

    iteration #2

--- starter-code/src/components/FoodBox.js
+++ starter-code/src/components/FoodBox.js
@@ -1,44 +1,60 @@
 import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
-import foods from './foods.json'
+import foods from '../foods.json'
 
-class FoodBox extends Component {
+class FoodBox extends Component 
+{
+    state = {
+        foods:foods
+    }
+
+  showFoods = () => 
+  {
+    let listItems = this.state.foods.map((item, i) => 
+    {
+        return (
+            <div className="box">
+            <article className="media">
+            <div className="media-left">
+                <figure className="image is-64x64">
+                <img src={item.image}/>
+                </figure>
+            </div>
+            <div className="media-content">
+                <div className="content">
+                <p>
+                    <strong>{item.name}</strong> <br />
+                    <small>{item.calories}</small>
+                </p>
+                </div>
+            </div>
+            <div className="media-right">
+                <div className="field has-addons">
+                <div className="control">
+                    <input
+                    className="input"
+                    type="number" 
+                    value="1"
+                    />
+                </div>
+                <div className="control">
+                    <button className="button is-info">
+                    +
+                    </button>
+                </div>
+                </div>
+            </div>
+            </article>
+        </div>
+        )
+    })
+    return listItems;
+  }
+
   render() {
-    return (
-        <div className="box">
-        <article className="media">
-          <div className="media-left">
-            <figure className="image is-64x64">
-              <img src="https://i.imgur.com/eTmWoAN.png" />
-            </figure>
-          </div>
-          <div className="media-content">
-            <div className="content">
-              <p>
-                <strong>{foods.name}</strong> <br />
-                <small>{foods.calories}</small>
-              </p>
-            </div>
-          </div>
-          <div className="media-right">
-            <div className="field has-addons">
-              <div className="control">
-                <input
-                  className="input"
-                  type="number" 
-                  value="1"
-                />
-              </div>
-              <div className="control">
-                <button className="button is-info">
-                  +
-                </button>
-              </div>
-            </div>
-          </div>
-        </article>
-      </div>
-    );
+      return (
+          <ul>{this.showFoods()}</ul>
+      )
   }
 }
 

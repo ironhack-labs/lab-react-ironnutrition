b0d2b417d3a261d7dcb3a493d59ba41822ffde2b
ernestogl88 <ernestogomezlopez@gmail.com>
2019-05-01 16:18:00 +0200

    Iteration 1 and 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './FoodBox';
+
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      foods: foods
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      this.state.foods.map(food=>{
+       return <FoodBox name={food.name} calories={food.calories} image={food.image}/>
+      })
     );
   }
 }
---
+++ starter-code/src/FoodBox.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default class FoodBox extends React.Component {
+  render(props) {
+    return (
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={this.props.image} alt='' />
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{this.props.name}</strong> <br />
+                <small>{this.props.calories} cal</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" type="number" value="1" />
+              </div>
+              <div className="control">
+                <button className="button is-info">+</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    );
+  }
+}

5da55eeb5f13322439feb4697b17f75c047ce063
Ruben Polo <rpoloredondo@outlook.es>
2019-05-01 16:53:11 +0200

    Second Iteration complete

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,11 +6,20 @@ import foods from './foods.json'
 import FoodBox from './FoodBox';
 
 class App extends Component {
+  constructor () {
+    super ()
+    this.state = {
+      foods : foods
+    } 
+  }
   render() {
     return (
       <React.Fragment>
-        <FoodBox />
+          {this.state.foods.map((food, i) => {
+            return <FoodBox {...food} key= {i}/>
+          })}
 
+       
       </React.Fragment>
     );
   }

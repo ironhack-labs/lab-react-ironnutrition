015e56524704bbebc5fdc46cf9a89a63247b288d
KarenOta <karenota@K.O.HP>
2019-05-08 17:24:07 -0300

    almost

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,69 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import FoodBox from './components/FoodBox';
+import foods from './foods.json';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      foodList: foods,
+      name: '',
+      calories: 0,
+      image: '',
+      quantity: 0,
+    }
+    this.addName = this.addName.bind(this);
+    this.addCal = this.addCal.bind(this);
+    this.addImg = this.addImg.bind(this);
+    this.addQtd = this.addQtd.bind(this);
+  }
+
+  addName(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+  addCal(e) {
+    this.setState({
+      calories: e.target.value
+    });
+  }
+  addImg(e) {
+    this.setState({
+      image: e.target.value
+    });
+  }
+  addQtd(e) {
+    this.setState({
+      quantity: e.target.value
+    });
+  }
+
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app-master">
+          <form className="form" onSubmit={this.submit}>
+            <label>Name</label>
+            <input type="text" name="name" value={this.state.name} onChange={(e) => this.addName(e)} />
+            <label>Calories</label>
+            <input type="text" name="calories" value={this.state.calories} onChange={(e) => this.addCal(e)} />
+            <label>Image</label>
+            <input type="text" name="image" value={this.state.image} onChange={(e) => this.addImg(e)} />
+            <label>Quantity</label>
+            <input type="text" name="quantity" value={this.state.quantity} onChange={(e) => this.addQtd(e)} />
+            <button type="submit">Submit</button>
+          </form>
+
+        {
+          this.state.foodList.map((food, idx) => {
+            return <FoodBox food={ food } key={ idx } />
+          }) 
+        }
       </div>
     );
   }

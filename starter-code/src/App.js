93a956bf9bb8076b46c0cbe26387c029dabe8ae1
KarenOta <karenota@K.O.HP>
2019-05-10 15:35:47 -0300

    almost

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
 import FoodBox from './components/FoodBox';
 import foods from './foods.json';
+import SearchFood from './components/SearchFood';
 
 class App extends Component {
   constructor() {
@@ -19,6 +19,9 @@ class App extends Component {
     this.addCal = this.addCal.bind(this);
     this.addImg = this.addImg.bind(this);
     this.addQtd = this.addQtd.bind(this);
+    this.submit = this.submit.bind(this);
+    this.AddFood = this.AddFood.bind(this);
+    this.SearchFood = this.SearchFood.bind(this);
   }
 
   addName(e) {
@@ -41,29 +44,60 @@ class App extends Component {
       quantity: e.target.value
     });
   }
+  submit(e) {
+    e.preventDefault();
+    this.AddFood(this.state);
+    this.setState({
+      name: '',
+      calories: 0,
+      image: '',
+      quantity: 0,
+    })
+  }
+  AddFood(e) {
+    const foodCopy = [...this.state.foodList];
+    foodCopy.push(e); 
+    this.setState( {
+      foodList: foodCopy,
+    })
+  }
 
+  SearchFood(e) {
+   const newSearch = [...this.state.foodList];
+   newSearch.push(e);
+   this.setState({
+      foodList: newSearch,
+   })
+  }
 
   render() {
 
     return (
       <div className="app-master">
-          <form className="form" onSubmit={this.submit}>
-            <label>Name</label>
-            <input type="text" name="name" value={this.state.name} onChange={(e) => this.addName(e)} />
-            <label>Calories</label>
-            <input type="text" name="calories" value={this.state.calories} onChange={(e) => this.addCal(e)} />
-            <label>Image</label>
-            <input type="text" name="image" value={this.state.image} onChange={(e) => this.addImg(e)} />
-            <label>Quantity</label>
-            <input type="text" name="quantity" value={this.state.quantity} onChange={(e) => this.addQtd(e)} />
-            <button type="submit">Submit</button>
-          </form>
+          <h1 className="title">IronNutrition</h1>
 
-        {
-          this.state.foodList.map((food, idx) => {
-            return <FoodBox food={ food } key={ idx } />
-          }) 
-        }
+          {
+            this.state.foodList.map((food, idx) => {
+              return <SearchFood food={ food } key={ idx } />
+            }) 
+          }
+
+          <form className="form" onSubmit={this.submit}>
+            <label> Name </label>
+            <input type="text" name="name" value={this.state.name} onChange={(e) => this.addName(e)} />
+            <label> Calories </label>
+            <input type="text" name="calories" value={this.state.calories} onChange={(e) => this.addCal(e)} />
+            <label> Image </label>
+            <input type="text" name="image" value={this.state.image} onChange={(e) => this.addImg(e)} />
+            <label> Quantity </label>
+            <input type="text" name="quantity" value={this.state.quantity} onChange={(e) => this.addQtd(e)} />
+            <button type="submit"> Submit</button>
+          </form>
+          {
+            this.state.foodList.map((food, idx) => {
+              return <FoodBox food={ food } key={ idx } />
+            }) 
+          }
       </div>
     );
   }
---
+++ starter-code/src/components/SearchFood.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const SearchFood = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Search..." />
+      </form>
+      </div>
+  );  
+}    
+    
+export default SearchFood;

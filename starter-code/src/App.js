15d18605074efb124ba88e4985cd2288f0905bb7
JOSUE VALENCIA <JVALENCIA@madrid.corpsermicro.es>
2019-05-23 12:22:48 +0200

    list

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import foods from './foods.json'
+import FoodBox from './components/FoodBox'
 
 class App extends Component {
 	render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          Some foods {foods.length}
-        </p>
+        {foods.map((e, i) => (
+					<FoodBox key={i} food={e}/>	
+				))}
       </div>
     );
   }

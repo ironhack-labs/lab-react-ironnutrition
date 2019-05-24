a96e765cfb1dbe5b2fda56b58c2713381af08cf8
Josue Valencia <josue.valencia@domo.ec>
2019-05-24 13:33:40 +0200

    add elements

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,21 +6,37 @@ import SearchFood from './components/SearchFood'
 
 class App extends Component {
 	state = {
-		foods: [...foods]
+		foods: [...foods],
+		today: []
 	}
 	handleSearch = query => {
-		console.log(query)
 		this.setState({
 			foods : foods.filter(food => food.name.toLowerCase().includes(query.toLowerCase()))
 		}) 
 	}
+	addFood = food => {
+		this.setState({
+			today: [...this.state.today, food]
+		})
+	}
+
 	render() {
     return (
       <div className="App">
 				<SearchFood search = {this.handleSearch}/>
-        {this.state.foods.map((e, i) => (
-					<FoodBox key={i} food={e}/>	
-				))}
+				<div className="columns">
+					<div className="column">
+					{this.state.foods.map((e, i) => (
+						<FoodBox key={i} food={e} add = {this.addFood}/>	
+					))}
+					</div>
+					<div className="column">
+						<h1>Today's foods</h1>
+						{this.state.today.map((e, i) => (
+							<p key={i}>{e.quantity} {e.name}, - {e.calories}cal</p>
+						))}
+					</div>
+				</div>
       </div>
     );
   }
--- starter-code/src/components/FoodBox.js
+++ starter-code/src/components/FoodBox.js
@@ -1,37 +1,56 @@
 import React from 'react'
 
-export default ({food}) => (
-	<div className="box">
-		<article className="media">
-			<div className="media-left">
-				<figure className="image is-64x64">
-					<img src={food.image} alt="..."/>
-				</figure>
-			</div>
-			<div className="media-content">
-				<div className="content">
-					<p>
-						<strong>{food.name}</strong> <br />
-						<small>{food.calories} cal</small>
-					</p>
-				</div>
-			</div>
-			<div className="media-right">
-				<div className="field has-addons">
-					<div className="control">
-						<input
-							className="input"
-							type="number" 
-							value="1"
-						/>
+class Foodbox extends React.Component {
+	state = {
+		quantity : 0
+	}
+	handleQunatity = () => {
+		this.setState({quantity: this.state.quantity + 1})
+	}
+	addButton = () => {
+		this.props.add({
+			name: this.props.food.name,
+			calories: this.props.food.calories,
+			quantity: this.state.quantity
+		})
+	}
+	render(){
+		return (
+			<div className="box">
+				<article className="media">
+					<div className="media-left">
+						<figure className="image is-64x64">
+							<img src={this.props.food.image} alt="..."/>
+						</figure>
 					</div>
-					<div className="control">
-						<button className="button is-info">
-							+
-						</button>
+					<div className="media-content">
+						<div className="content">
+							<p>
+								<strong>{this.props.food.name}</strong> <br />
+								<small>{this.props.food.calories} cal</small>
+							</p>
+						</div>
 					</div>
-				</div>
+					<div className="media-right">
+						<div className="field has-addons">
+							<div className="control">
+								<input
+									className="input"
+									type="number" 
+									value={this.state.quantity}
+									onChange={this.handleQunatity}
+								/>
+							</div>
+							<div className="control">
+								<button className="button is-info" onClick={this.addButton}>
+									+
+								</button>
+							</div>
+						</div>
+					</div>
+				</article>
 			</div>
-		</article>
-	</div>
-)
+		)}
+}
+
+export default Foodbox

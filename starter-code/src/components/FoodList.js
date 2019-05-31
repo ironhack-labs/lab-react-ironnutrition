33dec4404596b238024a977fcbf1678d39d4744e
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-05-31 18:41:15 +0200

    Iteratio 1 & 2

---
+++ starter-code/src/components/FoodList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FoodBox from './FoodBox'
+
+ const FoodList = ({foods}) => 
+ <div className="column">{foods.map((food,index) => 
+    <FoodBox key={index}{...food} />
+    )}
+ </div>;
+
+ export default FoodList;

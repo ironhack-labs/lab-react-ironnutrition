45e90ff5103767e8eb3ccf102700871bc17d12f7
Fran Enriquez <fjenriglez@gmail.com>
2019-06-02 20:52:11 +0200

    Iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import '../node_modules/bulma/css/bulma.min.css'
+import '../node_modules/bulma-helpers/css/bulma-helpers.min.css'
 import './App.css';
+import Header from './components/Header';
+import FoodBox from './components/FoodBox';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        <div className="container has-margin-top-50">
+        <FoodBox/>
+        </div>
+
       </div>
     );
   }
---
+++ starter-code/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+  <section className="hero is-primary is-bold">
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title has-text-weight-light">
+          Iron Lab - <strong className="has-text-weight-bold">Nutrition</strong>
+        </h1>
+      </div>
+    </div>
+  </section>
+);
+
+export default Header

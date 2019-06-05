7c313b34e06632725094ddda23fb6296c234dd35
letiescanciano <leticia.escanciano@gmail.com>
2019-06-05 15:39:41 +0200

    Iteration 1 and 2: Display food!

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FoodBox from './components/FoodBox';
+import foods from './foods.json';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			foods
+		};
+	}
+
+	render() {
+		return <div className="App">{this.state.foods.map((food, i) => <FoodBox key={i} food={food} />)}</div>;
+	}
 }
 
 export default App;

7a3cac259b9f2c12fdc8df9ab49dd0b5bbe71b69
Emiliano <emilianopb92@gmail.com>
2019-09-25 18:18:02 -0500

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods'
+import FoodBox from './components/FoodBox'
 
 class App extends Component {
+  state = {
+    foods: foods,
+    food: {},
+    filtered: []
+  
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ foods: [...this.state.foods, this.state.food] });
+
+  };
+
+  handleInput = (e) => {
+    const { food } = this.state;
+    const key = e.target.name;
+    food[key] = e.target.value;
+    this.setState({ food });
+  };
+
+  search = (e) => {
+    const { value } = e.target;
+    const { foods } = this.state;
+    const query = value.toLowerCase();
+    const filtered = foods.filter(
+      (foodPar) => foodPar.name.toLowerCase().includes(query)
+    );
+    this.setState({ filtered });
+  };
+
+
   render() {
+    const { foods, filtered } = this.state
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         
+
+      <form onSubmit={this.onSubmit}>
+          <input onChange={this.handleInput} type="text" name="name" placeholder="Name" />
+          <input onChange={this.handleInput} type="text" name="calories" placeholder="Calories" />
+          <input onChange={this.handleInput} type="text" name="image" placeholder="Image URL" />
+          <input type="submit" value="Add Food" />
+      </form>
+
+      <input onChange={this.search} type="text" name="search" placeholder="Search" />
+        {filtered.length === 0 &&
+          foods.map((food, idx) => (
+            <div key={idx}>
+            <FoodBox data={food}/>
+            </div>
+          ))}
+          {filtered.length !== 0 &&
+          filtered.map((food, idx) => (
+            <div key={idx}>
+            <FoodBox data={food}/>
+            </div>
+          ))}
+        
       </div>
     );
   }
 }
-
 export default App;

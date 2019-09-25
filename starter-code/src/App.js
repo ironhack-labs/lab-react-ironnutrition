d6d3f87827fef68159fba3004e3f338287d7ce2a
= <jacex17@gmail.com>
2019-09-25 16:29:38 -0400

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -18,7 +18,8 @@ class App extends React.Component {
       todayFood: [],
       newName: "",
       newCalories: "",
-      foodToSearch: ""
+      foodToSearch: "",
+      found: false
     }
   }
 
@@ -80,7 +81,7 @@ displayTheFood = () => {
 displayTodayFood = () =>{
   return this.state.todayFood.map((eachTodayFood, index) => {
     console.log(eachTodayFood)
-    return <li key={index}>{eachTodayFood.name}  {eachTodayFood.calories * eachTodayFood.quantity} Cal</li>
+    return <li key={index}>{eachTodayFood.name}  {eachTodayFood.total} Cal</li>
   })
 
 }
@@ -101,30 +102,40 @@ addTheFood = (e) => {
 
 }
 
-calculateQty = (e) => {
- // e.preventDefault();
-  //  return e.currentTarget.value;
-   console.log(e.currentTarget.value);
-}
-
 addToday = (choseOne) => {
-  // let quantity = this.calculateQty();
-  // console.log("The quantity is " + quantity);
- console.log(choseOne);
+  let flag = false;
+
+//  console.log(choseOne);
   // console.log(choseOne.name);
-  let newTodayFood = {name: choseOne.props.name, calories: choseOne.props.calories, quantity: choseOne.state.inValue}
+  let newTodayFood = {name: choseOne.props.name, calories: choseOne.props.calories, quantity: choseOne.state.inValue, total: (choseOne.props.calories * choseOne.state.inValue)}
   let newTodayList = [...this.state.todayFood]
 
+//CHECK REPEATED FOOD IN TODAY LIST
+  newTodayList.forEach(eachToday => {
+    console.log(eachToday)
+    for(let key in eachToday){
+    if(eachToday[key] === newTodayFood.name){
+      flag = true
+      eachToday.total += (newTodayFood.calories * newTodayFood.quantity)
+      break;
+    }else{
+     
+    }
+  
 
-  // newTodayList.forEach(eachToday => {
-  //   for(let key in eachToday){
-  //     if(eachToday.hasOwnProperty(key)){
-  //       console.log("The Key is " + key)
-  //       console.log("And value " + eachToday[key])
-  //     }
-  //   }
-  // })
+    }
+   
+  })
 
+  if(flag === false){
+    newTodayList.push(newTodayFood);
+  }
+
+  this.setState({
+    todayFood: newTodayList
+  })
+   
+  
   // let test = newTodayList.map(theFoodToPass => {
   //   return theFoodToPass.name
   // }).indexOf(choseOne.name);
@@ -132,11 +143,8 @@ addToday = (choseOne) => {
   // if(test >= 0){
   //   return test + "Hello"
   // }else{
-  newTodayList.push(newTodayFood);
-
-    this.setState({
-      todayFood: newTodayList
-    })
+  
+  
   // }
 
 

c750b8a2e02d47429212be03b8200495ce4c5f98
Dan Gil <dan.nek0.bass@gmail.com>
2019-09-25 22:10:43 -0500

    Iteration 5 missing

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -9,12 +9,14 @@ class App extends Component {
     food: {},
     formOn: false,
     filtered: [],
-    todayFoodArr: []
+    todayFoodArr: [],
+    fridayFood: {}
   };
 
   onSubmit = e => {
     e.preventDefault();
-    this.setState({foods: [...this.state.foods, this.state.food]});
+    // console.log(this.state.food);
+    this.setState({foods: [...this.state.foods, this.state.food], food: {}});
   };
 
   handleInput = e => {
@@ -32,30 +34,50 @@ class App extends Component {
     this.setState({filtered});
   };
 
-  addFood = () => {
-    this.setState({foods: [...this.state.foods, this.state.food]});
-  };
-
   displayForm = () => {
+    //console.log(this.state.food);
     this.setState({formOn: true});
   };
 
-  todayFood = () => {
-    this.setState({foods: [...this.state.todayFoodArr, this.state.todayfoodArr]});
+  todayFood = (name, calories) => {
+    console.log('this is the name: ' + name);
+    console.log('these are the calories: ' + calories);
+    this.setState({
+      todayFoodArr: [
+        ...this.state.todayFoodArr,
+        {
+          name,
+          calories
+        }
+      ]
+    });
   };
+
+  // addFood = (todayFood, index) => {
+  //   <div key={index}>
+  //     <p>{todayFood.name}</p>
+  //     <p>{todayFood.calories}</p>)
+  //   </div>;
+  // };
 
   render() {
     const {foods, formOn, filtered, todayFoodArr} = this.state;
-    console.log(formOn);
+    console.log('this is the array' + {todayFoodArr});
+
+    // console.log(formOn);
+    // console.log(todayFoodArr);
+
     return (
       <div>
         <div>
           <h1>Foods</h1>
-          <Button onClick={this.displayForm}>Add Food</Button>
+          <Button onClick={this.displayForm} type="primary">
+            Add Food
+          </Button>
           <input onChange={this.searchFood} type="text" name="search" placeholder="Search..." />
           {filtered.length === 0 &&
             foods.map((food, idx) => (
-              <Card key={idx} style={{width: 320}}>
+              <Card key={idx} style={{width: 520}}>
                 <div className="main-content">
                   <img
                     alt={food.name}
@@ -67,12 +89,16 @@ class App extends Component {
                   />
                 </div>
                 <div key={idx}>
-                  <p>{food.name}</p>
-                  <p>{food.calories}</p>
+                  <p>Name: {food.name}</p>
+                  <p>Calories: {food.calories}</p>
                 </div>
 
                 <div>
-                  <Button onClick={this.todayFood} type="primary">
+                  <Button
+                    className="button-add"
+                    onClick={() => this.todayFood(food.name, food.calories)}
+                    type="primary"
+                  >
                     +
                   </Button>
                 </div>
@@ -93,12 +119,22 @@ class App extends Component {
                   />
                 </div>
                 <div key={idx}>
-                  <p>{food.name}</p>
-                  <p>{food.calories}</p>
+                  <p>Name: {food.name}</p>
+                  <p>Calories: {food.calories}</p>
                 </div>
 
                 <div>
-                  <Button onClick={this.todayFood} type="primary">
+                  <Button
+                    className="button-add"
+                    onClick={
+                      this.todayFood
+                      // (this.todayFood,
+                      // function() {
+                      //   let id = this.idx;
+                      // })
+                    }
+                    type="primary"
+                  >
                     +
                   </Button>
                 </div>
@@ -107,19 +143,13 @@ class App extends Component {
 
           {formOn === true && (
             <Form onSubmit={this.onSubmit}>
-              <input onChange={this.handleInput} type="text" name="calories" placeholder="Calories" />
               <input onChange={this.handleInput} type="text" name="name" placeholder="Name" />
+              <input onChange={this.handleInput} type="text" name="calories" placeholder="Calories" />
               <input onChange={this.handleInput} type="text" name="image" placeholder="Image Url" />
               <input type="submit" value="Add new food" />
             </Form>
           )}
-          {/* {foods.map((todayFoodArr, idx) => (
-            <div>
-              <h1>Today's Foods</h1>
-              <p key={idx}>{todayFoodArr.name}</p>
-              <p key={idx}>{todayFoodArr.calories}</p>)
-            </div>
-          ))} */}
+          <h1>Today's Foods</h1>
         </div>
       </div>
     );

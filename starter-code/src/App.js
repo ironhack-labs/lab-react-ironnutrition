4604e6231fe5631a41d23a74d500f4076ae02001
Larissa <larissa@Xense8.local>
2019-09-25 23:11:11 -0500

    mi compu muere

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import foods from './foods.json'
 
 class App extends Component {
+  state = {
+    foods,
+    food: {},
+    filtered: []
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        foods: prevState.foods.filter((x, idx) => {
+          x.name === idx
+          console.log(x.name, idx)
+          return x.name
+        })
+
+      }
+    })
+  };
+
+
+  handleInput = (e) => {
+    const { food } = this.state;
+    const key = e.target.name;
+    food[key] = e.target.value;
+    this.setState({ food });   
+  };
+
+  plusOne = () => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        foods: prevState.foods.quantity + 1
+      }
+    })
+  }
+
+  addThis = (calories) => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        foods: prevState.foods.calories
+      }
+    })
+  }
+
+  search = (e) => {
+    const { value } = e.target;
+    const { foods } = this.state;
+    const query = value.toLowerCase();
+    const filtered = foods.filter((food) => food.name.toLowerCase().includes(query));
+    this.setState({ filtered });
+  };
+
   render() {
+    const { foods, filtered } = this.state;
+    //console.log(foods)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>foods</h1>
+        <input onChange={this.search} type="text" name="search" />
+        {filtered.length === 0 &&
+          foods.map((food, idx) => (
+            <div key={idx}>
+              <p>{food.name}</p>
+              <p>{food.calories}</p>
+              <form onSubmit={ this.onSubmit }>
+                <input type="number" onChange={this.plusOne}/>
+                <input className="button is-success" type="submit" value="add this" />
+              </form>
+            </div>
+          ))}
+        {filtered.length !== 0 &&
+          filtered.map((food, idx) => (
+            <div key={idx}>
+              <p>{food.name}</p>
+              <p>{food.calories}</p>
+              <form onSubmit={ this.onSubmit }>
+                <input type="number" onChange={this.plusOne}/>
+                <input className="button is-success" type="submit" value="add this" />
+              </form>
+            </div>
+          ))}
       </div>
     );
   }
--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
+import 'bulma/css/bulma.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 

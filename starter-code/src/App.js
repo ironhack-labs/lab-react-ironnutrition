7ee3d9a88883a988848d29cf640cc0fd619419cf
Hoshimotao <jthomas302@gmail.com>
2019-10-01 17:45:25 -0400

    first commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './FoodBox';
 
 class App extends Component {
+
+
+  state = {
+    foodList: foods    
+  }
+
+newerFoodArray = this.state.foodList.map((eachFood, i) =>{
+      return eachFood.name
+    })
+
+
+
   render() {
+    
+    console.log(this.newerFoodArray)
+    
+
     return (
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+<div className="foodList">
+
+<FoodBox/>
+
+
+
+</div>
       </div>
     );
   }
---
+++ starter-code/src/FoodBox.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import foods from "./foods.json";
+
+export default class FoodBox extends Component {
+  state = {
+    foodList: foods
+  };
+
+  newerFoodArray = this.state.foodList.map((eachFood, i) => {
+    return (<div className="box">
+    <article className="media">
+      <div className="media-left">
+        <figure className="image is-64x64">
+          <img alt="pizza" src={eachFood.image} />
+        </figure>
+      </div>
+      <div className="media-content">
+        <div className="content">
+          <p>
+            <strong> {eachFood.name}</strong> <br />
+            <small> Calories: {eachFood.calories}</small>
+          </p>
+        </div>
+      </div>
+      <div className="media-right">
+        <div className="field has-addons">
+          <div className="control">
+            <input className="input" type="number" value="1" />
+          </div>
+          <div className="control">
+            <button className="button is-info">+</button>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div>);
+  });
+
+  render() {
+    return (
+      <div>
+       {this.newerFoodArray}
+      </div>
+    );
+  }
+}

cd15d1d3388635a5ae8979b874c3ec23a29fc31c
marcellemcm <marcellemoreira@gmail.com>
2019-10-16 17:04:35 -0300

    Partial

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import foods from './foods.json'
+import FoodBox from './FoodBox';
+import AddFood from './AddFood';
+import './App.css'
+import 'bulma/css/bulma.css';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        allFoods: foods,
+        visible: false,
+    }
+  }
+
+  toggleFoods = () => {
+    this.setState({ visible: !this.state.visible });
+}
+
+  showFoods() {
+    console.log(this.state)
+    return this.state.allFoods.map((food, index) => {
+      console.log(foods)
+      return <FoodBox key={index} {...food} />
+    })
+  }
+
+  addFoodHandler = (theFood) => {
+    const foodsCopy = [...this.state.allFoods];
+    foodsCopy.push(theFood);
+    this.setState({
+      allFoods: foodsCopy,
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">IronNutrition</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={() => this.toggleFoods()}>AddFood
+        </button>
+        {this.state.visible ? <AddFood addTheFood={this.addFoodHandler.bind(this)}/> : null}    
+
+        
+        <div className="foods-list">
+        {this.showFoods()}
+        </div>
       </div>
     );
   }

5509ab8f3c1c9f2f1f39ef38060d78ea6ae73541
saruit0105 <saruits2@gmail.com>
2019-11-13 16:48:29 -0500

    added interation 1-4

--- starter-code/src/Components/Foodbox.jsx
+++ starter-code/src/Components/Foodbox.jsx
@@ -4,10 +4,46 @@ import foods from "../foods.json";
 class FoodBox extends Component {
   state = {
     foods: foods,
-    form: false
+    filteredFoods: foods,
+    form: false,
+    newFoodName: "",
+    newCalories: 0,
+    newImage: "",
+    searchTerm: ""
   };
   logFood = () => {
     console.log(foods.name);
+  };
+
+  searchBar = e => {
+    this.setState(
+      {
+        [e.target.name]: e.target.value
+      },
+      () => {
+        let clonedArr = [...this.state.foods];
+
+        let filtered = clonedArr.filter(food =>
+          food.name.includes(this.state.searchTerm)
+        );
+        this.setState({
+          filteredFoods: filtered
+          // foods : filtered
+        });
+      }
+    );
+  };
+  // clonedArr.filter(this.state.searchTerm);
+
+  updateInput = e => {
+    this.setState(
+      {
+        [e.target.name]: e.target.value
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
   };
   toggleForm = () => {
     this.setState({ form: !this.state.form });
@@ -15,10 +51,21 @@ class FoodBox extends Component {
 
   addFood = () => {
     this.setState({ form: !this.state.form });
+
+    const newFood = {
+      name: this.state.newFoodName,
+      calories: this.state.newCalories,
+      image: this.state.newImage
+    };
+    const copyFoodList = [...this.state.foods];
+    console.log(copyFoodList);
+    copyFoodList.push(newFood);
+    console.log(copyFoodList);
+    this.setState({ foods: copyFoodList });
   };
 
   showFoods = () => {
-    return this.state.foods.map((eachFoods, i) => {
+    return this.state.filteredFoods.map((eachFoods, i) => {
       return (
         <div className="box" key={i}>
           <article className="media">
@@ -56,16 +103,40 @@ class FoodBox extends Component {
   render() {
     return (
       <div>
+        <form>
+          <input
+            type="text"
+            name="searchTerm"
+            onChange={this.searchBar}
+            value={this.state.searchTerm}
+          />
+        </form>
+
         <button onClick={this.toggleForm}>Add your own food!</button>
         {this.state.form && (
           <form>
             <p>Food name</p>
-            <input type="text" name="newFoodName" onChange={this.updateInput} />
+            <input
+              type="text"
+              name="newFoodName"
+              onChange={this.updateInput}
+              value={this.state.newFoodName}
+            />
             <p>Calories</p>
-            <input type="text" name="newCalories" onChange={this.updateInput} />
+            <input
+              type="number"
+              name="newCalories"
+              onChange={this.updateInput}
+              value={this.state.newCalories}
+            />
             <p>Image Link</p>
-            <input type="text" name="newImage" onChange={this.updateInput} />
-            <button onClick={this.toggleForm}> Submit</button>
+            <input
+              type="text"
+              name="newImage"
+              onChange={this.updateInput}
+              value={this.state.newImage}
+            />
+            <button onClick={this.addFood}> Submit</button>
           </form>
         )}
         {this.showFoods()}

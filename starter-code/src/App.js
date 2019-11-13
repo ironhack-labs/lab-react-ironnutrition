f431afdfdfe80b351ab3cf616b95c65e911f82a1
Shahriyar <shahryar102@yahoo.com>
2019-11-13 10:46:02 -0500

    food items displayed

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import FoodBox from './FoodBox.js'
 
 class App extends Component {
+  displayFood(){
+    let foodList=[...foods]
+    foodList.splice(0,5);
+    return(
+      foodList.map((eachFood,i)=>{
+        return(
+          <FoodBox name={eachFood.name} cal={eachFood.calories} quantity={eachFood.quantity} image={eachFood.image}/>
+        )
+      }
+        
+      )
+    )
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +29,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* <FoodBox name="vegan pizza" cal="350" quantity="2" image="https://i.imgur.com/DupGBz5.jpg"/> */}
+        {this.displayFood()}
       </div>
     );
   }

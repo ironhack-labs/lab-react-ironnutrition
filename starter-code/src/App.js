66e9154a31082ac71794840167164e2c04ba8bc8
saruit0105 <saruits2@gmail.com>
2019-11-15 09:30:55 -0500

    added missing code

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Search, FoodList, FoodForm } from "./components";
 import foods from "./foods.json";
+import "bulma/css/bulma.css";
 
 class App extends Component {
   state = { foods, filteredFoods: foods, selectedFoods: [], formOpen: false };

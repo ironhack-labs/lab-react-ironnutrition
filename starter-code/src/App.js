50448e3797eb061d8bb962bf45c1bdf7a1585625
Daniel <daniel.gonvie@gmail.com>
2019-12-04 16:11:41 +0100

    Hello world

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -12,17 +12,70 @@ class App extends Component {
     super();
     
     this.state = {
-      foods: foods
+      currentFood: {
+        name: "",
+        image: "",
+        calories: 0,
+        quantity: 1
+      },
+      foods: foods,
+      showForm: false
     }
   }
+
+  displayForm(e){
+    e.preventDefault();
+    let showForm = !this.state.showForm
+    this.setState({
+      ...this.state,
+      showForm: showForm
+    })
+  }
+
+
+
+
+  updateFood(e, key){
+    let updatedFood = {...this.state.currentFood};
+    updatedFood[key] = e.target.value
+    this.setState({
+      ...this.state,
+      currentFood: updatedFood
+    })
+  }
+
+
+  /* addFood(e){
+    e.preventDefault();
+    
+
+    this.setState({
+      ...this.state,
+      foods: ) 
+  } */
 
 
 
   render() {
     return (
       <div className="App">
+      <nav className="main-nav">
+          <div>
+          <input type="search" value="" placeholder="Search food! :)"></input>
+          <button onClick={e => this.displayForm(e)}>Add new food!</button>
+          </div>
+           {this.state.showForm && (
+            <form>
+            <input onChange={e => this.updateFood(e, "name")} type="text" value={this.state.currentFood.name} placeholder="Food name"></input>
+            <input onChange={e => this.updateFood(e, "image")} type="text" value={this.state.currentFood.image} placeholder="Paste the img URL"></input>
+            <input onChange={e => this.updateFood(e, "calories")} type="number" value={this.state.currentFood.calories} placeholder="Food calories"></input>
+            <button>ADD!</button>
+          </form>
+           )}
+          
+      </nav>
       {this.state.foods.map((food, idx) =>(
-        <FoodBox food={food}  />
+        <FoodBox key={idx} food={food}  />
 
       ))}
         

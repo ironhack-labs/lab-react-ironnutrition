73d0745be59c3ad578557373e208f0ebfa2e0340
enriqueMontano <montanoenrique@outlook.com>
2019-12-04 17:53:30 +0100

    iteration 3 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -11,14 +11,66 @@ class App extends Component {
     this.foodsClone = [...foods];
 
     this.state = {
-      foods: this.foodsClone
+      currentFood: {
+        name: "",
+        calories: 0,
+        image: ""
+      },
+
+      form: false,
+
+      displayFoods: this.foodsClone
     };
+  }
+
+  updateFood(e, key) {
+    console.log(e.target);
+    let foodsUpdated = { ...this.state };
+    foodsUpdated.currentFood[key] = e.target.value;
+    this.setState(foodsUpdated);
+  }
+
+  addFood(e) {
+    e.preventDefault();
+
+    let foodsUpdate = [...this.state.displayFoods];
+
+    foodsUpdate.push({
+      name: this.state.currentFood.name,
+      calories: this.state.currentFood.calories,
+      image: this.state.currentFood.image,
+      quantity: 0
+    });
+
+    this.setState({
+      ...this.state,
+
+      currentFood: {
+        name: "",
+        calories: 0,
+        image: ""
+      },
+
+      form: false,
+
+      displayFoods: foodsUpdate
+    });
+
+    return;
+  }
+
+  showForm() {
+    this.setState({
+      ...this.state,
+      form: true
+    });
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.foods.map((element, idx) => {
+        <button onClick={() => this.showForm()}>Add new Food</button>
+        {this.state.displayFoods.map((element, idx) => {
           return (
             <FoodBox
               key={idx}
@@ -28,6 +80,32 @@ class App extends Component {
             ></FoodBox>
           );
         })}
+        {this.state.form && (
+          <form>
+            <input
+              type="text"
+              name="name"
+              placeholder="food name"
+              onChange={e => this.updateFood(e, "name")}
+              value={this.state.currentFood.name}
+            />
+            <input
+              type="text"
+              name="calories"
+              placeholder="food calories"
+              onChange={e => this.updateFood(e, "calories")}
+              value={this.state.currentFood.calories}
+            />
+            <input
+              type="text"
+              name="image"
+              placeholder="food image"
+              onChange={e => this.updateFood(e, "image")}
+              value={this.state.currentFood.image}
+            />
+            <button onClick={e => this.addFood(e)}>SAVE</button>
+          </form>
+        )}
       </div>
     );
   }

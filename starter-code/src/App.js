7b3697d4b431b627370efa732cc7463fc424d8f9
adelcanto <a.delcanto@outlook.com>
2019-12-04 17:04:43 +0100

    Iteration 3 -4 DONE

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -10,19 +10,106 @@ class App extends Component {
     super()
     this.foods = [...foods]
     this.state = {
-      foods: [...this.foods]
+      open: false,
+      foods: [...this.foods],
+      currentFood: {
+        name: "",
+        calories: 0,
+        image: "",
+        quantity: 0
+      }
     }
   }
+
+  toggle() {
+    let toggleState = !this.state.open;
+    this.setState({
+      open: toggleState
+    })
+  }
+
+  updateFood(e, key){
+    let updatedFood = {...this.state.currentFood}
+    updatedFood[key] = e.target.value;
+
+    this.setState({
+      ...this.state,
+      currentFood: updatedFood
+    })
+
+    console.log(this.state.currentFood)
+  }
+
+  addFood(e) {
+    e.preventDefault();
+    let updatedFoods = [...foods]
+    updatedFoods.push({
+      name: this.state.currentFood.name,
+      calories: this.state.currentFood.calories,
+      image: this.state.currentFood.image
+    })
+    this.setState({
+      ...this.state,
+      currentFood: {
+        name: "",
+        calories: 0,
+        image: "",
+        quantity: 0
+      },
+      foods: updatedFoods
+    })
+  }
+
+  findFood(e) {
+    let searchedFood = [...this.state.foods]
+    searchedFood = searchedFood.filter(food => 
+      food.name.toLowerCase().includes(e.target.value.toLowerCase()))
+    this.setState({
+      ...this.state,
+      foods: searchedFood
+    })
+  }
+
+
   render() {
     return (
-
-      <div class="container">
-        <h1 class="title">IronNutrition</h1>
+      <div className="container">
+        <h1 className="title">IronNutrition</h1>
+        <button onClick={() => this.toggle()}>Add New Food</button>
+        {this.state.open && (
+          <form>
+            <input 
+                type="text"
+                placeholder="food name"
+                onChange={(e) =>this.updateFood(e, "name")}
+                value={this.state.currentFood.name}
+                />
+            <input 
+                type="number"
+                placeholder="food calories"
+                onChange={(e) =>this.updateFood(e, "calories")}
+                value={this.state.currentFood.calories}
+                />
+            <input 
+                type="text"
+                placeholder="food image"
+                onChange={(e) =>this.updateFood(e, "image")}
+                value={this.state.currentFood.image}
+                />         
+            <button onClick={(e) => this.addFood(e)}>Save</button>
+          </form>
+        ) }
         <div>
-          <input type="text" class="input search-bar" name="search" placeholder="Search" value="" />
+          <input 
+            type="text" 
+            className="input search-bar" 
+            // name="search" 
+            placeholder="Search" 
+            onChange={(e) => this.findFood(e)}
+            />
         </div>
-        <div class="columns">
-          <div class="column">
+        <div className="columns">
+          <div className="column">
             {this.state.foods.map((food, idx) => (<Food key={idx} payLoad={food} />))}
           </div>
         </div>

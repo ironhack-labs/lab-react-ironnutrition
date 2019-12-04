f05deb8ffee269b82ed8669a29ab46e91b9908d3
Laura <laura.vivas.gamboa@gmail.com>
2019-12-04 16:20:15 +0100

    Third iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bulma/css/bulma.css';
-import foods from './foods.json'
-import Foodbox from './Foodbox/Foodbox';
-
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bulma/css/bulma.css";
+import foods from "./foods.json";
+import Foodbox from "./Foodbox/Foodbox";
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
-      foodsArray : [...foods]
-    }
+      foodsArray: [...foods],
+      currentFood: {
+        name: "",
+        image: "",
+        calories: 0,
+      },
+    };
   }
 
+  updateFood(e, key) {
+    let foodUpdated = { ...this.state.currentFood };
+    foodUpdated[key] = e.target.value;
+    // console.log(this.state.currentFood)
+    this.setState({
+      ...this.state,
+      currentFood: foodUpdated,
+    });
+  }
+
+  saveFood(e){
+    e.preventDefault()
+    let copyFood = [...this.state.foodsArray]
+    let copyCurrent = {...this.state.currentFood}
+    copyFood.push(copyCurrent)
+    this.setState({
+      ...this.state,
+      foodsArray: copyFood
+    })
+
+  }
   render() {
     return (
       <div className="App">
-       { this.state.foodsArray.map((food, i) => (<Foodbox key={i} payload ={({...food})}></Foodbox>))}
+        <button onClick={e => this.saveFood(e)}>Add New Food</button>
+        <div>
+          <form>
+            <input
+              type="text"
+              placeholder="Enter Food Name"
+              onChange={e => this.updateFood(e, "name")}
+              value={this.state.currentFood.name}
+            />
+            <input
+              type="text"
+              placeholder="Enter Image Url"
+              onChange={e => this.updateFood(e, "image")}
+              value={this.state.currentFood.image}
+            />
+            <input
+              type= "number"
+              placeholder="Enter Calories"
+              onChange={e => this.updateFood(e, "calories")}
+              value={this.state.currentFood.calories}
+            />
+          </form>
+        </div>
+        {this.state.foodsArray.map((food, i) => (<Foodbox key={i} payload={{ ...food }}></Foodbox>))}
       </div>
     );
   }

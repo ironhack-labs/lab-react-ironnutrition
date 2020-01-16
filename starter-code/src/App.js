c2f4f0a635339f5394f3dfd03ac6c1531faf518a
Jeferson Silva <jeferson.s.silva1@gmail.com>
2020-01-15 22:06:07 -0300

    Interação 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FoodBox from './components/FoodBox';
+import foods from './foods.json';
 
 class App extends Component {
-  render() {
+  // state = { 
+  //   name: null,
+  //   calories: null,
+  //   image: null,
+  //   quantity: null 
+  // }
+render() {
+  console.log(foods)
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        {
+          foods.map( (foodItem, index) => (
+            <FoodBox key={index} food={foodItem}/>
+          ))
+        }
       </div>
     );
   }

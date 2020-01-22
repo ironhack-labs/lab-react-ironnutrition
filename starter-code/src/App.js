feb77e2c8b205fd6f1eeb8535811f59ca0cd49cf
lpolon <lpolon@gmail.com>
2020-01-22 14:27:29 -0300

    first iteration done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import Foodbox from './Components/FoodBox'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      foods,
+    }
+  }
+
   render() {
+    const foodObj = this.state.foods[2];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Foodbox {...foodObj} />
+    )
   }
 }
 

43cc08593f158eec2b65e1befa31a36abf5ec435
Alison <alisonbessa@gmail.com>
2020-01-22 19:16:51 -0300

    fourth iteration done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -17,57 +17,67 @@ class App extends Component {
         display: "none"
       }
     }
+    this.handleSearchChange = this.handleSearchChange.bind(this);
   } 
-
-    addHandler = () => {
-      if(this.state.formStyle.display === "none"){
-        this.setState({
-          formStyle: {
+  
+  addHandler = () => {
+    if(this.state.formStyle.display === "none"){
+      this.setState({
+        formStyle: {
           display: "block"
         }
       })
+    }
+  }
+
+  submitHandler = (props, state) => {
+    props.preventDefault();
+
+    this.state.showfoods.push(state);
+
+    this.setState({
+      showfoods: this.state.showfoods,
+      formStyle: {
+        display: "none"
       }
-    }
+    })
+  }
   
-    submitHandler = (props, state) => {
-      props.preventDefault();
+  addFoodHandler = (food, state) => {
+    let pickedFoodCopy = [...this.state.pickedFood]
+    pickedFoodCopy.push(food)
+    this.setState({
+      pickedFood: pickedFoodCopy,
+    })
+    this.calculateCalories(food.calories, state.quantity)
+  }
 
-      this.state.showfoods.push(state);
-  
-      this.setState({
-        showfoods: this.state.showfoods,
-        formStyle: {
-          display: "none"
-        }
-      })
-    }
-    
-    addFoodHandler = (food, state) => {
-      let pickedFoodCopy = [...this.state.pickedFood]
-      pickedFoodCopy.push(food)
-      this.setState({
-        pickedFood: pickedFoodCopy,
-      })
-      this.calculateCalories(food.calories, state.quantity)
-    }
-
-
+  handleSearchChange(event) {
+    let inputValue= event.target.value;
+    this.setState({
+        search:inputValue
+    })
+  }
 
   render() {
     return (
       <div className="page-view">
         <h2 className="is-size-2 has-text-weight-bold">IronNutrition</h2>
         <button onClick={this.addHandler}>Add New Food</button>
-        <Form style={this.state.formStyle} submitHandler={this.submitHandler} />
-        {foods.map(food=>
-          <FoodBox
-              key = {food.name+food.image}
-              name = {food.name} 
-              calories = {food.calories}
-              image = {food.image}
-              quantity = {food.queantity}
-          />
-        )}
+        <input className="input" type="text" name="search" value={this.state.search} onChange={this.handleSearchChange} placeholder="Search"/>
+        {foods
+          .filter(food =>
+            food.name.toLowerCase().includes(this.state.search.toLowerCase()))
+            .map(food=>  
+              <FoodBox
+                key = {food.name+food.image}
+                name = {food.name} 
+                calories = {food.calories}
+                image = {food.image}
+                quantity = {food.queantity}
+              />
+            )
+        }
       </div>
     );
   }

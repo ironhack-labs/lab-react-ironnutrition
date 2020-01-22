16eff7e5365ebae32c912c004d2748b5e366f120
Juliane Arruda <juliane_ch.arruda@live.com>
2020-01-22 15:01:37 -0300

    interation 2 done

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -6,10 +6,18 @@ import foods from './foods.json'
 import FoodBox from "./components/FoodBox";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { foods };
+  }
+
+  // allfoods
+
   render() {
     return (
       <div className="App">
-        <FoodBox />
+        {this.state.foods.map((item) => <FoodBox image={item.image} name={item.name} calories={item.calories} />)}
       </div>
     );
   }

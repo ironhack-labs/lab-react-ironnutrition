2277cb3ca98bad9ed03701bb045abf9d1920cc05
Anastasia Schmitke <schmitke@mediamarktsaturn.com>
2020-01-27 15:22:15 +0100

    Iteration 3 finished

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
-import foods from './foods.json';
 import FoodBox from './FoodBox';
 
 class App extends Component {
--- starter-code/src/FoodBox.js
+++ starter-code/src/FoodBox.js
@@ -5,8 +5,39 @@ export default class FoodBox extends Component {
 
     constructor() {
         super();
-        this.state = {food: foods}
+        this.state = { foods: foods, 
+            name: '',
+            calories: '',
+            image: '',
+            quantity: 0,
+            formHidden: true,
+        }
     }
+
+    submitForm = (event) => {
+        event.preventDefault();
+        console.log("submit erledigt")
+        const newFood = {
+            name: this.state.name,
+            calories: this.state.calories,
+            image: this.state.image,
+            quantity: 0
+        }
+        this.setState({
+            foods: [...this.state.foods, newFood], 
+            formHidden: true
+        })
+    }
+
+    showForm = (event) => {
+        this.setState (this.state.formHidden ? {formHidden: false} : {formHidden: true} )
+    }
+
+    handleChange = (event) => {
+        let { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
 
 
     render() {
@@ -14,45 +45,63 @@ export default class FoodBox extends Component {
 
 
             <div className="section">
-                {this.state.food.map((oneFood, index) => {
+                <div>
+                    <button onClick={this.showForm}>Add new food</button>
+
+                    {this.state.formHidden === false &&
+                    <form onSubmit={this.submitForm}>
+                        <label>Name</label>
+                        <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+                        <label>Number of calories</label>
+                        <input type="number" name="calories" value={this.state.calories} onChange={this.handleChange} />
+                        <label>Image</label>
+                        <input type="text" name="image" value={this.state.image} onChange={this.handleChange} />
+
+                        <input type="submit" value="Submit" />
+
+                    </form>}
+                </div>
+
+                {this.state.foods.map((oneFood, index) => {
                     return (
 
-                
-                
-                <div className="container box">
-                    <article className="media">
-                        <div className="media-left">
-                            <figure className="image is-64x64">
-                                <img src={oneFood.image} alt="foodbild" />
-                            </figure>
-                        </div>
-                        <div className="media-content">
-                            <div className="content">
-                                <p>
-                                    <strong>{oneFood.name}</strong> <br />
-                                    <small>{oneFood.calories}</small>
-                                </p>
-                            </div>
-                        </div>
-                        <div className="media-right">
-                            <div className="field has-addons">
-                                <div className="control">
-                                    <input
-                                        className="input"
-                                        type="number"
-                                        value={oneFood.quantity}
-                                    />
+
+
+                        <div className="container box">
+                            <article className="media">
+                                <div className="media-left">
+                                    <figure className="image is-64x64">
+                                        <img src={oneFood.image} alt="foodbild" />
+                                    </figure>
                                 </div>
-                                <div className="control">
-                                    <button className="button is-info">
-                                        +
+                                <div className="media-content">
+                                    <div className="content">
+                                        <p>
+                                            <strong>{oneFood.name}</strong> <br />
+                                            <small>{oneFood.calories}</small>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="media-right">
+                                    <div className="field has-addons">
+                                        <div className="control">
+                                            <input
+                                                className="input"
+                                                type="number"
+                                                value="1"
+                                            />
+                                        </div>
+                                        <div className="control">
+                                            <button className="button is-info">
+                                                +
           </button>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            </article>
                         </div>
-                    </article>
-                </div>
-                 )})}
+                    )
+                })}
             </div>
         )
     }

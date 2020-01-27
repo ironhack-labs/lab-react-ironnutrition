830a37553d22b7a012e1fefc17c73fa3da404710
Bialas <bialaska@media-saturn.com>
2020-01-27 19:52:14 +0100

    until iteration 4

---
+++ starter-code/src/components/AddFood.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class AddFood extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            calories: '',
+            image: '',
+            quantity: 0
+        }
+    }
+
+    formSubmitHandler = (event) => {
+        event.preventDefault();
+        this.props.addSomeFood(this.state);
+        this.setState({
+            name: '',
+            calories: '',
+            image: '',
+            quantity: 0
+        });
+        this.props.toggleForm(this.state)
+    }
+    
+    handleChange = (event) => {
+        let { name, value } = event.target;        
+        this.setState({ [name]: value });
+    }
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.formSubmitHandler}>
+                    <label>Name:</label>
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+
+                    <label>Number:</label>
+                    <input type="text" name="quantity" value={this.state.quantity} onChange={this.handleChange} />
+
+                    <label>Calaries:</label>
+                    <input type="text" name="calories" value={this.state.calories} onChange={this.handleChange} />
+
+                    <label>Image:</label>
+                    <input type="text" name="image" value={this.state.image} onChange={this.handleChange} />
+                    
+
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default AddFood;
---
+++ starter-code/src/components/FoodBoxDynamic.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import foods from '../foods.json';
+import FoodBox from './FoodBox';
+import AddFood from './AddFood';
+
+class FoodBoxDynamic extends Component {
+    constructor() {
+        super();
+        this.state = {
+            foods: foods,
+            displayForm: false,
+            searchInput: ""
+        }
+    }
+
+    toggleForm = () => {
+        this.setState({ displayForm: !this.state.displayForm })
+      }
+
+    addFoodHandler = (someFood) => {
+        this.setState({
+            foods: [...this.state.foods, someFood]
+        });
+        
+    }
+
+    handleSearch = (event) => {               
+        this.setState({
+            searchInput: event.target.value
+        });
+    }
+
+    render() {                               
+        const filteredFood = this.state.foods.filter(food => food.name.toLowerCase().includes(this.state.searchInput.toLowerCase()))
+        /* const filteredFoodJSX = filteredFood.map((oneFood, index) => {
+            return <FoodBox key = {index} {...oneFood}/>;
+        }) */
+        
+
+        return(
+            <div>
+                <input type = "search" value={this.state.searchInput} onChange={this.handleSearch}/>
+                <div className = "addFood-container">
+                    <button onClick={this.toggleForm}>
+                    {/* {this.state.displayForm ? 'Hide Add Food Form' : 'Display Add Food Form'} */}
+                    Add Food
+                    </button>
+                    {this.state.displayForm ? <AddFood addSomeFood = {this.addFoodHandler} toggleForm = {this.toggleForm}/> : null}
+
+                </div>
+                <div>
+                    {/* {filteredFoodJSX} */}
+                    {filteredFood.map((oneFood, index) => {
+                        return <FoodBox key = {index} {...oneFood}/>;
+        })}
+                </div>
+            </div>
+        )        
+    }
+}
+
+export default FoodBoxDynamic;

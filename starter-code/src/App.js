3e0791d44d4eaefcc04ea1ebb77518966bc0dd50
Strapak, Sergej <strapak@mediamarktsaturn.com>
2020-01-27 13:47:39 +0100

    Iteration 1 & 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,46 +1,14 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import foods from './foods.json'
+import React, { Component } from 'react';
+import './App.css';
+import 'bulma/css/bulma.css';
+import FoodBox from './components/FoodBox'
+
 
 class App extends Component {
+ 
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='FoodBox'>
-          <div className='box'>
-            <article className='media'>
-              <div className='media-left'>
-                <figure className='image is-64x64'>
-                  <img src='https://i.imgur.com/eTmWoAN.png' />
-                </figure>
-              </div>
-              <div className='media-content'>
-                <div className='content'>
-                  <p>
-                    <strong>Pizza</strong> <br />
-                    <small>400 cal</small>
-                  </p>
-                </div>
-              </div>
-              <div className='media-right'>
-                <div className='field has-addons'>
-                  <div className='control'>
-                    <input className='input' type='number' value='1' />
-                  </div>
-                  <div className='control'>
-                    <button className='button is-info'>+</button>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </div>
-        </p>
-      </div>
+      <FoodBox />
     )
   }
 }
---
+++ starter-code/src/components/FoodBox.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import foods from '../foods.json';
+
+export default class FoodBox extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {foods}
+  }
+  render () {
+    return (
+      <div className='App'>
+        {this.state.foods.map(food => (
+          <div className='box'>
+            <article className='media'>
+              <div className='media-left'>
+                <figure className='image is-64x64'>
+                  <img src={food.image} />
+                </figure>
+              </div>
+              <div className='media-content'>
+                <div className='content'>
+                  <p>
+                    <strong>{food.name}</strong> <br />
+                    <small>{food.calories} cal</small>
+                  </p>
+                </div>
+              </div>
+              <div className='media-right'>
+                <div className='field has-addons'>
+                  <div className='control'>
+                    <input className='input' type='number' value='1' />
+                  </div>
+                  <div className='control'>
+                    <button className='button is-info'>+</button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}

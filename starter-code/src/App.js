bddc73b47aa32beb4a7a9ddf7c0471351990d1c6
kilianal <kilianal@mediamarktsaturn.com>
2020-01-27 13:48:28 +0100

    Iteration 3 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FoodBox from './components/FoodBox'
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import Foodform from './components/FoodForm'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      food: foods,
+      form: false,
+      
+    }
+  }
+
+  showForm=()=>{
+    this.setState({form: !this.state.form})
+    console.log(this.state.form)
+  }
+
+  addFoodHandler = (theFood) => {
+    const foodCopy = [...this.state.food];
+    foodCopy.push(theFood);
+    this.setState({
+      food: foodCopy,
+      form: !this.state.form
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +37,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* Button damit das showForm handler aufgerufen wird  */}
+        <button onClick={this.showForm}>Add Food</button>
+
+        {/* Wenn sich der Status form ändert, dann wird das Formular angezeigt oder nicht */}
+        {this.state.form ? <Foodform addFood={this.addFoodHandler}/>: <p></p>}
+
+        {/* Hier werden die einzelnen Elemente aus dem foods Array an FoodBox übergeben */}
+       {this.state.food.map((oneFood,index)=>{
+         return (<FoodBox key={index} {...oneFood} />)
+       })
+         
+         
+         }
+        
       </div>
     );
   }
---
+++ starter-code/src/components/FoodForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+export default class FoodForm extends Component {
+    constructor(){
+        super();
+        this.state={
+            name: '',
+            calories: 0,
+            image: ''
+        }
+    }
+
+  handleFormSubmit=(event)=>{
+      event.preventDefault();
+      this.props.addFood(this.state);
+      this.setState({
+          name: '',
+          calories: '',
+          image: ''
+      })
+  }
+
+    handleChange=(event)=>{
+        let{name, value}=event.target;
+        this.setState({[name]: value})
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleFormSubmit}>
+                    <label>Name: </label>
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+
+                    <label>Calories: </label>
+                    <input type="number" name="calories" value={this.state.calories} onChange={this.handleChange} />
+
+                    <label>Image: </label>
+                    <input type="url" name="image" value={this.state.image} onChange={this.handleChange}/>
+
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+}

e6cd8f3002de9a3806e4dfd6b0cccf55f6430ee4
Acostan13 <acostan@fau.edu>
2020-02-12 18:10:44 -0500

    first three iterations

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json'
+import FoodBox from './Foodbox'
+import 'bulma/css/bulma.css';
+console.log(foods)
+
 
 class App extends Component {
+
+  state = {
+    foods,
+    foodForm:false
+  }
+
+  showFoods = () => {
+    let foodList = this.state.foods.map((eachFood,i)=>{
+      return <FoodBox key = {i} {...eachFood} />
+    })
+    return foodList;
+  }
+
+  toggleFoodForm = () => {
+    this.setState({
+      foodForm: !this.state.foodForm
+    })
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    this.toggleFoodForm()
+    console.log('submit ', this.state)
+    let newFoods = [...this.state.foods]
+    newFoods.unshift({
+      name:this.state.food,
+      calories:null,
+      quantity:1,
+      image:null
+    })
+
+    this.setState({
+      foods:newFoods
+    })
+  }
+
+  handleInputChange = (e) => {
+    console.log('change', e.target.name, e.target.value)
+    this.setState( { [e.target.name] : e.target.value } ) //food : 'lasagn'
+  }
+
+  showFoodForm = () => {
+    if(this.state.foodForm){
+      return (
+      <form onSubmit={this.handleFormSubmit}>
+        <input onChange={this.handleInputChange} type="text" name="food"/>
+        <input  type="submit" />
+      </form>
+      )
+    } else {
+      return <button onClick={this.toggleFoodForm}>Add New Food</button>
+    }
+  }
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br></br>
+        {this.showFoodForm()}
+        <br></br>
+
+        
+
+        {this.showFoods()}
+
       </div>
     );
   }
 }
+
 
 export default App;

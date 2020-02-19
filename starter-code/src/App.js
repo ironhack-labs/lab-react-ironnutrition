7a0ce7e575076eb93f4d4b01971b989669ad38b5
Ale Ochoa <ale.ochoafi@gmail.com>
2020-02-19 16:13:15 -0600

    iteracion 3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import foodsAll from './foods.json'
 
 class App extends Component {
   state={
-    foods:[...foodsAll]
+    foods:[...foodsAll],
+    name:"",
+    calories:'',
+    image:'',
+    new:false
   }
+  agrega=()=>{
+    this.setState({new:true})
+  }
+  agregaPlato=(name,image,calories)=>{
+    let food={name,image,calories,quantity:0}
+    this.setState({foods:[...this.state.foods,food],
+    name:"",calories:"",image:"",new:false})
+  }
+
+  handleChange=(e)=>{
+    const {name,value}=e.target
+    this.setState({[name]:value})
+  }
+  
   render() {
     return (
       <div className="App">
+      {this.state.new ? <section>
+      <label>Name:</label> <input onChange={this.handleChange} name="name" value={this.state.name} />
+      <br></br>
+      <label>Calories:</label> <input onChange={this.handleChange} type="number" name="calories" value={this.state.calories} />
+      <br></br>
+      <label>Image URL:</label> <input onChange={this.handleChange} type="url" name="image" value={this.state.image} /> 
+      <br></br>
+      <button onClick={()=>this.agregaPlato(this.state.name,this.state.image,this.state.calories)}>Add</button>
+      </section> : <button onClick={this.agrega}>Add new food</button>}
       {this.state.foods.map((food,indx)=>
        <div className="box" key={indx}>
   <article className="media">
     <div className="media-left">
       <figure className="image is-64x64">
-        <img src={food.image} />
+        <img src={food.image} alt={food.name}/>
       </figure>
     </div>
     <div className="media-content">

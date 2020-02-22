75d3152d33b2515d3772e0907235c9075bbcda47
Jorch39 <jorch.finanzas@gmail.com>
2020-02-21 21:59:02 -0600

    step 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import FoodBox from './components/FoodBox';
+
+const foodList = [...foods]
 
 class App extends Component {
+  state = {
+    foodList : foodList
+  
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* < FoodBox /> */}
+        <section className='display'>
+          <div className='foodList'>
+            {this.state.foodList.map((item) => <FoodBox key={item} {...item}/>)}
+          </div>
+        </section>
       </div>
     );
   }

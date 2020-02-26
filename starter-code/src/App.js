0f53145fe50faad4faab9e049463a1dc4f076752
annkay108 <annkhachhibhoya@gmail.com>
2020-02-26 17:28:46 +0100

    form works

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
 import './App.css';
-
+import FoodBox from "./components/FoodBox";
+import FoodForm from "./components/FoodForm";
 class App extends Component {
+  state ={
+    foodsList: foods,
+    showForm: false
+  }
+
+  toogleForm = () => {
+    this.setState({ showForm: !this.state.showForm })
+  };
+
+  addNewFood = newFoodObj => {
+    const FoodCopy = [...this.state.foodsList];
+
+    FoodCopy.unshift(newFoodObj);
+
+    this.setState({ foodsList: FoodCopy });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <button onClick={this.toogleForm}>Add the food</button>
+          {this.state.showForm
+            ? <FoodForm toggle={this.toogleForm} addFood={this.addNewFood}/>
+            : null}
+        {
+          this.state.foodsList.map((foodObj,index)=>{
+            return(
+              <div className ="foodName" key= {index} >
+                <FoodBox
+                  {...foodObj}
+                />
+              </div>
+            );
+          })
+        }
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ starter-code/src/components/FoodForm.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+export default class FoodForm extends Component {
+    state ={
+        name: "",
+        image: "",
+        quantity: "",
+        calories: ""
+    }
+
+    handleChange = e =>{
+        let {value, name} = e.target;
+        this.setState({[name]: value});
+    }
+
+    handleSubmit = e =>{
+        e.preventDefault();
+        const newFood = this.state;
+        //newFood.id = this.props.secretCode;
+
+        this.props.addFood(newFood);
+
+        this.setState({
+            name: "",
+            image: "",
+            quantity: "",
+            calories: ""
+        });
+        this.props.toggle();
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit= {this.handleSubmit}>
+                    <label> name</label>
+                    <input
+                        type="text"
+                        name = "name"
+                        value = {this.state.name}
+                        onChange={this.handleChange}
+                    />
+                    <label> calories</label>
+                    <input
+                        type="text"
+                        name = "calories"
+                        value = {this.state.calories}
+                        onChange={this.handleChange}
+                    />
+                    <label>image</label>
+                    <input
+                        type="text"
+                        name = "image"
+                        value = {this.state.image}
+                        onChange={this.handleChange}
+                    />
+                    <button type="submit"> Submit</button>
+                </form>
+            </div>
+        )
+    }
+}

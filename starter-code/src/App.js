ca8d14e0bea3274ef3fd2dc6fa57a63b979ce55a
Daedra <foglzerika@gmail.com>
2020-03-03 11:46:04 -0600

    Display food

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json'
 import FoodBox from './components/FoodBox'
 class App extends Component {
   render() {
+ 
+
     return (
       <div className="App">
-        <FoodBox/>
+        <div className="columns">
+          <div className="column is-12">
+          {foods.map( (food, id) =>{
+         return( <FoodBox key= {id} data ={food} />)
+        })}
+          </div>
+        </div>
+        
       </div>
     );
   }
--- starter-code/src/components/FoodBox.js
+++ starter-code/src/components/FoodBox.js
@@ -2,31 +2,33 @@ import React, { Component } from 'react'
 
 export default class FoodBox extends Component {
     render() {
+        console.log(this.props.data)
+
         return (
             <div>
                 <div className="box">
                     <article className="media">
                         <div className="media-left">
                         <figure className="image is-64x64">
-                            <img src="https://i.imgur.com/eTmWoAN.png" />
+                            <img src={this.props.data.image} />
                         </figure>
                         </div>
                         <div className="media-content">
                         <div className="content">
                             <p>
-                            <strong>Pizza</strong> <br />
-                            <small>400 cal</small>
+                            <strong>{this.props.data.name}</strong> <br />
+                            <small>{this.props.data.calories}</small>
                             </p>
                         </div>
                         </div>
                         <div className="media-right">
                         <div className="field has-addons">
                             <div className="control">
-                            <input
+                             <input
                                 className="input"
                                 type="number" 
                                 value="1"
-                            />
+                            /> 
                             </div>
                             <div className="control">
                             <button className="button is-info">

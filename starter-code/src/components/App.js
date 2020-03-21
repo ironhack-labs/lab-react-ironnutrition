58118d9eeba14a4e5c8215e057bfc9e4f04b2a2f
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-03-21 22:21:45 +0100

    Add form component and basic structure

--- starter-code/src/components/App.js
+++ starter-code/src/components/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FoodBox } from './FoodBox';
 import foods from '../foods.json';
 import 'bulma/css/bulma.css';
+import { Form } from './Form';
 
 export const App = () => {
+  const [show, setShow] = useState(false);
+  const showForm = () => {
+    setShow(true);
+  };
   return (
-    <div class='container'>
-      <h1 class='title'>IronNutrition</h1>
+    <div className='container'>
+      <h1 className='title'>IronNutrition</h1>
       <div>
         <input
           type='text'
-          class='input search-bar'
+          className='input search-bar'
           name='search'
           placeholder='Search'
           value=''
         />
       </div>
+      {show && <Form />}
       <div className='columns'>
         <div className='column'>
           {foods.map((e, i) => {
             return <FoodBox key={i} {...e} />;
           })}
+          <div className='buttons'>
+            <button
+              className='button is-primary is-light'
+              onClick={e => showForm()}
+            >
+              Add Food
+            </button>
+          </div>
         </div>
       </div>
     </div>

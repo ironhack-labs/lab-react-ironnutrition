a4e54e75d89f0151771d6537648ede332904c14f
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-03-21 21:17:43 +0100

    Change bundler to parcel

---
+++ starter-code/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App-title'>Welcome to React</h1>
+      </header>
+      <p className='App-intro'>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+};

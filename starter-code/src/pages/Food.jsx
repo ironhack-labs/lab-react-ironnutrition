3a6067756a02ea17638f137420ec4c6604294837
Román Méndez <mendezbeck@gmail.com>
2020-03-22 10:29:39 +0100

    search bar on iteration 4 done

--- starter-code/src/pages/Food.jsx
+++ starter-code/src/pages/Food.jsx
@@ -1,14 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import FoodBox from "../components/FoodBox";
+import SearchBox from "../components/SearchBox";
 import { FoodContext } from "../api/food.api";
 
 const Food = () => {
   const { food } = useContext(FoodContext);
+  const [searchFilter, setSearchFilter] = useState("");
+
+  const filtered = food.filter(item => {
+    return item.name.toLowerCase().indexOf(searchFilter) !== -1;
+  });
+
   return (
     <div className="column">
-      {food.map((food, i) => (
-        <FoodBox key={i} {...food} />
-      ))}
+      <SearchBox {...{ setSearchFilter, searchFilter }} />
+      {filtered.map((food, i) => {
+        return <FoodBox key={i} {...food} />;
+      })}
     </div>
   );
 };

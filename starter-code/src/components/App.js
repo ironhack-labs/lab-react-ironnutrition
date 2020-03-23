2601735190e75e784adfe0f490e44903b86dd6a3
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-03-23 15:19:14 +0100

    Update Add button style

--- starter-code/src/components/App.js
+++ starter-code/src/components/App.js
@@ -3,6 +3,21 @@ import { FoodBox } from './FoodBox';
 import foods from '../foods.json';
 import 'bulma/css/bulma.css';
 import { Form } from './Form';
+import styled from 'styled-components';
+
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const ButtonContainer = styled.div`
+  padding: 10px;
+  border-radius: 50%;
+  background-color: white;
+  position: fixed;
+  bottom: 30px;
+  right: 50px;
+  border: 0.3px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0.5em 1em -0.125em rgba(0, 0, 0, 0.1);
+`;
 
 export const App = () => {
   const [show, setShow] = useState(false);
@@ -27,14 +42,15 @@ export const App = () => {
           {foods.map((e, i) => {
             return <FoodBox key={i} {...e} />;
           })}
-          <div className='buttons'>
-            <button
-              className='button is-primary is-light'
+
+          <ButtonContainer>
+            <FontAwesomeIcon
+              icon={faPlusCircle}
+              color='#3f3'
               onClick={e => showForm()}
-            >
-              Add Food
-            </button>
-          </div>
+              size='4x'
+            />
+          </ButtonContainer>
         </div>
       </div>
     </div>

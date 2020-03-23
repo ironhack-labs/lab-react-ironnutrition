5cd2075a130cc3677aee15fd91ff5e35b33e999f
Teresa Pelinski <teresapelinski@gmail.com>
2020-03-23 17:57:19 +0100

    created static components

---
+++ starter-code/src/components/SearchBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const SearchBar = () => (
+    <div>
+        <input type="text" className="input search-bar" name="search" placeholder="Search" value="" />
+    </div>
+);

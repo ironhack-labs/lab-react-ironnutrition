46e56be9b50bef909420f34d51a1ebc5bdc2439e
J. A. Hernández <joseanher81@gmail.com>
2020-03-23 16:27:34 +0100

    Iteration 4

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import FoodList from './Components/FoodList';
 import NewFood from './Components/NewFood';
 import FoodContextProvider from './contexts/FoodContext';
+import Search from './Components/Search';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <FoodContextProvider>
+          <Search/>
           <FoodList/>
           <NewFood/>
         </FoodContextProvider>

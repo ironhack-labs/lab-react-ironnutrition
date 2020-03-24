7ec6632a0e8945912c1f867cb272a93c517a6364
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-03-24 19:43:31 +0100

    Added little format and list

--- starter-code/src/components/FoodBox.js
+++ starter-code/src/components/FoodBox.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import styled from "styled-components"
+import { Card, Button } from 'react-bootstrap';
+import foodsArray from "../../src/foods"
+
+const Block = styled.section`
+  margin-left:100px;
+  margin-top:38px;
+  border: 1px solid red;
+  width: 300px;
+  height:140px;
+  
+`;
+
+
+const BtnFormat = styled.section`
+  padding: 10px 80px;
+`;
+
+
 
 class FoodBox extends Component {
 
   constructor() {
     super()
-    this.state = {
-      food: []
-    }
-
-
-    const foodSize = styled.div`
-
-    width: 20px;
-      height: 200px;
-    
-    `;
+    this.state = { foods: foodsArray }
 
 
   }
@@ -24,46 +32,63 @@ class FoodBox extends Component {
 
 
   render() {
-
+    console.log("This is the return Array of objects JSON", this.state.foods)
     return (
+      this.state.foods.map((food, i) =>
+        <Block key={i}>
+          <div className="box">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img style={{ width: 60, height: 50 }} variant="top" src={food.image} />
+              <Card.Body>
+                <Card.Title>{food.name} {food.quantity}</Card.Title>
+                <Card.Text>{food.calories} calories</Card.Text>
 
-      <div className="box">
-        <article className="media">
-          <div className="media-left">
-            <foodSize>
-              <figure className="image is-64x64">
-                <img style={{ widht: 60, height: 50 }} src="https://i.imgur.com/eTmWoAN.png" />
-              </figure>
-            </foodSize>
-          </div>
-          <div className="media-content">
-            <div className="content">
-              <p>
-                <strong>Pizza</strong> <br />
-                <small>400 cal</small>
-              </p>
-            </div>
-          </div>
-          <div className="media-right">
-            <div className="field has-addons">
-              <div className="control">
-                <input
-                  className="input"
-                  type="number"
-                  value="1"
-                />
-              </div>
-              <div className="control">
-                <button className="button is-info">
-                  +
-              </button>
-              </div>
-            </div>
-          </div>
-        </article>
-      </div >
+                <BtnFormat>
+                  <Button style={{ marginRight: 5 }} variant="primary">+</Button>
+                  <Button style={{ marginRight: 5 }} variant="primary">-</Button>
+                  <Button type="submit" value="Submit" variant="primary">Add New</Button>{' '}
+                </BtnFormat>
+              </Card.Body>
+            </Card>
+          </div >
+        </Block>
+      )
     );
   }
 }
 
 export default FoodBox;
+
+
+
+{/* <article className="media">
+<div className="media-left">
+  <figure style={{ width: 60, height: 50 }} className="image is-64x64">
+    <img style={{ float: "left", width: 60, height: 50 }} src="https://i.imgur.com/eTmWoAN.png" alt="food" />
+  </figure>
+</div>
+<div className="media-content">
+  <div className="content">
+    <p style={{ height: 20 }} >
+      <strong style={{ paddingLeft: 20 }}>Pizza</strong> <br />
+      <small style={{ paddingLeft: 20 }}>400 cal</small>
+    </p>
+  </div>
+</div>
+<div className="media-right">
+  <div className="field has-addons">
+    <div className="control">
+      <input style={{ float: "right", width: 20, height: 20 }}
+        className="input"
+        type="number"
+        value="1"
+      />
+    </div>
+    <div className="control">
+      <button style={{ float: "right", width: 30, height: 30 }} className="button is-info">
+        +
+  </button>
+    </div>
+  </div>
+</div>
+</article> */}

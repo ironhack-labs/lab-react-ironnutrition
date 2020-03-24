b824e1569a78a6e87c0b88531b211998c44054cf
Teresa Pelinski <teresapelinski@gmail.com>
2020-03-24 15:39:16 +0100

    foods is now context

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import 'bulma/css/bulma.css';
 import './App.css';
@@ -8,16 +8,26 @@ import { Layout } from "./components/Layout"
 import { MainPage } from './pages/Main.page';
 import { AddNewFoodPage } from './pages/AddNewFood.page'
 
-const App = () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route path="/" exact component={MainPage} />
-        <Route path="/add" component={AddNewFoodPage} />
-      </Switch>
-    </Layout >
-  </Router>
+//Context
+import initialFoods from "./foods.json";
+export const FoodContext = React.createContext(initialFoods);
 
-);
+
+const App = () => {
+  const [foods, setFoods] = useState(initialFoods);
+  return (
+    <FoodContext.Provider value={{ foods, setFoods }}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={MainPage} />
+            <Route path="/add" component={AddNewFoodPage} />
+          </Switch>
+        </Layout >
+      </Router>
+    </FoodContext.Provider>
+
+  )
+};
 
 export default App;
--- starter-code/src/pages/Main.page.js
+++ starter-code/src/pages/Main.page.js
@@ -1,36 +1,40 @@
-import React from "react";
-import foods from "../foods.json";
+import React, { useContext } from "react";
+import { FoodContext } from "../App";
+import { withRouter } from "react-router-dom";
 
-
-//Components
+//Visual components
 import { FoodBox } from "../components/FoodBox"
 import { SearchBar } from '../components/SearchBar';
 import { AddNewFoodBtn } from '../components/AddNewFood.button';
 
-import { withRouter } from "react-router-dom";
 
-export const MainPage = withRouter(({ history }) => (
-    <div>
-        <div className="columns">
-            <div className="column">
-                <AddNewFoodBtn onClick={() => history.push("/add")} />
+
+
+export const MainPage = withRouter(({ history }) => {
+    const { foods, } = useContext(FoodContext);
+    return (
+        <div>
+            <div className="columns">
+                <div className="column">
+                    <AddNewFoodBtn onClick={() => history.push("/add")} />
+                </div>
+                <div className="column" style={{ paddingTop: 20 }}>or</div>
+                <div className="column is-four-fifths">
+                    <SearchBar />
+                </div>
             </div>
-            <div className="column" style={{ paddingTop: 20 }}>or</div>
-            <div className="column is-four-fifths">
-                <SearchBar />
+            <div className="columns">
+                <div className="column">
+                    {foods.map((e, i) => <FoodBox key={i} food={e} />)}
+                </div>
+                <div className="column content">
+                    <h2 className="subtitle">Today's foods</h2>
+                    <ul>
+                        <li>1 Pizza = 400 cal</li>
+                        <li>2 Salad = 300 cal</li>
+                    </ul>
+                    <strong>Total: 700 cal</strong>
+                </div>
             </div>
-        </div>
-        <div className="columns">
-            <div className="column">
-                {foods.map((e, i) => <FoodBox key={i} food={e} />)}
-            </div>
-            <div className="column content">
-                <h2 className="subtitle">Today's foods</h2>
-                <ul>
-                    <li>1 Pizza = 400 cal</li>
-                    <li>2 Salad = 300 cal</li>
-                </ul>
-                <strong>Total: 700 cal</strong>
-            </div>
-        </div>
-    </div>));
+        </div>)
+});

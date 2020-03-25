73325cb53f945b7a54e2cc9db3c283d260e2583d
Iván Herranz <ihcuesta@gmail.com>
2020-03-25 18:39:48 +0100

    Iteration 5: done

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bulma/css/bulma.css";
+import Home from "./components/pages/Home";
+
+const App = () => {
+  return <Home></Home>;
+};
+
+export default App;

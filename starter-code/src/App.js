ba7754f13d7603d35a45d38471eef1752d9f9d3a
Carlos Mateo <cmc2k5@gmail.com>
2020-03-25 01:00:08 +0100

    Iteration 1 completed

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+// DATA
+import foods from './foods.json';
+
+// COMPONENTS
+import { FoodBox } from './components/FoodBox';
+
+
+export const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+        <FoodBox />
+    )
 }
-
-export default App;

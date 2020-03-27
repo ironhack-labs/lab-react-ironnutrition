8a8bfeb015d00eb7a85e455aee2faf6cd8bfcb93
Carlos Mateo <cmc2k5@gmail.com>
2020-03-27 12:04:56 +0100

    Iteration 5 + Bonuses - Completed

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,7 +5,7 @@ import foodArray from './food.json';
 
 // COMPONENTS
 import { FoodBox } from './components/FoodBox';
-import { AddFoodForm } from './components/AddFoodForm';
+import { AddFoodModal } from './components/AddFoodModal';
 import { TodayFood } from './components/TodayFood';
 
 export const App = () => {
@@ -13,52 +13,56 @@ export const App = () => {
     const [food, setFood] = useState([]);
     const [query, setQuery] = useState('');
     const [todayFood, setTodayFood] = useState([]);
-
-    const [formVisibility, setFormVisibility] = useState(false);
+    const [modalVisibility, setModalVisibility] = useState(false);
 
     useEffect(() => {
-        const filter = foodArray.filter(f => f.name.toLowerCase().includes(query));
+        const filter = foodArray.filter(f => f.name.toLowerCase().includes(query.toLowerCase()));
         setFood(filter);
     }, [query]);
 
-    const toggleFormVisibility = (e, isVisible) => {
-        setFormVisibility(isVisible);
-    }
-
     const handleAddFood = (data) => {
-        let newFood = [...food];
-        newFood.unshift(data);
+        let newFood = [...food, data];
         setFood(newFood);
     }
 
     return (
-        <div className="container">
-            <header className="app-header">
-                <h1 className="title">IronNutrition</h1>
-                <button className="button is-primary" onClick={e => toggleFormVisibility(e, !formVisibility)}>
-                    <span className="icon">
-                        <span className="icon-add">+</span>
-                    </span>
-                    <span>Add Food</span>
-                </button>
-
-                <div className="field search-bar">
-                    <div className="control">
-                        <input className="input" id="search" name="search" type="text" value={query} onChange={e => setQuery(e.target.value)} />
-                    </div>
-                </div>
-            </header>
-            <AddFoodForm
-                isVisible={formVisibility}
+        <div>
+            <AddFoodModal
+                visibility={modalVisibility}
+                setVisibility={setModalVisibility}
                 onFormSubmit={data => handleAddFood(data)}
             />
-            <div className="columns">
-                <div className="column is-three-quarter food-list">
-                    {food.length && food.map((f, i) => {
-                        return <FoodBox key={i} food={f} todayFood={todayFood} setTodayFood={setTodayFood} />
-                    })}
+            <div className="container">
+                <header className="app-header">
+                    <h1 className="title">IronNutrition</h1>
+                    <button className="button is-primary" onClick={() => setModalVisibility(!modalVisibility)}>
+                        <span className="icon">
+                            <span className="icon-add">+</span>
+                        </span>
+                        <span>Add Food</span>
+                    </button>
+
+                    <div className="field search-bar">
+                        <div className="control">
+                            <input
+                                className="input"
+                                id="search"
+                                name="search"
+                                type="search"
+                                placeholder="Filter by food name..."
+                                value={query}
+                                onChange={e => setQuery(e.target.value)} />
+                        </div>
+                    </div>
+                </header>
+                <div className="columns">
+                    <div className="column is-three-quarter food-list">
+                        {food.length && food.map((f, i) => {
+                            return <FoodBox key={i} food={f} todayFood={todayFood} setTodayFood={setTodayFood} />
+                        })}
+                    </div>
+                    <TodayFood todayFood={todayFood} setTodayFood={setTodayFood} />
                 </div>
-                <TodayFood todayFood={todayFood} />
             </div>
         </div>
     )
--- starter-code/src/components/TodayFood.js
+++ starter-code/src/components/TodayFood.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import _ from 'lodash';
 
-export const TodayFood = ({ todayFood }) => {
+export const TodayFood = ({ todayFood, setTodayFood }) => {
 
-    const food = _.groupBy(todayFood, 'name');
+    let food = _.groupBy(todayFood, 'name');
+    let foodArr = _.keys(food).map((name) => ({
+        name: name,
+        quantity: _.sumBy(food[name], 'quantity'),
+        calories: _.first(food[name])['calories'],
+    }))
 
     return (
         <div className="column is-one-quarter food-added">
             <h2 className="title is-4">Today's food</h2>
-            <div class="content">
+            <div className="content">
                 <ul>
                     {
-                        _.keys(food).map((name, i) => {
-                            const totalQty = _.sumBy(food[name], 'quantity');
-                            const calories = _.first(food[name])['calories'];
-                            return <li key={i}>
-                                {`${totalQty} ${name} = ${calories * totalQty} calories`}
+                        foodArr.map((food, i) => (
+                            <li key={i} className="today-food-item">
+                                <span>{`${food.quantity} ${food.name} = ${food.calories * food.quantity} calories`}</span>
+                                <a className="delete is-small" onClick={(e) => setTodayFood(todayFood.filter(f => f.name !== food.name))}></a>
                             </li>
-                        })
+                        ))
                     }
                 </ul>
             </div>

da87d781b6a2db8f7a60889cb381caf4bb8a047f
Carlos Mateo <cmc2k5@gmail.com>
2020-03-27 13:41:32 +0100

    Adds Total calories + small refactor

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,8 +4,10 @@ import React, { useState, useEffect } from 'react';
 import foodArray from './food.json';
 
 // COMPONENTS
-import { FoodBox } from './components/FoodBox';
 import { AddFoodModal } from './components/AddFoodModal';
+import { Header } from './components/Header';
+import { Search } from './components/Search';
+import { FoodList } from './components/FoodList';
 import { TodayFood } from './components/TodayFood';
 
 export const App = () => {
@@ -33,34 +35,16 @@ export const App = () => {
                 onFormSubmit={data => handleAddFood(data)}
             />
             <div className="container">
-                <header className="app-header">
-                    <h1 className="title">IronNutrition</h1>
-                    <button className="button is-primary" onClick={() => setModalVisibility(!modalVisibility)}>
-                        <span className="icon">
-                            <span className="icon-add">+</span>
-                        </span>
-                        <span>Add Food</span>
-                    </button>
-
-                    <div className="field search-bar">
-                        <div className="control">
-                            <input
-                                className="input"
-                                id="search"
-                                name="search"
-                                type="search"
-                                placeholder="Filter by food name..."
-                                value={query}
-                                onChange={e => setQuery(e.target.value)} />
-                        </div>
-                    </div>
-                </header>
-                <div className="columns">
-                    <div className="column is-three-quarter food-list">
-                        {food.length && food.map((f, i) => {
-                            return <FoodBox key={i} food={f} todayFood={todayFood} setTodayFood={setTodayFood} />
-                        })}
-                    </div>
+                <Header
+                    modalVisibility={modalVisibility}
+                    setModalVisibility={setModalVisibility}
+                />
+                <Search
+                    query={query}
+                    setQuery={setQuery}
+                />
+                <div className="columns is-desktop is-multiline">
+                    <FoodList food={food} todayFood={todayFood} setTodayFood={setTodayFood} />
                     <TodayFood todayFood={todayFood} setTodayFood={setTodayFood} />
                 </div>
             </div>
--- starter-code/src/components/FoodBox.js
+++ starter-code/src/components/FoodBox.js
@@ -5,33 +5,35 @@ export const FoodBox = ({ food, todayFood, setTodayFood }) => {
     const [quantity, setQuantity] = useState(food.quantity);
 
     return (
-        <div className="box">
-            <article className="media">
-                <div className="media-left">
-                    <figure className="image is-64x64">
-                        <img src={food.image} alt="" />
-                    </figure>
-                </div>
-                <div className="media-content">
-                    <div className="content">
-                        <p>
-                            <strong>{food.name}</strong>
-                            <br />
-                            <small>{food.calories} cal</small>
-                        </p>
+        <div className="column is-half">
+            <div className="box">
+                <article className="media">
+                    <div className="media-left">
+                        <figure className="image is-64x64">
+                            <img src={food.image} alt="" />
+                        </figure>
                     </div>
-                </div>
-                <div className="media-right">
-                    <div className="field has-addons">
-                        <div className="control">
-                            <input type="number" className="input" value={quantity} onChange={e => setQuantity(parseInt(e.target.value))} />
-                        </div>
-                        <div className="control">
-                            <button className="button is-info" disabled={quantity <= 0} onClick={() => setTodayFood([...todayFood, { ...food, quantity }])}>+</button>
+                    <div className="media-content">
+                        <div className="content">
+                            <p>
+                                <strong>{food.name}</strong>
+                                <br />
+                                <small>{food.calories} cal</small>
+                            </p>
                         </div>
                     </div>
-                </div>
-            </article>
+                    <div className="media-right">
+                        <div className="field has-addons">
+                            <div className="control">
+                                <input type="number" className="input" value={quantity} onChange={e => setQuantity(parseInt(e.target.value))} />
+                            </div>
+                            <div className="control">
+                                <button className="button is-info" disabled={quantity <= 0} onClick={() => setTodayFood([...todayFood, { ...food, quantity }])}>+</button>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
         </div>
     )
 }

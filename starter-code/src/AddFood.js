a8ba3fd9e9da24930db3fd4142cd05f48e7d8d79
Paola <paolavee@gmail.com>
2020-04-01 23:10:51 +0200

    done

---
+++ starter-code/src/AddFood.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class AddFood extends Component {
+ state = {
+      name: "",
+      calories: "",
+      image: "",
+      quantity: ""
+    };
+  
+
+  handleFormSubmit = (event) => {
+      event.preventDefault();
+      this.props.addTheFood(this.state);
+      this.setState({
+        name: "",
+        calories: "",
+        image: "",
+        quantity: ""
+      })
+  }
+
+  handleChange = (event) => {
+    let { name, value } = event.target;
+    this.setState({ [name]: value})
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={e => this.handleChange(e)}
+          />
+
+          <label>Calories:</label>
+          <input
+            type="number"
+            name="calories"
+            value={this.state.calories}
+            onChange={e => this.handleChange(e)}
+          />
+
+          <label>Image:</label>
+          <input
+            type="text"
+            name="image"
+            value={this.state.image} alt=''
+            onChange={e => this.handleChange(e)}
+          />
+
+          <label>Quantity:</label>
+          <input
+            type="number"
+            name="quantity"
+            value={this.state.quantity}
+            onChange={e => this.handleChange(e)}
+          />
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+    }
+  }
+export default AddFood;
--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bulma/css/bulma.css";
+import foods from "./foods.json";
+import FoodBox from "./FoodBox";
+import AddFood from "./AddFood";
+
 
 class App extends Component {
+  state = {
+    food: foods,
+    addFoodButton:false
+  };
+
+addFoodButton= () => {
+  this.setState({
+    addFoodButton: !this.state.addFoodButton
+  })
+}
+
+  addFoodHandler = (theFood) => {
+    const foodsCopy = [...this.state.food];
+    foodsCopy.unshift(theFood)
+    this.setState({
+      food: foodsCopy
+    })
+  }
+
+
   render() {
+
+    let addFoodForm = null
+    if(this.state.addFoodButton===true){
+      addFoodForm = <AddFood addTheFood={this.addFoodHandler}/>
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.addFoodButton}>Add Food</button>
+        {addFoodForm}
+        {this.state.food.map((oneItem, index) => (
+          <FoodBox
+            key={index}
+            name={oneItem.name}
+            calories={oneItem.calories}
+            image={oneItem.image}
+            quantity={oneItem.quantity}
+          />
+        ))}
       </div>
     );
   }
-}
-
+  }
 export default App;

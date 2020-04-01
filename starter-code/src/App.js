daddb77e04955d94631c47c097757b50e4846c74
unai gonzalez <unaigoe91@gmail.com>
2020-04-01 20:59:05 +0200

    BCN-FT-Unai Gonzalez, almost done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import food from './foods.json'
+import 'bulma/css/bulma.css';
+import FoodBox from './Components/FoodBox'
+import AddFood from './Components/AddFood'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    allFood: food
+  }
+
+  addFoodHandler = (theFood) => {
+    const foodCopy = [...this.state.allFood];
+    foodCopy.unshift(theFood)
+    this.setState({
+      allFood: foodCopy
+    })
+  }
+
+  showFood = () => {
+    this.setState({
+      showFood: !this.state.showFood
+    })
+  }
+
+  
+  filteredFoods = (e) => {
+    const filteredFoods = [...this.state.allFood]
+     filteredFoods.filter(food => {
+       console.log(e.target.value)
+      return food.name.toLowerCase().includes(e.target.value)
+    })
+
+    this.setState({
+      foods:filteredFoods
+    })
+  }
+
   render() {
+
+    let addFoodForm = null
+    if(this.state.showFood === true){
+      addFoodForm = <AddFood addTheFood={this.addFoodHandler}/>
+     // console.log(addFoodForm)
+    }
+  
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <input type="text" onChange={this.filteredFoods} placeholder='Search Food'/> 
+
+        <button onClick= {this.showFood}> Add new Food</button>
+        {addFoodForm}
+        {this.state.allFood.map((food, index) =>
+          <FoodBox key={index} {...food} addTheFood={this.addFoodHandler}/>
+        )}
+
       </div>
-    );
+            )
   }
 }
+
 
 export default App;

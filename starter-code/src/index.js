4dd8d3f47981ad9451796f0276a38ac5cd85ff88
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-04-01 16:43:08 +0200

    bulma css

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
+import 'bulma/css/bulma.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));

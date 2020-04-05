3d788abb31dad948f1d86b5beaf7e98dc9827a68
miranda <miranda@LAPTOP-R0JVELH6.localdomain>
2020-04-05 15:34:11 +0200

    not finished

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import Foodbox from './components/Foodbox.js'
 
 class App extends Component {
+
+  state = {
+    foods:foods
+  } 
+
+  addFood(e) {
+    let foodsCopy = [...this.state.foods];
+    foodsCopy.push({
+      
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="Foodbox-container">
+      {this.state.foods.map((foods) => ( 
+          <Foodbox
+            image={foods.image}
+            name={foods.name}
+            calories={foods.calories}
+          />
+          ))}
+        </div>
     );
   }
 }

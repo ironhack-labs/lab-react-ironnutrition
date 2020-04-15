b8037e58c861886ef56683deef4d661bf4fc0176
Jaime <jaimemoravallejo@gmail.com>
2020-04-15 18:37:40 +0200

    Iteration 1,2  done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
+
 
 class App extends Component {
+  state = {
+    food: foods.slice(),
+  }
   render() {
+    console.log(this.state.food)
+    const { food } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.food.map((food, index)=> { 
+          return <FoodBox name={food.name} img={food.image} calories={food.calories}/> 
+        })}
       </div>
     );
   }
---
+++ starter-code/src/components/FoodBox.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class FoodBox extends Component {
+  render() {
+    const { img, name, calories } = this.props;
+    return(
+    <div className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={img} />
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{name}</strong> <br />
+              <small>{calories} cal</small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input
+                className="input"
+                type="number"
+                value="1"
+              />
+            </div>
+            <div className="control">
+              <button className="button is-info">
+                +
+        </button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>)
+  }
+}
+
+export default FoodBox;

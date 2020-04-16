33f1ebf14f3ba7e963434ae069d1454c57cdc0ee
Thibault-d <thibaultdelfaud@gmail.com>
2020-04-16 18:17:20 +0200

    issues with adding the new food to the array

--- starter-code/src/Components/FoodBox/FoodBox.js
+++ starter-code/src/Components/FoodBox/FoodBox.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import foods from '../../foods.json'
+import Foods from '../../foods.json'
 import 'bulma/css/bulma.css';
 import './FoodBox.css'
 import AddFood from '../AddFood/AddFood.js'
 
 class FoodBox extends Component {
     state = {
-        all : foods,
+        all : Foods,
     }
 
     show = () => {
       console.log(this.props.click)
+      let newFood = this.props.click;
+      this.setState({
+        all: [...this.state.all, newFood]
+      })
+      console.log(this.state.all)
     }
 
     render(){
         return(
             <div>
-             <button onClick={this.show} className="button is-info">Check Callback</button>
+             <button onClick={this.show} className="button is-info test">Check Callback</button>
 
                 {this.state.all.map((item, index) => { //render the content of foods.json
                     return (

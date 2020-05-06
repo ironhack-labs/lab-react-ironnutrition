61a9378ba46e6a7c8e6ce33e89bd2604e6578530
Emma-Solano <solano.c.emmanuel@gmail.com>
2020-05-06 13:26:26 -0500

    iteraciones 1 y 2 completas

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import FoodBox from './components/FoodBox.js'
+import Search from './components/Search';
+
 
 class App extends Component {
+  state = {
+    foods,
+    filteredFoods:[],
+  }
+  searchFood= (e) =>{
+    //const foundFood = [... this.state.foods]
+    const  filteredFoods= [...this.state.foods].filter(food=> food.name.includes(e.target.value))
+    this.setState({filteredFoods})
+    
+    
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Search fcn={this.searchFood}/>
+        {this.state.foods.map((food, idx)=> <FoodBox food={food} key={idx}></FoodBox>)}
       </div>
     );
   }
---
+++ starter-code/src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search({fcn}) {
+    return (
+        
+          <input type='text' onChange={fcn}></input>  
+        
+    )
+}
+
+export default Search

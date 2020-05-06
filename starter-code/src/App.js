a6c0b0ab7a45c47a13c63b3f41d20008c404b8ac
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-06 17:27:45 +0200

    create FoodBox component

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'bulma/css/bulma.css'
+import Foods from './foods.json'
+import FoodBox from './components/foodBox/foodBox'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="columns">
+        <div className="column is-half">
+          <FoodBox
+            name="pizza"
+            calories="420"
+            image="https://i.imgur.com/DupGBz5.jpg"
+          />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

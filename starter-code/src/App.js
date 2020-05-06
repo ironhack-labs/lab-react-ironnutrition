5004b08dfdc1cbd5151d80ff6fb2230031bb0cf9
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-06 17:59:13 +0200

    create addFood form

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import 'bulma/css/bulma.css'
 import Foods from './foods.json'
 import FoodBox from './components/foodBox/foodBox'
+import FoodForm from './components/foodForm/foodForm'
 import './App.css'
 
 class App extends Component {
@@ -12,21 +13,41 @@ class App extends Component {
     }
   }
 
+  pushNewFood = (newFood) => {
+    const foodsCopy = [...this.state.foods]
+    foodsCopy.push(newFood)
+    this.setState({ foods: foodsCopy })
+  }
+
   render() {
     return (
-      <div className="columns">
-        <div className="column is-half">
-          {this.state.foods.map((elm, idx) => (
-          <FoodBox
-            key={idx}
-            name={elm.name}
-            calories={elm.calories}
-            image={elm.image}
-            quantity={elm.quantity}
-          />
-          ))}
+      <main>
+        <div className="columns">
+          <div className="column">
+            <nav>
+              <a href="#" className="button">
+                Add Food
+              </a>
+            </nav>
+          </div>
         </div>
-      </div>
+        <div className="columns">
+          <div className="column is-half">
+            {this.state.foods.map((elm, idx) => (
+              <FoodBox
+                key={idx}
+                name={elm.name}
+                calories={elm.calories}
+                image={elm.image}
+                quantity={elm.quantity}
+              />
+            ))}
+          </div>
+          <div className="column is-half">
+            <FoodForm addFood={this.pushNewFood} />
+          </div>
+        </div>
+      </main>
     )
   }
 }
---
+++ starter-code/src/components/foodForm/foodForm.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import 'bulma/css/bulma.css'
+
+class FoodForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      calories: '',
+      image: '',
+      quantity: '',
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    this.props.addFood(this.state)
+    this.setState({
+      name: '',
+      calories: '',
+      image: '',
+      quantity: '',
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <label>
+          Name:
+          <input
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            type="text"
+          />
+        </label>
+        <br />
+        <label>
+          Calories:
+          <input
+            name="calories"
+            value={this.state.calories}
+            onChange={this.handleChange}
+            type="text"
+          />
+        </label>
+        <br />
+        <label>
+          Image:
+          <input
+            name="image"
+            value={this.state.image}
+            onChange={this.handleChange}
+            type="text"
+          />
+        </label>
+        <br />
+        <input type="submit" value="Create food" />
+      </form>
+    )
+  }
+}
+
+export default FoodForm

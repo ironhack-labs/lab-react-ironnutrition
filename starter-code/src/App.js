09b9a9ca78d99804df5246933ef171837fbad915
Kevin McCallum <kevingmcc@gmail.com>
2020-05-06 20:28:53 -0500

    Iteration 4 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,15 +4,34 @@ import './App.css'
 import 'bulma/css/bulma.css'
 import foods from './foods.json'
 import FoodBox from './components/FoodBox'
+import Form from './components/Form'
 
 class App extends Component {
   state = {
     allFoods: foods,
+    on: false,
   }
+
+  showForm = () => {
+    this.setState({
+      on: !this.state.on,
+    })
+  }
+
+  addFood = (food) => {
+    const newFood = [...this.state.allFoods, food]
+    this.setState({
+      allFoods: newFood,
+    })
+  }
+
+  add
 
   render() {
     return (
       <div className="App">
+        {this.state.on && <Form addFood={this.addFood} />}
+        <button onClick={this.showForm}>Add Food</button>
         {this.state.allFoods.map((food) => (
           <FoodBox
             key={food.id}
--- starter-code/src/components/Form/index.js
+++ starter-code/src/components/Form/index.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Form = () => {
-  return (
-    <div>
-      <form>
-        <label>Name</label>
-        <input type="text" name="name" value={} />
-      </form>
-    </div>
-  )
+export default class Form extends Component {
+  state = {
+    name: '',
+    calories: '',
+    quantity: '',
+    image: '',
+  }
+
+  handleInputChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    this.props.addFood(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+
+          <label>Calories:</label>
+          <input
+            type="text"
+            name="calories"
+            value={this.state.calories}
+            onChange={this.handleInputChange}
+          />
+
+          <label>Image:</label>
+          <input
+            type="text"
+            name="image"
+            value={this.state.image}
+            onChange={this.handleInputChange}
+          />
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
 }

038fbb5cd3407ea1ca39c7028dfe0d28d89e03d6
Mayank Srivastava <mayank.srivastav84@gmail.com>
2020-05-10 22:23:08 +0200

    Commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,29 +1,98 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 //CSS
-import './App.css';
-import 'bulma/css/bulma.css';
+import "./App.css";
+import "bulma/css/bulma.css";
 //Data
-import foods from './foods.json'
+import foods from "./foods.json";
 //Components
-import Foodbox from './components/foodbox';
+import Foodbox from "./components/foodbox";
 
 class App extends Component {
   state = {
-    foods: foods
-  }
+    foods: foods,
+    newFood: {},
+    formIsShowed: false,
+  };
+
+  handleInput = (e) => {
+    let { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  displayForm = () => {
+    this.setState({
+      formIsShowed: !this.state.formIsShowed,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.displayForm();
+    const newFood = {
+      name: this.state.name,
+      calories: this.state.calories,
+      quantity: this.state.quantity,
+      image: this.state.image
+    }
+    this.setState({
+      foods: [...this.state.foods, newFood]
+    })
+  };
+
   render() {
     return (
-      <div className ="App">
-        {this.state.foods.map((element,key) =>
-          <Foodbox 
-          key = {key}
-          name = {element.name}
-          calories = {element.calories}
-          image={element.image}
-          quantity={element.quantity}
+      <div className="App">
+        {!this.state.formIsShowed && (
+          <button className="button is-primary" onClick={this.displayForm}>
+            Add New Food
+          </button>
+        )}
+        {this.state.formIsShowed && (
+          <form className="food-Form" onSubmit={(e) => this.handleSubmit(e)}>
+            <label name="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={(e) => this.handleInput(e)}
+            ></input>
+            <label name="calories"> Calories</label>
+            <input
+              type="number"
+              name="calories"
+              value={this.state.calories}
+              onChange={(e) => this.handleInput(e)}
+            ></input>
+            <label name="quantity">Quantity</label>
+            <input
+              type="number"
+              name="quantity"
+              value={this.state.quantity}
+              onChange={(e) => this.handleInput(e)}
+            ></input>
+            <label name="image">Image</label>
+            <input
+              type="text"
+              name="image"
+              value={this.state.image}
+              onChange={(e) => this.handleInput(e)}
+            >
+            </input>
+            <input 
+              type="submit" 
+              value="submit">
+            </input>
+          </form>
+        )}
+        {this.state.foods.map((element, key) => (
+          <Foodbox
+            key={key}
+            name={element.name}
+            calories={element.calories}
+            image={element.image}
+            quantity={element.quantity}
           />
-          )}
+        ))}
       </div>
     );
   }

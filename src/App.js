3a36b1357b810f10062a39265ff86465720db221
Renzo Pederzoli <rdp73@miami.edu>
2020-06-30 18:46:37 -0400

    1-3 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "bulma/css/bulma.css"
+import foods from "./foods.json"
+import FoodBox from "./foodbox/FoodBox"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    foodArr: foods,
+    showForm: false
+  }
+  
+  allFoods = () => {
+    return this.state.foodArr.map((item,index) => {
+      return (
+        <FoodBox key={index} food={item}/>
+      )
+    })
+  }
+
+  handleSubmit = (e) => { //adds new food to foodArr
+    e.preventDefault();
+    this.toggleForm();
+    let newFood = {
+      name: this.state.name,
+      calories: this.state.calories,
+      iamge: this.state.image
+    };
+    let newArr = [...this.state.foodArr]
+    newArr.push(newFood)
+    this.setState({
+      foodArr: newArr
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+  }
+
+  toggleForm = () => {
+    this.setState({
+      showForm: !this.state.showForm
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleForm}>Add Food</button>
+
+        {this.state.showForm ?
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name: </label>
+          <input onChange={this.handleChange} type="text" name="name"></input>
+          <label htmlFor="calories">Calories: </label>
+          <input onChange={this.handleChange} type="number" name="calories"></input>
+          <label htmlFor="iamge">Image Link: </label>
+          <input onChange={this.handleChange} type="text" name="image"></input>
+          <button type="submit">Submit</button>
+        </form>
+        : null}
+
+        {this.allFoods()}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/foodbox/FoodBox.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import "bulma/css/bulma.css"
+import "./FoodBox.css"
+
+const FoodBox = (props) => {
+  return (
+    <div className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={props.food.image} alt="foodgood"/>
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{props.food.name}</strong> <br />
+              <small>{props.food.calories} cal</small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input className="input" type="number" />
+            </div>
+            <div className="control">
+              <button className="button is-info">
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default FoodBox;

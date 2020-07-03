709e83c37d000c233a59bc25ca53f996a3bb04f0
Aurore CR <aurorenaturerouge@hotmail.fr>
2020-07-03 15:10:01 +0200

    last

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
+import AddFood from './components/AddFood';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +28,35 @@ function App() {
   );
 }
 
-export default App;
+class App extends Component {
+  state = {
+   food: foods,
+   showForm: false,
+
+  };
+
+handleAddFood = (event) => { 
+  event.preventDefault()
+      let name = event.target.name.value
+      let calories = event.target.calories.value
+      let image = event.target.image.value
+      event.target.name.value = ''
+      event.target.calories.value = ''
+ 
+  this.setState({
+      foods: [...this.state.foods, {name: name, calories: calories}],
+      showingForm: !this.state.showingForm
+      })
+    }
+
+    handleFilter = (event) => {
+      let input = event.target.value;
+      let newFoods = this.state.food.filter(() => {
+        return foods.name.includes(inp);
+      });
+      this.setState({
+        filteredFoods: newFoods,
+      });
+    };
+  }
+
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Search" name="search" onChange={e => this.props.searching(e.target.value)} />
+      </div>
+    )
+  }
+}
+
+export default Search; 

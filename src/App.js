c335940b4f873d4b69d2315a73532a571fa3b9dc
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-07-17 21:35:19 +0200

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import 'bulma/css/bulma.css';
 import foods from './foods.json';
 import FoodComponent from './components/FoodComponent';
 
+const copyFood = [...JSON.parse(JSON.stringify(foods))]
+
 function App() {
   return (
     <div className="App">
-      <FoodComponent food={foods} />
+      <FoodComponent food={copyFood} />
     </div>
   );
 }
--- src/components/FoodComponent.js
+++ src/components/FoodComponent.js
@@ -1,32 +1,43 @@
 import React from 'react';
 
-function FoodComponent() {
+const FoodComponent = props => {
+  
+    const product = [...props.food].map(prod => {
+        return (
+            <div className="box">
+                <article key={prod.name} className="media">
+                    <div className="media-left">
+                    <figure className="image is-64x64">
+                        <img src={prod.image} alt="product" />
+                    </figure>
+                    </div>
+                    <div className="media-content">
+                    <div className="content">
+                        <p>
+                        <strong>{prod.name}</strong> <br />
+                        <small>{prod.calories} cal</small>
+                        </p>
+                    </div>
+                    </div>
+                    <div className="media-right">
+                    <div className="field has-addons">
+                        <div className="control">
+                        <input className="input" type="number" value="1" />
+                        </div>
+                        <div className="control">
+                        <button className="button is-info">+</button>
+                        </div>
+                    </div>
+                    </div>
+                </article>
+            </div>
+        )
+    })
+
   return (
-      <article className="media">
-        <div className="media-left">
-          <figure className="image is-64x64">
-            <img src="https://i.imgur.com/eTmWoAN.png" />
-          </figure>
-        </div>
-        <div className="media-content">
-          <div className="content">
-            <p>
-              <strong>Pizza</strong> <br />
-              <small>400 cal</small>
-            </p>
-          </div>
-        </div>
-        <div className="media-right">
-          <div className="field has-addons">
-            <div className="control">
-              <input className="input" type="number" value="1" />
-            </div>
-            <div className="control">
-              <button className="button is-info">+</button>
-            </div>
-          </div>
-        </div>
-      </article>
+      <div>
+          {product}
+      </div>
   );
 }
 

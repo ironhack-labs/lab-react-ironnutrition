4224132c8b4ed9153043e9729b78a3d1ef9113f7
Marc De Mena <mdemena@gmail.com>
2020-07-17 19:47:11 +0200

    Iteration 2 Finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/foodbox/FoodBox';
 
 function App() {
+  const initialState = {
+    selectedFoods: [],
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const handleAdd = (food) => {
+    const newFoods = [...state.selectedFoods];
+    newFoods.push(food);
+    setState({ selectedFoods: newFoods });
+  };
+
+  const foodsList = foods.map((food) => {
+    return <FoodBox key={food.name} food={food} handleAdd={handleAdd} />;
+  });
+  const selectedFoodsList = foods.map((food) => {
+    return <FoodBox key={food.name} food={food} />;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="column is-half">{foodsList}</div>
+      <div className="column is-half">{selectedFoodsList}</div>
     </div>
   );
 }
---
+++ src/components/foodbox/FoodBox.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+function FoodBox(props) {
+  const initialState = {
+    name: props.food.name,
+    image: props.food.image,
+    calories: props.food.calories,
+    quantity: props.food.quantity,
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+  return (
+    <div className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={state.image} alt={state.name} />
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{state.name}</strong> <br />
+              <small>{state.calories} cal</small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input
+                className="input"
+                type="number"
+                name="quantity"
+                value={state.quantity}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="control">
+              <button
+                className="button is-info"
+                onSubmit={() => props.handleAdd(state)}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+}
+
+export default FoodBox;

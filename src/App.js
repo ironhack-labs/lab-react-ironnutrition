72ac88ca22445fdfcd50f164b331064605125faf
irensvm <irens.vm@gmail.com>
2020-07-19 12:43:17 +0200

    it1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
-import foods from './foods.json';
 import FoodBox from './components/FoodBox'
 
 
@@ -11,6 +9,9 @@ function App() {
   return (
     <div className="App">
       <FoodBox></FoodBox>
+      <FoodBox></FoodBox>
+
+
     </div>
   );
 }
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,37 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-function FoodBox() {
-    return (
-        <div className="box">
-  <article className="media">
-    <div className="media-left">
-      <figure className="image is-64x64">
-        <img src="https://i.imgur.com/eTmWoAN.png" />
-      </figure>
-    </div>
-    <div className="media-content">
-      <div className="content">
-        <p>
-          <strong>Pizza</strong> <br />
-          <small>400 cal</small>
-        </p>
-      </div>
-    </div>
-    <div className="media-right">
-      <div className="field has-addons">
-        <div className="control">
-          <input className="input" type="number" value="1" />
+const FoodBox = (props) => {
+  return (
+    <div className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src="https://i.imgur.com/eTmWoAN.png" />
+          </figure>
         </div>
-        <div className="control">
-          <button className="button is-info">
-            +
-          </button>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>Pizza</strong>
+              <br />
+              <small>400 cal</small>
+            </p>
+          </div>
         </div>
-      </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input className="input" type="number" value="1" />
+            </div>
+            <div className="control">
+              <button className="button is-info">+</button>
+            </div>
+          </div>
+        </div>
+      </article>
     </div>
-  </article>
-</div>
-    )
-}
+  );
+};
 
-export default FoodBox
+export default FoodBox;

817fb04ba200042766fead52f3d4aa128815bf18
David de Lamo <d.lamo.valverde@gmail.com>
2020-07-20 18:54:51 +0200

    search bar refactored

--- src/view/FoodList/FoodList.js
+++ src/view/FoodList/FoodList.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import FoodBox from '../../components/FoodBox';
 import FormNewFood from '../../components/FormNewFood';
-import { capitalizeStr } from '../../tools/capitalizeString'
+//import { capitalizeStr } from '../../tools/capitalizeString'
 import TodayListElement from '../../components/TodayListElement';
 import SearchBar from '../../components/SearchBar';
 import TodayFoods from '../../components/TodayFoods';
 
 function FoodList({state, handleChangeForm, handleSubmitForm, handleNewFoodBtn, handleSearchBar, handleFoodQuantity, handleAddFood, handleDeleteOnToday}) {
-    const wordToFind = capitalizeStr(state.searchBar)
+    //const wordToFind = capitalizeStr(state.searchBar)
     const allFoodCards = state.foods
-                                .filter(food => food.name.substring(0, state.searchBar.length) === wordToFind)
+                                .filter(food => food.name.toLowerCase().includes(state.searchBar))
                                 .map(food => <FoodBox 
                                                 key={food.name} 
                                                 food={food} 

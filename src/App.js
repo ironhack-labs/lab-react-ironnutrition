ed8312f8193e65ad11896af91923d64ef280bdf6
Noiret <sergio.ko@icloud.com>
2020-08-12 11:55:58 +0200

    IT2 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css'
 import foods from './foods.json'
 import FoodBox from './components/Foobox/FoodBox'
@@ -6,7 +6,7 @@ import FoodBox from './components/Foobox/FoodBox'
 function App() {
   return (
     <div className="App">
-      
+      <FoodBox food={foods}/>
     </div>
   );
 }
--- src/components/Foobox/FoodBox.js
+++ src/components/Foobox/FoodBox.js
@@ -1,26 +1,27 @@
 import React from 'react'
 
-const FoodBox = (props) => {
-    return (
-        <div className="box">
+const FoodBox = ({ food }) => {
+    console.log(food)
+    const foodList = food.map((elem, index) => (
+        <div className="box" key={index} >
             <article className="media">
                 <div className="media-left">
                     <figure className="image is-64x64">
-                        <img src="https://i.imgur.com/eTmWoAN.png" alt=""/>
+                        <img src={elem.image} alt=""/>
                     </figure>
                 </div>
                 <div className="media-content">
                     <div className="content">
                         <p>
-                            <strong>Pizza</strong> <br />
-                            <small>400 cal</small>
+                            <strong>{elem.name}</strong> <br />
+                            <small>{elem.calories} cal</small>
                         </p>
                     </div>
                 </div>
                 <div className="media-right">
                     <div className="field has-addons">
                         <div className="control">
-                            <input className="input" type="number" value="1" />
+                            <input className="input" type="number" value={elem.quantity} onChange/>
                         </div>
                         <div className="control">
                             <button className="button is-info">
@@ -31,6 +32,9 @@ const FoodBox = (props) => {
                 </div>
             </article>
         </div>
+    ))
+    return (
+        <div className="food-list">{foodList}</div>
     )
 }
 

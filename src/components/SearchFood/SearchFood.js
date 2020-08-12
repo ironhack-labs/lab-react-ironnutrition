59ea0d904e0675afb4bc9a12b38bce4d24d3a06a
Natalia Silva <natalia.pupushi@gmail.com>
2020-08-12 13:40:19 -0300

    interaction 1,2,3 and 4 done

---
+++ src/components/SearchFood/SearchFood.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import FoodBox from '../FoodBox/FoodBox';
+import FoodList from '../FoodList/FoodList';
+export default class SearchFood extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            valueInput: '',
+        }
+    }
+    handleChange = event => {
+        let searchFood = event.target.value;
+        this.setState({
+            valueInput: searchFood,
+        }, () => { this.props.searchTheFood(searchFood) })
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleFormSubmit}>
+                    <input type="text" value={this.state.valueInput} onChange={(e) => this.handleChange(e)} placeholder="Search.."/>
+                    <input type="submit" value="submit"/>
+                </form>
+            </div>
+        )
+    }
+}

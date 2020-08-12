2101dc6f2dd1959464c0ea4ec3fec36fedaa5980
Luis De Gouveia <degouveiasousa@gmail.com>
2020-08-12 22:27:50 +0200

    iteration 0, 1, 2, 3 and 4 done, iteration 5 is missing.

--- src/components/FoodList/FoodList.js
+++ src/components/FoodList/FoodList.js
@@ -1,51 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import foodData from '../../foods.json';
 import FoodBox from '../FoodBox/FoodBox';
 import AddFood from '../AddFood/AddFood';
 import SearchFood from '../SearchFood/SearchFood';
 
 export default class FoodList extends Component {
+
     state = {
         foods: foodData,
         filtered: foodData,
-    }
+    };
 
-    addFoodHandler = theFood => {
+    addFoodHandler = (theFood) => {
         const foodsCopy = [...this.state.foods];
         foodsCopy.push(theFood);
         this.setState({
-            foods: foodsCopy
-        })
-    }
+        foods: foodsCopy,
+    });
+    };
 
     handleSearch = (value) => {
         const foodsCopy = [...this.state.foods];
-
-        let newList = foodsCopy.filter(item => {
+        let newList = foodsCopy.filter((item) => {
         const lowerCase = value.toLowerCase();
         return item.name.toLowerCase().includes(lowerCase);
     });
-    this.setState({
-        filtered: newList
+        this.setState({
+        filtered: newList,
     });
-}
-
-    this.handleSearch = this.handleSearch.bind(this);
+  };
 
     render() {
         return (
+
             <div>
-
-            <AddFood addTheFood={this.addFoodHandler}/>
-
-            <SearchFood searchTheFood={this.handleSearch}/>
-
-            {this.state.foods.map((food, index) => {
-                return <FoodBox key={food.image} food={food}/>
-                })}
-                
+                <AddFood addTheFood={this.addFoodHandler} />
+                <SearchFood searchTheFood={this.handleSearch} />
+                {this.state.filtered.map((food, index) => {
+                return <FoodBox key={food.image} food={food} />;
+            })}
             </div>
-        )
-    }
-}
 
+    );
+  }
+}

07cc5d68903826dae3ffbf429cb4fbc558a31b3d
Marta Jankowska <martajan218@gmail.com>
2020-08-15 18:47:22 +0200

    food addition working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,99 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.css';
 import FoodBox from './components/FoodBox'
+import AddFood from './components/AddFood'
+import Search from './components/Search'
+import foods from './foods.json';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    Food: foods,
+    filtered: foods,
+    isClicked: false,
+    searchTerm: '',
+    calculatedFoods: []
+  }
+  
+  dynamicSearch = (searchTerm) => {
+    const filtered = this.state.Food.filter(food => {
+      console.log(food.name)
+      return food.name.toLowerCase().includes(searchTerm.toLowerCase())
+    });
+    this.setState({
+      filtered
+    })
+  }
+
+  showForm = () => {
+    this.setState({isClicked: !this.state.isClicked})
+  }
+
+  insertFood = (newFood) => {
+    const copyFood = [...this.state.filtered]
+    copyFood.push(newFood)
+    this.setState({filtered: copyFood, isClicked: false})
+  }
+
+  addNewFood = (foodToAddObj) => {
+    console.log('foodToAddObj', foodToAddObj)
+    const calculatedFoodsCopy = [...this.state.calculatedFoods];
+
+    if (calculatedFoodsCopy.length === 0) { // si el array de calculatedFoods es vacío
+      calculatedFoodsCopy.push(foodToAddObj);
+      this.setState({ calculatedFoods: calculatedFoodsCopy }); // actualiza el `state`
+    }
+    else { // si el array de calculatedFoods ya tiene algunas comidas añadidas
+      let foundFood = false; // la variable indicadora
+
+      const updatedCalculatedFoods = calculatedFoodsCopy.map((addedFoodObj) => {
+        // si la comida ya existe, actualiza el objeto
+        if (addedFoodObj.name === foodToAddObj.name) {
+          addedFoodObj.quantity += foodToAddObj.quantity;
+          foundFood = true; // cambiamos la variable indicadora a true, porque hemos encontrado y actualizado la comida 
+        }
+        return addedFoodObj;
+      })
+
+      if (!foundFood) {
+        // si comida con este nombre ya no existe en calculatedFoods, 
+        // si no la hemos encontrado durante de iteracion con el calculatedFoodsCopy.map
+        // añade la comida al array
+        console.log('HERE',)
+        console.log('foundFood', foundFood)
+        console.log('foodToAddObj **', foodToAddObj)
+
+        updatedCalculatedFoods.push(foodToAddObj);
+        console.log('after push', updatedCalculatedFoods)
+
+      }
+
+      this.setState({ calculatedFoods: updatedCalculatedFoods }); // actualiza el `state`
+    }
+  }
+
+
+  render() {
+    return (
     <div className="App">
       <h1>IronNutrition</h1>
-      <FoodBox />
+      <div>
+        {this.state.isClicked ?<AddFood addFood={this.insertFood}/>:null}
+        <button onClick={this.showForm} className="btn">Add Food</button> 
+      </div>
+      <Search filterFood={this.dynamicSearch} />
+      {this.state.filtered.map((food, index) => (
+      <FoodBox food={food} key={index} addNewFood={this.addNewFood} />))}
+      {
+        this.state.calculatedFoods.map((addedFoodObj, i) => {
+          const totalCalories = addedFoodObj.calories * addedFoodObj.quantity;
+          return <p key={i} > {addedFoodObj.quantity} {addedFoodObj.name} {totalCalories}kcal</p>
+        })
+      }
     </div>
   );
+  }
+  
 }
 
 export default App;
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,89 +1,64 @@
 import React, { Component } from 'react'
-import foods from '../foods.json';
-
-import AddFood from './AddFood'
-import Search from './Search'
-import TodayList from './TodayList';
 
 class FoodBox extends Component {
     constructor() {
         super();
         this.state = {
-            Food: foods,
-            filtered: foods,
-            isClicked: false,
-            searchTerm: ''
+            quantity: 1
         }
     }
 
-    /* editSearchTerm = (e) => {
-      this.setState({searchTerm: e.target.value})
-    } */
-
-    dynamicSearch = (searchTerm) => {
-      const filtered = this.state.Food.filter(food => {
-        console.log(food.name)
-        return food.name.toLowerCase().includes(searchTerm.toLowerCase())
-      });
-      this.setState({
-        filtered
-      })
+    handleQuantityInput = (e) => {
+      const newQuantity = e.target.value;
+  
+      this.setState({ quantity:newQuantity })
     }
+  
+    handleAddFood = () => {
+      const foodToAdd = {
+        quantity: this.state.quantity,
+        name: this.props.food.name,
+        calories: this.props.food.calories
+        }
 
-    showForm = () => {
-      this.setState({isClicked: !this.state.isClicked})
-    }
-
-    insertFood = (newFood) => {
-      const copyFood = [...this.state.filtered]
-      copyFood.push(newFood)
-      this.setState({filtered: copyFood, isClicked: false})
+        this.props.addNewFood(foodToAdd);
     }
   
 
     render() {
+        const { food } = this.props;
         return (
         <div>
-          <div>
-            {this.state.isClicked ?<AddFood addFood={this.insertFood}/>:null}
-            <button onClick={this.showForm} className="btn">Add Food</button> 
-          </div>
-          <Search filterFood={this.dynamicSearch} />
-        {/*   <TodayList /> */}
-          {this.state.filtered.map((oneMeal, index) => {
-            return(
-               <div className="box">
-                  <article className="media">
-                    <div className="media-left">
-                      <figure className="image is-64x64">
-                        <img src={oneMeal.image} alt="meal" />
-                      </figure>
-                    </div>
-                    <div className="media-content">
-                      <div className="content">
-                        <p>
-                          <strong>{oneMeal.name}</strong> <br />
-                          <small>{oneMeal.calories} cal</small>
-                        </p>
-                      </div>
-                    </div>
-                    <div className="media-right">
-                      <div className="field has-addons">
-                        <div className="control">
-                          <input className="input" type="number" value="1" />
-                        </div>
-                        <div className="control">
-                          <button className="button is-info">
-                            +
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </article>
-                </div> 
-            )
-        })
-        }
+          <div className="box">
+            <article className="media">
+              <div className="media-left">
+                <figure className="image is-64x64">
+                  <img src={food.image} alt="meal" />
+                </figure>
+              </div>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>{food.name}</strong> <br />
+                    <small>{food.calories} cal</small>
+                  </p>
+                </div>
+                
+              </div>
+              <div className="media-right">
+                <div className="field has-addons">
+                  <div className="control">
+                    <input className="input" type="number" name="quantity" value={this.state.quantity} onChange={this.handleQuantityInput} />
+                  </div>
+                  <div className="control">
+                    <button onClick={this.handleAddFood} className="button is-info">
+                      +
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div> 
         </div>
       )
     }

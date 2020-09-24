308408c0b9a4362c34dd64f7358dc8cd99236089
Matthew Howard <mattalanhoward@gmail.com>
2020-09-24 20:28:42 +0200

    iteration 1

---
+++ src/Component/Foodbox/FoodBox.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './FoodBox.css';
+import foods from '../../foods.json';
+
+class FoodBox extends React.Component {
+  state = {
+    foods: foods,
+  };
+
+  render() {
+    // console.log(`STATE: `, this.state.);
+
+    return (
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src="https://i.imgur.com/eTmWoAN.png" alt="food" />
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>Pizza</strong> <br />
+                <small>400 cal</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" type="number" value="1" />
+              </div>
+              <div className="control">
+                <button className="button is-info">+</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    );
+  }
+}
+
+export default FoodBox;

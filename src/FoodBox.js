c2fc710cf62a70ad869eeed771109c1896b2c481
alejanglez <64974849+alejanglez@users.noreply.github.com>
2020-09-24 20:53:50 +0200

    It 1 and 2

---
+++ src/FoodBox.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './FoodBox.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+
+// const firstFifty = foods.slice(0, 50);
+
+class FoodBox extends Component {
+  //   state = {
+  //     foodList: firstFifty,
+  //   };
+
+  render() {
+    return (
+      <>
+        {foods.map((food, index) => {
+          return (
+            <>
+              <div className="box" key={index}>
+                <article className="media">
+                  <div className="media-left">
+                    <figure className="image is-64x64">
+                      <img src={food.image} />
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <div className="content">
+                      <p>
+                        <strong>{food.name}</strong> <br />
+                        <small>{food.calories}</small>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="media-right">
+                    <div className="field has-addons">
+                      <div className="control">
+                        <input className="input" type="number" value="1" />
+                      </div>
+                      <div className="control">
+                        <button className="button is-info">+</button>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </div>
+            </>
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default FoodBox;

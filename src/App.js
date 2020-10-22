4bcc8a684b39561954ff565a2a55e1b5c4e6022a
anna dodo <anna.dorenskaya@gmail.com>
2020-10-22 21:32:07 +0200

    2nd iteration form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
-import FoodBox from './components/foodbox/FoodBox'
+import FoodBox from './components/foodbox/FoodBox';
+import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
+import Form from './components/form/Form';
 
-function App() {
-  return (
-    <div className="App">
-      {foods.map(item => 
-          <FoodBox
-          key={item.name}
-          name={item.name}
-          calories={item.calories}
-          image={item.image}
-          quantity={item.quantity}
-           />
-     )}
-     
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    foods: foods,
+    name: '', 
+    calories: '',
+    image: ''
+  };
+
+  showForm = (e) => {
+    console.log(e);
+  };
+
+  addData = (e) => {
+    const { name, value } = e.target
+    this.setState({[name]: value})
+  }
+
+  submitForm = (e) => {
+    e.preventDefault()
+    let newFood = {
+      name: this.state.name, 
+      image: this.state.image, 
+      calories: this.state.calories
+    }
+    this.setState({
+    //  foods: [...foods, newFood]
+      foods: this.state.foods.concat([newFood])
+     /*  foods.push(newFood) */
+    })
+    
+    console.log(this.state.name);
+    console.log(this.state.image);
+    console.log(this.state.calories);
+    console.log(this.state.foods)
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.foods.map((item) => (
+          <FoodBox key={item.name} item={item} />
+        ))}
+        <button onClick={this.showForm}>Show a form</button>
+        <div>
+          <form onSubmit={this.submitForm}>
+            <label htmlFor="name">Food name</label>
+            <input type="text" name="name" value={this.state.name}
+             onChange={this.addData} />
+            <label htmlFor="image">Image</label>
+            <input type="text" name="image" value={this.state.image}
+            onChange={this.addData} />
+            <label htmlFor="cal">Calories</label>
+            <input type="text" name="calories" value={this.state.calories}
+            onChange={this.addData} />
+            <button type="submit">Add new food</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
-
+/* name={item.name}
+              calories={item.calories}
+              image={item.image}
+              quantity={item.quantity} */
--- src/components/foodbox/FoodBox.js
+++ src/components/foodbox/FoodBox.js
@@ -2,27 +2,28 @@ import React from 'react';
 
 
 export default function FoodBox(props) {
+  //console.log(props.name)
   return (
     <div>
       <div className="box">
         <article className="media">
           <div className="media-left">
             <figure className="image is-64x64">
-              <img src={props.image} />
+              <img src={props.item.image} />
             </figure>
           </div>
           <div className="media-content">
             <div className="content">
               <p>
-                <strong>{props.name}</strong> <br />
-                <small>{props.calories} cal</small>
+                <strong>{props.item.name}</strong> <br />
+                <small>{props.item.calories} cal</small>
               </p>
             </div>
           </div>
           <div className="media-right">
             <div className="field has-addons">
               <div className="control">
-                <input className="input" type="number" value={props.quantity} />
+                <input className="input" type="number" defaultValue={props.item.quantity} />
               </div>
               <div className="control">
                 <button className="button is-info">+</button>

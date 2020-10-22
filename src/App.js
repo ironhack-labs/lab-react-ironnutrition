7bf7a23e9fbeaf9c2e89152d9379af592bfbaf33
allecramarcella <allecramarcella.dev@gmail.com>
2020-10-22 21:30:53 +0200

    it 1 -2 and part 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react'
+import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './component/foodbox/FoodBox';
+
+
+export default class App extends Component {
+
+  state = {
+    foods: foods
+  }
+
+  addNewFood = (e) => {
+    console.log('check')
+    return(
+    <div>
+      <form >
+        <label>Name:</label>
+        <input type="text" name="name" value='' />
+
+        <label>Number of calories:</label>
+        <input type="text" name="calories" value=''  />
+
+        <label>Image:</label>
+        <input type="image" src='' alt="Submit" width="48" height="48" />
+
+        
+        <button onClick={this.submitFood}>Submit</button>
+      </form>  
     </div>
-  );
+    )
+  }
+
+  submitFood = () => {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="title">IronNutrition</h1>
+        {this.state.foods.map((food, index) => 
+          <FoodBox key='food' foodType={food}/>
+        )}
+        <button onClick={(e) => this.addNewFood(e)}>Add new food</button>
+  
+      </div>
+    )
+  }
 }
 
-export default App;

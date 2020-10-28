49dce8bc4604e309e6bdc61e5e7708a82e62f69d
Lovro <hodak.lovro@gmail.com>
2020-10-28 20:57:05 +0100

    did first 4, more or less copy pasting from what we were doing during lessons. Honestly things dont yet make sense to me. Oh life. All good :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import AddFood from './components/AddFood'
+import Foodbox from './components/FoodBox'
 
-function App() {
+class App extends Component {
+
+  state = {
+    foodList: foods,
+    filteredList: foods,
+    showForm: false
+  }
+
+
+
+  handleAdd = (event) => {
+    event.preventDefault()
+    
+
+    let food = event.target
+    let newFood = {
+        name: food.name.value,
+        calories: food.calories.value,
+        quantity: 0,
+        image: food.image.value, 
+    }
+
+    this.setState({
+      foodList: [...this.state.foodList, newFood],
+      filteredList: [...this.state.filteredList, newFood],
+      showForm: false
+    })
+
+    }
+
+
+
+    handleFormShow = () => {
+      this.setState({
+        showForm: true
+      })
+    }
+
+
+    handleSearch = (event) => {
+    event.preventDefault()
+    let search = event.target.value
+    const {foodList} = this.state
+
+    let searchedList = foodList.filter((food) => {
+      return food.name.toLowerCase().includes(search.toLowerCase())
+    })
+
+    this.setState({
+      filteredList: searchedList
+    })
+    }
+
+
+
+  render() {
+
+  const {filteredList, showForm} = this.state
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div className="app">
+          <h1>What da hell?!</h1>
+
+          {
+            showForm ? (
+              <AddFood onAdd={this.handleAdd} />
+            ) : (
+              <button className="button" variant="outline-primary" onClick={this.handleFormShow}>Add new food</button>
+            )
+          }
+
+          <input onChange={this.handleSearch} type="text" placeholder="Search"></input>
+
+          {
+            filteredList.map((food, i) => {
+              return <Foodbox key={i} food={food} addQuantity={this.handleQuantity}/>
+              })
+          }
+
     </div>
-  );
+ 
+    )
+  }
 }
 
 export default App;

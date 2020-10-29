3c59efa7be37e1f5525b956722948436c3c8a537
14imran <14.imran.mohammed@gmail.com>
2020-10-29 13:16:17 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import uuid from 'react-uuid';
+import 'bulma/css/bulma.css';
+import FoodBox from './component/FoodBox';
+import AddFood from './component/AddFood';
+
+import foodsData from './foods.json';
 
 function App() {
+  const [foods, setFoods] = useState([]);
+  const [searchedFoods, setSearchFoods] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isForm, setIsForm] = useState(true);
+  const [query, setQuery] = useState('');
+
+  const [addingFood, setAddingFood] = useState([])
+  const [addedFood,setAddedFood] =useState([])
+
+  useEffect(() => {
+    setFoods(foodsData);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, [1000]);
+  }, []);
+
+  let handleAdd = (event) => {
+    event.preventDefault();
+    let name = event.target.name.value;
+    let calories = event.target.calories.value;
+    let image = event.target.image.value;
+    setFoods([...foods, { name, calories, image }]);
+    setIsForm(true);
+  };
+
+  const hanleShowForm = () => {
+    setIsForm(false);
+  };
+
+  const handleSearch = (query) => {
+    setQuery(query);
+    setSearchFoods(
+      foods.filter((elem) =>
+        elem.name.toLowerCase().includes(query.toLowerCase())
+      )
+    );
+  };
+
+  const handleAddButton = (adding,value) => {
+    
+    console.log('hellofrom App', value);
+    // setAddingFood(adding)
+    setAddingFood(adding)
+    
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading ? (
+        <h1>loading </h1>
+      ) : (
+        <div className="App">
+          {isForm ? (
+            <>
+              <button onClick={hanleShowForm}>show</button>
+              <input
+                type="search"
+                placeholder="search"
+                onChange={(event) => handleSearch(event.target.value)}
+              />
+
+              <div style={{marginLeft:"60%"}}>Today's Food
+              
+              <p>{addingFood.name}</p>
+              <p>{addingFood.calories}</p>
+              <p>{addingFood.value}</p>
+              
+              </div>
+
+
+            </>
+          ) : (
+            <AddFood onAdd={handleAdd} />
+          )}
+
+          {query == ''
+            ? foods.map((elem) => {
+                return (
+                  <FoodBox
+                    key={uuid()}
+                    food={elem}
+                    onAdding={handleAddButton}
+                  />
+                );
+              })
+            : searchedFoods.map((elem) => {
+                return <FoodBox key={uuid()} food={elem} />;
+              })}
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/component/FoodBox.js
@@ -0,0 +1,53 @@
+import React,{useState} from 'react';
+
+export default function FoodBox(props) {
+      const {name,calories,image,quantity} = props.food
+      const [value, setValue] = useState(0);
+
+     const handleValue = (event) =>{
+console.log('hii',event)
+
+setValue(event)
+
+     }
+
+
+    
+
+  return (
+    
+    <div>
+      <br /><br />
+     
+     <div style={{ width: "35rem"}}>
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={image}/>
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{name}</strong> <br />
+                <small>{calories}</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input onChange={(event)=>{handleValue(event.target.value)}}  className="input" type="number"/>
+              </div>
+              <div className="control">
+                <button onClick={()=>props.onAdding(props.food,value)} className="button is-info">+</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+      </div>
+    </div>
+  );
+}

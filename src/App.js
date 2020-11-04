ede4e68f46cbb1454ce7cf342f7f917e4097cdf1
unimatrix2 <dnny.nis@gmail.com>
2020-11-04 17:08:38 -0300

    finished iteration 4

--- src/App.js
+++ src/App.js
@@ -10,17 +10,30 @@ class App extends Component {
 	state = {
 		foods,
 		filteredFoods: foods,
+		searchString: '',
 	}
 
 	addFood = foodForm => {
 		this.setState({ foods: [...this.state.foods, foodForm], filteredFoods: [...this.state.filteredFoods, foodForm] }, () => console.log(this.state.foods));
 	}
 
+	handleSearch = event => {
+		this.setState({searchString: event.target.value}, () => {
+			const filtered = this.state.foods.filter(food => food.name.toLowerCase().includes(this.state.searchString.toLowerCase()));
+			this.setState({filteredFoods: filtered});
+		});
+	}
+
 	render() {
 		return (
 			<div className='container'>
 				<h1 className='title'>IronNutrition</h1>
-				<input type='text' className='input search-bar' name='search' placeholder='Search' />
+				<input type='text'
+				className='input search-bar'
+				name='search'
+				placeholder='Search'
+				onChange={this.handleSearch}
+				value={this.state.searchString} />
 				<div className='columns'>
 					<div className='column'>
 						{this.state.filteredFoods.map(food => {

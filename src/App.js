c9118e5637839322be42356ee169975046691879
Cristina Castro <cristinacastro87@gmail.com>
2020-11-11 21:24:03 +0100

    semi done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import foods from './foods.json';
+import 'bulma/css/bulma.css';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FoodBox from './components/FoodBox';
+import AddFood from './components/AddFood'
+class App extends Component {
+  state = {
+    foods: foods
+  }
+  addFoodHandler = theDish => {
+    const dishesCopy = [...this.state.foods]
+    // theDish.id = [...this.state.dishes].pop().id + 1
+    dishesCopy.push(theDish)
+    console.log(dishesCopy)
+    this.setState({
+      dishesCopy: dishesCopy
+    })
+  }
+  searchFood = event => {
+    const dishesCopy2 = [...this.state.foods]
+    const newList = dishesCopy2.filter(food => food.name.includes(event.target.value))
+    this.setState({
+      dishesCopy2: newList
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <input type="text" className="input" placeholder="Search..." onChange={this.searchFood}/>
+        </div>
+        <AddFood addFood={this.addFoodHandler} />
+        <div className="add-form">
+          {this.state.foods.map((oneFood, index) => {
+            return (
+              <FoodBox key = {index} name={oneFood.name} image={oneFood.image} calories={oneFood.calories} quantity={oneFood.quantity} />
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;

f6c65b6f7bb1dfb064c943d3c801794192bfd988
luezu-42 <leonardouezu@gmail.com>
2020-11-16 08:57:03 -0300

    Done?

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Search(props){
+
+
+
+    return(
+        <div>
+            <input onChange={props.onChange} type="text" name="food" className="input is-primary" value={props.value}></input>
+        </div>
+    )
+}
+
+export default Search; 

1a4fd97ab4be4c5a3ab86d9e2f9adefdbb6ddb9b
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-25 17:26:01 +0100

    added search form

--- src/App.js
+++ src/App.js
@@ -8,13 +8,34 @@ import AddFoodForm from './addfood';
 function App() {
   const [foods, setFoods] = useState(foodList);
   const [showForm, setShowForm] = useState(false);
+  const [searchInput, setSearchInput] = useState('');
 
   const toggleForm = () => setShowForm(!showForm);
 
   const addFoodHandler = (newFood) => {
     const foodCopy = [...foods];
+    foodList.push(newFood);
     foodCopy.push(newFood);
     setFoods(foodCopy);
+  };
+
+  const handleSearchInput = (event) => {
+    event.preventDefault();
+    setSearchInput(event.target.value);
+  };
+
+  const searchFoods = (event) => {
+    event.preventDefault();
+    let foodCopy = [...foods];
+    if (searchInput === '') {
+      setFoods(foodList)
+    } else {
+      setFoods(
+        foodCopy.filter((item) =>
+          item.name.toUpperCase().includes(searchInput.toUpperCase())
+        )
+      );
+    }
   };
 
   return (
@@ -22,6 +43,13 @@ function App() {
       <h1>IronNutrition</h1>
       <div className="foodWrapper">
         <div className="foodList">
+          <form className="search" onSubmit={searchFoods}>
+            <input
+              type="text"
+              placeholder="Search for food"
+              onChange={handleSearchInput}
+            />
+          </form>
           {foods.map((item, index) => (
             <Foodbox key={index} food={item} />
           ))}

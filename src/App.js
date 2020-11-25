7449eb21d4474f3907f45edb89b030a93104158b
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-25 15:31:17 -0600

    blue btn

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import data from './foods.json';
-import FoodBox from './Foodbox';
-import { Table, Button } from 'antd';
+// import FoodBox from './Foodbox';
+import { Table } from 'antd';
+import { Button } from 'antd';
 
 const columns = [
   {
@@ -25,29 +26,29 @@ const columns = [
     width: 150,
   },
   {
+    title: 'calories',
+    dataIndex: 'calories',
+  },
+  {
     title: 'quantity',
     dataIndex: 'quantity',
+    width: 150,
     render: () => (
       <div className="field has-addons">
         <div className="control">
           <input className="input" type="number" value="1" />
         </div>
         <div className="control">
-          <button className="button is-info">+</button>
+          <Button type="primary">Primary Button</Button>
         </div>
       </div>
     ),
-  },
-  {
-    title: 'calories',
-    dataIndex: 'calories',
   },
 ];
 
 function App() {
   return (
     <div className="App">
-      <FoodBox></FoodBox>
       <Table
         columns={columns}
         dataSource={data}

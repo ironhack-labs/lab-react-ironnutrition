2ec8ea6de46ec19428b22276be98116d50d9debc
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-25 17:07:58 -0600

    form on top

--- src/App.js
+++ src/App.js
@@ -38,51 +38,45 @@ const columns = [
           <input className="input" type="number" value="1" />
         </div>
         <div className="control">
-          <Button type="primary" onClick={() => formik()}>
-            +
-          </Button>
+          <Button type="primary">+</Button>
         </div>
       </div>
     ),
   },
 ];
 
-function formik() {
-  const BasicExample = () => (
-    <div>
-      <h1>My Form</h1>
-      <Formik
-        initialValues={{ name: 'jared' }}
-        onSubmit={(values, actions) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
-          }, 1000);
-        }}
-      >
-        {(props) => (
-          <form onSubmit={props.handleSubmit}>
-            <input
-              type="text"
-              onChange={props.handleChange}
-              onBlur={props.handleBlur}
-              value={props.values.name}
-              name="name"
-            />
-            {props.errors.name && <div id="feedback">{props.errors.name}</div>}
-            <button type="submit">Submit</button>
-          </form>
-        )}
-      </Formik>
-    </div>
-  );
-  console.log(BasicExample);
-  return BasicExample;
-}
-
 function App() {
   return (
     <div className="App">
+      <div>
+        <h1>My Form</h1>
+        <Formik
+          initialValues={{ name: 'jared' }}
+          onSubmit={(values, actions) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              actions.setSubmitting(false);
+            }, 1000);
+          }}
+        >
+          {(props) => (
+            <form onSubmit={props.handleSubmit}>
+              <input
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.name}
+                name="name"
+              />
+              {props.errors.name && (
+                <div id="feedback">{props.errors.name}</div>
+              )}
+              <button type="submit">Submit</button>
+            </form>
+          )}
+        </Formik>
+      </div>
+
       <Table
         columns={columns}
         dataSource={data}

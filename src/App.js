f60328ae060da7d2642c30a79b469fa250f856ad
Maya Ausset <mayaausset@gmail.com>
2020-11-26 21:16:22 +0100

    iteration 1&2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import foodList from './foods.json';
+import FoodBox from './components/FoodBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  state = {
+    foodState : foodList,
+  };
+  render(){ 
+    return (
+      <div className="App">
+      <h3 className="title">IronNutrition</h3>
+         {this.state.foodState.map((foodItem, index) => (
+         <FoodBox key={index} {...foodItem}/>
+        ))};
+      </div>
+    );
+  }
+};
 
 export default App;

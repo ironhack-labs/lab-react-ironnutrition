a6ab22180f2450f9139926db704861a7f46dbf6a
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-26 16:27:52 +0100

    Iteration 3 complete

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import foodList from './foods.json';
 import FoodBox from './Components/FoodBox';
+import FoodForm from './Components/FoodForm';
 
 
 class App extends Component {
   state = {
-    foodState: foodList
+    foodState: foodList,
+    form: false
   };
+
+  handleRenderForm = () => {
+    const stateCopy = { ...this.state };
+    stateCopy.form = !this.state.form;
+    this.setState(stateCopy);
+  }
+
+  handleAddNewFood = (newFoodItem) => {
+    const stateCopy = { ...this.state };
+    stateCopy.foodState = [...stateCopy.foodState, newFoodItem];
+    stateCopy.form = !stateCopy.form;
+    this.setState(stateCopy);
+  }
+  
 
   render() {
     return (
-      <div>
+      <div className = "App">
+        <button onClick={this.handleRenderForm}>Add Food</button>
+        {this.state.form && (<FoodForm handleLiftFoodFormState = {this.handleAddNewFood} />
+        )}
         {this.state.foodState.map((foodItem, index) => (
           <FoodBox key={index} {...foodItem} />
         ))}

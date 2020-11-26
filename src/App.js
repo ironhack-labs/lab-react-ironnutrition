892c2d5dc3e7ef7319e0106ed11bd73c08cd751d
sophiepa <sophiepa@hotmail.com>
2020-11-26 13:05:35 +0100

    iteration1 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import FoodBox from './FoodBox/FoodBox'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>IronNutrition</h1>
+
+    {foods.map(foodItem => (
+      <FoodBox
+      name = {foodItem.name}
+      image = {foodItem.image}
+      calories = {foodItem.calories}
+       />
+    ))}
     </div>
   );
 }

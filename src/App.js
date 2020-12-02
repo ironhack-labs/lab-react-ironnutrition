50a0f1a3fec06a3b28553c2c59429fb5a33d55bc
steven payne <djnosey@hotmail.co.uk>
2020-12-02 16:48:57 +0100

    iterations 1,2,3,4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
+
+import AddFood from './components/AddFood';
+import FoodBox from './components/FoodBox';
+import foods from './foods.json';
 
 function App() {
+  const [formDisplay, setFormDisplay] = useState(false);
+  const [foodList, setFoodList] = useState(foods);
+  const [search, setSearch] = useState('');
+  
+  const toggleForm = () => {
+    const clicked = !formDisplay;
+    setFormDisplay(clicked);
+  };
+
+  const addNewFood = (newFoodObj) => {
+    console.log('function called');
+    const foodsCopy = [newFoodObj, ...foodList];
+    setFoodList(foodsCopy);
+    toggleForm();
+  };
+
+  useEffect(() => {
+    const foodCopy = [...foodList];
+    const filteredFood = foodCopy.filter((food) => food.name.includes(search));
+    setFoodList(filteredFood);
+  }, [search]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          value={search}
+          className="input is-info"
+          type="text"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+      </form>
+      <button onClick={toggleForm} className="button is-info">
+        {!formDisplay ? 'Show Add Food Form' : 'Hide Form'}
+      </button>
+      {formDisplay ? (
+        <div className="AddFoodForm">
+          <AddFood addFood={addNewFood} />
+        </div>
+      ) : null}
+
+      <div className="foodBoxes">
+        {foodList.map((item) => {
+          return <FoodBox key={item.name} food={item} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/AddFood.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import 'bulma/css/bulma.css';
+
+function AddFood(props) {
+  const [image, setImage] = useState('');
+  const [name, setName] = useState('');
+  const [calories, setCalories] = useState(undefined);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newFoodObj = { name, image, calories };
+    props.addFood(newFoodObj);
+    setImage('');
+    setName('');
+    setCalories(undefined);
+  };
+
+  return (
+    <div className="AddFood__container">
+      <form onSubmit={handleSubmit}>
+        <input
+          class="input is-info"
+          value={name}
+          name="name"
+          type="text"
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          class="input is-info"
+          value={calories}
+          name="calories"
+          type="number"
+          placeholder="Calories"
+          onChange={(e) => setCalories(e.target.value)}
+        />
+        <input
+          class="input is-info"
+          value={image}
+          name="image"
+          type="text"
+          placeholder="picture url"
+          onChange={(e) => setImage(e.target.value)}
+        />
+        <button type="submit" className="button is-info">
+          Add a food
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddFood;

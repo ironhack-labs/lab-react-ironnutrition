1c1edfb6cd7a7edcf738489a18cd9c5d67940853
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2020-12-02 22:04:42 +0100

    iteration 3

--- src/App.js
+++ src/App.js
@@ -7,40 +7,83 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   state = {
-    myKey: foods,
+    myFoods: foods,
+    showForm: false,
+    name: '',
+    calories: 0,
+    image: '',
+  };
+  handleClick = () => {
+    this.setState({ showForm: !this.state.showForm });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let floor = [...this.state.myFoods];
+    floor.push({
+      name: this.state.name,
+      calories: this.state.calories,
+      image: this.state.image,
+    });
+    this.setState({ myFoods: floor });
+  };
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    //get the value entered by user
+    //szwconst value = event.target.value;
+    console.log(value);
+    //set the value in the state (will update the field)
   };
 
   render() {
     return (
       <div>
-        {this.state.myKey.map((el, i) => {
-          return(
-            <FoodBox 
-            key={i}
-            name={el.name}
-            calories={el.calories}
-            image={el.image}
-          />
-          )
-          
+        <button onClick={this.handleClick}>New</button>
+        {this.state.showForm === true ? (
+          <form onSubmit={this.handleSubmit}>
+            <label>Name: </label>
+            <br />
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleInput}
+            />
+            <br />
+            <label>Calories: </label>
+            <br />
+            <input
+              type="text"
+              name="calories"
+              value={this.state.calories}
+              onChange={this.handleInput}
+            />
+            <br />
+            <label>Image: </label>
+            <br />
+            <input
+              type="text"
+              name="image"
+              value={this.state.image}
+              onChange={this.handleInput}
+            />
+            <br />
+            <button type="submit"> Add new food </button>
+          </form>
+        ) : null}
+
+        {this.state.myFoods.map((el, i) => {
+          return (
+            <FoodBox
+              key={i}
+              name={el.name}
+              calories={el.calories}
+              image={el.image}
+            />
+          );
         })}
       </div>
     );
   }
 }
-
-// import React from 'react';
-// function App() {
-//   return (
-//     <div className="App">
-//       <FoodBox
-//         name = {"Salad"}
-//         calories = {150}
-//         image = {"https://i.imgur.com/DupGBz5.jpg"}
-//         quantity = {0}
-
-//       />
-//     </div>
-//   );
-// }
-// export default App;

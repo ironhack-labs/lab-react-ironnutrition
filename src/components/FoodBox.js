042aa072f70fe7a82ab805658fac2cc0426a7756
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2020-12-03 12:00:00 +0100

    mutated function to class component (FoodBox)

--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,24 +1,28 @@
 import './FoodBox.css';
 import 'bulma/css/bulma.css';
-//import foods from './../foods.json';
 
-import React from 'react'
+import React, { Component } from 'react'
 
-const FoodBox = (props) => {
-  const {name, calories, image} = props;
+// const {name, calories, image} = props;
+export class FoodBox extends Component {
+  state = {
+    quantity: 1
+  }
+  
+  render() {
     return (
-        <div className="box">
+      <div className="box">
         <article className="media">
           <div className="media-left">
             <figure className="image is-64x64" >
-              <img src={image} alt="food"/>
+              <img src={this.props.image} alt="food"/>
             </figure>
           </div>
           <div className="media-content">
             <div className="content">
               <p>
-                <strong>{name}</strong> <br />
-                <small>{calories} cal</small>
+                <strong>{this.props.name}</strong> <br />
+                <small>{this.props.calories} cal</small>
               </p>
             </div>
           </div>
@@ -35,26 +39,30 @@ const FoodBox = (props) => {
         </article>
       </div>
     )
+  }
 }
 
 export default FoodBox
 
 
-// export default class FoodBox extends Component {
-//   render() {
+// SAME SAME WITH A FUNCTION COMPONENT
+//import React from 'react'
+
+// const FoodBox = (props) => {
+//   const {name, calories, image} = props;
 //     return (
-//       <div className="box">
+//         <div className="box">
 //         <article className="media">
 //           <div className="media-left">
-//             <figure className="image is-64x64">
-//               <img src="https://i.imgur.com/eTmWoAN.png" />
+//             <figure className="image is-64x64" >
+//               <img src={image} alt="food"/>
 //             </figure>
 //           </div>
 //           <div className="media-content">
 //             <div className="content">
 //               <p>
-//                 <strong>Pizza</strong> <br />
-//                 <small>400 cal</small>
+//                 <strong>{name}</strong> <br />
+//                 <small>{calories} cal</small>
 //               </p>
 //             </div>
 //           </div>
@@ -70,6 +78,9 @@ export default FoodBox
 //           </div>
 //         </article>
 //       </div>
-//     );
-//   }
+//     )
 // }
+
+// export default FoodBox
+
+

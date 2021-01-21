491faae71b5af7089f17d8a23c6b381b2d7013bf
Zhenni Qian <zhenni.qian913@gmail.com>
2021-01-22 00:24:34 +0100

    began iteration 3

--- src/App.js
+++ src/App.js
@@ -5,25 +5,46 @@ import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 import FoodList from './components/FoodList';
 import SelectedFoodList from './components/SelectedFoodList';
+import AddFood from './components/AddFood';
 
-function App() {
-  return (
-    <>
-      <div className="columns">
-        <FoodList>
-          {foods.map(food => {
-            return <FoodBox key={food.name} {...food} />
-          })}
-        </FoodList>
-        <div className="column content">
-          <SelectedFoodList />
-          <div>
-            <button className="button is-info">Add a new food</button>
-          </div>         
+class App extends React.Component {
+  state = {
+    foods,
+    showAddForm: false
+  };
+
+  clickAddFoodHandler = () => {
+    this.setState({
+      showAddForm: !this.state.showAddForm
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">IronNutrition</h1>
+        <div class="columns">
+          <FoodList>
+            {this.state.foods.map(food => {
+              return <FoodBox key={food.name} {...food} />
+            })}
+          </FoodList>
+          <div className="column content">
+            <SelectedFoodList />
+            <div>
+              {!this.state.showAddForm && (
+                <button onClick={this.clickAddFoodHandler} className="button is-info">
+                  Add a new food
+                </button>
+              )}
+              {this.state.showAddForm && <AddFood />}
+            </div>         
+          </div>
         </div>
       </div>
-    </>
-  );
-}
+    )
+  }
+};
+
 
 export default App;
--- src/components/AddFood.js
+++ src/components/AddFood.js
@@ -31,3 +31,5 @@ class AddFood extends React.Component {
         )
     }
 }
+
+export default AddFood;

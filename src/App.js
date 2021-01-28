4e377eb5c92a1eadaac4473f009aeca9f44016e3
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-01-28 11:00:07 +0100

    complete iteration 2 and 3

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
 import FoodBox from './components/FoodBox';
+import AddFood from './components/AddFood';
 
 class App extends React.Component {
   state = {
     foods: foods,
     displayedFoods: [...foods]
+  }
+
+  handleAddFood = (newFoodItem) => {
+    const newFoods = [newFoodItem, ...this.state.foods]
+    this.setState({
+      foods: newFoods,
+      displayedFoods: newFoods
+    })
   }
 
   render() {
@@ -17,7 +26,10 @@ class App extends React.Component {
 
     return (
       <div className="App">
+        <AddFood addFood={this.handleAddFood}/>
         {foodList}
+
+
       </div>
     )
   }

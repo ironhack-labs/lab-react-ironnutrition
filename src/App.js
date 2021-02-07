2c6423ccdea3db780c2a5c2632714b9120ccbdb9
MCiappina <mciappops@gmail.com>
2021-02-07 13:13:12 -0300

    FoodBox component ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import 'bulma/css/bulma.css';
 
+import foods from './foods.json';
+
+// {
+//     "name": "Almonds",
+//     "calories": 75,
+//     "image": "https://i.imgur.com/JRp4Ksx.jpg",
+//     "quantity": 0
+//   },
+
+import FoodBox from './components/foodbox/FoodBox';
 
 function App() {
+  console.log(foods);
   return (
     <div className="App">
+    
+      <FoodBox name="Almonds" calories={75} image="https://i.imgur.com/JRp4Ksx.jpg" quantity={0} />
     </div>
   );
 }

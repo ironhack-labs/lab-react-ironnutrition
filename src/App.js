f4306ad986857d6c5700912a2eba0b06bb153085
carmen valencia <carmenpilar1990@gmail.com>
2021-02-12 19:08:47 +0100

    1 y 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './Components/FoodBox'
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {foods.map((food) => {
+      return (
+        <div>
+          <FoodBox name={food.name} image={food.image} calories={food.calories} quantity={food.quantity} />
+        </div>
+      )
+    })}
     </div>
   );
 }
 
-export default App;
+export default  App;

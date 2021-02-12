7d04ea636cc44df39a8da6e07e71e196b2223f14
shtesh <vshtefanuka@gmail.com>
2021-02-12 15:57:15 +0100

    Iterations 1-4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
+import AddFood from './components/AddFood';
+import SearchBar from './components/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    foods: foods,
+    displayedFoods: [...foods]
+  }
+
+  handleAddFood = (newFoodItem) => {
+    const newFoods = [newFoodItem, ...this.state.foods]
+    this.setState({
+      foods: newFoods,
+      displayedFoods: newFoods
+    })
+  }
+
+  handleFilterFood = (value) => {
+    const newFoods = this.state.foods.filter(food => (food.name.toUpperCase().includes(value.toUpperCase())))
+    this.setState({
+      displayedFoods: newFoods
+    })
+  }
+
+  render() {
+    const foodList = this.state.displayedFoods.map((element) => {
+      return <FoodBox key={element.name} food={element} />
+    })
+
+    return (
+      <div className="App">
+
+        <SearchBar filterFood={this.handleFilterFood} />
+
+        <AddFood addFood={this.handleAddFood} />
+        {foodList}
+
+        </div>
+   )
+ }
 }
 
 export default App;
---
+++ src/components/AddFood.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import FoodBox from './FoodBox';
+
+// It creates a button to add new food. When a user clicks the button, a form will appear for a name, number of calories, and an image. When the user clicks submit, the food is added to the list. The form disappears when the user clicks the submit button.
+class AddFood extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showForm: false,
+            name: '',
+            calories: 0,
+            image: ''
+        };
+    }
+
+    displayForm = () => {
+        const newShowForm = !this.state.showForm;
+        this.setState({ showForm: newShowForm })
+    }
+
+    handleChange = (e) => {
+        const propertyName = e.target.name
+        this.setState({
+            [propertyName]: e.target.value
+        });
+    }
+
+    handleSubmission = (e) => {
+        e.preventDefault()
+
+        this.props.addFood({
+            name: this.state.name,
+            calories: this.state.calories,
+            image: this.state.image
+        })
+
+        this.setState({
+            showForm: false,
+            name: '',
+            calories: 0,
+            image: ''
+        });
+    }
+
+    render() {
+        const formStyle = {
+            display: this.state.showForm ? "block" : "none"
+        }
+
+        return (
+            <div>
+                <button onClick={this.displayForm}>{this.state.showForm ? "Hide form" : "Display form"}</button>
+
+                <form onSubmit={this.handleSubmission} style={formStyle}>
+                    <label>Name:</label>
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+
+                    <label>Calories:</label>
+                    <input type="number" name="calories" value={this.state.calories} onChange={this.handleChange} />
+
+                    <label>Image:</label>
+                    <input type="text" name="image" value={this.state.image} onChange={this.handleChange} />
+
+                    <button>Submit</button>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddFood;

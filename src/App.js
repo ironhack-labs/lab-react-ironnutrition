b10410259f2c9d5d85dc1bad524ba903f4310bed
honghainguyen777 <n.h.hai2015@gmail.com>
2021-02-24 13:02:46 +0100

    iteration 7

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import NewFood from './components/NewFood';
 class App extends React.Component {
   state = {
     displayedFoods: foods,
-    query: "",
+    query: '',
     foodToday: {},
-    totalCal: 0
+    totalCal: 0,
   };
 
   showInfo = (event) => {
@@ -22,70 +22,106 @@ class App extends React.Component {
     event.target.classList.toggle('hide');
     const foodsCopy = [...this.state.displayedFoods];
     // dont want to use server or save the image
-    const newFood = {name: state.name, calories: state.cal, image: "https://i.imgur.com/hGraGyR.jpg", quantity: 0 };
+    const newFood = {
+      name: state.name,
+      calories: state.cal,
+      image: 'https://i.imgur.com/hGraGyR.jpg',
+      quantity: 0,
+    };
     foodsCopy.push(newFood);
-    this.setState(() => ({displayedFoods: foodsCopy}));
+    this.setState(() => ({ displayedFoods: foodsCopy }));
     event.preventDefault();
-  }
+  };
 
   handleQuery = (event) => {
     const query = event.target.value;
     // this.setState({query: event.target.value});
-    const queriedFood = foods.filter(food => (food.name.toLowerCase()).includes(query.toLowerCase()));
-    if (query === "") {
+    const queriedFood = foods.filter((food) =>
+      food.name.toLowerCase().includes(query.toLowerCase())
+    );
+    if (query === '') {
       this.setState({
         displayedFoods: foods,
-        query: query
+        query: query,
       });
     } else {
       this.setState({
         displayedFoods: queriedFood,
-        query: query
+        query: query,
       });
     }
   };
 
   addFoodToday = (food, quantity) => {
-    const foodTodayCopy = JSON.parse(JSON.stringify(this.state.foodToday))
+    const foodTodayCopy = JSON.parse(JSON.stringify(this.state.foodToday));
     let currentQuantity = foodTodayCopy.quantity || 0;
     let updatedQuantity = currentQuantity + Number(quantity);
     let totalCal = updatedQuantity * food.calories;
-    foodTodayCopy[food.name] = {quantity: updatedQuantity, totalCal: totalCal }
+    foodTodayCopy[food.name] = {
+      quantity: updatedQuantity,
+      totalCal: totalCal,
+    };
     this.setState({
       foodToday: foodTodayCopy,
-      totalCal: this.state.totalCal + totalCal
+      totalCal: this.state.totalCal + totalCal,
+    });
+  };
+
+  removeFoodToday(name) {
+    const foodTodayCopy = JSON.parse(JSON.stringify(this.state.foodToday));
+    const subCal = foodTodayCopy[name].totalCal;
+    delete foodTodayCopy[name];
+    this.setState({
+      foodToday: foodTodayCopy,
+      totalCal: this.state.totalCal - subCal
     })
   }
 
   render() {
     const foodList = this.state.displayedFoods.map((food, index) => {
-      return <FoodBox key={index} food={food} addFoodToday={this.addFoodToday} />;
+      return (
+        <FoodBox key={index} food={food} addFoodToday={this.addFoodToday} />
+      );
     });
 
     const todayFoods = Object.keys(this.state.foodToday).map((key, index) => {
       return (
-        <li className="has-text-left" key={index}>{this.state.foodToday[key].quantity} {key} = {this.state.foodToday[key].totalCal} cal</li>
-      )
-    })
+        <li className="has-text-left" key={index}>
+          {this.state.foodToday[key].quantity} {key} ={' '}
+          {this.state.foodToday[key].totalCal} cal{' '}
+          <i
+            className="fas fa-trash fa-lg has-text-danger"
+            onClick={() => this.removeFoodToday(key)}
+          ></i>
+        </li>
+      );
+    });
     return (
       <div className="App">
         <button className="button is-primary" onClick={this.showInfo}>
           Add a food
         </button>
         <NewFood handleSubmitNewFood={this.handleSubmitNewFood} />
-        <br/>
-        <input className="input" name="query" value={this.state.query} type="text" placeholder="Searching for a food" onChange={e => this.handleQuery(e)} />
+        <br />
+        <input
+          className="input"
+          name="query"
+          value={this.state.query}
+          type="text"
+          placeholder="Searching for a food"
+          onChange={(e) => this.handleQuery(e)}
+        />
         <div className="columns">
-          <div className="column">
-            {foodList}
-          </div>
-          <div className="m-3 pl-5 column" >
-            <h2 className="is-size-4"><b>Today's foods:</b></h2>
-            <ul>
-              {todayFoods}
-            </ul>
-            <hr/>
-            <h3 className="is-size-5 has-text-left"><i>Total:</i> {this.state.totalCal} cal</h3>
+          <div className="column">{foodList}</div>
+          <div className="m-3 pl-5 column">
+            <h2 className="is-size-4">
+              <b>Today's foods:</b>
+            </h2>
+            <ul>{todayFoods}</ul>
+            <hr />
+            <h3 className="is-size-5 has-text-left">
+              <i>Total:</i> {this.state.totalCal} cal{' '}
+            </h3>
           </div>
         </div>
       </div>

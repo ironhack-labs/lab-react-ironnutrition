53d06c805f01a27e93c5e54438ea40e3a0c1b26e
Barbara Diaz <barbarad91@gmail.com>
2021-02-24 15:28:01 +0100

    Create FoodBox component

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import FoodBox from './FoodBox';
+
+import foods from '../foods.json';
+
+function App() {
+  return (
+    <>
+      <FoodBox />
+    </>
+  );
+}
+
+export default App;

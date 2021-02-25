938e5d3016a332a6caf497ad11d7cf874844fa04
Diego Barrera <di_barrera@hotmail.com>
2021-02-25 10:25:28 -0600

    search bar

--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,69 +1,107 @@
-import React,{useState} from 'react'
-import foods from '../foods.json'
-
+import React, {useState} from 'react'
+import foods from '../foods.json';
+import { nanoid } from 'nanoid'
+import Search from './Search'
 export default function FoodBox(props) {
-
-    const [formState, setFormState] = useState(false)
-
-    function displayForm(){
-    setFormState(true)
-    }
-    function hideForm(){
-        setFormState(false)
-    }
-    function form(){
-        return (
-            <form>
-    <p><label >Name: </label>
-    <input name="newName" type="text"/></p>
-    <p><label>Number of calories: </label>
-    <input name="newNumberOfCalories" type="number"/></p>
-    <p><label>Image: </label>
-    <input name="newImage" type="text"/></p>
-    <button onClick={()=>hideForm()}type="submit">Add</button>
-</form>
-        )
-    }
-    return (
-<div>
-<button className="button is-info" onClick={()=>{displayForm()}}>Add New Food</button>
-{formState? form() : null }
-    {foods.map((e,id)=>{
-        return(
-    <div key={id}>
-        <div className="box">
-        <article className="media">
-            <div className="media-left">
-                <figure className="image is-64x64">
-                    <img src={e.image} />
-                </figure>
-            </div>
-        <div className="media-content">
-            <div className="content">
-                <p>
-                <strong>{e.name}</strong> <br />
-                <small>{e.calories} cal</small>
-                </p>
-            </div>
-        </div>
-        <div className="media-right">
-            <div className="field has-addons">
-                <div className="control">
-                    <input className="input" type="number" value="1" />
-                </div>
-            <div className="control">
-                <button className="button is-info">
-                     +
-                </button>
-            </div>
-        </div>
-    </div>
-            </article>
-</div>
-</div>
-)
+//STATES
+    const [newFood, setNewFood]= useState({
+        image:"",
+        name:"",
+        calories:""
+    })
+    const [food, setFood] = useState(foods)
+    const [searchFood, setSearchFood] = useState(food)
+    const [addMode, setEdition] = useState(false)
+    const [error, setError] = useState(null)
+    const [id, setId] = useState("")
+//FUNCIONES CRUD
+const handleChange = (event) => {
+    console.log("Hola! Acaban de tocar una tecla")
+    setNewFood({
+        ...newFood,
+        id: nanoid(),
+        [event.target.name]: event.target.value
     })
 }
-        </div>
+    const addFood = (event) => {
+        event.preventDefault()
+        if(!newFood.name.trim() || !newFood.calories.trim()){
+            setError("Inserta el nombre de la comida y sus calorias por favor.")
+            return;
+        }
+        setFood([
+            ...food,
+            newFood
+        ])
+        setError(null)
+        setNewFood({
+            image: "",
+            name: "",
+            calories: ""
+        })
+    setSearchFood([
+        ...searchFood,
+            newFood
+    ])
+    }
+    return (
+        <>
+        <Search data={food} setSearchFood={setFood} searchFood={searchFood}/>
+        {food.map((e, id) => {
+            return(
+                    <div className="box" key={id}>
+                    <article className="media">
+                        <div className="media-left">
+                        <figure className="image is-64x64">
+                            <img src={e.image} />
+                        </figure>
+                        </div>
+                        <div className="media-content">
+                        <div className="content">
+                            <p>
+                            <strong>{e.name}</strong> <br />
+                            <small>{e.calories}</small>
+                            </p>
+                        </div>
+                        </div>
+                        <div className="media-right">
+                        <div className="field has-addons">
+                            <div className="control">
+                            <input className="input" type="number" value="1" />
+                            </div>
+                            <div className="control">
+                            <button className="button is-info">
+                                +
+                            </button>
+                            </div>
+                        </div>
+                        </div>
+                    </article>
+                    </div>
+            )
+        })
+        }
+        <form onSubmit={addFood}>
+        <h3>Image</h3>
+                <input
+                    name="image"
+                    onChange={(e) => {handleChange(e)}}
+                    value={newFood.image}
+                />
+            <h3>Name</h3>
+                <input
+                    name="name"
+                    onChange={(e) => {handleChange(e)}}
+                    value={newFood.name}
+                />
+            <h3>Calories</h3>
+                <input
+                    name="calories"
+                    onChange={(e) => {handleChange(e)}}
+                    value={newFood.calories}
+                />
+            <button>Add Food</button>
+        </form>
+        </>
     )
 }

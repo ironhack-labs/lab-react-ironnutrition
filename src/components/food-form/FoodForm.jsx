7af81b153f27f5a0e13a25930bcd46722fc8ddd6
Llorenc Palliser <palliser@palliser.es>
2021-03-04 21:25:58 +0100

    Interacción 1, 2 y finalizando la 3

---
+++ src/components/food-form/FoodForm.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+
+const initial = {
+    name: '',
+    calories: '',
+    image: '',
+    quantity: ''
+}
+
+class FoodForm extends React.Component {
+    state = {
+        data: initial
+    }
+
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        this.props.addFood({
+            ...this.state.data,
+            image: "https://i.imgur.com/WCzJDWz.jpg",
+            quantity: 1
+        })
+
+        this.setState({
+            data: initial
+        })
+    }
+
+
+
+    handleChange = (event) => {
+        const { id, value } = event.target
+
+        this.setState((prev) => {
+            return {
+                data: {
+                    ...prev.data,
+                    [id]: value
+                }
+            }
+        })
+    }
+
+
+
+
+
+
+    render() {
+        return (
+            <>
+
+                <form className="mb-5 mt-5" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            className="input"
+                            id="name"
+                            placeholder="Enter name"
+                            value={this.state.data.name}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div className="form-group mb-2">
+                        <label htmlFor="calories">Calories</label>
+                        <input
+                            type="number"
+                            className="input"
+                            id="calories"
+                            placeholder="Enter number of calories"
+                            value={this.state.data.calories}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="button is-link">
+                        Submit
+                        </button>
+                </form>
+            </>
+        )
+    }
+
+
+
+
+}
+
+export default FoodForm
--- src/components/foodbox/FoodBox.jsx
+++ src/components/foodbox/FoodBox.jsx
@@ -1,48 +1,87 @@
 import { Component } from 'react'
-import foods from '../../foods.json';
+import foodsFromJason from '../../foods.json'
+import FoodForm from '../food-form/FoodForm';
 
 
+
+
+const foods = foodsFromJason
 
 class FoodBox extends Component {
+
+    state = {
+        foods: foods,
+        foodForm: null
+    }
+
+
+    addFood = (food) => {
+        foods.push(food)
+        this.setState((prev) => {
+            return {
+                foods: [food, ...prev.foods]
+            }
+        })
+        this.setState.saved = true
+        console.log(foods.length)
+    }
+
 
     render() {
 
         return (
 
-            <>
-                        {foods.map((food, i) => (
 
-                <div key={i} className="box">
-                    <article className="media">
-                        <div className="media-left">
-                            <figure className="image is-64x64">
-                                <img src={food.image} />
-                            </figure>
-                        </div>
-                        <div className="media-content">
-                            <div className="content">
-                                <p>
-                                    <strong>{food.name}</strong> <br />
-                                    <small>{food.calories} Cal</small>
-                                </p>
-                            </div>
-                        </div>
-                        <div className="media-right">
-                            <div className="field has-addons">
-                                <div className="control">
-                                    <input className="input" type="number" value="1" />
+
+            <>
+
+
+
+                <button
+                    onClick={() => this.setState.foodForm = true}
+                    className="button is-link my-3">
+                    Add new food
+                        </button>
+
+                {this.state.foodForm &&
+                    <FoodForm
+                        addFood={this.addFood} />}
+
+                {
+                    foods.map((food, i) => (
+
+                        <div key={i} className="box">
+                            <article className="media">
+                                <div className="media-left">
+                                    <figure className="image is-64x64">
+                                        <img src={food.image} />
+                                    </figure>
                                 </div>
-                                <div className="control">
-                                    <button className="button is-info">
-                                        +
+                                <div className="media-content">
+                                    <div className="content">
+                                        <p>
+                                            <strong>{food.name}</strong> <br />
+                                            <small>{food.calories} Cal</small>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="media-right">
+                                    <div className="field has-addons">
+                                        <div className="control">
+                                            <input className="input" type="number" value="1" />
+                                        </div>
+                                        <div className="control">
+                                            <button className="button is-info">
+                                                +
           </button>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            </article>
                         </div>
-                    </article>
-                </div>
-                        ))} 
-                        </>
+                    ))
+                }
+            </>
         )
 
     }

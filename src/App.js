5695197aebd4d0456bd01ada65f746095aad26a8
digomattar21 <digomattar@gmail.com>
2021-03-18 16:45:49 -0300

    Iterations 1,2,3 complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox/index';
 
 function App() {
+  const [name, setName] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [image, setImage] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    foods.push({ name: name, calories: calories, image: image, quantity:quantity });
+    console.log(foods)
+  };
+
+  useEffect(()=>{
+    
+  },[name])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button >Add Food</button>
+        <form method='post' onSubmit={handleSubmit}>
+          <input
+            name="name"
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            value={name}
+            id="foodNameInput"
+            placeholder="name"
+          />
+          <input
+            name="calories"
+            type="number"
+            onChange={(e) => setCalories(e.target.value)}
+            value={calories}
+            id="foodCaloriesInput"
+            placeholder="calories"
+          />
+          <input
+            name="image"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+            type="text"
+            id="foodImageInput"
+            placeholder="img Url"
+          />
+          <input
+            name="quantity"
+            onChange={(e) => setQuantity(e.target.value)}
+            value={quantity}
+            type="number"
+            id="foodQuantityInput"
+          />
+
+          <button type="submit">Save</button>
+        </form>
+      </div>
+      <div className="foods-container">
+        {foods.map((food) => {
+          return <FoodBox key={food.name} food={food} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/FoodBox/index.jsx
@@ -0,0 +1,36 @@
+import 'bulma/css/bulma.css';
+import React from 'react'
+
+const FoodBox = ({food}) => {
+  return (
+    <div className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img alt='img' src={food.image} />
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{food.name}</strong> <br />
+              <small>{food.calories} cal</small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input className="input" type="number" defaultValue={food.quantity} />
+            </div>
+            <div className="control">
+              <button className="button is-info">+</button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default FoodBox;

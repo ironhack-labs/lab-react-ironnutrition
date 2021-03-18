af84a4f587fccc92743f2f76153aaae98c5a0392
Florian Jam <jam.florian@gmail.com>
2021-03-18 10:11:29 +0100

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css';
-import foods from './foods.json'
-import FoodBox from './components/FoodBox.jsx'
+import foodsjson from './foods.json';
+import FoodBox from './components/FoodBox.jsx';
+import AddFood from './components/AddFood'
 
-function App() {
+class App extends React.Component {
+  state = {
+    foods: foodsjson,
+  };
 
-  return ( 
-    <div className="App">
-      {foods.map((food) => <FoodBox food={food} /> )}
-    </div>
-  )
+  add = (food) => {
+    this.setState({foods: [food, ...this.state.foods]})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {this.state.foods.map((food) => (
+            <FoodBox food={food} />
+          ))}
+        </div>
+        <div>
+          <AddFood add={this.add}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/AddFood.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+export default class AddFood extends Component {
+  state = {
+    formVisible: false,
+      name: '',
+      image: '',
+      calories: 0
+  };
+
+  handleClick = () => {
+    const visibility = this.state.formVisible;
+    this.setState({ formVisible: !visibility });
+  };
+
+  handleChange = (e) => {
+    const entry = e.target.name;
+    const value = e.target.value;
+    this.setState({ [entry]: value });
+  };
+
+  handleSubmit = (e) => {
+      e.preventDefault()
+      this.props.add({
+          name: this.state.name,
+          image: this.state.image,
+          calories: this.state.calories
+      })
+      this.setState({
+        name: '',
+        image: '',
+        calories: 0
+      })
+      this.handleClick()
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Add new food</button>
+        {this.state.formVisible && (
+          <form action="">
+            <div>
+              <label htmlFor="name">Name</label>
+              <input
+                onChange={this.handleChange}
+                value={this.state.name}
+                type="text"
+                name="name"
+              />
+            </div>
+            <div>
+              <label htmlFor="calories">Calories</label>
+              <input
+                onChange={this.handleChange}
+                value={this.state.calories}
+                type="number"
+                name="calories"
+              />
+            </div>
+            <div>
+              <label htmlFor="image">Image</label>
+              <input
+                onChange={this.handleChange}
+                value={this.state.image}
+                type="text"
+                name="image"
+              />
+            </div>
+            <button onClick={this.handleSubmit}>
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
+    );
+  }
+}

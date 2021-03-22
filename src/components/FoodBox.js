e202714e832e30aea90010ef2e13a37a19b9ecf9
joselrdg <josesietepicos@gmail.com>
2021-03-22 21:34:56 +0100

    iteration 1

---
+++ src/components/FoodBox.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './css/FoodBox.css'
+
+function handleChange() {
+    console.log('hola')
+}
+
+const FoodBox  = () => {
+
+    return (
+        <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src="https://i.imgur.com/eTmWoAN.png" alt='img'/>
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>Pizza</strong> <br />
+                <small>400 cal</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" type="number" value="1" onChange={handleChange} />
+              </div>
+              <div className="control">
+                <button className="button is-info">
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+      )
+}
+
+export default FoodBox;

b3d67a536a5eb639b28f92d23f8fcd20567afc7d
Ángela Herrador <angelaherrador1991@gmail.com>
2021-03-23 18:19:15 +0100

    Work on it3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import 'bulma/css/bulma.css'
 import Foods from './components/Foods/Foods'
+import FoodForm from './components/FoodForm/FoodForm'
 
 function App() {
   return (
     <div className="App">
+      <FoodForm />
+  
       <Foods />
+
     </div>
+
+
   );
 }
 
---
+++ src/components/FoodForm/FoodForm.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import './FoodForm.css'
+
+
+ class FoodForm extends Component {
+    state = {
+        showForm: false,
+        newFood: {
+            name: "",
+            calories: "",
+            image: ""
+        }
+    }
+
+    handleDrawForm () {
+        // On click we change our state – this will trigger our `render` method
+        this.setState({ showForm: true });
+    }
+
+    handleChange = (e) => {
+        let value = e.target.value;
+        const newFood = this.state.newFood
+        newFood[e.target.id] = value
+        //console.log(newFood)
+        this.setState({ newFood });
+    };
+     
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state.newFood.name)
+        console.log(this.state.newFood.calories)
+    }
+
+    render() {
+        return (
+            <div className="FoodForm">
+                <button onClick={this.handleDrawForm.bind(this)} className="button">Add Food</button>
+                {
+                this.state.showForm && 
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Name: </label>
+                    <input id="name" name="name" value={this.state.newFood.name} onChange={this.handleChange}/>
+
+                    <label htmlFor="calories">Calories: </label>
+                    <input id="calories" name="calories" value={this.state.newFood.calories} onChange={this.handleChange}/>
+
+
+                    <label htmlFor="image">Image: </label>
+                    <input id="image" name="image" value={this.state.newFood.image} onChange={this.handleChange} />
+
+                    <button> Submit</button>
+
+                </form>
+                }
+            </div>
+        )
+    }
+
+}
+
+
+
+export default FoodForm
+
+

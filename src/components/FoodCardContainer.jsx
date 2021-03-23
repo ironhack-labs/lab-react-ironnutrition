f677f705f9a88c0b1ac440803b301e479fc7cc5a
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-03-23 11:26:05 +0100

    Add food filter

--- src/components/FoodCardContainer.jsx
+++ src/components/FoodCardContainer.jsx
@@ -4,20 +4,44 @@ import data from '../data/foods.json'
 //components
 import FoodCard from './FoodCard'
 
+//assets
+import '../assets/FoodCardContainer.css'
+
 let uniqid = require('uniqid');
 
 
 class FoodCardContainer extends Component{
     state ={
-        foods: [...data]
+        foods: [...data],
+        search: ''
     }
 
+    onSearch = (event) => this.setState({search: event.target.value}) 
+
+
+    filterFoods = () => {
+        let foods = [...this.state.foods]
+        if (this.state.search) {
+            foods = this.state.foods.filter(({ name }) => name.toLowerCase().includes(this.state.search))
+        }
+        return foods
+    }
+  
+
+
     render(){
+
+        const foods = this.filterFoods()
+
         return(
-            
             <div className="food__card__container container">
+
                 <div className="row">
-                    {this.state.foods.map(food => (
+                    <input placeholder="Search" type="text" className="form-control search-bar" onChange={this.onSearch}/>
+                </div>
+
+                <div className="row">
+                    {foods.map(food => (
                         <div className="col" key={uniqid(this.name)}>
                             <FoodCard {...food}/>
                         </div>
@@ -25,7 +49,6 @@ class FoodCardContainer extends Component{
                 </div>
             </div>
         )
-
     }
 }
 

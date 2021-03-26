30ad6ce779b24ea51bc3e5ccaa4523b867a1be20
monteromario <59968975+monteromario@users.noreply.github.com>
2021-03-26 07:42:46 +0100

    State update

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,67 +8,109 @@ export default class Main extends Component {
     state = {
         foods: [...foods],
         modalIsActive: false,
+        newName: '',
+        newCalories: 0,
+        newImage: '',
     }
-
-    foods = this.state.foods
 
     openModal = () => {
         this.setState({modalIsActive: true})
-        // let buttonBtn = document.getElementById('modalBtn')
-        // buttonBtn.classList.add("is-active");
     }
 
     closeModal = () => {
         this.setState({modalIsActive: false})
-        // let buttonBtn = document.getElementById('modalBtn')
-        // buttonBtn.classList.remove("is-active");
     }
 
-    addFood = () => {
-        let foodName = document.getElementById('inputName').value
-        let foodCalories = document.getElementById('inputCalories').value
-        let foodImage = document.getElementById('inputImage').value
+    handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
 
-        this.setState( (prev) => ({
-            foods: [...prev.foods,{
-                "name": foodName,
-                "calories": foodCalories,
-                "image": foodImage,
+        this.setState({
+        [name]: value
+        });
+    }
+
+    submit = (e) => {
+
+        e.preventDefault();
+
+        // let prevFoods = [...this.state.foods];
+
+        // let newFoods = [...prevFoods, {
+        //         "name": this.state.newName,
+        //         "calories": this.state.newCalories,
+        //         "image": this.state.newImage,
+        //         "quantity": 0
+        //         }]
+
+        // this.setState({
+        //     foods: newFoods,
+        //     modalIsActive: false,
+        //     newName: '',
+        //     newCalories: 0,
+        //     newImage: '',
+        //     })
+        
+        this.setState((prevState => {
+            return {
+            foods: [...prevState.foods, {
+                "name": this.state.newName,
+                "calories": this.state.newCalories,
+                "image": this.state.newImage,
                 "quantity": 0
                 }],
             modalIsActive: false,
+            newName: '',
+            newCalories: 0,
+            newImage: '',
+            }
         }))
     }
 
     render() {
         return (
-        <div>
-            <button className="button is-info modal-button" data-target="modal" aria-haspopup="true" onClick={() => this.openModal()}>Add food</button>
+        <div className="mb-5">
+            <button className="button is-info modal-button mb-3" data-target="modal" aria-haspopup="true" onClick={() => this.openModal()}>Add food</button>
             <div className={ this.state.modalIsActive ? "modal is-active" : "modal" }>
                 <div className="modal-background">    
                 </div>
                 <div className="modal-content">
                     <p className="pb-2">Add food</p>
-                    <div className="field">
-                        <label className="label has-text-white has-text-left">Name</label>
-                        <div className="control">
-                            <input className="input" type="text" placeholder="Food name" id="inputName"/>
+                    <form onSubmit={this.submit}>
+                        <div className="field">
+                            <label className="label has-text-white has-text-left">Name</label>
+                            <div className="control">
+                                <input
+                                    className="input"
+                                    name="newName"
+                                    type="text"
+                                    value={this.state.newName}
+                                    onChange={this.handleInputChange} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="field">
-                        <label className="label has-text-white has-text-left">Calories</label>
-                        <div className="control">
-                            <input className="input" type="number" placeholder="Calories" id="inputCalories"/>
+                        <div className="field">
+                            <label className="label has-text-white has-text-left">Calories</label>
+                            <div className="control">
+                                <input className="input"
+                                name="newCalories"
+                                type="number"
+                                value={this.state.newCalories}
+                                onChange={this.handleInputChange} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="field">
-                        <label className="label has-text-white has-text-left">Image</label>
-                        <div className="control">
-                            <input className="input" type="text" placeholder="Image url" id="inputImage"/>
+                        <div className="field">
+                            <label className="label has-text-white has-text-left">Image</label>
+                            <div className="control">
+                                <input className="input"
+                                    name="newImage"
+                                    type="text"
+                                    value={this.state.newImage}
+                                    onChange={this.handleInputChange} />
+                            </div>
                         </div>
-                    </div>
-                    <button className="button is-info modal-button" data-target="modal" onClick={() => this.addFood()}>Add food</button>
-
+                        <button className="button is-info">Add food</button>
+                    </form>
                 </div>
                 <button className="modal-close is-large" aria-label="close" onClick={() => this.closeModal()}></button>
             </div>

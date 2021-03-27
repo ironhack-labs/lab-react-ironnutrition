d72f2164fcdeaf9ec0a7826bc3b696c223d60c5f
luisga32 <luisga32@hotmail.com>
2021-03-27 19:03:58 +0100

    Iteration 3 done

--- src/components/Addfood.js
+++ src/components/Addfood.js
@@ -1,21 +1,51 @@
 
-import {Component} from "react";
+import { Component } from "react";
+
 export default class AddFood extends Component {
 
     state = {
-        name:"",
+        name: "",
         image: "",
         calories: 0,
     };
+    onChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+    onSubmitClick = () => {
+        this.props.addFoodSubmit(this.state)
+
+
+    }
+
+    render() {
+        return (
+            <div className="Addfood">
+                <label className="label">Name: </label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Text input" name="name" value={this.state.name} onChange={this.onChange} />
+                </div>
+                <label className="label">Calories: </label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Text input" name="calories" value={this.state.calories} onChange={this.onChange} />
+                </div>
+                <label className="label">Image: </label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Text input" name="image" value={this.state.image} onChange={this.onChange} />
+                </div>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button className="button is-link" onClick={this.onSubmitClick}>
+                            Submit
+                         </button>
+                    </div>
+                </div>
+
+            </div>
+        )
+    };
+
 
 };
 
 
-onChange = (event) =>{
-    this.setState({[event.target.name]: event.target.value})
-}
-onSubmitClick = () =>{
-
-    this.props.onSubmit
-}
 

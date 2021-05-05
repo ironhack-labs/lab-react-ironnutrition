f1e52a047f3b84d61230d750275d2dc80d062a10
Esther Perez Serrajordi <estherperezserrajordi@MacBook-Pro-de-Esther.local>
2021-05-05 16:06:08 +0200

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
+import FoodBox from "../src/Components/FoodBox/FoodBox";
 
 function App() {
   return (
     <div className="App">
-      
+    <h1>IronNutrition</h1>
+    <FoodBox />
     </div>
   );
 }

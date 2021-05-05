0557e546cb61a052f0a95cb70d2da09a1671b065
Pablo del Barrio <pablodbb@gmail.com>
2021-05-05 22:19:04 +0200

    Iteration 3 almost done

--- src/App.js
+++ src/App.js
@@ -3,29 +3,24 @@ import foodList from './foods.json';
 import Foodbox from './components/FoodBox/FoodBox';
 import 'bulma/css/bulma.css';
 import './App.css';
-
+import Form from './components/Form/Form';
 
 class App extends React.Component {
   state = {
     foods: foodList 
   }
-  
-  addFood = () => {
+   
+  addFood = (food) => {
     const arrayCopy = [ ...this.state.foods ];
-    arrayCopy.push({ id: this.state.length, title: "Nueva película", year: "1988", runtime: "92", genres: ["Comedy", "Fantasy"], director: "Tim Burton", actors: "Alec Baldwin, Geena Davis, Annie McEnroe, Maurice Page", plot: "A couple of recently deceased ghosts contract the services of a \"bio-exorcist\" in order to remove the obnoxious new owners of their house.", posterUrl: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwODE3MDE0MV5BMl5BanBnXkFtZTgwNTk1MjI4MzE@._V1_SX300.jpg" });
-
+    arrayCopy.push(food);
     this.setState({ foods: arrayCopy });
   }
 
   displayFoods = () => {
     return this.state.foods.map((singleFood) => {
-
-      return (
-        
-          <Foodbox {...singleFood}/>
-        
+      return (        
+          <Foodbox {...singleFood}/>        
       )
-
     })
   }
 
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -14,11 +14,59 @@ export default class Form extends Component {
             }
         }
     }
+     handleSubmit(event){
+         event.preventDefault();
+         
+             this.props.addFood(this.state.fields);
+             this.setState({
+                fields:{
+                    name: "",
+                    calories: "",
+                    image: "",
+                    quantity: ""
+                }
+             })
+         
+     }
+    
+    handleChange(event){
+        const { name, value } = event.target;
+        this.setState({
+            fields: {
+                ...this.state.fields,
+                [name]: value
+            }
+        });
+    }
+    //TODO:TODO:
+    // isValid(){
+    //     const { errors } = this.state;
+    //     return !Object.keys(errors).some(key => errors[key]);
+    // }
 
-
-
-
-
-
-
+    render(){
+        const { fields, errors } = this.state;
+        return (
+          <form onSubmit={(e) => this.handleSubmit(e)} style={{ marginBottom: 50 }} >
+            <div className="form-item">
+              <label htmlFor="name">Name: </label>
+              <input placeholder="Name.." type="text" name="name" value={fields.name} onChange={(e) => this.handleChange(e)} />              
+            </div>
+            <div className="form-item">
+              <label htmlFor="calories">Calories: </label>
+              <input type="text" name="calories" value={fields.calories} onChange={(e) => this.handleChange(e)} />              
+            </div>
+            <div className="form-item">
+              <label htmlFor="image">Image: </label>
+              <input type="text" name="image" value={fields.image} onChange={(e) => this.handleChange(e)} />              
+            </div>
+            <div className="form-item">
+              <label htmlFor="quantity">Quantity: </label>
+              <input type="text" name="quantity" value={fields.quantity} onChange={(e) => this.handleChange(e)} />              
+            </div>
+            <button type="submit">Create food</button>
+            {/* <button disabled={!this.isValid()} type="submit">Create food</button> */}
+          </form>
+        ) 
+    }
 }

755a3bc24b11bf157911016088157f3316249f86
Esther Perez Serrajordi <estherperezserrajordi@MacBook-Pro-de-Esther.local>
2021-05-05 16:25:38 +0200

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import 'bulma/css/bulma.css';
-import foods from './foods.json';
+import foodsData from './foods.json';
 import FoodBox from "../src/Components/FoodBox/FoodBox";
 
-function App() {
-  return (
-    <div className="App">
-    <h1>IronNutrition</h1>
-    <FoodBox />
-    </div>
-  );
-}
+const foodList = [...foodsData]
 
-export default App;
+export default class App extends Component {
+
+  state = { food: foodList };
+
+  displayFood = () =>{
+    return this.state.food.map((food) =>{
+      return(
+        <FoodBox {...food} key={food.id} />
+      )
+    })
+  }
+
+  render() {
+
+    return (
+
+      <div className="App">
+      <h1>IronNutrition</h1>
+      <div>
+        { this.displayFood() }
+      </div>
+
+      </div>
+    );
+  }
+}

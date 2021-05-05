b096efc38d5ed32c6a38c3788d46e7c7e72083db
Pablo del Barrio <pablodbb@gmail.com>
2021-05-05 18:42:53 +0200

    Iteration 3 half done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ class App extends React.Component {
   state = {
     foods: foodList 
   }
+  
+  addFood = () => {
+    const arrayCopy = [ ...this.state.foods ];
+    arrayCopy.push({ id: this.state.length, title: "Nueva película", year: "1988", runtime: "92", genres: ["Comedy", "Fantasy"], director: "Tim Burton", actors: "Alec Baldwin, Geena Davis, Annie McEnroe, Maurice Page", plot: "A couple of recently deceased ghosts contract the services of a \"bio-exorcist\" in order to remove the obnoxious new owners of their house.", posterUrl: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwODE3MDE0MV5BMl5BanBnXkFtZTgwNTk1MjI4MzE@._V1_SX300.jpg" });
+
+    this.setState({ foods: arrayCopy });
+  }
 
   displayFoods = () => {
     return this.state.foods.map((singleFood) => {
@@ -25,9 +32,13 @@ class App extends React.Component {
   render() {
     return (
       <div>
-      {
-      this.displayFoods()
-      }
+      <Form addFood={(food) => this.addFood(food)} />
+      <button onClick={() => this.addFood()}>Add food</button>
+        <div>
+        {
+        this.displayFoods()
+        }
+        </div>
       </div>
     )
   }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import "./Form.css";
+
+export default class Form extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            fields: {
+                name: "",
+                calories: "",
+                image: "",
+                quantity: ""
+            }
+        }
+    }
+
+
+
+
+
+
+
+}

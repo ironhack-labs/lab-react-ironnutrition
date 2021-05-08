d9ebbeb9a3060bb020fa9f4b284cedeec8b92c00
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-05-08 10:15:28 -0400

    iteration 2 DONE

--- src/App.js
+++ src/App.js
@@ -4,10 +4,54 @@ import 'bulma/css/bulma.css';
 import './App.css';
 import FoodBox from './components/FoodBox';
 
+function Boxs(){
+  return(
+    <div>
+    {
+      foods.map((food, index) => {
+        return (
+          <div className="box">
+            <article className="media">
+                <div className="media-left">
+                <figure className="image is-64x64">
+                    <img src={food.image} />
+                </figure>
+                </div>
+                <div className="media-content">
+                <div className="content">
+                    <p>
+                    <strong>{food.name}</strong> <br />
+                    <small>{food.calories}</small>
+                    </p>
+                </div>
+                </div>
+                <div className="media-right">
+                <div className="field has-addons">
+                    <div className="control">
+                    <input className="input" type="number" value="1" />
+                    </div>
+                    <div className="control">
+                    <button className="button is-info">
+                        +
+                    </button>
+                    </div>
+                </div>
+                </div>
+            </article>
+          </div>
+        )
+      })
+    }
+      </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
       <FoodBox/>
+      <Boxs/>
     </div>
   );
 }

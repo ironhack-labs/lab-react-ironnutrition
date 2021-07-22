6ff2580cd49e51a507426fa01c3066e720bacee7
Victor Campos <vccacampos@outlook.com>
2021-07-22 20:27:31 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import Main from './components/Main.js';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>IronNutrition</h1>  
       </header>
+    <div className='App'>
+     
+
+      <Main/>
+    </div>
     </div>
   );
-}
 
+}
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,37 @@
+import React, { Component }  from 'react';
+import foods from '../foods.json'
+import FoodBox from "./FoodBox";
+import AddFood from './AddFood';
+import './Main.css';
+
+class Main extends Component {
+    state = {
+        foodList: foods,
+    }
+
+    handleAddFood = (food) => {
+        const newFoods = [...this.state.foodList, food];
+        this.setState({
+          foodList: newFoods,
+        });
+    };
+
+
+    render() {
+        return (
+            <>
+            <div className='form-div'>
+                <AddFood addFood={this.handleAddFood} />
+            </div>
+            <div className='food-cards'>
+                {this.state.foodList.map((item) => (
+                      <FoodBox food={item}/>
+                    )
+                )}
+            </div>
+          </>
+        )
+    }
+}
+
+export default Main; 

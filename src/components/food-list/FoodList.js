bb11e2a81be341369b82a0a4a81ae99bfaa85a2d
Daniel Abad Noriega <Daniel.Abad.Noriega@gmail.com>
2021-08-19 10:03:18 +0200

    saving

--- src/components/food-list/FoodList.js
+++ src/components/food-list/FoodList.js
@@ -1,62 +1,43 @@
 import React from 'react';
-import foodsData from '../../data/foods.json'
-import Foodbox from "../food-box/FoodBox";
-import uuid from 'uuid'
-
+import foodsData from '../../data/foods.json';
+import Foodbox from '../food-box/FoodBox';
 
 class FoodList extends React.Component {
+  state = {
+    foods: [],
+  };
 
-    state= {
-        foods: [],
-    }
+  componentDidMount() {
+    this.setState({
+      foods: foodsData.map((food, i) => {
+        return {
+          name: food.name,
+          calories: food.calories,
+          image: food.image,
+          quantity: food.quantity,
+          id: i,
+        };
+      }),
+    });
+  }
 
-    componentDidMount () {
-        this.setState({ 
-/*             foods: foodsData.map((food) => {
-                food: {
-                    food.name;
-                    food.calories;
-                    food.image;
-                    food.quantity;
-                    food['id']= uuid.v4().toString()
-                }
-                return food;
-            })  */
-            foods: foodsData.map((food) => 
-            {return food.name,
-            food.calories,
-            food.image,
-            food.quantity,
-            food['id']= uuid.v4().toString()}) 
-            
-            /* tweetsDB.push({
-                id: uuid.v4().toString(),
-                user: user,
-                message: message,
-                createdAt: new Date()
-              }) */
-        })
-    }
+  handleAddFood(id) {
+    console.log(id);
+  }
 
-    handleAddFood(id) {
+  render() {
+    const { foods } = this.state;
 
-        console.log(id)
-    }
-
-    render() {
-        const { foods } = this.state;
-
-        return(
-            <div className="row mb-2">
-                <div className="col-9">
-                {foods.map( food => 
-                    <Foodbox {...food } onAddFood={(id) => this.handleAddFood(id)}/>
-                )}
-
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="row mb-2">
+        <div className="col-9">
+          {foods.map((food) => (
+            <Foodbox {...food} onAddFood={(id) => this.handleAddFood(id)} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FoodList;

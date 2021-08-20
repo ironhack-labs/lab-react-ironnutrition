2011e179c75e1bc8b6d7014775a0797b0435ab00
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-20 19:36:39 +0200

    Add folder with the component structure examples

---
+++ style-guide/FoodBox.example.js
@@ -0,0 +1,28 @@
+// Style Guide:
+// In this file you can find a reference example of the structure
+// that the component should have.
+// Remember to import Ant Design components before using them.
+import { Card, Col, Button } from "antd";
+
+// Iteration 2
+function FoodBox(props) {
+  return (
+    <Col>
+      
+      <Card title={"SHOW_FOOD_NAME_HERE"} style={{ width: 230, height: 300, margin: 10 }} >
+        
+        <img src={"SHOW_FOOD_IMAGE_HERE"} alt="food" height={60} />
+        <p> Calories: SHOW_FOOD_CALORIES_HERE </p>
+        <p> Servings: <b>SHOW_FOOD_SERVINGS_HERE</b> </p>
+        <p>
+          <b>Total Calories: FOOD_SERVINGS * FOOD_CALORIES</b> kcal
+        </p>
+        <Button type="primary" onClick={() => { }}> Delete </Button>
+        
+      </Card>
+
+    </Col>
+  );
+}
+
+export default FoodBox;
---
+++ style-guide/Search.example.js
@@ -0,0 +1,20 @@
+// Style Guide:
+// In this file you can find a reference example of the structure
+// that the component should have.
+// Remember to import Ant Design components before using them.
+import { Divider, Input } from "antd";
+
+
+// Iteration 5
+function Search(props) {
+  return (
+    <>
+      <Divider>Search</Divider>
+
+      <label>Search</label>
+      <Input value={undefined} type="text" onChange={() => {}} />
+    </>
+  );
+}
+
+export default Search;

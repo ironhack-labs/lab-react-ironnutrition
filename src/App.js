b85d7a55f74e1553ffe75a7c45edff07bbceaa7e
Israel Zambrana <zambrana90@gmail.com>
2021-09-01 21:38:21 +0200

    iteration 1&2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import foods from './foods.json';
+
+import FoodBox from './components/FoodBox/FoodBox';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    foods: foods,
+  };
+  render() {
+    return (
+      <div className="App container">
+        {this.state.foods.map((food) => {
+          return (<FoodBox {...food}/>);
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FoodBox/FoodBox.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './FoodBox.css';
+
+export default function FoodBox({ name, calories, image, quantity }) {
+  return (
+    <div className="FoodBox">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={image} alt={name}/>
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{name}</strong> <br />
+              <small>{calories} cal</small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input className="input" type="number" value="1" />
+            </div>
+            <div className="control">
+              <button className="button is-info">+</button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+}

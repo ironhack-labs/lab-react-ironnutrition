688c02692debc818357bd1becaebf778bda5983b
Marcos Salvo <marcossalvo95@gmail.com>
2021-09-03 18:24:31 +0200

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import FoodBoox from "./components/FoodBox/FoodBox"
 import 'bulma/css/bulma.css';
 import './App.css';
 
-import foods from './foods.json';
-
 function App() {
   return (
     <div className="App">
       <h1>Iron Nutrition</h1>
-      <FoodBoox />
+      <FoodBoox food="Salad" />
     </div>
   );
 }
--- src/components/FoodBox/FoodBox.js
+++ src/components/FoodBox/FoodBox.js
@@ -1,34 +1,44 @@
 import React,{ Component } from "react"
+import foodsArr from '../../foods.json';
 
 import "./FoodBox.css"
 
 class FoodBox extends Component {
     constructor(props) {
         super(props)
-        
+
+        this.state = {
+            quantity: 0,
+        }
     }
 
     render() {
+        const { food } = this.props
+        let selectedFood = foodsArr.filter( (foodItem) => {
+            return foodItem.name === food
+        })
+        console.log(selectedFood)
         return (
             <div className="box">
                 <article className="media">
                     <div className="media-left">
                     <figure className="image is-64x64">
-                        <img src="https://i.imgur.com/eTmWoAN.png" />
+                        <img src={selectedFood[0].image} />
                     </figure>
                     </div>
                     <div className="media-content">
                     <div className="content">
                         <p>
-                        <strong>Pizza</strong> <br />
-                        <small>400 cal</small>
+                        
+                        <strong>{selectedFood[0].name}</strong> <br />
+                        <small>{selectedFood[0].calories}</small>
                         </p>
                     </div>
                     </div>
                     <div className="media-right">
                     <div className="field has-addons">
                         <div className="control">
-                        <input className="input" type="number" value="1" />
+                        <input className="input" type="number" value={this.state.quantity} />
                         </div>
                         <div className="control">
                         <button className="button is-info">

ca1aaeb6a7ca03a85d4e429c0cc9f7be8b9b7ba3
Marcos Salvo <marcossalvo95@gmail.com>
2021-09-03 18:42:21 +0200

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import FoodBoox from "./components/FoodBox/FoodBox"
+import FoodBox from "./components/FoodBox/FoodBox"
 import 'bulma/css/bulma.css';
 import './App.css';
+import foods from './foods.json';
 
 function App() {
   return (
     <div className="App">
       <h1>Iron Nutrition</h1>
-      <FoodBoox food="Salad" />
+      {foods.map(foodItem => <FoodBox food={foodItem.name} />)}
     </div>
   );
 }

45c1b3b34cc157e33716c1da5397cbe23986eba6
Paloma Borro <palomabs93@gmail.com>
2021-09-07 19:39:07 +0200

    iteración 3 con error al añadir comida

--- src/components/newfood/NewFood.jsx
+++ src/components/newfood/NewFood.jsx
@@ -2,42 +2,66 @@ import React from 'react';
 import './NewFood.css';
 import 'bulma/css/bulma.css';
 
+const initialState = {
+    showForm: false,
+    image: "",
+    name: "",
+    calories: 0,
+}
+
 export default class NewFood extends React.Component {
-    state = {
-        name: '',
-        calories: '',
-        image: ''
-    }
+    state = {...initialState}
+
+    onChange = (event) => {
+        const { name, value, type } = event.target;
+    
+        this.setState({
+          [name]: type === 'number' ? Number(value) : value,
+        });
+      };
+
+    toggleShowForm = () => {
+        this.setState((prevState) => ({ showForm: !prevState.showForm }))
+      }
 
     onSubmit = (event) => {
         event.preventDefault();
-    }
-
-    // onChange()
+        if (!this.state.name || !this.state.calories || !this.state.image) {
+          return
+        }
+        this.props.onAddFood(this.state);
+        this.setState({ ...initialState });
+    };
 
     render() {
         return(
-            <form className="NewFood" onSubmit={this.onSubmit}>
-                <input 
-                type="text"
-                value={this.state.name}
-                onChange={this.onChange}
-                name='name'
-                />
-                <input 
-                type="number"
-                value={this.state.calories}
-                onChange={this.onChange}
-                name='calories'
-                />
-                <input 
-                type="text"
-                value={this.state.image}
-                onChange={this.onChange}
-                name='image'
-                />
-                <button>Submit</button>
-            </form>
+            <div className="NewFood">
+                <button onClick={this.toggleShowForm}>Show form</button>
+                {this.state.showForm
+                    && (
+                    <form onSubmit={this.onSubmit}>
+                    <input 
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                    name='name'
+                    />
+                    <input 
+                    type="number"
+                    value={this.state.calories}
+                    onChange={this.onChange}
+                    name='calories'
+                    />
+                    <input 
+                    type="text"
+                    value={this.state.image}
+                    onChange={this.onChange}
+                    name='image'
+                    />
+                    <button type="submit">Add New Food</button>
+                    </form>
+                    )}
+            </div>
         )
     }
 }

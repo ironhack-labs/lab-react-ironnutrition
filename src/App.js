4d288bd20bef49e44fba44f7bf98f7e73c2f821b
sarebro <sarebro77@gmail.com>
2021-09-09 17:44:10 -0500

    lab complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
@@ -6,9 +6,51 @@ import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 
 function App() {
+	const [ showForm, setShowForm ] = useState(false);
+	const [ theFood, setTheFood ] = useState('');
+	const [ copyFood, setFood ] = useState(foods);
+
+	const ShowFoods = () => {
+		return copyFood.map((foodItem) => {
+			return <FoodBox key={foodItem.name} food={foodItem} />;
+		});
+	};
+
+	const handleChange = (e) => {
+		setTheFood(e.target.value);
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let newFoodArray = [ ...copyFood ];
+		newFoodArray.push({ name: theFood });
+		setFood(newFoodArray);
+	};
+
+	const mealSearch = (e) => {
+		//No Bruno
+		let newFoodArray = [ ...foods ];
+		const newMealSearch = newFoodArray.filter((newFoodMeal) => {
+			return newFoodMeal.name.toLowerCase().includes(e.target.value.toLowerCase());
+		});
+		setFood(newMealSearch);
+	};
+
 	return (
 		<div className="App">
-			<FoodBox food={foods} />
+			<input placeholder="search" onChange={mealSearch} />
+			<br />
+
+			<button onClick={() => setShowForm(!showForm)}>Add New Foods</button>
+			{showForm ? (
+				<form onSubmit={handleSubmit}>
+					<input onChange={handleChange} placeholder="Add FoodName" />
+					<button>Add Food</button>
+				</form>
+			) : (
+				'Not Showing!'
+			)}
+
+			<ShowFoods />
 		</div>
 	);
 }

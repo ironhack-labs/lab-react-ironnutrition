ebc641af3d797284635931997739be9f6f832f90
pedroneto2 <pedro.neto992@gmail.com>
2021-09-11 14:52:58 -0300

    form validation added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,67 +1,198 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Form.css';
 
-const handleSubmit = (
-  setHide,
-  foodList,
-  setFoodList,
-  name,
-  calories,
-  link,
-  e
-) => {
+const handleSubmit = (error, setHide, foodList, setFoodList, value, e) => {
   e.preventDefault();
-  if (foodList.some((food) => food.name === name)) {
+
+  const invalidForm =
+    Object.keys(error).some((key) => error[key]) ||
+    Object.keys(value).some((key) => !value[key]);
+
+  if (invalidForm) {
+    return;
+  }
+
+  if (foodList.some((food) => food.name === value.name)) {
     window.alert('THIS FOOD ALREAD EXIST!');
-  } else if (!calories) {
-    window.alert("This food don't have calories...");
   } else {
-    foodList.push({ name: name, calories: calories, image: link, quantity: 0 });
-    setFoodList([...foodList]);
+    setFoodList([...foodList, { ...value, quantity: 0 }]);
     setHide(true);
   }
 };
 
+const validateForm = (error, setError, value) => {
+  const urlRegexMatch =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const formFieldValidations = {
+    name: [
+      {
+        validation: value.name.length < 1,
+        errorMessage: 'Required Field!',
+      },
+      {
+        validation: value.name.length < 3,
+        errorMessage: 'Minimum of 3 characters!',
+      },
+      {
+        validation: value.name.length > 50,
+        errorMessage: 'Maximum of 50 characters!',
+      },
+    ],
+    calories: [
+      {
+        validation: value.calories < 1,
+        errorMessage: 'Required Field!',
+      },
+      {
+        validation: value.calories > 10000,
+        errorMessage: 'Maximum calories of 10.000!',
+      },
+    ],
+    image: [
+      {
+        validation: value.image.length < 1,
+        errorMessage: 'Required Field!',
+      },
+      {
+        validation: value.image.length < 3,
+        errorMessage: 'Minimum of 3 characters!',
+      },
+      {
+        validation: value.image.length > 100,
+        errorMessage: 'Maximum of 100 characters!',
+      },
+      {
+        validation: !value.image.match(urlRegexMatch),
+        errorMessage: 'Invalid URL!',
+      },
+    ],
+  };
+
+  const newErrors = { name: '', calories: '', image: '' };
+
+  Object.keys(formFieldValidations).forEach((key) => {
+    formFieldValidations[key].find((verification) => {
+      if (verification.validation) {
+        newErrors[key] = verification.errorMessage;
+        return true;
+      } else return false;
+    });
+  });
+
+  Object.assign(error, newErrors);
+
+  setError({ ...error });
+};
+
+const handleChange = (e, stateValue, setValue) => {
+  const { name, value } = e.target;
+  setValue({ ...stateValue, [name]: value });
+};
+
+const handleBlur = (e, touched, setTouched) => {
+  const { name } = e.target;
+  setTouched({ ...touched, [name]: true });
+};
+
+const handleSpanClasses = (error, touch) => {
+  if (touch && error) {
+    return 'error-text';
+  } else if (touch && !error) {
+    return 'correct-text';
+  } else {
+    return ' hide-text';
+  }
+};
+
+const handleInputClasses = (error, touch) => {
+  if (touch && error) {
+    return ' error-field';
+  } else if (touch && !error) {
+    return ' correct-field';
+  } else {
+    return '';
+  }
+};
+
 const Form = ({ setHide, foodList, setFoodList }) => {
-  const [name, setName] = useState('');
-  const [calories, setCalories] = useState('');
-  const [link, setLink] = useState('');
+  const [value, setValue] = useState({
+    name: '',
+    calories: '',
+    image: '',
+  });
+
+  const [touched, setTouched] = useState({
+    name: false,
+    calories: false,
+    image: false,
+  });
+
+  const [error, setError] = useState({
+    name: '',
+    calories: '',
+    image: '',
+  });
+
+  useEffect(() => {
+    validateForm(error, setError, value);
+  }, [value]);
+
   return (
     <form
       className="form-container is-flex"
       onSubmit={(e) =>
-        handleSubmit(setHide, foodList, setFoodList, name, calories, link, e)
+        handleSubmit(error, setHide, foodList, setFoodList, value, e)
       }
     >
       <h2>NEW FOOD</h2>
       <label className="label is-small">Name</label>
       <input
-        label="name"
-        className="input is-small"
+        name="name"
+        className={
+          'input is-small' + handleInputClasses(error.name, touched.name)
+        }
         type="text"
         placeholder="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        value={value.name}
+        onChange={(e) => handleChange(e, value, setValue)}
+        onBlur={(e) => handleBlur(e, touched, setTouched)}
       />
+      <span className={handleSpanClasses(error.name, touched.name)}>
+        {error.name || 'Ok'}
+      </span>
       <label className="label is-small">Calories</label>
       <input
-        label="calories"
-        className="input is-small"
+        name="calories"
+        className={
+          'input is-small' +
+          handleInputClasses(error.calories, touched.calories)
+        }
         type="number"
         placeholder="Calories"
         min="0"
-        value={calories}
-        onChange={(e) => setCalories(e.target.value)}
+        max="10000"
+        value={value.calories}
+        onChange={(e) => handleChange(e, value, setValue)}
+        onBlur={(e) => handleBlur(e, touched, setTouched)}
       />
+      <span className={handleSpanClasses(error.calories, touched.calories)}>
+        {error.calories || 'Ok'}
+      </span>
       <label className="label is-small">Image Link</label>
       <input
-        label="image link"
-        className="input is-small"
+        name="image"
+        className={
+          'input is-small' + handleInputClasses(error.image, touched.image)
+        }
         type="text"
         placeholder="Image Link"
-        value={link}
-        onChange={(e) => setLink(e.target.value)}
+        value={value.image}
+        onChange={(e) => handleChange(e, value, setValue)}
+        onBlur={(e) => handleBlur(e, touched, setTouched)}
       />
+      <span className={handleSpanClasses(error.image, touched.image)}>
+        {error.image || 'Ok'}
+      </span>
       <div className="form-buttons">
         <button className="button is-info">Add!</button>
         <button

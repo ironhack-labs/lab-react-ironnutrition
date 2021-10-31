7d2ebea9ac963490b3c547510870e382f89a0a63
CherylHuang1988 <cherylhuang1988@gmail.com>
2021-10-31 15:51:52 +0100

    Done with Iteration 2

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
         {foodList.map((food) => {
           return <FoodBox food={food} />;
         })}

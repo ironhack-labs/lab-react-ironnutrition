4f275264c9e8d8877e1024e1464b40e6c1a46e46
Dominika Lipka <dominika.lipka96@gmail.com>
2021-11-06 19:35:35 +0100

    Iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import './App.css';
 import FoodBox from './components/FoodBox'
+import AddFood from './components/AddFood';
 import foods from './foods.json';
 
 class App extends React.Component {
-
   state = {
-    foodToDisplay: foods 
-  }
+    foodToDisplay: foods,
+    formVisible: false,
+  };
 
+  handleFormVisibility = () => {
+    this.setState({ formVisible: !this.state.formVisible });
+  };
+
+  addFoodHandler = (FoodInfo) => {
+    const foodToDisplayCopy = [...this.state.foodToDisplay, FoodInfo];
+    this.setState({ foodToDisplay: foodToDisplayCopy });
+  };
 
   render() {
-  return (
-    <div>
-      <h1>Iron-Nutrition</h1>
-      <div className='foodbox-container'>
-        {this.state.foodToDisplay.map((food) => (
-          <FoodBox key={food.id} {...food} />
-        ))}
+    return (
+      <div>
+        <h1>Iron-Nutrition</h1>
+        <div>
+          <button onClick={this.handleFormVisibility}>Add new food</button>
+          {this.state.formVisible === true && <AddFood addFoodHandler={this.addFoodHandler}/>}
+        </div>
+        <div className="foodbox-container">
+          {this.state.foodToDisplay.map((food) => (
+            <FoodBox key={food.id} {...food} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
   }
-
 }
 
 export default App;
---
+++ src/components/AddFood.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+class AddFood extends React.Component {
+  state = {
+    name: '',
+    calories: '',
+    image: '',
+  };
+
+  handleChange = (event) => {
+    const nameOfTheField = event.target.name;
+    this.setState({ [nameOfTheField]: event.target.value });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const FoodInfo = {
+      name: this.state.name,
+      calories: this.state.calories,
+      image: this.state.image,
+    };
+
+    this.props.addFoodHandler(FoodInfo);
+
+    this.setState({
+      name: '',
+      calories: '',
+      image: '',
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Add new food here:</h3>
+        <form onSubmit={this.handleFormSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Calories:
+            <input
+              type="text"
+              name="calories"
+              value={this.state.calories}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Image:
+            <input
+              type="text"
+              name="image"
+              value={this.state.image}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddFood;

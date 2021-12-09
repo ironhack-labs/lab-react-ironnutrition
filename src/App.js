4259ec123a3cf7121ae7fd9e4fdf00309ab97c47
rodrigosamaniego <rodrigosamaniegopiza@gmail.com>
2021-12-08 20:30:08 -0600

    iteración 1 y 2 completas. Iteración 3 casi terminada

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import 'bulma/css/bulma.css';
+
+import React, { useState } from 'react';
+import foods from './foods.json'
+import Foodbox from './components/FoodBox';
+
 import './App.css';
 
 function App() {
+  //A. definir nueva comida
+  const [comida, setFood] = useState({
+    name: "",
+    calories: 0,
+    image: "",
+    quantity: 0,
+
+  })
+
+  //Funciones del componente
+  const capturarDatos = (e) => {
+    return setFood({
+      ...comida,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  //CRUD
+  //Crear
+  const addFood = (e) => {
+    e.preventDefault()
+    setFood({
+      name: "",
+      calories: 0,
+      image: "",
+      quantity: 0,
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h4>Add food</h4>
+      <form onSubmit={(event) => { addFood(event) }}>
+        <input
+          name="name"
+          value={comida.name}
+          onChange={(event) => { capturarDatos(event) }}
+        />
+        <input
+          name="calories"
+          value={comida.calories}
+          onChange={(event) => { capturarDatos(event) }}
+        />
+        <input
+          name="image"
+          value={comida.image}
+          onChange={(event) => { capturarDatos(event) }}
+        />
+        <input
+          name="quantity"
+          value={comida.quantity}
+          onChange={(event) => { capturarDatos(event) }}
+        />
+        <br />
+        <button>Add this food</button>
+      </form>
+
+      <div className="App">
+        <Foodbox data={foods} />
+
+      </div>
+
+    </>
   );
 }
 

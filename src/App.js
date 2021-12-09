9a304462ade6c0cdb81917be313e15127a0011c0
Edgar Uco <edgarin@gmail.com>
2021-12-08 21:32:36 -0600

    lab-react-ironnutrition it 2 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FoodBox from './components/FoodBox';
+import foods from './foods.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {foods.map((e) => {
+        return (
+          <FoodBox
+            name={e.name}
+            calories={e.calories}
+            image={e.image}
+            quantity={e.quantity}
+          />
+        );
+      })}
+    </>
   );
 }
 

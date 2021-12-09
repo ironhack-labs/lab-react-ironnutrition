8dc1a2a949d286bc786efec5c91c0ef4f81ee8c8
Hector Biminchumo <hectorbiminchumo@gmail.com>
2021-12-08 21:58:26 -0500

    se acomoda las imagenes

--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,62 +1,52 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+const FoodBox = (props) => {
+  const name = props.name;
+  const calories = props.calories;
+  const image = props.image;
+  const quantity = props.quantity;
 
-const FoodBox= (props)=>{
-    const name=props.name
-    const calories = props.calories
-    const image= props.image
-    const quantity= props.quantity
-
-
-    return(
-        <>
-        
-        <div className="box">
-  <article className="media">
-    <div className="media-left">
-      <figure className="image is-64x64">
-        <img src={image}/>
-      </figure>
-    </div>
-    <div className="media-content">
-      <div className="content">
-        <p>
-          <strong>{name}</strong> <br />
-          <small>{calories}</small>
-        </p>
+  return (
+    <>
+        <div id="direct">
+      <ListGroup>
+        <ListGroup.Item>
+          <div className="box">
+            <article className="media">
+              <div className="media-left">
+                <figure className="image is-64x64">
+                  <img id="img" src={image} />
+                </figure>
+              </div>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>{name}</strong> <br />
+                    <small>{calories}</small>
+                  </p>
+                </div>
+              </div>
+              <div className="media-right">
+                <div className="field has-addons">
+                  <div className="control">
+                    <input className="input" type="number" value={quantity} />
+                  </div>
+                  <div className="control">
+                    <Button variant="primary" className="button is-info">
+                      +
+                    </Button>{' '}
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+        </ListGroup.Item>
+      </ListGroup>
       </div>
-    </div>
-    <div className="media-right">
-      <div className="field has-addons">
-        <div className="control">
-          <input className="input" type="number" value="1" />
-        </div>
-        <div className="control">
-          <button className="button is-info">
-            +
-          </button>
-        </div>
-      </div>
-    </div>
-  </article>
-</div>
+    </>
+  );
+};
 
-
-
-
-
-
-
-
-
-
-
-            </>
-    )
-
-
-
-
-}
-
-
-export default FoodBox
+export default FoodBox;

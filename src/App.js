a1f8d63b9da64e495f4ac1f244cdc147fb9a7eb1
Philipp Kastl <picastle@posteo.de>
2022-01-21 22:22:25 +0100

    iteration 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import FoodBox from './Components/FoodBox';
+import food from './foods.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [foodList, setFoodList] = React.useState(food);
+
+  //iterate through list of food
+  const foodListToJSX = foodList.map((entry) => (
+    <FoodBox name={entry.name} image={entry.image} calories={entry.calories} />
+  ));
+
+  return <div className="App">{foodListToJSX}</div>;
 }
 
 export default App;

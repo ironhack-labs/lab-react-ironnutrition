d9e6ad8884db03d5ae14d744085030e498ec46ff
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-21 18:36:04 +0100

    iteration 1

---
+++ src/components/foodbox/Foodbox.js
@@ -0,0 +1,17 @@
+import React from "react";
+import 'bulma/css/bulma.css';
+
+
+function Foodbox (props) {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+
+export default Foodbox

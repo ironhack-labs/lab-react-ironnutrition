9117a656ccf34222dd4ffd286797e1d70bec3fc8
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-21 18:42:40 +0100

    iteration 1 fix

--- src/components/foodbox/Foodbox.js
+++ src/components/foodbox/Foodbox.js
@@ -1,17 +1,38 @@
 import React from "react";
-import 'bulma/css/bulma.css';
+import "bulma/css/bulma.css";
 
-
-function Foodbox (props) {
-
-    return (
-        <div>
-            
-        </div>
-    )
+function Foodbox(props) {
+  return (
+    <div>
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src="https://i.imgur.com/eTmWoAN.png" alt="pizza" />
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>Pizza</strong> <br />
+                <small>400 cal</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" type="number" value="1" />
+              </div>
+              <div className="control">
+                <button className="button is-info">+</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  );
 }
 
-
-
-
-export default Foodbox
+export default Foodbox;

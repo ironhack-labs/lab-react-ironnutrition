d19a6fc6993d0cdf4cac7a69cbc43f7649f6817c
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-01-24 03:16:22 +0100

    Done

--- src/components/TodayFood.js
+++ src/components/TodayFood.js
@@ -4,9 +4,13 @@ const TodayFood = ({ todayFood }) => {
   return (
     <>
       <h5 className="title is-5">Today's Food</h5>
-      {todayFood.map((food) => (<li> {food.name}</li>))}
+      {todayFood.map(({ name, calories, quantity }) => (
+        <li>
+          {quantity} {name} : {quantity * calories}
+        </li>
+      ))}
+      <h6 className="title is-6">Total Calories: {todayFood.reduce((acc, {calories, quantity}) =>  {return acc += (calories * quantity)}, 0)}</h6>
     </>
-     
   );
 };
 

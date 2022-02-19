b1298f3bd23bd3b2876367ebcdcc1c4bc123d99d
patricia ruz corcho <patriciaruzcorcho@gmail.com>
2022-02-19 18:17:47 +0100

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import FoodBox from './components/FoodBox/FoodBox';
+import foods from './foods.json';
 
 
-function App() {
-  return (
-    <div className="container">
-      <FoodBox></FoodBox>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    foods: [],
+  };
+
+  componentDidMount() {
+    this.setState({ foods })
+  }
+
+
+  render() {
+    const { foods } = this.state
+    return (
+      <div className='w-25'>
+        {foods.map(food =>
+          <FoodBox {...food} />
+        )}
+      </div>
+
+    )
+  }
 }
 
-export default App;
+export default App

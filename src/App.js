e747adbe58dffe1aac6bc28d6304c00180d0cedc
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-23 12:47:30 +0100

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { useState } from 'react';
 import foods from './foods.json';
+import { v4 as randomId } from 'uuid';
 
 function App() {
-  return (
-    <div className="App">
-      
+const [allFoods, setFoods] = useState(foods);
+
+
+
+  return ( 
+    <div className={"App" + allFoods}>
+      <h1>Food List</h1>
+      {allFoods.map(function (foods) {
+       return (
+         <div key={randomId()}>
+           <p> {foods.name} </p>
+           <img src={foods.image} width={200} alt="food" />
+         </div>
+       );
+      }
+      )}
     </div>
   );
 }

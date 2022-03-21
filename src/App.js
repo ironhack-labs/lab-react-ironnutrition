8d4c6087b1d06a3ca03218e4c878ff740bfe7aa4
Calvin T <58273749+Calvin087@users.noreply.github.com>
2022-03-21 21:43:01 +0100

    search working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
-import foods from './foods.json';
 import 'bulma/css/bulma.css';
+import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import SearchBar from './components/SearchBar';
 
 function App() {
   const [foodList, setFoodList] = useState(foods);
-  const [showForm, setShowForm] = useState(true);
+  const [showForm, setShowForm] = useState(false);
 
-  console.log(foodList);
-  const addFoodToList = (form) => {
-    console.log(form);
-    setFoodList([...foodList, form]);
+  const addFoodToList = (formData) => {
+    setFoodList([...foodList, formData]);
+  };
+
+  const searchForFood = (e) => {
+    const { value } = e.target;
+
+    let filteredList =
+      value === ''
+        ? foods
+        : foodList.filter((el) =>
+            el.name.toLowerCase().includes(value.toLowerCase())
+          );
+
+    setFoodList(filteredList);
   };
 
   return (
     <div className="App">
       {/* show form here */}
+      <SearchBar searchForFood={searchForFood} />
+
       {showForm && <AddFoodForm addFoodToList={addFoodToList} />}
+
+      <div className="control">
+        <button
+          className="button is-info m-3"
+          onClick={() => setShowForm(!showForm)}
+        >
+          Show Form
+        </button>
+      </div>
+
       {foodList &&
         foodList.map((food, i) => {
           return (
---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+const SearchBar = ({ searchForFood }) => {
+  return (
+    <div className="box">
+      <input
+        className="input"
+        type="text"
+        placeholder="Text input"
+        onChange={(e) => {
+          searchForFood(e);
+        }}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;

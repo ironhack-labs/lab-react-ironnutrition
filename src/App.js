3ce82b996447433febb12d4243980ea9e2b8a574
Arturo Vivar <arthas1237@hotmail.com>
2022-03-22 18:10:29 +0100

    iteration3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
 import foods from './foods.json';
+import foodArr from './foods.json';
 import FoodBox from './components/FoodBox';
+import { useState } from 'react';
+import AddFood from './components/AddFood';
 
 function App() {
+  const [foods, setFoods] = useState(foodArr);
+  const [displayForm, setDisplayForm] = useState(false);
+
+  function toggleFormDisplay() {
+    setDisplayForm(!displayForm);
+  }
+
+  function addFood(newFood) {
+    setFoods([newFood, ...foods]);
+  }
+
   return (
     <div className="App">
+      <button className="button is-info m-3" onClick={toggleFormDisplay}>
+        {displayForm ? 'CloseForm' : 'AddFood'}
+      </button>
+
+      {displayForm && (
+        <AddFood addFood={addFood} toggleFormDisplay={toggleFormDisplay} />
+      )}
+
       {/* realizamos un .map de foods, añadimos componente con atributo valor food */}
-     {foods.map(food => <FoodBox food={ food } />)}
+      {foods.map((food, index) => (
+        <FoodBox key={ index } food={food} />
+      ))}
     </div>
   );
 }
---
+++ src/components/AddFood.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useState } from 'react';
+
+function AddFood({ addFood, toggleFormDisplay }) {
+  //useState ponemos el valor inicial de nuestra variable
+  //el primero es su valor "inputs", y la segunda es su funcion para modificarla o actualizarla "setInputs"
+  const [inputs, setInputs] = useState({ name: '', calories: 0, image: '' });
+
+  // documentacion para formularios https://es.reactjs.org/docs/forms.html
+  // https://bluuweb.github.io/react/formularios/
+
+
+  // acordarse siempre que lanzamos eventos (e)
+  function handleInputChange(e) {
+    const newInputs = { ...inputs };
+    newInputs[e.target.name] = e.target.value;
+    setInputs(newInputs);
+  }
+
+  function enviarDatos() {
+    addFood(inputs);
+    toggleFormDisplay();
+  }
+
+
+  //formulario basico recogido de la documentacion anteriormente mencionada
+  return (
+    <form className="row m-2" onSubmit={enviarDatos}>
+      <label>
+        <div className="col-md-3">
+          <input
+            type="text"
+            placeholder="FOOD NAME"
+            className="input"
+            onChange={handleInputChange}
+            name="name"
+            value={inputs.name}
+          ></input>
+        </div>
+      </label>
+      <label>
+        <div className="col-md-3">
+          <input
+            type="number"
+            placeholder="CALORIES"
+            className="input"
+            onChange={handleInputChange}
+            name="calories"
+            value={inputs.calories}
+          ></input>
+        </div>
+      </label>
+      <label>
+        <div className="col-md-3">
+          <input
+            type="url"
+            placeholder="URL IMAGE"
+            className="input"
+            onChange={handleInputChange}
+            name="image"
+            value={inputs.image}
+          ></input>
+        </div>
+      </label>
+
+      <button type="submit" className="button is-info">
+        Enviar
+      </button>
+    </form>
+  );
+}
+
+export default AddFood;

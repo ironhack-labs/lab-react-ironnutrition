d775b0abb7f40d8d2b9f826a49f553833866d454
Maria Gallego <mariagestionredes@gmail.com>
2022-03-23 21:14:03 +0100

    iteration 1 done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bulma/css/bulma.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(

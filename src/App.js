c27e3625d53a84a77f8aff13409c8aaa928e1c8a
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-03-23 21:29:39 +0100

    finish iteration 0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
+import FoodBox from './components/FoodBox/FoodBox.js';
 import React from 'react';
-import logo from './logo.svg';
+import foods from './foods.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+
+  };
+
+  render(){
+    return (
+      <div className="App">
+        <FoodBox />
+      </div>
+    );
+  };
+};
 
 export default App;

3e4c43bbba896463fd84e79f52adb8c25ee6da8d
Maria Gallego <mariagestionredes@gmail.com>
2022-03-24 10:38:26 +0100

    done iteration 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-/* import foods from './foods.json'; */
-import FoodBox from './components/FoodBox'
+import React, { Component } from 'react';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
-function App() {
-  return (
-    <div className="IronNutrition">
-     <FoodBox />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    foods: [...foods]
+  }
+  render() {
+
+    const { foods } = this.state;
+    
+    return (
+      <div className="IronNutrition">
+        <FoodBox foods={ foods }/>
+      </div>
+    );
+  }
 }
 
 export default App;

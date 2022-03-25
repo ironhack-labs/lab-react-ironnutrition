da75a31e9f352a632910d829f55fc0df692a2fe2
Alejandra Lopez <alejandralopez91@gmail.com>
2022-03-25 22:11:44 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React from 'react';
+import  { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import foods from "./foods.json";
@@ -8,6 +9,8 @@ import FoodBox from './components/FoodBox/FoodBox';
 class App extends Component {
   state = {
     foods: [...foods],
+    search: '',
+    sort: '',
   };
 
   onAddFood = (food) => {
@@ -15,14 +18,42 @@ class App extends Component {
       ...food,
       id: uuidv4(),
     };
-    this.setState({ foods: [newFood, ...this.state.foods]});
+    this.setState({ foods: [newFood, ...this.state.foods] });
+  };
+
+  getFoodsBySearchFilter = () => {
+    const { foods, search } = this.state;
+
+    return foods.filter((food) => {
+      if (food.name.toLowerCase().includes(search.toLowerCase())) {
+        return true;
+      }
+
+      if (food.calories.toLowerCase().includes(search.toLowerCase())) {
+        return true;
+      }
+
+      return false;
+    });
   };
 
   render() {
-    const { foods } = this.state;
+    const { foods , search} = this.state;
     return (
       <div className="App">
         <header className="App-header">
+          <div>
+            <label htmlFor="search" className="me-2">
+              Search
+            </label>
+            <input
+              id="search"
+              name="search"
+              type="text"
+              value={search}
+              onChange={this.handleOnChange}
+            />
+          </div>
           <NewForm onAddFood={this.onAddFood} />
           <FoodBox foods={foods} />
         </header>

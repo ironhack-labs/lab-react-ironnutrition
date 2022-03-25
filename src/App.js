b534a93469cff169ce8f0a0201754507d5761e0a
Pedro Alberto Baños Folcra <pedroalbertobanosfolcra@MacBook-Pro-de-Pedro-2.local>
2022-03-25 20:45:42 +0100

    iteration 4 done remain styling

--- src/App.js
+++ src/App.js
@@ -14,47 +14,79 @@ class App extends Component {
   state = {
     foods: [...foods],
     formStatus: false,
-    search:'' 
+    search: ''
   }
-  
+
+
+  handleSearchOnChange = (event) => {
+    const { value, name } = event.target
+
+    this.setState({ [name]: value })
+  }
+
+  getSearchByName = () => {
+    const { foods, search } = this.state
+
+    return foods.filter(food => {
+      if (food.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+        return true
+      }
+      return false
+    })
+  }
+
   setFormStatus = () => {
-    this.setState({formStatus : !this.formStatus});
+    this.setState({ formStatus: !this.formStatus });
   }
- 
+
 
   onAddFood = (food) => {
     const newFood = {
       ...food,
-      id: uuidv4()  
+      id: uuidv4()
     }
 
-    this.setState ({foods: [newFood,...this.state.foods],
-                    formStatus: false
-                  })
+    this.setState({
+      foods: [newFood, ...this.state.foods],
+      formStatus: false
+    })
 
   }
 
-  
-  render(){
-    const { foods, formStatus } =this.state
-    
+
+  render() {
+    const { formStatus, search } = this.state
+    const listedFood = this.getSearchByName()
+
     return (
-    
+
       <div className="App">
         <h1 className='title'>Iron Nutrition</h1>
-        <div>
-          
-        {formStatus ? 
-          <CreateFoodForm onAddFood={this.onAddFood}/>
-          : <button onClick={this.setFormStatus}
-            className="button is-info"
-          > add New Dish
-          </button>    
-        }
-        
+        <div className='column'>
+          <label className="label is-justify-content-flex-start"
+            htmlFor='search'
+          >Search</label>
+          <div className="control">
+            <input className="input"
+              type="text"
+              placeholder="Search"
+              name="search"
+              value={search}
+              onChange={this.handleSearchOnChange} />
+          </div>
         </div>
-        <div style={{width:500}} className='mx-4 mt-5'>
-             <FoodBox foods={foods} />
+        <br />
+        <div className='column'>
+          {formStatus ?
+            <CreateFoodForm onAddFood={this.onAddFood} />
+            : <button onClick={this.setFormStatus}
+              className="button is-info"
+            > add New Dish
+            </button>
+          }
+        </div>
+        <div style={{ width: 500 }} className='mx-4 mt-5'>
+          <FoodBox foods={listedFood} />
         </div>
       </div>
     )

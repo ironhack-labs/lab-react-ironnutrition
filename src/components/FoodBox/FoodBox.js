4f7b34abca5d284cd47b961f73659f8ab567751e
Robert Cabrera <cabrera.rch@gmail.com>
2022-03-25 11:58:33 +0100

    Iteration 2 done

--- src/components/FoodBox/FoodBox.js
+++ src/components/FoodBox/FoodBox.js
@@ -3,23 +3,29 @@ import './FoodBox.scss'
 
 class FoodBox extends Component {
   render() {
-    const food  = this.props.food;
+    const foods  = this.props.food;
 
     return (
-      <div className="box">
-        <div className="box__img">
-          <img src="https://i.imgur.com/eTmWoAN.png" className="box__photo" />
-        </div>
+      <div>
+        {foods.map((food) => {
+          return (
+            <div className="box">
+              <div className="box__img">
+                <img src={food.image} className="box__photo" />
+              </div>
 
-        <div className="box__info">
-          <h3>{food[0].name}</h3>
-          <p>{food[0].calories} cal</p>
-        </div>
+              <div className="box__info">
+                <h3>{food.name}</h3>
+                <p>{food.calories} cal</p>
+              </div>
 
-        <div className="box__controls">
-          <input className="box__input" type="number" value="1" />
-          <button className="box__btn">+</button>
-        </div>
+              <div className="box__controls">
+                <input className="box__input" type="number" value="1" />
+                <button className="box__btn">+</button>
+              </div>
+            </div>
+          )
+        })}
       </div>
     )
   }

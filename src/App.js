867640917773038b0fcc4ea880aa38be031d5b3f
ricomasgu <ricard.comas.gomez@gmail.com>
2022-03-31 21:52:19 +0200

    Iteration 4

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ import 'bulma/css/bulma.css';
 class App extends Component {
   state = {
     foods: foods,
-    showForm: false
+    showForm: false,
+    search: ""
   }
 
-  onClickHandler = ( event ) => {
+  onClickFormHandler = ( event ) => {
     this.setState(
       {
         ...this.state,
@@ -26,6 +27,7 @@ class App extends Component {
   addFood = (food) => {
     this.setState(
       {
+        ...this.state,
         foods: 
         [
           ...this.state.foods,
@@ -36,14 +38,28 @@ class App extends Component {
     )
   }
 
+  onChangeSearchHandler = (event) => {
+    const { value } = event.target;
+
+    this.setState(
+      {
+        ...this.state,
+        search: value
+      }
+    )
+  }
+
   render () {
-    const foodList = this.state.foods.map( (food) => {
+    const foodList = this.state.foods
+      .filter( (food) => food.name.toLowerCase().includes(this.state.search.toLowerCase() ) )
+      .map( (food) => {
       return <FoodBox key={food.name} {...food}/>;
     });
     return (
       <div>
-        <button className="button is-info" onClick={this.onClickHandler} >Add new food</button>
+        <button className="button is-info" onClick={this.onClickFormHandler} >Add new food</button>
         {this.state.showForm && <AddFoodForm addFood={this.addFood} />}
+        <input className="input" type="text" onChange={ this.onChangeSearchHandler } value={ this.state.search } />
         {foodList}
       </div>
     );

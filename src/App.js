fef7d23e7bc24283ed51f2741426203c98ab40a1
Marija Strahinjic <marija.strahinjic@hotmail.com>
2022-05-11 19:07:56 +0200

    print entire collection of foods using map method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
 function App() {
+
+  const [foodState, setFoods] = useState(foods);
+
+  const displayFoods = () => {
+    setFoods(() => {
+      return foodState;
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <FoodBox item={foods} />
     </div>
   );
 }

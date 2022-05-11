b637cf9cafc9b4b1b4d66a838665f8a9d77e2b35
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-11 21:08:35 +0200

    iteration 1+2 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './Components/FoodBox';
 
 function App() {
+  const [food, setFoods] = useState(foods);
+
+  const displayFoods = () => {
+    setFoods((food) => {
+      return food;
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FoodBox foods={food} callbackDisplayFoods={displayFoods} />
     </div>
   );
 }
+
 
 export default App;

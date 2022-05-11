78e8b5642b4b7becf08f4751c34fe0ac22b7292c
Ivo Silva <silvaivosoares@gmail.com>
2022-05-11 18:41:12 +0100

    Completed iterations 1 and 2

---
+++ src/components/FoodBox.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function FoodBox(props) {
+  console.log(props);
+
+  return props.listOfFoods.map((food) => {
+    return (
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={food.image} />
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{food.name}</strong> <br />
+                <small>{food.calories} cal</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" type="number" value="1" />
+              </div>
+              <div className="control">
+                <button className="button is-info">+</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    );
+  });
+}
+
+export default FoodBox;

0a27d24fe24b4bafe0f79f1937cccddf652b0c3f
Ivo Silva <silvaivosoares@gmail.com>
2022-05-12 07:06:33 +0100

    Completed iteration 3

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import './App.css';
 import 'bulma/css/bulma.css';
 import foodsArr from './foods.json';
 import FoodBox from './components/FoodBox';
+import AddNewFood from './components/addNewFood';
 
 function App() {
   const [foods, setFoods] = useState(foodsArr);
 
-
+  const addNewFood = newFood => {
+    setFoods(prevFood => {
+      return [...prevFood, newFood]
+    })
+  }
 
   return (
     <div className="App">
+      <AddNewFood addFood={addNewFood}/>
       <FoodBox listOfFoods={foods}/>
     </div>
   );
---
+++ src/components/addNewFood.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {useState} from 'react';
+
+
+function AddNewFood({addFood}) {
+
+    const [name, setName] = useState("");
+    const [calories, setCalories] = useState("");
+    const [image, setImage] = useState("");
+    const [isVisible, setIsVisible] = useState(false);
+    
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newFood = {
+        name: name,
+        calories: calories,
+        image: image
+    };
+
+    addFood(newFood);
+    setName("");
+    setCalories("");
+    setImage("");
+    setIsVisible(false);
+}
+
+const showForm = () => {
+    setIsVisible(true);
+}
+
+
+
+
+    return(
+        <div className="addFood">
+        <button onClick={showForm}>Add New Food</button>
+            {isVisible ? 
+            <form onSubmit={handleSubmit}>
+                <label>Name:<input type='text' name='name' value={name} onChange={(e) => {setName(e.target.value)}}></input> </label>
+                <label>Calories: <input type='number' name='calories' value={calories} onChange={(e) => {setCalories(e.target.value)}}></input></label>
+                <label>Image: <input type='text' name='image' value={image} onChange={(e) => {setImage(e.target.value)}}></input></label>
+                <button>Submit</button>
+            </form> : ""}
+
+        </div>
+    )
+}
+
+export default AddNewFood;

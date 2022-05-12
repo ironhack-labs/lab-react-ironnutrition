c0355002d7cb6c697f53097ce27e6ad62b8c8ed1
jidekickpush <jeandenisdrane@gmail.com>
2022-05-12 05:14:15 +0200

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
 import FoodBox from "./components/FoodBox"
 
 function App() {
+
+  const [foodList, setFoodList] = useState(foods)
+
   return (
     <div className="App">
-      <FoodBox />
+      <FoodBox foodData={foodList} />
 
     </div>
   );
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,37 +1,41 @@
 import React from "react"
 
-function FoodBox(){
+function FoodBox(props) {
     return (
-        
         <>
-          <article className="media">
-            <div className="media-left">
-              <figure className="image is-64x64">
-                <img src="https://i.imgur.com/eTmWoAN.png" alt=""/>
-              </figure>
-            </div>
-            <div className="media-content">
-              <div className="content">
-                <p>
-                  <strong>Pizza</strong> <br />
-                  <small>400 cal</small>
-                </p>
-              </div>
-            </div>
-            <div className="media-right">
-              <div className="field has-addons">
-                <div className="control">
-                  <input className="input" type="number" value="1" />
-                </div>
-                <div className="control">
-                  <button className="button is-info">
-                    +
-                  </button>
-                </div>
-              </div>
-            </div>
-          </article>
-    </>
+            {props.foodData.map((food,index) => {
+                return (
+                    <article className="media" key={index}>
+                        <div className="media-left">
+                            <figure className="image is-64x64">
+                                <img src={food.image} alt="" />
+                            </figure>
+                        </div>
+                        <div className="media-content">
+                            <div className="content">
+                                <p>
+                                    <strong>{food.name}</strong> <br />
+                                    <small>{food.calories} cal</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="media-right">
+                            <div className="field has-addons">
+                                <div className="control">
+                                    <input className="input" type="number" value="1" />
+                                </div>
+                                <div className="control">
+                                    <button className="button is-info">
+                                        +
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                );
+            })}
+        </>
+
     )
 }
 

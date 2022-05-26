c46b48c7b5c0297382304dcb43b1d69835477666
unknown <cesyrub@gmail.com>
2022-05-26 09:59:12 +0200

    Formatting

--- src/components/TodaysFood.jsx
+++ src/components/TodaysFood.jsx
@@ -11,7 +11,15 @@ export default function TodaysFood(props) {
         <>
         <h2 className='todayFoodHeading'>Today's Food:</h2>
         {
-            food.map(elem => <div key={uuid()} className="is-flex is-flex-direction-row is-justify-content-center mgt-small mgb-small"><figure className='image is-64x64'><img src={elem.image} alt=""/></figure><p>{elem.quantity}u - {elem.name} - Calories/unit: {elem.calories} Total Calories: {elem.calories*elem.quantity}</p> <button className='btnMove delete is-medium' onClick={() => removeTodayFood(elem.name)}>REMOVE</button></div>)
+            food.map(elem =>  {
+                 return (<div key={uuid()} className="is-flex is-flex-direction-row is-justify-content-center mgt-small mgb-small">
+                            <figure className='image is-64x64'>
+                                <img src={elem.image} alt=""/>
+                            </figure>
+                            <p>{elem.quantity}u - {elem.name} - Calories/unit: {elem.calories} Total Calories: {elem.calories*elem.quantity}</p> 
+                            <button className='btnMove delete is-medium' onClick={() => removeTodayFood(elem.name)}>REMOVE</button>
+                        </div>)
+                })
         }
         <h4 className='todayFoodFooter'>Total Calories: {total}</h4>
         </>

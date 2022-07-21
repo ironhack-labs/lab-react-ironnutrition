8bdaca7e46dd6cb8941e8b8fb6460198e4fa1d63
Val Mejia <valemegaestrellalectora@gmail.com>
2022-07-20 21:41:48 -0500

    :tada:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import { Empty } from 'antd';
 
 function App() {
+  //console.log(foods);
+  //const [elValor,FnParaActualizarElValor] = useState(valorInicial)
+  const [comidas, setComidas] = useState(foods);
+  const agregarComida = (nuevaComida) => {
+    const nuevasComidas = [...comidas]
+    nuevasComidas.push(nuevaComida)
+    setComidas(nuevasComidas)
+  }
+  console.log(comidas);
+  //arreglo.map( (dato) => {
+  // return <AlgoNuevo key> - <Componente key>
+  //})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <AddFoodForm agregarComida={agregarComida}/>
+    <div className="tabla">
+    {comidas.map((comida, index) => {
+      return(
+        <FoodBox key={index} comida={comida}/>
+ 
+      )})}
+      </div>
+      {
+        comidas.length === 0 && <img src={Empty} alt=""/>
+      }
+      
+  </div>
+
+  )
 }
 
 export default App;

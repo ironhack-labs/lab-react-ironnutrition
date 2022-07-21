4fb05add8f33d1a989e92235e13957071a4233d9
SamPetherick1898 <fuentessamuelignacio@gmail.com>
2022-07-20 19:38:10 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+import FoodBox from "./Components/FoodBox"
+import AddFoodForm from './Components/AddFoodForm';
+import Empty from './error.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  //console.log(foods)
+  //const [el valor, FnParaActualizar}] = useState(valorInicial)
+  const [comidas, setComidas] = useState(foods);
+  
+  //arreglo.map( () => ){
+  //return <AlgoNuevo key> - <Componente key>
+  //})
+
+  const agregarComida = (nuevaComida) => {
+      console.log('Nueva comida', nuevaComida);
+      const nuevaComidas = [ ...comidas ]
+      nuevaComidas.push(nuevaComida)
+      setComidas(nuevaComidas);
+  };
+
+  return ( 
+    <div>
+    <AddFoodForm agregarComida = {agregarComida} />
+      <div className='tabla'>
+      {comidas.map((comida, index) => {
+          return (<FoodBox key= {index} comida={comida} />
+        )
+        })}
+        </div>
+        {
+          comidas.length === 0 && <img src={Empty} alt='error'/>
+        }
     </div>
   );
 }
---
+++ src/Components/AddFoodForm.js
@@ -0,0 +1,62 @@
+// Style Guide:
+// In this file you can find a reference example of the structure
+// and content that the component should render.
+// Remember to import Ant Design components before using them.
+import { Divider, Input } from 'antd';
+import {useState} from "react";
+
+// Iteration 4
+function AddFoodForm(props) {
+    const {agregarComida} = props
+    const [name, setName] = useState("")
+    const [image, setImage] = useState("")
+    const [calories, setCalories] = useState("")
+    const [servings, setServings] = useState("")
+  
+  const registrarDatos =(e) => {
+    //Deshabilitamos el comportamiento por defecto
+    //el cual trata de envias los datos a algun lugar y
+    //recarga la pagina/app
+    e.preventDefault();
+    console.log(name, image, calories, servings)
+    agregarComida({ name, image, calories, servings })
+    
+    setName("")
+    setImage("")
+    setCalories("")
+    setServings("")
+  };
+  
+    return (
+    <form onSubmit={registrarDatos}>
+      <Divider>Add Food Entry</Divider>
+
+      <label>Name</label>
+      <Input value={name} type="text" onChange={(e) => {
+        setName(e.target.value)
+      }} />
+
+      <label>Image</label>
+      {/* render antd <Input /> type="text" here */}
+      <Input value={image} type="text" onChange={(e) => {
+        setImage(e.target.value)
+      }} />
+
+      <label>Calories</label>
+      {/* render antd <Input /> type="number" here */}
+      <Input value={calories} type="number" onChange={(e) => {
+        setCalories(e.target.value)
+      }} />
+
+      <label>Servings</label>
+      {/* render antd <Input /> type="number" here */}
+      <Input value={servings} type="number" onChange={(e) => {
+        setServings(e.target.value)
+      }} />
+
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default AddFoodForm;

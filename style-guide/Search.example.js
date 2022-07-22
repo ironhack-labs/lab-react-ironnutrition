4fabd1aeb32f445719c8890cbf06178ea1bf8467
Moisés Vargas Rangel <vargasrangelaudio@gmail>
2022-07-21 22:26:49 -0500

    Listo

--- style-guide/Search.example.js
+++ style-guide/Search.example.js
@@ -1,19 +1,19 @@
-// Style Guide:
-// In this file you can find a reference example of the structure
-// and content that the component should render.
-// Remember to import Ant Design components before using them.
-import { Divider, Input } from 'antd';
+  // Style Guide:
+  // In this file you can find a reference example of the structure
+  // and content that the component should render.
+  // Remember to import Ant Design components before using them.
+  import { Divider, Input } from 'antd';
 
-// Iteration 5
-function Search(props) {
-  return (
-    <>
-      <Divider>Search</Divider>
+  // Iteration 5
+  function Search(props) {
+    return (
+      <>
+        <Divider>Search</Divider>
 
-      <label>Search</label>
-      <Input value={undefined} type="text" onChange={() => {}} />
-    </>
-  );
-}
+        <label>Search</label>
+        <Input value={undefined} type="text" onChange={() => {}} />
+      </>
+    );
+  }
 
-export default Search;
+  export default Search;

4c879ceae8e9616690618a698578a5841208690b
Paul Tiry <tirypaul@gmail.com>
2022-07-27 16:43:06 +0200

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import { useState } from 'react'
+import foodList from './foods.json'
+import { Row, Divider, Button } from 'antd'
+import 'antd/dist/antd.css'
 
 function App() {
+  const [food, setFood] = useState(foodList)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      {food.map((item, i) => (
+        <div key={i}>
+          <p>{item.name}</p>
+          <img src={item.image} width={100} alt={item.name} />
+        </div>
+      ))}
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>{/* Render the list of Food Box components here */}</Row>
+    </Wrapper>
+  )
 }
 
-export default App;
+export default App
+
+const Wrapper = styled.div`
+  text-align: center;
+`

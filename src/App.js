eb7a73ce3622e5460c53e084fdf61c2f5fef89ee
Ruben Andino <rubenanlo@gmail.com>
2022-07-27 17:29:23 +0200

    iterations 1, 2, and 3 but 3 with not exact format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+import FoodBox from './components/FoodBox';
 
 function App() {
+  const [foodArray, setFood] = useState(foods);
+
+  //useEffect
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Food List</h1>
       </header>
+      {foodArray.map((food) => {
+        return <FoodBox {...food} />;
+      })}
     </div>
   );
 }
---
+++ src/components/FoodBox.js
@@ -0,0 +1,24 @@
+// Style Guide:
+// In this file you can find a reference example of the structure
+// and content that the component should render.
+// Remember to import Ant Design components before using them.
+import { Card, Col, Button } from 'antd';
+
+// Iteration 2
+function FoodBox(props) {
+  return (
+    <Col>
+      <Card title={props.name} style={{ width: 230, height: 300, margin: 10 }}>
+        <img src={props.image} height={60} alt="food" />
+        <p>Calories: {props.calories}</p>
+        <p>Servings: {props.servings}</p>
+        <p>
+          <b>Total Calories: {props.calories * props.servings} </b> kcal
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
+  );
+}
+
+export default FoodBox;

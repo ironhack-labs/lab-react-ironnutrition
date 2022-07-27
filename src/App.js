370d9f6c001be72a61e8e54054f4077bfc5cc182
Vale Quiroga <valentinqramos@outlook.com>
2022-07-26 21:39:23 -0500

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import foodsData from './foods.json';
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import {useState} from 'react'
+import FoodBox from './components/FoodBox';
 
 
 function App() {
@@ -10,13 +11,24 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Food List</h1>
-      {foods.map((food, index) => (
-        <div key={index} >
-          <p> {food.name} </p>
-          <img src={food.image} width={0} alt="food" />
-        </div>
-      ))}
+
+      <div>
+        <h1>Food List</h1>
+        {foods.map((food, index) => (
+          <div key={index} >
+            <p> {food.name} </p>
+            <img src={food.image} width={0} alt="food" />
+          </div>
+        ))}
+      </div>
+
+      <FoodBox food={{
+        name: 'Orange',
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      }} />
+
     </div>
   );
 }

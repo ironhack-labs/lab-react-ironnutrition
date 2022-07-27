9f989285f94269648469f06350a9997f8018a3dd
Paul Tiry <tirypaul@gmail.com>
2022-07-27 17:40:49 +0200

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import foodList from './foods.json'
 import { Row, Divider, Button } from 'antd'
 import 'antd/dist/antd.css'
 import FoodBox from './components/FoodBox'
+import AddFoodForm from './components/AddFoodForm'
 
 function App() {
   const [food, setFood] = useState(foodList)
+
+  const addFoodHandler = (item) => {
+    setFood((prevList) => [item, ...prevList])
+  }
   return (
     <Wrapper>
-      {/* Display Add Food component here */}
+      <AddFoodForm addFood={addFoodHandler} />
 
       <Button> Hide Form / Add New Food </Button>
 
@@ -35,7 +40,7 @@ function App() {
 
       <Row style={{ width: '100%', justifyContent: 'center' }}>
         {food.map((item, i) => (
-          <FoodBox food={item} />
+          <FoodBox key={i} food={item} />
         ))}
       </Row>
     </Wrapper>

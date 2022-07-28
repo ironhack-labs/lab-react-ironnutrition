0a3c105f0ab12263fcb276acff7057dfc12aa68a
jackletran <jack.letran@moebel.de>
2022-07-28 09:03:08 +0200

    done including iteration 6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
+import 'antd/dist/antd.css';
+// To start using the pre-made Ant Design components we must first import them:
+import foods from './foods.json';
+import { useState } from 'react';
 
 function App() {
+  const [food, setFoods] = useState(foods);
+
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState('');
+  const [servings, setServings] = useState('');
+
+  const deleteFood = (foodName) => {
+    setFoods((prevFoods) => {
+      const newList = prevFoods.filter((element) => {
+        return element.name !== foodName;
+      });
+      return newList;
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newFood = {
+      name: name,
+      image: image,
+      calories: calories,
+      servings: servings,
+    };
+    // update list of foods
+    setFoods((prevFoods) => {
+      return [newFood, ...prevFoods];
+    });
+    // clear form
+    setName('');
+    setImage('');
+    setCalories('');
+    setServings('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search food={food} setFoods={setFoods} />
+      <AddFoodForm
+        food={food}
+        setFoods={setFoods}
+        name={name}
+        setName={setName}
+        image={image}
+        setImage={setImage}
+        calories={calories}
+        setCalories={setCalories}
+        servings={servings}
+        setServings={setServings}
+        handleSubmit={handleSubmit}
+      />
+      {food.map((foodObj) => {
+        return (
+          <div key={foodObj.name}>
+            {/* <p>{foodObj.name}</p>
+            <img src={foodObj.image} width={200} alt={foodObj.name} /> */}
+            <FoodBox {...foodObj} callbackToDeleteFood={deleteFood} />
+          </div>
+        );
+      })}
     </div>
   );
 }
-
 export default App;
---
+++ src/components/FoodBox.js
@@ -0,0 +1,27 @@
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+
+// Iteration 2
+function FoodBox(props) {
+  return (
+    <Col>
+      <Card title={props.name} style={{ width: 230, height: 300, margin: 10 }}>
+        <img src={props.image} height={60} alt="food" />
+        <p>Calories: {props.calories}</p>
+        <p>Servings: {props.servings}</p>
+        <p>
+          <b>Total Calories: {props.calories * props.servings} </b> kcal
+        </p>
+        <Button
+          type="primary"
+          onClick={() => {
+            props.callbackToDeleteFood(props.name);
+          }}
+        >
+          Delete
+        </Button>
+      </Card>
+    </Col>
+  );
+}
+
+export default FoodBox;

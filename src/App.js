1de6be91b2a0b1c9fbaf3f9f1a3dfaa43deb2046
Enrique Orta <enriqueorta1994@gmail.com>
2022-07-27 21:15:09 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+import FoodBox from "./components/FoodBox"
+import AddFoodForm from './components/AddFoodForm';
+import Empty from "./error.png"
 
 function App() {
+  const [comidas, setComidas] = useState(foods);
+
+  console.log(comidas);
+
+  const agregarComida =(nuevaComida) => {
+    const nuevasComidas = [...comidas];
+    nuevasComidas.push(nuevaComida);
+    setComidas(nuevasComidas);
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddFoodForm agregarComida={agregarComida}/>
+      <div className="tabla">
+      {comidas.map((comida, index) => {
+        const { name, image } = comida;
+        return <FoodBox key={index} comida ={comida}/>;
+    })}
+    </div>
+    {}
+    {
+      comidas.length === 0 && <img src={Empty} alt="error" />
+    }
     </div>
   );
 }

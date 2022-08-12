956e434ed93ea126aceb70814595dd3c6fcdcdbb
Hugo Lomba <hmlomba@gmail.com>
2022-08-11 23:17:18 -0300

    iteration 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+// import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import { useState } from 'react';
+
 import './App.css';
+import FoodBox from './components/FoodBox';
+import foodsArr from './foods.json';
 
 function App() {
+  const [foods, setFoodsArr] = useState(foodsArr);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* // iteration 1 */}
+      {/* {foods.map((eachFood) => {
+        return (
+          <div>
+            <p> {eachFood.name} </p>
+            <img src={eachFood.image} width={150} />
+          </div>
+        );
+      })} */}
+      {/* // ------------------------- // iteration 2 */}
+      {/* <FoodBox
+        food={{
+          name: 'Orange',
+          calories: 85,
+          image: 'https://i.imgur.com/abKGOcv.jpg',
+          servings: 1,
+        }}
+      /> */}
+      {/* // -------------------- // iteration 3 */}
+
+      {foods.map((fruit) => {
+        return <FoodBox food={fruit} />;
+      })}
     </div>
   );
 }
---
+++ src/components/FoodBox.js
@@ -0,0 +1,24 @@
+import { Card, Col, Button } from 'antd';
+
+// Iteration 2
+function FoodBox(food) {
+  const { name, calories, image, servings } = food.food;
+  return (
+    <Col>
+      <Card title={food.name} style={{ width: 230, height: 300, margin: 10 }}>
+        <img src={image} height={60} alt="food" />
+        <p>Calories: {calories}</p>
+        <p>Servings: {servings}</p>
+        <p>
+          <b>
+            Total Calories: {calories} * {servings}{' '}
+          </b>{' '}
+          kcal
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
+  );
+}
+
+export default FoodBox;

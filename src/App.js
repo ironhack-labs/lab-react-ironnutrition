46e25a55d99a06fb8bc88ee2e75e9b6dfbb81c5b
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-11 23:01:08 -0300

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState  } from 'react';
 import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
 function App() {
 
@@ -9,6 +10,12 @@ function App() {
   return (
     <div className="App">
       <h1>Food List</h1>
+        <FoodBox food={ {
+          name: "Orange",
+          calories: 85,
+          image: "https://i.imgur.com/abKGOcv.jpg",
+          servings: 1
+        }} />
      <div className="food-list">
         {foods.map((eachfood) => {
           return <div>

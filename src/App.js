e9360669f3311f1d61e2fa2d47bc03cac18e1f12
heliodcf <heliodcf@gmail.com>
2022-08-13 00:05:02 -0400

    Interation 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import foods from './foods.json';
+import { useState } from "react";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [listFoods, setFoods] = useState(foods)
+  
+  
 
+  return (
+  <div className="App">
+{listFoods.map( element => {
+     return(
+       <div>
+         <p> {element.name} </p>
+         <img src={element.image} width={100} />
+       </div>
+     )    
+   })
+   }
+
+  </div>
+  )
+}
 export default App;

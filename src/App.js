48394e7b993df9e48a7636520df3ee7a72e0ad81
Joao Paulo <joaopaulo.bco@gmail.com>
2022-08-13 16:26:39 -0300

    lab nutrition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import foods from './foods.json';
+import { useState } from 'react';
+import FoodBox from './components/FoodBox';
 
 function App() {
+  const [listFoods, setFoods] = useState(foods);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {listFoods.map((element) => {
+        return (
+          
+            <FoodBox key={element.name} food={element}/>
+          
+        );
+      })}
     </div>
   );
 }

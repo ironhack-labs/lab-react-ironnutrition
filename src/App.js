651d92a12b4c4c8877c10ea7f8618020ae4fb39c
cra123 <onixmohamed@yahoo.fr>
2022-09-01 10:54:20 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+// import { useState } from 'react';
+import food from './foods.json'
+import FoodBox from './components/FoodBox';
+
+
+// const [foodList, setFoodList] = useState(food);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  console.log(food[0].image)
+  return <div className="App">
+    {/* {
+      food.map(food =>{
+        return(
+          <div>
+            <p> {food.name} </p>
+            <img src={food.image} width={100} alt=".." />
+          </div>
+        )
+      })
+    } */}
 
+    <FoodBox food={{
+      name: "Orange",
+      calories: 85,
+      image: "https://i.imgur.com/abKGOcv.jpg",
+      servings: 1
+    }} />
+
+
+  </div>;
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//  make the Ant Design styles available in the entire app
+import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

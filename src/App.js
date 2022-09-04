09af5397a87282797faf0b16087bd02c0c94abfc
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-09-04 12:54:24 +0200

    Iterations 1, 2 & 3 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import foodsData from './foods.json';
+import FoodBox from './components/FoodBox';
+import { Divider, Row } from "antd";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {foodsData.map((food) => (
+          <FoodBox {...food}/>
+        ))}
+      </Row>
     </div>
   );
 }
---
+++ src/components/FoodBox.js
@@ -0,0 +1,22 @@
+import { Col, Card, Button } from "antd";
+
+function FoodBox({ name, image, calories, servings }) {
+  return (
+    <Col>
+      <Card
+        title={name}
+        style={{ width: 230, height: 300, margin: 10 }}
+      >
+        <img src={image} height={60} alt={name} />
+        <p>Calories: {calories}</p>
+        <p>Servings: {servings}</p>
+        <p>
+          <b>Total Calories: {calories * servings} kcal</b>
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
+  );
+}
+
+export default FoodBox;

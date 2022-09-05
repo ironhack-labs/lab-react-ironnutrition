a2b899b041647c6fc8ededd695aa8aef01b09460
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-09-05 17:56:05 +0200

    added clean form on hide or not, code

--- src/App.js
+++ src/App.js
@@ -81,3 +81,69 @@ function App() {
 }
 
 export default App;
+
+/* 
+import { Divider, Row } from 'antd';
+import FoodBox from './components/food-box/FoodBox';
+import './App.css';
+import AddFoodForm from './components/add-food-form/AddFoodForm';
+import foods from './data/foods.json';
+import { useState } from 'react';
+import SearchBar from './components/search-bar/SearchBar';
+
+function App() {
+  const [foodArray, setFoodArray] = useState(foods);
+  const [search, setSearch] = useState('');
+  const [show, setShow] = useState(true)
+
+  const handleDelete = (name) => {
+    setFoodArray(foodArray.filter((f) => name !== f.name));
+  };
+
+  return (
+    <div className="App container">
+    
+    <Divider>Add Food Entry</Divider>
+
+      {show ? (
+        <div>
+          <AddFoodForm foodArray={foodArray} setFoodArray={setFoodArray}
+          />
+          <div className='show-hide-buttons'>
+            <button onClick={() => {setShow(!show)}}>Hide Form</button>
+          </div>
+          
+        </div> ) : (
+          <div className='show-hide-buttons'>
+            <button onClick={() => {setShow(!show)}}>Show Form</button>
+          </div>
+      )}
+      
+
+      <Divider> Search </Divider>
+
+      <SearchBar search={search} setSearch={setSearch} />
+
+      <Divider> Food List </Divider>
+
+      {foodArray.length === 0 ? (
+        <div className='no-more-items'> 
+          <h3>Oops! Thre is no more content to show.</h3>
+          <img src="https://cdn-icons-png.flaticon.com/512/594/594468.png" alt="no more items" />
+        </div>
+      ) : (
+        <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {foodArray
+          .filter((food) =>
+            food.name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((food) => (
+            <FoodBox key={food.name} {...food} handleDelete={handleDelete} />
+          ))}
+      </Row>
+      )}      
+    </div>
+  );
+}
+
+export default App; */
--- src/components/add-food-form/AddFoodForm.js
+++ src/components/add-food-form/AddFoodForm.js
@@ -31,3 +31,44 @@ AddFoodForm.defaultProps = {
 }
 
 export default AddFoodForm
+
+/* import { Input } from "antd"
+import { useState } from 'react';
+
+function AddFoodForm({ foodArray, setFoodArray }) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(1);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    foodArray.push({ name, calories, image, servings });
+    setFoodArray([...foodArray]);
+  };
+
+  return(
+    <form  onSubmit={handleSubmit}>
+
+      <label>Name</label>
+      <Input value={name} type="text" onChange={(event) => {setName(event.target.value)}} />
+
+      <label>Image</label>
+      <Input value={image} type="text" onChange={(ev) => {setImage(ev.target.value !== "" ? ev.target.value : null)}} />
+    
+      <label>Calories</label>
+      <Input value={calories} type="number" onChange={(ev) => {setCalories(ev.target.value)}} />
+    
+      <label>Servings</label>
+      <Input value={servings} type="number" onChange={(ev) => {setServings(ev.target.value)}} />
+    
+      <button className="create-btn" type="submit" >Create</button>
+    </form>
+  )
+}
+
+AddFoodForm.defaultProps = {
+  image: "https://elviajerofeliz.com/wp-content/uploads/2019/11/Comida-t%C3%ADpica-de-India-_-10-Platos-Imprescindibles.jpg"
+}
+
+export default AddFoodForm */

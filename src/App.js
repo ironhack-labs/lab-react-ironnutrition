41ca394e399b50c9a13b0c357fc120c464b7c4bd
SimonHackHQ <simonhack.wdptapr22@protonmail.com>
2022-09-05 16:58:45 +0000

    Iterations 2 and 3: <FoodBox /> and <List /> (antdesign)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React from 'react';
 import { useState } from 'react';
 import foods from './foods.json';
 import './App.css';
+import FoodBox from './components/FoodBox';
+import { List } from 'antd';
 
 function App() {
     const [dishes, setDishes] = useState(foods);
 
     return (
         <div className="App">
-            {dishes.map(dish => (
-                <div>
-                    <p>{dish.name}</p>
-                    <img src={dish.image} width={60} />
-                </div>
-                )
-            )}
+            <List
+                itemLayout="vertical"
+                dataSource={dishes}
+                grid={{gutter: 16, column: 3}}
+                header="Food List"
+                renderItem={(item) => (
+                    <List.Item>
+                        <FoodBox name={item.name} calories={item.calories} image={item.image} servings={item.servings} />
+                    </List.Item>
+                )}
+
+                //{dishes.map(dish => <FoodBox name={dish.name} calories={dish.calories} image={dish.image} servings={dish.servings} />)}
+            />
         </div>
     );
 }
---
+++ src/components/FoodBox.js
@@ -0,0 +1,15 @@
+import { Card, Button } from "antd";
+
+function FoodBox(props) {
+    return (
+        <Card title={props.name}>
+            <img src={props.image} width={60} />
+            <p>Calories: {props.calories}</p>
+            <p>Servings: {props.servings}</p>
+            <p><bold>Total calories: {props.calories}</bold> kcal</p>
+            <Button type="primary">Delete</Button>
+        </Card>
+    );
+}
+
+export default FoodBox;

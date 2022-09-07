fa36f8e725a6fb68566e2b379c184e97b63fb43f
WilsonF78 <fortes.wilson78@gmail.com>
2022-09-07 19:57:58 +0200

    Iteration 8 fini

--- src/App.js
+++ src/App.js
@@ -1,72 +1,82 @@
-import { useState } from "react";
+import { useState } from 'react';
 import './App.css';
 import FoodBox from './components/FoodBox';
 import foodsJson from './foods.json';
 import AddFoodForm from './components/AddFoodForm';
-import Search from "./components/Search";
+import Search from './components/Search';
 
 function App() {
-  const [foods, setFoods] = useState(foodsJson)
-  const [searchText, setSearchText] = useState("")
+  const [foods, setFoods] = useState(foodsJson);
+  const [searchText, setSearchText] = useState('');
 
-  function addTheFood(name, image, calories, servings){
-    setFoods([ ...foods, { name, image, calories, servings } ])
+  function addTheFood(name, image, calories, servings) {
+    setFoods([...foods, { name, image, calories, servings }]);
   }
 
-  function searchFood(event){
+  function searchFood(event) {
     // Modifie le champ texte
-    setSearchText(event.target.value)
+    setSearchText(event.target.value);
 
     // Filtre dans une variable le contenu du champ texte
-    const filteredFood = [ ...foodsJson ].filter(food => food.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    const filteredFood = [...foodsJson].filter((food) =>
+      food.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
 
     // Modifie foods, ce qui change l'aspect visuel
-    setFoods(filteredFood);  
+    setFoods(filteredFood);
   }
 
-  function destroy(name){
-    let foodsCopy = [... foods]
+  function destroy(name) {
+    let foodsCopy = [...foods];
     // Recherche l'index ou l'object .name contient le nom de la food
-    const index = foodsCopy.map(el => el.name).indexOf(name);
-    foodsCopy.splice(index,1) // Supprime la food dans le tableau
-    setFoods(foodsCopy) // Met à jour foods qui met aussi a jour l'affichage
+    const index = foodsCopy.map((el) => el.name).indexOf(name);
+    foodsCopy.splice(index, 1); // Supprime la food dans le tableau
+    setFoods(foodsCopy); // Met à jour foods qui met aussi a jour l'affichage
+  }
+  const $addNewFood = document.querySelector('.addNewFood');
+  const $BtnAddNewFood = document.querySelector('.BtnAddNewFood');
+
+  function hideNewFood() {
+    if ($addNewFood.style.display === 'none') {
+      $BtnAddNewFood.innerHTML = 'Hide form';
+      $addNewFood.style.display = 'block';
+    } else {
+      $addNewFood.style.display = 'none';
+      $BtnAddNewFood.innerHTML = 'Add new food';
+    }
   }
 
   return (
     <div className="App">
-      <h2>Add Food Entry</h2>
-      <AddFoodForm addFood={addTheFood}/>
+      <div className="addNewFood">
+        <h2>Add Food Entry</h2>
+
+        <AddFoodForm addFood={addTheFood} />
+      </div>
+      <button className="BtnAddNewFood" onClick={hideNewFood}>
+        Add new food
+      </button>
 
       <h2>Search</h2>
-      <Search searchFood={searchFood} searchText={searchText}/>
+      <Search searchFood={searchFood} searchText={searchText} />
 
       <h2>Food List</h2>
       <div className="Foodlist">
-      {
-        foods.map((el) => {
-        return (
-          <FoodBox
-            key={el.name}
-            destroy={() => destroy(el.name)}
-            food={{
-              name: el.name,
-              calories: el.calories,
-              image: el.image,
-              servings: el.servings,
-            }}
-          />
-        );
-      })
-      }
+        {foods.map((el) => {
+          return (
+            <FoodBox
+              key={el.name}
+              destroy={() => destroy(el.name)}
+              food={{
+                name: el.name,
+                calories: el.calories,
+                image: el.image,
+                servings: el.servings,
+              }}
+            />
+          );
+        })}
       </div>
-      {/* <FoodBox
-        food={{
-          name: 'Orange',
-          calories: 85,
-          image: 'https://i.imgur.com/abKGOcv.jpg',
-          servings: 1,
-        }}
-      /> */}
     </div>
   );
 }

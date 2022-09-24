7d21d60676767ba9023eff05844c97b2d5172a4e
Guillermo Cubells <guillermocubells@gmail.com>
2022-09-24 14:42:31 +0200

    feature: iteration1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
 
 function App() {
+  const [food, setFood] = useState(foods);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {food.map((item) => {
+        return (
+          <div>
+            <p> {item.name} </p>
+            <img src={item.image} width={100} alt="food" />
+          </div>
+        );
+      })}
     </div>
   );
 }

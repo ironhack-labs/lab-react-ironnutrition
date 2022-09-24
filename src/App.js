07e3628f247162c3be822f7a63525924e31b2cad
Kira Soba <kira.soba@gmx.de>
2022-09-24 14:29:43 +0200

    Iteration 2: done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import foods from './foods.json';
 import { useState } from 'react';
+import { Card, Col, Button } from 'antd';
 
 function App() {
   const [foodsArr, setFoodsArr] = useState(foods);
@@ -9,6 +10,14 @@ function App() {
   return (
     <div className="App">
       <h1>Food List</h1>
+      <FoodBox
+        food={{
+          name: 'Orange',
+          calories: 85,
+          image: 'https://i.imgur.com/abKGOcv.jpg',
+          servings: 1,
+        }}
+      />
       {foodsArr.map((food) => {
         return (
           <div>
@@ -18,6 +27,24 @@ function App() {
         );
       })}
     </div>
+  );
+}
+
+function FoodBox({ food }) {
+  const { name, calories, image, servings } = food;
+
+  return (
+    <Col>
+      <Card title={name} style={{ width: 230, height: 300, margin: 10 }}>
+        <img src={image} height={60} alt="food" />
+        <p>Calories: {calories}</p>
+        <p>Servings: {servings}</p>
+        <p>
+          <b>Total Calories: {calories * servings} </b> kcal
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
   );
 }
 

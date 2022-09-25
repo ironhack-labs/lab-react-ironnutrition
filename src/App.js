675e2ed685a37e6e61c2efc259484d4086754731
Daniela Romero <danyrp45@gmail.com>
2022-09-24 19:28:22 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import foods from './foods.json';
 import './App.css';
+import React, { useState } from 'react';
+import FoodBox from './components/FoodBox';
+import { Row, Divider } from 'antd';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
+  const [food, setFood] = useState(foods);
+
+  const foodEntry = (newFood) =>{
+    const arr = [...food, newFood]
+    setFood(arr); 
+  }
+
+  const deleteFood = (index) => {
+    console.log(index)
+    const arr = [...food]
+    arr.splice(index, 1);
+
+    setFood(arr);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddFoodForm  foodEntry={foodEntry}/>
+      <Divider>Food List</Divider>
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {food.map((oneFood, index) => {
+          return (
+            <FoodBox
+              key={index} food={{...oneFood}} deleteFood={deleteFood} index={index}
+            />
+          );
+        })}
+        
+      </Row>
     </div>
   );
 }
---
+++ src/components/AddFoodForm.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { Divider, Input, Button } from 'antd';
+
+const AddFoodForm = (props) => {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState('');
+  const [servings, setServings] = useState('');
+  const [showForm, setShowForm] = useState(false);
+
+  console.log(showForm);
+  const nameEntry = (e) => {
+    setName(e.target.value);
+  };
+  const imageEntry = (e) => {
+    setImage(e.target.value);
+  };
+  const caloriesEntry = (e) => {
+    setCalories(e.target.value);
+  };
+  const servingsEntry = (e) => {
+    setServings(e.target.value);
+  };
+  const sendData = (e) => {
+    e.preventDefault();
+    props.foodEntry({ name, image, calories, servings });
+  };
+
+  return (
+    <div>
+      <form onSubmit={sendData}>
+        {showForm === false && <Button
+          onClick={() => {
+            setShowForm(true);
+          }}
+        >
+          Add New Form
+        </Button>}
+        {showForm === true && (
+          <div>
+            <Divider>Add Food Entry</Divider>
+
+            <label>Name</label>
+            <Input value={name} type="text" onChange={nameEntry} />
+
+            <label>Image</label>
+            <Input value={image} type="text" onChange={imageEntry} />
+
+            <label>Calories</label>
+            <Input value={calories} type="number" onChange={caloriesEntry} />
+
+            <label>Servings</label>
+            <Input value={servings} type="number" onChange={servingsEntry} />
+
+            <button type="submit">Create</button>
+
+            <br />
+            <br />
+            <Button
+              onClick={() => {
+                setShowForm(false);
+              }}
+              className="btn"
+            >
+              Hide Form
+            </Button>
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default AddFoodForm;

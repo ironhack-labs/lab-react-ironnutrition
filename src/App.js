a8938f751fd278b1e8330c3dd8a41009504d037b
mareklaz <mareklaz@MacBook-Air-de-Marek.local>
2022-09-26 10:02:23 +0200

    it3 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import FoodBox from './FoodBox/FoodBox';
+import foods from './foods.json';
+import { Row, Divider, Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {
+          foods.map((food) => {
+            return (
+            <FoodBox key={food.name} {...food} />
+            )
+          })
+        }
+      </Row>
     </div>
   );
 }
--- style-guide/AddFoodForm.example.js
+++ style-guide/AddFoodForm.example.js
@@ -8,22 +8,25 @@ import { Divider, Input } from 'antd';
 function AddFoodForm(props) {
   return (
     <form>
-      <Divider>Add Food Entry</Divider>
+    <Divider>Add Food Entry</Divider>
 
-      <label>Name</label>
-      <Input value={undefined} type="text" onChange={() => {}} />
+    <label>Name</label>
+    <Input name='name' id='name' placeholder='Name of the food' value={undefined} type="text" onChange={() => {}} />
 
-      <label>Image</label>
-      {/* render antd <Input /> type="text" here */}
+    <label>Image</label>
+    {/* render antd <Input /> type="text" here */}
+    <Input name='image' id='image' placeholder='Select image URL of the food' value={undefined} type="text" onChange={() => {}} />
 
-      <label>Calories</label>
-      {/* render antd <Input /> type="number" here */}
+    <label>Calories</label>
+    {/* render antd <Input /> type="number" here */}
+    <Input name='calories' id='calories' placeholder='Number of calories' value={undefined} type="number" onChange={() => {}} />
 
-      <label>Servings</label>
-      {/* render antd <Input /> type="number" here */}
+    <label>Servings</label>
+    {/* render antd <Input /> type="number" here */}
+    <Input name='servings' id='servings' placeholder='Number of servings' value={undefined} type="number" onChange={() => {}} />
 
-      <button type="submit">Create</button>
-    </form>
+    <button type="submit">Create</button>
+  </form>
   );
 }
 

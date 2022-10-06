7223fa0ded1ad87c679dc2bcb66df6fa8fe4e55f
Daiane Reina <daiane.veras.reina@gmail.com>
2022-10-06 23:57:39 +0100

    Done: Interation 7 and 8

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
 import { useState } from 'react';
-import { Row, Divider } from 'antd';
+import { Row, Divider, Button } from 'antd';
 import Search from './components/Search';
 
 function App() {
   const [foodList, setFoodList] = useState([...foods]);
   const [filter, setFilter] = useState('');
-  // console.log('Filter:', filter);
+  const [showForm, setShowForm] = useState(false);
 
   const oneFood = foodList
     .filter((food) => food.name.toLowerCase().includes(filter))
@@ -25,10 +25,24 @@ function App() {
 
   return (
     <div className="App">
-      <AddFoodForm addFood={setFoodList} />
+      {showForm ? <AddFoodForm addFood={setFoodList} /> : null}
+      <Button type="primary" ghost onClick={() => setShowForm(!showForm)}>
+        {showForm ? 'Hide Form' : 'Add New Food'}
+      </Button>
+
       <Search filterFood={setFilter} />
       <Divider>Food List</Divider>
-      <Row>{oneFood}</Row>
+      {foodList.length === 0 ? (
+        <div>
+          <h2>Opps! There is no more content to show.</h2>
+          <img
+            src="https://www.clipartmax.com/png/small/47-479052_empty-set-mathematical-symbol-comments-icono-vacio-png.png"
+            alt="Empty Set Mathematical Symbol Comments - Icono Vacio Png @clipartmax.com"
+          ></img>
+        </div>
+      ) : (
+        <Row>{oneFood}</Row>
+      )}
     </div>
   );
 }

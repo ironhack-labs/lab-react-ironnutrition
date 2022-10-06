9b91d1b4c4ded77d4d0e293420888b5994b3276a
Daiane Reina <daiane.veras.reina@gmail.com>
2022-10-06 23:00:55 +0100

    Done: Interation 5

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
 import { useState } from 'react';
 import { Row, Divider } from 'antd';
+import Search from './components/Search';
 
 function App() {
   const [foodList, setFoodList] = useState([...foods]);
+  const [filter, setFilter] = useState('');
+  // console.log('Filter:', filter);
 
-  const oneFood = foodList.map((food) => {
-    return (
-      <Row key={food.name}>
-        <FoodBox food={food} />
-      </Row>
-    );
-  });
+  const oneFood = foodList
+    .filter((food) => food.name.toLowerCase().includes(filter))
+    .map((food) => {
+      return (
+        <Row key={food.name}>
+          <FoodBox food={food} />
+        </Row>
+      );
+    });
 
   return (
     <div className="App">
       <AddFoodForm addFood={setFoodList} />
+      <Search filterFood={setFilter} />
       <Divider>Food List</Divider>
       <Row>{oneFood}</Row>
     </div>
---
+++ src/components/Search.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Divider, Input } from 'antd';
+
+function Search({ filterFood }) {
+  const [search, setSearch] = useState('');
+  console.log('Search:', search);
+  return (
+    <div>
+      <Divider> Search </Divider>
+      <label>Search:</label>
+      <Input
+        value={search}
+        type="text"
+        onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
+      />
+      {filterFood(search)}
+    </div>
+  );
+}
+
+export default Search;

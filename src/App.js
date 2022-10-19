92e71a14a8aee5519bf799cccd3bfe8e5cc1a8fc
HaridhaRavi <haridha.gr@gmail.com>
2022-10-19 18:09:54 +0200

    iteration 2 fis commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+
+import { Row, Divider, Button } from 'antd';
+import FoodBox from './component/FoodBox';
+
 
 function App() {
+  const [listOfFood, setListOfFood] = useState(foods);
+  
+  const foodList =(prevFoodList)=>{
+    
+
+  }
+  listOfFood.map((food) => {
+    return (
+      <FoodBox food ={foodList} />
+    );
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {/* Render the list of Food Box components here */}
+        
+      </Row>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Setup : import Ant Design
+import 'antd/dist/antd.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(

03f985939ba2bfa0617619d68edfdfd418b21ad0
NFTL0ve <mbrandtk14@gmail.com>
2022-10-20 01:55:47 +0200

    getting closer but still stuck

--- src/components/AddFoodForm.js
+++ src/components/AddFoodForm.js
@@ -1,8 +1,11 @@
 import { Divider, Input } from 'antd';
-import React, { useState } from "react";
+import { Header } from 'antd/lib/layout/layout';
+import { useState } from "react";
+
 
 // Iteration 4
-function AddFoodForm({}) {
+
+function AddFoodForm({callbackToCreate}) {
     const [name, setName] = useState("");
     const [image, setImage] = useState("");
     const [calories, setCalories] = useState("");
@@ -10,48 +13,53 @@ function AddFoodForm({}) {
     
     const handleSubmit = (event) => {
         event.preventDefault();
-        const newFood = {
-            name: name,
-            image: image,
-            calories: calories,
-            servings: servings
-        }
-        AddFoodForm(newFood)
-      }  
-
-
-    //   const handleSubmit = (e) => {
-    //     e.preventDefault();
     
-    //     const newMovie = {
-    //       title: title,
-    //       year: "2000",
-    //       rating: 9
-    //     }
-        
-    //     setListOfMovies();
-    
-    //   }
+      const newFood = {
+        name: name,
+        image: image,
+        calories: calories,
+        servings: servings
+      }
+      callbackToCreate(newFood);
+
       
-  return (
+       //clear form
+       setName("");
+       setImage("");
+       setName("");
+       setImage("");
+        }
+      
     
-    <form onSubmit={handleSubmit}>
-      <Divider>Add Food Entry</Divider>
 
-      <label>Name</label>
+  return (
+
+    <form>
+      
+      
+      <Divider>Add Food Entry</Divider>
+    
+     <label>Name</label>
       <Input value={name} key="changeName" type="text" onChange={(event) => {setName(event.target.value)} }/>
-           
+
       <label>Image</label>
      <Input key="changeImage" value={image} type="text" onChange={(event) => {setImage(event.target.value)} }/>
 
-      <label>Calories</label>
+     <label>Calories</label>
   <Input value={calories} key="changeCalories" type="number" onChange={(event) => {setCalories(event.target.value)} }/>
 
-      <label>Servings</label>
+  <label>Servings</label>
       <Input value={servings} key="changeServings" type="number" onChange={(event) => {setServings(event.target.value)}} />
+
 
       <button type="submit">Create</button>
     </form>
-  );
+    
+   
+
+
+
+
+  )
 }
 export default AddFoodForm;

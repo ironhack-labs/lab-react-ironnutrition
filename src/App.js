6120be64e0479fd827f334f3fad7f632ac6d9df6
NFTL0ve <mbrandtk14@gmail.com>
2022-10-19 18:32:32 +0200

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
+import foods from "./foods.json"
+
 
 function App() {
-  return <div className="App"></div>;
-}
+  return (<div className="App">
+
+    {foods.map(food =>
+      <div>
+        <p>Food_name_here</p>
+        <img src={food.image} width={100} alt={food.n}/>
+   
+    </div> )}
+   </div>
+  );
+    }
+    
+    
 export default App;
+

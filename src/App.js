b6f12fa2ef477c401482190f3997f5638fc63e9a
Liz Manresa <manresa.liz@gmail.com>
2022-10-19 19:02:22 +0200

    solved iteration 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import FoodBox from './components/FoodBox';
+import { Row, Divider, Button } from 'antd';
+
+import foods from './foods.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {/* Render the list of Food Box components here */}
+        {foods.map((food) => (
+          <div>
+            <p> {food.name} </p>
+            <img src={food.image} width={100} alt={food.name} />
+          </div>
+        ))}
+
+        <FoodBox
+          food={{
+            name: 'Orange',
+            calories: 85,
+            image: 'https://i.imgur.com/abKGOcv.jpg',
+            servings: 1,
+          }}
+        />
+      </Row>
     </div>
   );
 }

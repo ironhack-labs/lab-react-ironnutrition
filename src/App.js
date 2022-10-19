35dc4d6c5f87295026ce7aaffb17f43e49606e6b
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-10-19 18:21:28 +0200

    finished iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+//import { useState } from 'react';
 import './App.css';
+
+import foods from './foods.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    {foods.map(food =>
+    <div>
+      <p>{food.name}</p>
+      <img src={food.image} width={100} alt={food.name} />
+    </div>)}
+  </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// src/index.js
+import 'antd/dist/antd.min.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

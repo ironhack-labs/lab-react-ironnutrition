b21028810342998021d9d7cac484d9d6791665d2
Farzin khaledi <Khaledifarzin1@gmail.com>
2022-10-19 19:01:58 +0100

    the iteratins 1,2,3, has been solved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import foods from './foods.json';
 import './App.css';
-
+import React, { useState } from 'react';
 import FoodBox from './components/FoodBox';
+import { Row, Divider } from 'antd';
 
 function App() {
+  const [food, setFood] = useState(foods);
+
+  const deleteFood = (index) => {
+    console.log(index);
+    const arr = [...food];
+    arr.splice(index, 1);
+
+    setFood(arr);
+  };
+
   return (
     <div className="App">
-      <FoodBox foods={foods} />
+      <Divider>Food List</Divider>
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {food.map((oneFood, index) => {
+          return (
+            <FoodBox
+              key={index}
+              food={{ ...oneFood }}
+              deleteFood={deleteFood}
+              index={index}
+            />
+          );
+        })}
+      </Row>
     </div>
   );
 }
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,20 +1,28 @@
+import React from 'react';
 import { Card, Col, Button } from 'antd';
-// Iteration 2
-function FoodBox({ foods }) {
-  console.log({ foods });
+
+const FoodBox = (props) => {
+  const { name, calories, image, servings } = props.food;
+  const { deleteFood, index } = props;
   return (
     <Col>
-      <Card title={foods.name} style={{ width: 230, height: 300, margin: 10 }}>
-        <img src={foods.image} height={60} alt={foods.name} />
-        <p>Calories: {foods.calories}</p>
-        <p>Servings: {foods.servings}</p>
+      <Card title={name} style={{ width: 230, height: 300, margin: 10 }}>
+        <img src={image} height={60} alt="food" />
+        <p>Calories: {calories}</p>
+        <p>Servings: {servings}</p>
         <p>
-          <b>Total Calories: {foods.calories * foods.servings} </b> kcal
+          <b>
+            Total Calories: {calories} * {servings}{' '}
+          </b>{' '}
+          kcal
         </p>
-        <Button type="primary"> Delete </Button>
+        <Button onClick={() => deleteFood(index)} type="primary">
+          {' '}
+          Delete{' '}
+        </Button>
       </Card>
     </Col>
   );
-}
+};
 
 export default FoodBox;

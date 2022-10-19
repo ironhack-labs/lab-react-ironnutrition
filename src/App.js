e476c4d4dbb2ec15b714dd544a334f71bdfacf4c
NFTL0ve <mbrandtk14@gmail.com>
2022-10-19 20:22:34 +0200

    iteration 3 styling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { OmitProps } from 'antd/lib/transfer/ListBody';
 import './App.css';
 import FoodBox from './components/FoodBox';
 import food from "./foods.json"
-
+import { Row, Divider, Button } from 'antd';
 
 function App() {
   return (<div className="App">
-
-    {food.map(banana =>
+   
+   <Row style={{ width: '100%', justifyContent: 'center' }}>
+   {food.map(banana =>
       <div>
       <FoodBox food={banana}/>
+      
    
     </div> )}
+      </Row>
+    
    </div>
   );
 

d93e0db6c3cb049a1859f176d86716eff28c5b01
julianBrandon <p0lterg3istt@gmail.com>
2022-10-19 17:46:50 +0200

    Add iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { Row, Divider, Button } from 'antd';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {/* Render the list of Food Box components here */}
+        <div className="App">
+          {foods.map(food =>
+            <div>
+              <p> {food.name} </p>
+              <img src={food.image} width={150} alt={food.name} />
+            </div>
+          )}
+        </div>
+      </Row>
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'antd/dist/antd.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';

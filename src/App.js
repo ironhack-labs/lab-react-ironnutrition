fa0872a51fd947df79415aef3b2c9b9071381f1e
hvkst <bernd@hvkst.de>
2022-11-02 23:07:22 +0100

    Iteration I

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import foods from './foods.json';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [food, setFood] = useState(foods);
+  return (
+    <>
+      <h2>Food List</h2>
+      {food.map((food) => {
+        return (
+          <div>
+            <p> {food.name} </p>
+            <img src={food.image} alt={food.name} width={200} />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 export default App;

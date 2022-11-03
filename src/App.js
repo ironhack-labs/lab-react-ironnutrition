a3d92184195459de5951e9d55010202e4ee643b8
JoaoSerafim3001 <seafim.joao@gmail.com>
2022-11-03 20:20:21 +0000

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import { FoodBox } from './components/FoodBox';
 import foods from './foods.json';
 
 function App() {
@@ -7,12 +8,21 @@ function App() {
 
   return (
     <div className="App">
-      {foodList.map((food) => (
-        <div>
+      {/* {foodList.map((food) => (
+        <div key={food.name}>
           <p>{food.name}</p>
           <img src={food.image} width={100} alt={food.name} />
         </div>
-      ))}
+      ))} */}
+
+      <FoodBox
+        food={{
+          name: 'Orange',
+          calories: 85,
+          image: 'https://i.imgur.com/abKGOcv.jpg',
+          servings: 1,
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/FoodBox.jsx
@@ -0,0 +1,20 @@
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+
+export const FoodBox = ({ food }) => {
+  return (
+    <Col>
+      <Card title={food.name} style={{ width: 230, height: 300, margin: 10 }}>
+        <img src={food.image} height={60} alt="food" />
+        <p>Calories: {food.calories}</p>
+        <p>Servings: {food.servings}E</p>
+        <p>
+          <b>
+            Total Calories: {food.calories} * {food.servings}{' '}
+          </b>{' '}
+          kcal
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
+  );
+};

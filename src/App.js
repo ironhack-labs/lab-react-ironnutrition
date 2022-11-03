ac8439fe35ed9b2847d04520a42158f73cb5f010
Patrick-Groot <patrick@candis.io>
2022-11-03 21:42:19 +0100

    Iteration 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import { Card, Col, Button } from 'antd';
+import { Card, Col, Button, Input, Divider } from 'antd';
 import foods from './foods.json';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function List({ foodsList }) {
   const listItems = foodsList.map((elem) => {
-    return <li key={elem.name}>
-    <FoodBox food={elem}/>
-    </li>;
+    return (
+      <li key={elem.name}>
+        <FoodBox food={elem} />
+      </li>
+    );
   });
   return <ul>{listItems}</ul>;
 }
@@ -15,10 +17,7 @@ export function List({ foodsList }) {
 export function FoodBox({ food }) {
   return (
     <Col>
-      <Card
-        title={food.name}
-        style={{ width: 230, height: 300, margin: 10 }}
-      >
+      <Card title={food.name} style={{ width: 230, height: 300, margin: 10 }}>
         <img src={food.image} height={60} alt="food" />
         <p>{food.calories}</p>
         <p>{food.servings}</p>
@@ -28,16 +27,85 @@ export function FoodBox({ food }) {
         <Button type="primary"> Delete </Button>
       </Card>
     </Col>
-  )
+  );
 }
 
+const defaultState = {
+  name: '',
+  image: '',
+  calories: 0,
+  servings: 0,
+};
 
+export function AddFoodForm({ setFoodsList }) {
+  const [state, setState] = useState(defaultState);
+
+  function handleChange(event) {
+    setState((old) => {
+      let newValue = event.target.value;
+      if (typeof old[event.target.name] === 'number') {
+        newValue = parseFloat(event.target.value);
+      }
+      return { ...old, [event.target.name]: newValue };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newFood = { ...state };
+    setFoodsList((old) => [...old, newFood]);
+    setState(defaultState);
+  }
+
+  useEffect(() => console.log(state), [state]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Divider>Add Food Entry</Divider>
+
+      <label>Name</label>
+      <Input
+        name="name"
+        value={state.name}
+        type="text"
+        onChange={handleChange}
+      />
+
+      <label>Image</label>
+      <Input
+        name="image"
+        value={state.image}
+        type="text"
+        onChange={handleChange}
+      />
+
+      <label>Calories</label>
+      <Input
+        name="calories"
+        value={state.calories}
+        type="number"
+        onChange={handleChange}
+      />
+
+      <label>Servings</label>
+      <Input
+        name="servings"
+        value={state.servings}
+        type="number"
+        onChange={handleChange}
+      />
+
+      <button type="submit">Create</button>
+    </form>
+  );
+}
 
 function App() {
   const [foodsList, setFoodsList] = useState(foods);
   return (
-    <div>      
-        <List foodsList={foodsList} />      
+    <div className="mainDiv">
+      <AddFoodForm setFoodsList={setFoodsList} />
+      <List foodsList={foodsList} />
     </div>
   );
 }

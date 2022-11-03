dab03aa775d782dc48f5b3620a5bf24b3cbf988f
Patrick-Groot <patrick@candis.io>
2022-11-03 20:38:21 +0100

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import './App.css';
-import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import { Card, Col, Button } from 'antd';
 import foods from './foods.json';
 import { useState } from 'react';
 
-function List({ foodsList }) {
+export function List({ foodsList }) {
   const listItems = foodsList.map((elem) => {
     return <li key={elem.name}>
-    <p>{elem.name}</p> 
-    <img src={elem.image} alt={elem.name} width={100}/>
+    <FoodBox food={elem}/>
     </li>;
   });
   return <ul>{listItems}</ul>;
 }
 
+export function FoodBox({ food }) {
+  return (
+    <Col>
+      <Card
+        title={food.name}
+        style={{ width: 230, height: 300, margin: 10 }}
+      >
+        <img src={food.image} height={60} alt="food" />
+        <p>{food.calories}</p>
+        <p>{food.servings}</p>
+        <p>
+          <b>Total Calories: {food.calories * food.servings} </b> kcal
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
+  )
+}
+
+
+
 function App() {
   const [foodsList, setFoodsList] = useState(foods);
   return (
-    <div>
-      <li>
-        <List foodsList={foodsList} />
-      </li>
+    <div>      
+        <List foodsList={foodsList} />      
     </div>
   );
 }

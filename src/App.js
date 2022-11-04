97a076ea40bcd74c2dca11ebb9ba9000fd8da80d
Ryan Essel <spucti19@gmail.com>
2022-11-04 14:56:45 -0400

    iteration 1 - Render a simple list

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import foods from './foods.json';
 
 function App() {
+  const [allFood, setFoods] = foods
+  console.log({ALLFOOD: foods}) 
+
   return (
     <div className="App">
  <iframe src="https://free.timeanddate.com/clock/i8kna5ii/n179/fn6/fs16/fc9ff/tc000/ftb/bas2/bat1/bacfff/pa8/tt0/tw1/th1/ta1/tb4" frameborder="0" width="216" height="60"></iframe>
 
+{foods.map((food) => {
 
+console.log({QUICKTES:food.image})
+return <div>
+  <p> {food.name} </p>
+  <img src={food.image} width={100} />
+</div>
+})}
 
  
     </div>

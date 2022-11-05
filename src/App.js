d66ec5586208de633ee2ab973943fd4bc94f4047
Ryan Essel <spucti19@gmail.com>
2022-11-05 12:30:26 -0400

    Iteration 3 | Render a List of FoodBox Components Completed

--- src/App.js
+++ src/App.js
@@ -9,23 +9,24 @@ function App() {
 
   return (
     <div className="App">
- <iframe src="https://free.timeanddate.com/clock/i8kna5ii/n179/fn6/fs16/fc9ff/tc000/ftb/bas2/bat1/bacfff/pa8/tt0/tw1/th1/ta1/tb4" frameborder="0" width="216" height="60"></iframe>
 
+<div className="foodBox">
 {foods.map((food) => {
 
 console.log({QUICKTES:food.image})
-return <div>
-  <p> {food.name} </p>
-  <img src={food.image} width={100} />
+return <div >
+  <FoodBox  food={{
+    name: food.name,
+    calories: food.calories,
+    image: food.image,
+    servings: food.servings
+  }}/>
+ 
+
 </div>
 })}
+</div>
 
-  <FoodBox food={{
-    name:"Orange",
-    calories: 85,
-    image: "https://i.imgur.com/abKGOcv.jpg",
-    servings: 1
-  }}/>
  
     </div>
   );

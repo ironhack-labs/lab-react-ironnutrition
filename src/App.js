4916adb6326f8626389cb4ae108823930d6d4726
Elidiane Bezerra <elidiane.asb@gmail.com>
2022-11-16 14:34:42 +0000

    done interation 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+
 
 function App() {
+  const [food, setFood] = useState(foods)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {food.map((eachFood) => {
+        return (
+          <div>
+            <p> {eachFood.name} </p>
+            <img src={eachFood.image} width={100} />
+          </div>
+        )
+      })}
     </div>
   );
 }
---
+++ src/components/FoodBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FoodBox() {
+  return (
+    <div>FoodBox</div>
+  )
+}
+
+export default FoodBox

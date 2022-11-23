0a37e6fd80621f3e98e86881e9d8763e72bf16ba
Jandson Costa <jandsoncosta55@gmail.com>
2022-11-23 18:02:34 -0300

    foods in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Row, Divider, Button } from 'antd';
+import foods from './foods.json';
+
 
 function App() {
   return (
     <div className="App">
-      
+     <div className="App">
+     <div>
+  <p> PIZZA </p>
+  <img src="https://i.imgur.com/eTmWoAN.png" width={0} />
+</div>
+      {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {/* Render the list of Food Box components here */}
+      </Row>
+    </div>
     </div>
   );
 }

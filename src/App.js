37783a8716fe0fa66a3657fe9cd2c3fc32e653e5
sijincodes <sijinmathews03@gmail.com>
2022-11-30 14:26:49 +0100

    iteration2

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@
 import './App.css';
 import foods from './foods.json';
 import React, { useState } from 'react';
+import { FoodBox } from './components/FoodBox';
 
 function App() {
-  const [food, setFood] = useState(foods)
-  return (<div>
-  {food.map(elm => {
-      return (
-        <div>
-  <p>{elm.name}</p>
-  <img src={elm.image} alt="img" width={100}/>
-</div>
-      )
-  })
-  }
-
-  </div>);
-
-
+  const [food, setFood] = useState(foods);
+  return (
+    <div>
+      {food.map((elm) => {
+        return (
+          <div>
+            <p>{elm.name}</p>
+            <img src={elm.image} alt="img" width={100} />
+            <FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+}} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;

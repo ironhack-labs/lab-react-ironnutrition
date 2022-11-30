8607443b038c7ffbada02f842a0e103e11798382
Vika Shcherbak <vikashcherbaka@gmail.com>
2022-11-30 21:12:35 +0100

    all done, bonuses too

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import { useState } from "react";
 import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
-import { Row, Button} from 'antd';
+import { Row, Button } from 'antd';
 import Search from './components/Search'
 
 function App() {
 
   const [food, setFood] = useState(foods);
   const [searchTerm, setSearchTerm] = useState("");
-  const[show,setShow]=useState(true);
+  const [show, setShow] = useState(true);
 
-  function addNewFood(newFood){
+  function addNewFood(newFood) {
     const updatedFood = [...food, newFood];
     setFood(updatedFood);
   }
 
-  function removeFood(name){
+  function removeFood(name) {
     const deletedFood = food.filter(food => food.name !== name);
     setFood(deletedFood);
   }
@@ -25,23 +25,24 @@ function App() {
   return <div className="App">
 
     {/* search for food */}
-    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} /> 
+    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 
     {/* add new food */}
-    <Button onClick={()=>setShow(!show)}>{!show ? "Show Form" : "Hide Form" }</Button>
-    {show ? <AddFoodForm addFood={addNewFood}/> :null}
-    
+    <Button onClick={() => setShow(!show)}>{!show ? "Show Form" : "Hide Form"}</Button>
+    {show ? <AddFoodForm addFood={addNewFood} /> : null}
+
 
     {/* map and list all cards depending on search as well */}
-    <Row style={{ width: '100%', justifyContent: 'center' }}>
+    {food.length === 0 ? <p> 😩 </p> : (<Row style={{ width: '100%', justifyContent: 'center' }}>
       {food.filter(el => {
-        return searchTerm === "" ? true : el.name.includes(searchTerm)})
+        return searchTerm === "" ? true : el.name.includes(searchTerm)
+      })
         .map((anyFood) => (
-        <div key={anyFood.name}>
-          <FoodBox food={anyFood} deleteFood={removeFood}/>
-        </div>
-      ))}
-    </Row>
+          <div key={anyFood.name}>
+            <FoodBox food={anyFood} deleteFood={removeFood} />
+          </div>
+        ))}
+    </Row>)}
 
   </div>;
 }

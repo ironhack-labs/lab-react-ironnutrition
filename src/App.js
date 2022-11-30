0781b6c96a0c67546a2a0604c8793f35caa07349
sijincodes <sijinmathews03@gmail.com>
2022-11-30 15:31:09 +0100

    iteration4

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import foods from './foods.json';
 import React, { useState } from 'react';
 import { FoodBox } from './components/FoodBox';
+import AddFoodForm  from './components/AddFoodForm';
+import { v4 as uuidv4 } from "uuid"
 
 function App() {
-  const [food, setFood] = useState(foods);
+  const [food,setFood] = useState(foods)
+ 
   return (
     <div>
+     <AddFoodForm  food={food} setFood={setFood}/>
       {food.map((elm) => {
         return (
-       
+          <div>
             <FoodBox food={elm} />
+            
+    </div>
           
         );
       })}

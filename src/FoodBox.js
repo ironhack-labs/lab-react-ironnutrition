37cd2360e3f61c146694d0c536e5930908f4b6cd
Ioannis Piastopoulos <ioannispiastopoulos@gmail.com>
2022-12-07 17:17:41 +0100

    done

---
+++ src/FoodBox.js
@@ -0,0 +1,27 @@
+import { Card, Col } from 'antd';
+
+
+function FoodBox(props) {
+    const {name, calories, image, servings} = props.food;
+    const {random, food} = props;
+    console.log(props)
+    return (
+
+        <Col> 
+      <Card
+        title={name}
+        style={{ width: 230, height: 300, margin: 10 }} >
+
+          <img src={image} height={60} alt={name} />
+          <p>Calories: {calories}</p>
+          <p>Servings: {servings}</p>
+          <p>
+            <b>Total Calories: {calories*servings} </b> kcal
+          </p>
+          <button type="primary"> Delete </button>
+        <p>{random}</p>
+      </Card></Col>
+    );
+  }
+
+  export default FoodBox;

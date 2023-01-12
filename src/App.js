71f86462a9e5add7e605ab29802e2d7506719ebf
Alfie Collins <alfiecollins44@hotmail.co.uk>
2023-01-12 20:13:36 +0000

    iterations 1-3

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ function App() {
   return(
     <div className="App">
 
-       {foods.map((e) =>{
-return (
-<div id='Food' key= {e.name} >   
-<p className="Name">{e.name}</p>
-<img className="Image" src={e.image} alt='food'/>
-</div> )
-})}
+{foods.map((e) => {
+        return (
+          <div>
+            <FoodBox food={e} />
 
-<FoodBox food={ {
-  name: "Orange",
-  calories: 85,
-  image: "https://i.imgur.com/abKGOcv.jpg",
-  servings: 1
-}} />
-    </div>  
-   )
-}
+    </div>
+
+        );
+      })}
+</div>)}
 export default App;

a6ebfb0173be8d117c139481a9b95028dc7884a0
Anil Ozsoy <anilozsoy@gmail.com>
2023-01-13 13:36:24 +0100

    Iteration 3 | Render a List of FoodBox Components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import './App.css';
 import { useState } from 'react';
 import foods from './foods.json';
 import FoodBox from './components/FoodBox';
-//import { Card, Row, Col, Divider, Input, Button } from 'antd';
-
+import { Row, Divider, Button } from 'antd';
 function App() {
   const [allFoods, setAllFoods] = useState(foods);
 
   return (
     <div className="App">
-      {allFoods.map((food) => {
-        return (
-          <FoodBox food={food} />
-          // <div>
-          //   <p> {food.name} </p>
-          //   <img src={food.image} alt={food.name} width={100} />
-          // </div>
-        );
-      })}
+      {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {allFoods.map((food) => {
+          return <FoodBox food={food} />;
+        })}
+      </Row>
     </div>
   );
 }

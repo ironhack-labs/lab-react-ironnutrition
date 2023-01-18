9755dec8d39a78ccfc026891230a83bb9a52967d
Laura Bährs <laura.baehrs@googlemail.com>
2023-01-18 21:51:38 +0100

    finish iteration 4

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import foodsFromJson from './foods.json';
 import './App.css';
 import { useState } from 'react';
 import FoodBox from './components/Foodbox';
+import AddFoodForm from './components/AddFoodForm';
 //import FoodBox from './components/FoodBox';
 
 function App() {
   const [foodsArray, setFoodsArray] = useState(foodsFromJson);
 
+  const createMeal = (newMealObj) => {
+    setFoodsArray((prevListOfMeal) => {
+      const newList = [newMealObj, ...prevListOfMeal];
+      return newList;
+    });
+  };
+
   return (
     <div className="App">
+    <AddFoodForm callbackToCreate={createMeal} />
       {foodsArray.map((foodObj) => {
         return (
           <div>
---
+++ src/components/AddFoodForm.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { Input } from 'antd';
+
+function AddFoodForm(props) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newFood = {
+      name: name,
+      image: image,
+      calories: calories,
+      servings: servings,
+    };
+    props.callbackToCreate(newFood);
+
+    //clear form
+    setName('');
+    setImage('');
+    setCalories(0);
+    setServings(0);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+
+      <label>Name</label>
+      <Input
+        value={name}
+        type="text"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+
+      <label>Image</label>
+      <Input
+        value={image}
+        type="text"
+        onChange={(e) => {
+          setImage(e.target.value);
+        }}
+      />
+
+      <label>Calories</label>
+      <Input
+        value={calories}
+        type="text"
+        onChange={(e) => {
+          setCalories(e.target.value);
+        }}
+      />
+
+      <label>Servings</label>
+      <Input
+        value={servings}
+        type="text"
+        onChange={(e) => {
+          setServings(e.target.value);
+        }}
+      />
+
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default AddFoodForm;

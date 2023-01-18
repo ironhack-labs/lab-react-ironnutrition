661a52b023f5b0dbe188e1f3caebfb9d1e43d121
Laura Bährs <laura.baehrs@googlemail.com>
2023-01-18 21:29:07 +0100

    finish iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import foodsFromJson from './foods.json';
 import './App.css';
+import { useState } from 'react';
+//import FoodBox from './components/FoodBox';
 
 function App() {
+  const [foodsArray, setFoodsArray] = useState(foodsFromJson);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodsArray.map((foodObj) => {
+        return (
+          <div>
+            <p> {foodObj.name}</p>
+            <img src={foodObj.image} alt="food" width="100px" />
+          </div>
+        );
+      })}
+      ;
     </div>
   );
 }

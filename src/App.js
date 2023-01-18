9b3d92eb2e17462a07e23a1e78ad6e3ca403d3c9
SarahK-coder <117637388+SarahK-coder@users.noreply.github.com>
2023-01-18 18:36:47 +0100

    Completed Iteration 4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import { useState } from 'react';
 import './App.css';
 import foodsFromJson from './foods.json';
 import FoodBox from './components/FoodBox';
+import AddFood from './components/AddFood';
 
 function App() {
   const [foodArr, setFoodArr] = useState(foodsFromJson);
+  
+  const addFood = (newFoodObj) => {
+    //update list of foods
+        setFoodArr( (prevListOfFoods) => {
+            const newList = [newFoodObj, ...prevListOfFoods];
+            return newList;
+        });
+  }
 
   return (
     <div className="App">
       <h1>Food List</h1>
-
+      
+      <AddFood callbackToAdd={addFood} />
       {foodArr.map((foodObj) => {
         return (
           <>

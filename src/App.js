46b4269f6c3adc9951228d263bbe1e027f191771
Marta <martinha_peres@hotmail.com>
2023-01-23 16:32:02 +0000

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -8,14 +8,9 @@ function App() {
 
   return (
     <div className="App">
-      <FoodBox
-        food={{
-          name: 'Orange',
-          calories: 85,
-          image: 'https://i.imgur.com/abKGOcv.jpg',
-          servings: 1,
-        }}
-      />
+      {foodsList.map((food) => {
+        return <FoodBox food={food} />;
+      })}
     </div>
   );
 }

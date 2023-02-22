d2d65fd5a90da615e68778140ff08735d1ede7cf
binghatch <aron.seiffert@gmail.com>
2023-02-22 15:58:56 +0100

    feat: solve lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import foods from './foods.json';
+
+import { Button, Divider, Row, Input } from 'antd';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
+  const [foodListData, setFoodListData] = useState(foods);
+  const [foodList, setFoodList] = useState(foods);
+  const [formVisibility, setFormVisibility] = useState(false);
+
+  const addFood = (food) => {
+    setFoodListData([...foodListData, food]);
+    setFoodList([...foodList, food]);
+  }
+
+  const deleteFood = (name) => {
+    const updatedFoodListData = foodListData.filter(food => food.name !== name);
+    const updatedFoodList = foodList.filter(food => food.name !== name);
+
+    setFoodListData(updatedFoodListData);
+    setFoodList(updatedFoodList);
+  }
+
+  const filterFoods = (e) => {
+    if (e.target.value === "") {
+      setFoodList(foodListData);
+    }
+
+    const filteredFoodList = foodListData.filter(food => food.name.includes(e.target.value))
+    console.log(filteredFoodList);
+    setFoodList(filteredFoodList);
+  }
+
+  const toggleFormVisibility = () => {
+    setFormVisibility(!formVisibility);
+    console.log(formVisibility);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Display Add Food component here */}
+
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        <Button onClick={toggleFormVisibility} type="primary"> Hide Form / Add New Food </Button>
+      </Row>
+
+      {/* Display Search component here */}
+      {formVisibility && <Row style={{ width: '100%', justifyContent: 'center' }}><AddFoodForm addFood={addFood}/></Row>}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '50%', justifyContent: 'center', margin: "auto" }}>
+        <Input placeholder='Search ...' onChange={filterFoods}></Input>
+      </Row>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {foodList.map(food => <FoodBox key={food.name} food={food} deleteFood={deleteFood}/>)}
+      </Row>
     </div>
   );
 }

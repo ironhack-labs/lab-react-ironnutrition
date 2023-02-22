c393b49f77f960d53c0718992347f9e5542be9dc
Maria Fernanda Echeverria Colorado <fernanda.ecmf@gmail.com>
2023-02-22 15:54:51 -0600

    Update FoodBox.js

--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -3,17 +3,21 @@ import { Button } from 'antd';
 import { Card, Col } from 'antd';
 
 function FoodBox(props) {
+  const handleDelete = () => {
+    props.onDelete(props.food.id);
+  };
+
   return (
     <Col>
       <Card title={props.food.name}>
         <img src={props.food.image} alt={props.food.name} width={225} />
 
         <div>
-          <p style={{ fontSize: '25px' }}>Calories: {props.food.calories}</p>
-          <p style={{ fontSize: '25px' }}>Servings: {props.food.servings}</p>
-          <p style={{ fontSize: '25px' }}>Total Calories: {props.food.calories * props.food.servings} kcal</p>
+          <p style={{ fontSize: '25px' }}><strong>Calories: </strong>{props.food.calories}</p>
+          <p style={{ fontSize: '25px' }}><strong>Servings:</strong> {props.food.servings}</p>
+          <p style={{ fontSize: '25px' }}><strong>Total Calories: </strong> {props.food.calories * props.food.servings} kcal</p>
 
-          <Button type="primary">Delete</Button>
+          <Button type="primary" onClick={handleDelete}>Delete</Button>
         </div>
       </Card>
     </Col>

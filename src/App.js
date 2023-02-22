ffd1b3a2363fb5126cdda60864f4bd2d0a7cb148
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-02-22 14:02:32 +0100

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-import foods from "./foods.json";
+import foodsJSON from "./foods.json";
 import { Card, Row, Col, Divider, Input, Button } from "antd";
+import { useState } from 'react';
 
 function App() {
+  const [foods, setFoods] = useState(foodsJSON)
+
   return (
     <div className="App">
-   
+      {foods.map(food => {
+        return (
+    <div>
+  <p> {food.name} </p>
+  <img src={food.image} width={100} />
+  </div>)
+      })}
     </div>
   );
 }

fca8a873b5fdf89c57194b5c458bb95a5678ae44
HenrikH <henrik.hellem@live.com>
2023-02-22 16:52:31 +0100

    Iteration 5

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import foods from './foods.json';
 import { useState } from 'react';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
-
   const [food, setFood] = useState(foods);
+  const [foodData, setFoodData] = useState(foods);
 
   const addNewFood = (newFood) => {
-    const updatedFood = [...food, newFood]
+    const updatedFood = [...food, newFood];
 
-    setFood(updatedFood)
-  }
+    setFood(updatedFood);
+  };
 
+  const searchForFood = (input) => {
+    let filteredFood;
+    input === ''
+      ? (filteredFood = foodData)
+      : (filteredFood = foodData.filter((item) =>
+          item.name.toLocaleLowerCase().includes(input)
+        ));
+
+    setFood(filteredFood);
+  };
+
+  
 
   return (
     <div className="App">
       <h1>Food List</h1>
-      <AddFoodForm addNewFood={addNewFood}/>
+      <AddFoodForm addNewFood={addNewFood} />
+      <Search searchForFood={searchForFood} />
       <div className="container">
         {food.map((item) => {
           return <FoodBox food={item} />;
---
+++ src/components/Search.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { Divider, Input } from 'antd';
+
+
+function Search(props) {
+  const [inputText, setInputText] = useState('');
+
+  const inputHandler = (e) => {
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+
+    props.searchForFood(lowerCase)
+  };
+
+  return (
+    <>
+      <Divider>Search</Divider>
+      <label>Search</label>
+      <Input value={inputText} type="text" onChange={inputHandler} />
+    </>
+  );
+}
+
+export default Search;

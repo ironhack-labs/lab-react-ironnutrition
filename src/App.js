0d1433fff02c1d18930f4fb5600c613dc4fb4289
Maria Fernanda Echeverria Colorado <fernanda.ecmf@gmail.com>
2023-02-22 12:52:03 -0600

    First modification

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import foods from './foods.json';
+import { Divider, Input } from 'antd';
+
 
 function App() {
+  const [foodList] = useState(foods);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodList.map((food) => (
+        <div key={food.name}>
+          <p>{food.name}</p>
+          <img src={food.image} width={100} alt={food.name} />
+        </div>
+      ))}
     </div>
   );
 }

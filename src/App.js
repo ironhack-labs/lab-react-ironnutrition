59965b3caeac37a0b4bc07a1fb2bb21770489714
Zefanja Timorason <z.timorason@gmail.com>
2023-02-24 00:52:08 +0100

    made until iteration 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import foodsData from './foods.json'
 import './App.css';
+import { useState } from 'react';
+import FoodBox from './components/FoodBox';
+import { Row, Divider, Button } from 'antd';
+import AddFoodForm from './components/AddFoodForm';
 
-function App() {
+
+function App(props) {
+  const [foods, setFoods] = useState(foodsData)
+  const [eten, setEten] = useState(foodsData)
+  
+  const addNewFood = (newFood) => {
+    const updatedFoods = [...foods, newFood]
+    const updatedEten = [...eten, newFood]
+    
+    setFoods(updatedFoods)
+    setEten(updatedEten)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronNutrition Lab!</h1>
+      <AddFoodForm AddFoodForm={addNewFood} />
+      {/* {foods.map((food) => {
+        return <div><p>{food.name}</p>
+          
+          <img src={food.image} alt="FoodPicture" width={200}/>
+        </div>
+      })}
+
+      <FoodBox food={{
+        name: "orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings:1
+      }} /> */}
+      <Row style={{width:'100%', justifyContent:'center'}}>
+      {foods.map((food) => {
+        return <FoodBox food={food} />
+      })}
+      </Row>
     </div>
   );
 }
---
+++ src/components/AddFoodForm.jsx
@@ -0,0 +1,53 @@
+import { Divider, Input } from "antd";
+import { useState } from 'react';
+
+function AddFoodForm(props) {
+    const[name,setName] = useState('')
+    const [image, setImage] = useState('')
+    const [calories, setCalories] = useState(0)
+    const [servings, setServings] = useState(0)
+    
+    const handleImageInput = (e) => setImage(e.target.value)
+    const handleCaloriesInput = (e) => setCalories(e.target.value)
+    const handleServingsInput = (e) => setServings(e.target.value)
+    const handleNameInput = (e) => setName(e.target.value)
+  
+    const handleSubmit = (e) => {
+        e.preventDefault()
+  
+        const newFood = { name,image, calories, servings }
+        console.log(newFood)
+        props.AddFoodForm(newFood)
+
+        setName('')
+        setImage('')
+        setCalories(0)
+        setServings(0)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+      <Divider>Add Food Entry</Divider>
+
+      <label>Name</label>
+            {/* <Input value={name} type="text" onChange={() => {}} /> */}
+            <Input value={name} type='text' onChange={handleNameInput} />
+
+      <label>Image</label>
+            {/* render antd <Input /> type="text" here */}
+            <Input type="text" value={image} onChange={handleImageInput} />
+
+      <label>Calories</label>
+            {/* render antd <Input /> type="number" here */}
+            <Input type="text" value={calories} onChange={handleCaloriesInput} />
+
+      <label>Servings</label>
+            {/* render antd <Input /> type="number" here */}
+            <Input type="text" value={servings} onChange={handleServingsInput} />
+
+      <button type="submit">Create</button>
+    </form>
+    )
+}
+
+export default AddFoodForm

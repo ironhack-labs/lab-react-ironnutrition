14e8b4229a1bb34c8a50388a6aa5122dfb3db520
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-26 21:38:54 +0100

    imported React from react at App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import foods from './foods.json';
+import React from 'react';
 
 function App() {
   return <div className="App"></div>;

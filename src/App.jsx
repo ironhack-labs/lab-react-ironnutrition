fbf45c8e2c916120999f21a08eca623f7f887771
Joanadgomes <joanadiogog@gmail.com>
2023-03-01 21:34:52 +0000

    lab solved + bonus

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import Search from './components/Search';
 function App() {
   const [foods, setFood] = useState(foodsJSON);
   const [showFood, setShowFood] = useState(foodsJSON);
+  const [visible, setVisible] = useState(true);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
 
   const addToList = (food) => {
     const foodCopy = [food, ...foods];
@@ -16,37 +21,47 @@ function App() {
   };
 
   const searchFoods = (query) => {
-    const filteredFoods = foods.filter((food) => food.name.toLowerCase().includes(query.toLowerCase()));
+    const filteredFoods = foods.filter((food) =>
+      food.name.toLowerCase().includes(query.toLowerCase())
+    );
     setShowFood(filteredFoods);
-  }
+  };
 
   const deleteFood = (foodName) => {
     const filteredFoods = foods.filter((food) => {
       return food.name !== foodName;
-    })
+    });
+    setFood(filteredFoods);
     setShowFood(filteredFoods);
-  }
+  };
 
   return (
     <div className="App">
       <div className="food-list">
         <h1>Food List</h1>
-        <Search searchFoods = {searchFoods}/>
-        <AddFoodForm addToList={addToList} />
-
-        {showFood.map((food) => {
-          return (
-            <FoodBox
-              key={food.name} deleteFood={deleteFood}
-              food={{
-                name: food.name,
-                calories: food.calories,
-                image: food.image,
-                servings: food.servings,
-              }}
-            />
-          );
-        })}
+        <button onClick={toggleVisibility}>
+          {visible ? 'Hide Form' : 'Add New Food'}
+        </button>
+        {visible && <AddFoodForm addToList={addToList} />}
+        <Search searchFoods={searchFoods} />
+        {showFood.length ? (
+          <div>
+            {showFood.map((food) => (
+              <FoodBox
+                key={food.name}
+                deleteFood={deleteFood}
+                food={{
+                  name: food.name,
+                  calories: food.calories,
+                  image: food.image,
+                  servings: food.servings,
+                }}
+              />
+            ))}
+          </div>
+        ) : (
+          <p>Oops! There in no more content to show.</p>
+        )}
       </div>
     </div>
   );

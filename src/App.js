112644cdb309825c4488f982bca2c216b6767be1
unknown <contacto@erikazdesign.com>
2023-03-01 20:49:50 +0100

    notallsolved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+import foods from "./foods.json";
+import { useState } from 'react';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import Searchbar from './components/Searchbar';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [foodList, setFoodList] = useState(foods);
+
+  const addFood = (food) => {
+    const foodListCopy = [...foodList, food]
+    setFoodList(foodListCopy)
+  }
+
+  const searchFoods = (query) => {
+    const filteredFoods = foods.filter((food) =>
+    food.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFoodList(filteredFoods);
+  };
+
+  const deleteFoods = (foods) => {
+    const deletedFoods = foods.filter((food) => {
+      return food.name !== foods;
+    });
+    setFoodList(deletedFoods);
+  };
+
+  
+
+  return <div className="App">
+
+
+
+    <AddFoodForm addFood={addFood} 
+    />
+
+    {/* <Searchbar searchFoods={searchFoods} /> */}
+
+    <Searchbar searchFoods={searchFoods} />
+      {foods.map((food) => {
+        return <FoodBox food={food} deleteFoods={deleteFoods} key={food.name} />;
+      })}
+
+    {foodList.map((food) => {
+      return <FoodBox food={food} key={food.name}/>;
+    })}
+
+
+
+
+
+  </div>;
+
+
+
 }
 
 export default App;
+

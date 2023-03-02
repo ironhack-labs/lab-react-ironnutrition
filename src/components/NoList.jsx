c42214432b9ceb5b34f7a478b8ede997faa4ea74
Jizzus <miguel.angelo.jesus@hotmail.com>
2023-03-02 09:01:15 +0000

    final touch

--- src/components/NoList.jsx
+++ src/components/NoList.jsx
@@ -4,7 +4,9 @@ import logo from '../logo.svg';
 
 function noList() {
   return (
-    <div> <object data={logo} style={{width: 300, height: 300}}> </object> </div>
+    <div> <h2>Oops! There's no more food to show!</h2>
+    <object data={logo} style={{width: 300, height: 300}}> </object> 
+    </div>
   )
 }
 

0c071a93eb4c7dab2fecc8543b4c859bbf5973e6
Barbara Rodrigues <barbara.rodrigues@cabify.com>
2023-03-11 10:24:30 +0100

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from "./foods.json"
+import { v4 as uuidv4 } from 'uuid'; // Universal Unique Identifier
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }

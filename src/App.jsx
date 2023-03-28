e726c765fe49771b0babddab1a09584664f88663
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-03-28 15:33:29 +0200

    Iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import foods from './foods.json';
-import './App.css';
 import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import './App.css';
 
 function App() {
+  const [foodList, setFoodList] = useState(foods);
+
+  const handleSubmit = (value) => {
+    setFoodList((prev) => [value, ...prev]);
+  };
+
   return (
     <div className="App">
-      <FoodBox
-        food={{
-          name: 'Orange',
-          calories: 85,
-          image: 'https://i.imgur.com/abKGOcv.jpg',
-          servings: 1,
-        }}
-      />
+      <AddFoodForm addFood={handleSubmit} />
+
+      {foodList?.map((food, i) => (
+        <FoodBox key={i} food={food} />
+      ))}
     </div>
   );
 }

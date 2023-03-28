8d8064bd187e6ef3dab71d820e5feef01f0d0a98
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-03-28 14:48:50 +0200

    Iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import foods from './foods.json';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {foods?.map((food, i) => (
+        <div>
+          <p> {food.name} </p>
+          <img src={food.image} width={50} alt={food.name} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;

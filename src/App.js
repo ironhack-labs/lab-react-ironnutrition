3b59e242d7c446eb01bc6fcd6ca89a129a4b98f5
silvia Edreira <silviaedreiragg@gmail.com>
2023-03-29 19:54:00 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import foods from "./foods.json";
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import SimpleList from './components/SimpleList';
+import FoodBox from './components/FoodBox';
 
 function App() {
   const [allFoods, setAllFoods] = useState(foods)
   return (
     <div>
-    <SimpleList />
-    
+      <SimpleList />
+      <FoodBox food={{
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      }} />
     </div>
   );
 }

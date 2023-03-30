7ee1f579168d4c5c42f25796afd3f72f7ff65728
Ander Zulaika <anderzu85@gmail.com>
2023-03-30 16:44:55 +0200

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+import foods from "./foods.json";
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App () {
+
+  const [foodList, setFoodList] = useState (foods);
+
+  return <div className="App">
+    
+        {foodList.map(food => (
+          <div className="d-flex flex-row border border-dark m-1 ">
+          <span>{food.name}</span>
+          <img src={food.image} alt= {food.name} height={50} />
+          </div>
+        ))}
+      
+  </div>
 }
-
 export default App;

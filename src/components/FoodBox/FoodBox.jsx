4ee0a34ed81ab37cf945a106ea7873d4205a41ac
Ana Gutiérrez Ruiz <agutrui@hotmail.com>
2023-03-31 12:03:48 +0200

    iterations 2 and 3

--- src/components/FoodBox/FoodBox.jsx
+++ src/components/FoodBox/FoodBox.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import { Card, Col, Button } from 'antd';
 
 function FoodBox({ food }) {
+
+  const totalCalories = food.calories * food.servings
+
   return (
-    <div>
-      <p>{food.name}</p>
-      <p>{food.calories} kcal</p>
-      <img src={food.image} style={{width: 300}}  alt="food image"/>
-    </div>
+    <Col span={8}>
+      <Card
+        title={food.name}
+        style={{ width: 230, height: 300, margin: 10 }}
+      >
+        <img src={food.image} height={60} alt="food" />
+        <p>Calories: {food.calories} kcal</p>
+        <p>Servings: {food.servings}</p>
+        <p>
+          <b>Total Calories: {totalCalories} </b> kcal
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
   )
 }
 
--- src/components/Foodlist/Foodlist.jsx
+++ src/components/Foodlist/Foodlist.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import FoodBox from '../FoodBox/FoodBox'
+import { Row } from 'antd'
 
 function Foodlist({ foods }) {
   return (
     <div className="container">
       <h1>Food List</h1>
-      {foods.map(food => <FoodBox food={food} key={food.name} />)}
+      <Row>
+        {foods.map(food => <FoodBox food={food} key={food.name} />)}
+      </Row>
     </div>
   )
 }

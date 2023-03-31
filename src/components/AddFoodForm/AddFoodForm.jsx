d0b8e5b9d20579cffcae3b80a715ef491f3f61fc
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@Anas-iMac.home>
2023-04-01 00:28:06 +0200

    bonus: iteration  7

--- src/components/AddFoodForm/AddFoodForm.jsx
+++ src/components/AddFoodForm/AddFoodForm.jsx
@@ -1,4 +1,4 @@
-import { Divider, Input } from 'antd';
+import { Divider, Input, Button } from 'antd';
 import { useState } from 'react';
 
 const emptyFood = {
@@ -11,6 +11,7 @@ const emptyFood = {
 function AddFoodForm({ addFood }) {
 
   const [food, setFood] = useState(emptyFood);
+  const [showAddForm, setShowAddForm] = useState(false);
 
 
   const handleChange = (event) => {
@@ -29,25 +30,36 @@ function AddFoodForm({ addFood }) {
     setFood(emptyFood);
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <Divider>Add Food Entry</Divider>
+  if (showAddForm) {
 
-      <label htmlFor="name">Name</label>
-      <Input value={food.name} id="name" type="text" onChange={handleChange} />
+    return (
+      <form onSubmit={handleSubmit} className="">
+        <Divider>Add Food Entry</Divider>
+        <div className='m-3'>
+          <label htmlFor="name">Name</label>
+          <Input value={food.name} id="name" type="text" onChange={handleChange} />
 
-      <label htmlFor='image'>Image</label>
-      <Input value={food.image} id="image" type="text" onChange={handleChange} />
+          <label htmlFor='image'>Image</label>
+          <Input value={food.image} id="image" type="text" onChange={handleChange} />
 
-      <label htmlFor="calories">Calories</label>
-      <Input value={food.calories} id="calories" type="number" onChange={handleChange} />
+          <label htmlFor="calories">Calories</label>
+          <Input value={food.calories} id="calories" type="number" onChange={handleChange} />
 
-      <label htmlFor="servings">Servings</label>
-      <Input value={food.servings} id="servings" type='number' onChange={handleChange} />
+          <label htmlFor="servings">Servings</label>
+          <Input value={food.servings} id="servings" type='number' onChange={handleChange} />
 
-      <button type="submit">Create</button>
-    </form>
-  );
+          <button type="submit" className='mt-3 btn btn-secondary'>Create</button>
+        </div>
+        <Button onClick={() => setShowAddForm(!showAddForm)}>Hide Form</Button>
+      </form>
+    );
+  } else {
+    return (
+      <Button onClick={() => setShowAddForm(!showAddForm)}>Add New Food</Button>
+    )
+
+  }
 }
+
 
 export default AddFoodForm;

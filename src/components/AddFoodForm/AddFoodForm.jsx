53f92d27ceea3174cf819e67b233c91d6c74d449
Victor Lopez <victorlpz2301@gmail.com>
2023-04-02 20:05:09 +0200

    Iteration 7 Done

--- src/components/AddFoodForm/AddFoodForm.jsx
+++ src/components/AddFoodForm/AddFoodForm.jsx
@@ -1,4 +1,4 @@
-import { Divider, Input } from 'antd';
+import { Divider, Input, Button } from 'antd';
 import { useState } from 'react';
 
 function AddFoodForm({ onFoodCreated }) {
@@ -7,6 +7,7 @@ function AddFoodForm({ onFoodCreated }) {
   const [image, setImages] = useState('');
   const [calories, setCalories] = useState(0)
   const [servings, setServings] = useState(0)
+  const [activeAdd, setActiveAdd] = useState(true)
 
 
   const handleSubmit = (event) => {
@@ -18,30 +19,34 @@ function AddFoodForm({ onFoodCreated }) {
     setServings(0)
   }
 
+  const handleOnclick = () => {
+    setActiveAdd(!activeAdd)
+  }
+
 
   return (
-    <form className='mb-5 p-3' onSubmit={handleSubmit} >
-      <Divider>Add Food Entry</Divider>
-
-      <label>Name</label>
-      <Input value={name} type="text" onChange={(event) => { setNames(event.target.value) }} />
-
-      <label>Image</label>
-      <Input value={image} type="text" onChange={(event) => { setImages(event.target.value) }} />
-
-      <label>Calories</label>
-      <Input value={calories} type="number" onChange={(event) => { setCalories(event.target.value) }} />
-
-      <label>Servings</label>
-      <Input value={servings} type="number" onChange={(event) => { setServings(event.target.value) }} />
-
-      <button className='btn btn-primary mt-1' type="submit">Create</button>
-    </form>
+    <div className='mb-5'>
+      {activeAdd && (<>
+        <form className='mb-2 p-3' onSubmit={handleSubmit} >
+          <Divider>Add Food Entry</Divider>
+          <label>Name</label>
+          <Input value={name} type="text" onChange={(event) => { setNames(event.target.value) }} />
+          <label>Image</label>
+          <Input value={image} type="text" onChange={(event) => { setImages(event.target.value) }} />
+          <label>Calories</label>
+          <Input value={calories} type="number" onChange={(event) => { setCalories(event.target.value) }} />
+          <label>Servings</label>
+          <Input value={servings} type="number" onChange={(event) => { setServings(event.target.value) }} />
+          <button className='btn btn-primary mt-1' type="submit">Create</button>
+        </form>
+      </>)}
+      <div><Button onClick={handleOnclick}> {activeAdd ? "Hide Form" : "Add New Food"} </Button></div>
+    </div>
   );
 }
 
 AddFoodForm.defaultValue = {
-  onFoodCreated: () => {}
+  onFoodCreated: () => { }
 }
 
 

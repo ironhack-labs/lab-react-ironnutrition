c2e805820be68a511fc0ffab350e1992b43f5577
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-04 11:14:01 +0200

    Iteration 1: add state variable for foodlist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 // src/App.js
 import './App.css';
 import foods from './foods.json';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [food, setFoods] = useState([...foods]);
+
+  const foodList = food.map((foodItem) => {
+    return (
+      <div>
+        <p> {foodItem.name} </p>
+        <img src={foodItem.image} alt="pics of food" width={150} />
+      </div>
+    );
+  });
+
+  return (
+    <div className="App">
+      <h1>Food List</h1>
+      {foodList}
+    </div>
+  );
 }
 export default App;

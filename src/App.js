3e1c3c2127b4ca96c1cf55f11d6cefc91afa2fa7
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-04 13:40:20 +0200

    Add searchbar, solve iteration 5

--- src/App.js
+++ src/App.js
@@ -3,26 +3,39 @@ import './App.css';
 import foods from './foods.json';
 import { useState } from 'react';
 import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import Searchbar from './components/Searchbar';
 
 function App() {
   const [food, setFoods] = useState([...foods]);
 
-  // const foodList = food.map((foodItem) => {
-  //   return (
-  //     <div>
-  //       <p> {foodItem.name} </p>
-  //       <img src={foodItem.image} alt="pics of food" width={150} />
-  //     </div>
-  //   );
-  // });
+  const addFood = (newFood) => {
+    const updatedFoodList = [...food, newFood];
+    setFoods(updatedFoodList);
+  };
 
-  const foodBox = food.map((foodItem) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const searchList = food.filter((foodItem) =>
+    foodItem.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const foodBox = searchList.map((foodItem) => {
     // console.log('FoodBox Food Item', foodItem);
     return <FoodBox key={foodItem.name} food={foodItem} />;
   });
 
   return (
     <div className="App">
+      <Searchbar search={searchTerm} onSearch={handleSearch} />
+
+      <div className="add-form">
+        <AddFoodForm addFood={addFood} />
+      </div>
       <h1>Food List</h1>
 
       <div className="boxes-container ">{foodBox}</div>
---
+++ src/components/Searchbar.js
@@ -0,0 +1,20 @@
+import { Divider, Input } from 'antd';
+
+// Iteration 5
+function Searchbar({ search, onSearch }) {
+  return (
+    <>
+      <Divider>Search</Divider>
+
+      <label>Search</label>
+      <Input
+        placeholder="Your search here"
+        value={search}
+        type="text"
+        onChange={onSearch}
+      />
+    </>
+  );
+}
+
+export default Searchbar;

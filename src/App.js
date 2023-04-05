283c4083fb06e697f25b88c1e95ed8da7bc4ecb4
drose123456 <acg_danial@hotmail.com>
2023-04-05 18:03:46 +0100

    second iteration complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import foods from "./foods.json";
 import React, { useState } from 'react'
+import FoodBox from './components/FoodBox'
 
 function App () {
   const [foodList, setFoodList] = useState(foods);
-    return(foodList.map((foods) => (
+    return foodList.map((foods) => (
       <div>
 <p> {foods.name} </p>
 <img className="foods-image" src={foods.image}/>
 </div>
-    )))};
-
+    ))}
+<FoodBox foods={{
+      name: foods.name,
+      calories: foods.calories,
+      image: foods.image,
+      servings: foods.servings
+}} />
 export default App;

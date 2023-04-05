a1398c4d5d624085ee888b87711e4e5f4c8a5f3f
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-04-05 16:23:05 +0200

    Add first iteration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import foods from './foods.json';
 import './App.css';
+
+// ANTD IMPORTS
+import { Row, Divider, Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Divider>Food List</Divider>
+      {foods.map((foodObj) => {
+        return (
+          <div>
+            <p>{foodObj.name}</p>
+            <img src={foodObj.image} alt={foodObj.name} width={100} />
+          </div>
+        )
+      })}
     </div>
   );
 }

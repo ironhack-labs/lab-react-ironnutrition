986b2969df7445c8325dbab29341224f3bff2694
Ismael Freitas <ismael.freitas@sapo.pt>
2023-04-05 15:28:02 +0100

    Implement Iteration 2 and 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import foods from "./foods.json";
+import FoodBox from "./components/FoodBox";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       {foods.map(food => {
+
         return(
-          <div>
-            <p>{food.name}</p>
-            <img src={food.image} width={100} alt={food.name} />
-          </div>
+          <FoodBox food={ {
+            name: food.name,
+            calories: food.calories,
+            image: food.image,
+            servings: food.servings
+          }} />
         );
+
       })}
     </div>
   );

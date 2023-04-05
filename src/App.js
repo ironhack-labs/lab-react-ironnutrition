85e927aaa8d5fae36326cc7cf00ac1e91d463d5f
Ismael Freitas <ismael.freitas@sapo.pt>
2023-04-05 16:25:52 +0100

    Implement Ieration 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import foods from "./foods.json";
 import FoodBox from "./components/FoodBox";
 import AddFoodForm from "./components/AddFoodForm";
+import Search from "./components/Search";
 import './App.css';
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 function App() {
 
   const [foodsArr, setFoodsArr] = useState([...foods]);
+
+  const [query, setQuery] = useState("");
+  
+  const filteredFoodsArr = useMemo(() => { 
+    return foodsArr.filter((food) => {
+      return food.name.toLowerCase().includes(query.toLowerCase());
+    })
+  }, [query, foodsArr]);
 
   const addNewFood = (newFoodObj) => {
 
@@ -16,12 +25,21 @@ function App() {
 
   };
 
+
   return (
     <div className="App">
-      <h1>Food List</h1>
+      <h1>Add Food Entry</h1>
       <AddFoodForm addNewFood={addNewFood}/>
+      <hr />
+
+      <label>Search:
+        <Search query={query} setQuery={setQuery} />
+      </label>
+      <hr />
+
+      <h1>Food List</h1>
       <div className="food-container">
-        {foodsArr.map(food => {
+        {filteredFoodsArr.map(food => {
 
           return(
             <FoodBox food={ food } />

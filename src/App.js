91db660969d00d1b7adc6301976195da67176f38
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-05 16:56:38 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from "react";
 import foods from "./foods.json";
+import FoodBox from "./components/FoodBox";
 
 
 
@@ -16,7 +17,12 @@ function App() {
   <div>
   <p>{foodObj.name}</p> 
   <img src={foodObj.image}  alt={foodObj.name}/>
-
+  <FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+}} />
 
   </div>
  
---
+++ src/components/FoodBox.js
@@ -0,0 +1,24 @@
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+function FoodBox(props) {
+    console.log(props);
+  return (
+    <div>
+     
+        <Col>
+          <Card>
+           <p>{props.food.name} </p> 
+            <img src={props.food.image} height={60} alt="food" />
+            <p>Calories:{props.food.calories}</p>
+            <p>Servings: {props.food.servings}</p>
+            <p>
+              <b>Total Calories: {props.food.calories} * {props.food.servings} </b> kcal
+            </p>
+            <Button type="primary"> Delete </Button>
+          </Card>
+        </Col>
+
+    </div>
+  );
+}
+
+export default FoodBox;

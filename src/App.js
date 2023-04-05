7fd01e7e60ea7f7615e1090e9b73117b4a90a6ed
Peng Chen <chenpeng1221@outlook.com>
2023-04-05 16:24:13 +0200

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
+import { useState } from 'react';
 import './App.css';
 import foods from "./foods.json";
 
 function App() {
+
+  const [foodsArr, setFoodsArr] = useState(foods);
+
   return (
     <div className="App">
+      {foodsArr.map((foodsDetails) => {
+        return (
+          <div>
+            <p> {foodsDetails.name} </p>
+            <img src={foodsDetails.image} />
+          </div>
+        )
+      })
+      }
     </div>
   );
 }

3b39f7a1c11665c785e30ccf34cdb3adc80ae71c
Peter Sekan <sekan.pete@gmail.com>
2023-04-05 17:50:50 +0200

    it.5 done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-
 import { useState } from 'react';
 import './App.css';
-import foods from "./foods.json";
-import Foodbox from "./component/FoodBox";
+import foods from './foods.json';
+import Foodbox from './component/FoodBox';
 import { Row, Divider, Button } from 'antd';
 import AddFoodForm from './component/AddFoodForm';
-function App() {
+import Search from './component/Search';
 
+function App() {
   const [foodsArr, setFoodsArr] = useState(foods);
   const addFood = (newFood) => {
     console.log(newFood);
 
     setFoodsArr((prevFoodsArr) => {
       return [newFood, ...prevFoodsArr];
-    })
+    });
+  };
 
-  }
+  const searchFilter = (searchDetail) => {
+    const newList = foodsArr.filter((food) => {
+      return food.name.includes(searchDetail);
+    });
+
+    setFoodsArr(newList);
+  };
 
   return (
     <div className="App">
       <AddFoodForm addFood={addFood} />
+      <Search searchFilter={searchFilter} />
       <Divider>Food List</Divider>
 
       <Row style={{ width: '100%', justifyContent: 'center' }}>
         {foodsArr.map((foodsDetails, index) => {
-          return <Foodbox foodsDetails={foodsDetails} key={index} />
-        })
-        }
+          return <Foodbox foodsDetails={foodsDetails} key={index} />;
+        })}
       </Row>
     </div>
   );

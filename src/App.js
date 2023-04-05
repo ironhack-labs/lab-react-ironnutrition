399d611afe262d07e177ac07eae2306c8acc0768
Ismael Freitas <ismael.freitas@sapo.pt>
2023-04-05 15:53:45 +0100

    Implement Iteration 4
    Co-authored-by: Èric Vega Noguera <ericveganoguera@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import foods from "./foods.json";
 import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [foodsArr, setFoodsArr] = useState([...foods]);
+
+  const addNewFood = (newFoodObj) => {
+
+    setFoodsArr((prevFoodsArr) =>
+      [newFoodObj, ...prevFoodsArr]
+    );
+
+  };
+
   return (
     <div className="App">
-      {foods.map(food => {
+      <h1>Food List</h1>
+      <AddFoodForm addNewFood={addNewFood}/>
+      <div className="food-container">
+        {foodsArr.map(food => {
 
-        return(
-          <FoodBox food={ {
-            name: food.name,
-            calories: food.calories,
-            image: food.image,
-            servings: food.servings
-          }} />
-        );
+          return(
+            <FoodBox food={ food } />
+          );
 
-      })}
+        })}
+      </div>
+
     </div>
   );
 }

e03ae4c5056c5930f4bcf3ec10c2ae5919482284
valyy151 <marin.valenta1@gmail.com>
2023-04-05 17:48:58 +0200

    Iteration 4 complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { useState } from 'react';
 import FoodBox from './components/FoodBox';
 import foodsFromJson from './foods.json';
+import AddFoodForm from './components/AddFoodForm';
 import './App.css';
 
 function App() {
   const [foodItems, setFoodItems] = useState(foodsFromJson);
 
+  const handleAddFood = (newFood) => {
+    setFoodItems([newFood, ...foodItems]);
+  };
+
   return (
     <div>
-      {foodItems.map((food) => (
-        <FoodBox
-          key={food.name}
-          food={{
-            name: food.name,
-            calories: food.calories,
-            image: food.image,
-            servings: food.servings,
-          }}
-        />
-      ))}
+      <AddFoodForm onAddFood={handleAddFood} />
+      <div className="wrap">
+        {foodItems.map((food) => (
+          <FoodBox
+            key={food.name}
+            food={{
+              name: food.name,
+              calories: food.calories,
+              image: food.image,
+              servings: food.servings,
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,13 +1,16 @@
 import { Card, Col, Button } from 'antd';
 
 function FoodBox(props) {
+  const defaultImage =
+    'https://images.unsplash.com/photo-1517765808589-729a992ea0ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
+
   return (
     <Col>
       <Card
         title={props.food.name}
         style={{ width: 230, height: 400, margin: 10 }}
       >
-        <img src={props.food.image} height={60} alt="food" />
+        <img src={props.food.image || defaultImage} height={60} alt="food" />
         <p>Calories: {props.food.calories}</p>
         <p>Servings: {props.food.servings}</p>
         <p>

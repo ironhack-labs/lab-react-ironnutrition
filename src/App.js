081431d08e2b2cde2ac63d16e5489936d4be61e7
ABenat1988 <ale.benat@gmail.com>
2023-04-05 17:46:13 +0200

    iteration 5git add .git add .git add .git add .git add .git add .git add .git add .git add .

--- src/App.js
+++ src/App.js
@@ -7,28 +7,47 @@ import foods from './foods.json';
 
 import Foodbox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 import { Row, Divider } from 'antd';
 
 function App() {
   const [foodsArray, setFoodsArray] = useState(foods);
+  const [filterFoodsArray, setFilterFoodsArray] = useState(foods);
 
   const addFood = (newFood) => {
     setFoodsArray((prevFoodArray) => {
       return [newFood, ...prevFoodArray];
     });
-  }
+    setFilterFoodsArray((prevFoodArray) => {
+      return [newFood, ...prevFoodArray];
+    });
+  };
+
+  const searchFood = (searchValue) => {
+    setFilterFoodsArray(
+      foodsArray.filter((element) =>
+        element.name
+          .toLowerCase()
+          .includes(searchValue.target.value.toLowerCase())
+      )
+    );
+  };
 
   return (
     <div className="App">
       <Divider>Add Food Entry</Divider>
 
-      <AddFoodForm callbackNewFood={addFood}/>
+      <AddFoodForm callbackNewFood={addFood} />
+
+      <Divider>Search</Divider>
+
+      <Search callbacSearchFood={searchFood} />
 
       <Divider>Food List</Divider>
 
       <Row style={{ width: '100%', justifyContent: 'center' }}>
-        {foodsArray.map((foodObj, index) => {
+        {filterFoodsArray.map((foodObj, index) => {
           return <Foodbox key={index} foodItem={foodObj} />;
         })}
       </Row>

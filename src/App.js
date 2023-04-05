38894e79c34aba2c2acf2e881e534061e110deae
Sophie White <sophie.white@mordrid.net>
2023-04-05 16:52:46 +0200

    First iteration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import foods from './foods.json';
+
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let foodsFromJson= foods;
+const [foodArr, setFoodArr] = useState(foodsFromJson);
+
+return (
+  <div>
+    {foodArr.map((food) => (
+      <div key={food.id}>
+        <p>{food.name}</p>
+        <img src={food.image} width={200} alt={food.name} />
+      </div>
+    ))}
+  </div>
+);
+    }
+
 
 export default App;

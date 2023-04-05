a19f3d2234914921fe50414a64119d064225555a
Ismael Freitas <ismael.freitas@sapo.pt>
2023-04-05 15:18:49 +0100

    Implement Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import foods from "./foods.json";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foods.map(food => {
+        return(
+          <div>
+            <p>{food.name}</p>
+            <img src={food.image} width={100} alt={food.name} />
+          </div>
+        );
+      })}
     </div>
   );
 }

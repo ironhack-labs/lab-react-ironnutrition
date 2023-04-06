66e4f106a217a4b0090813c3bf5cd98df986f572
David Reguart <reguartdavid@gmail.com>
2023-04-07 00:50:26 +0200

    Iteration 3 FoodBox List Components + Style => Done

--- src/App.js
+++ src/App.js
@@ -8,34 +8,22 @@ function App() {
   const [foods, setFoods] = useState(foodList)
 
   return (
-      <div className="App font-[Montserrat] bg-gray-800 text-white p-8">
+    <div className="App font-[Montserrat] bg-gray-800 text-white p-8">
 
-        <h1 className="text-5xl font-black text-center">Food List</h1>
+      <h1 className="text-5xl font-black text-center">Food List</h1>
 
-        <div>
-        <FoodBox food={
-          {
-            "name": "Orange",
-            "calories": 85,
-            "image": "https://i.imgur.com/abKGOcv.jpg",
-            "servings": 1
-          }
-        } />
-      </div>
-
-        <div className="flex justify-center my-6">
-          <div className="w-2/3 grid grid-cols-3 place-items-center">
-            {foods.map((food) => (
-              <div key={food.name} className="w-60 h-40 my-8 rounded-xl border border-gray-400 hover:scale-110 transition-all duration-300 ease-in-out hover:cursor-pointer hover:bg-white hover:border-0 hover:text-gray-800 hover:font-semibold flex flex-col items-center justify-center">
-                <div>
-                  <p className="text-center mb-2">{food.name}</p>
-                  <img src={food.image} alt={food.name} className="w-32 h-auto rounded-xl" />
-                </div>
+      <div className="flex justify-center my-6">
+        <div className="w-2/3 grid grid-cols-3 place-items-center">
+          {foods.map((food) => (
+            <div key={food.name}>
+              <div className="hover:scale-110 transition-all duration-300 ease-in-out hover:bg-white hover:bg-opacity-10 hover:shadow-lg hover:shadow-gray-600 my-10">
+                <FoodBox food={food} />
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
+    </div>
 
   );
 }

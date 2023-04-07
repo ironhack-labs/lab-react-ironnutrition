7e49f5e970a08588d5a90c11e510cca819748d4e
Alex Garrido Baldrich <alex.garrido.baldrich@gmail.com>
2023-04-07 16:29:55 +0200

    iteration 5

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import { useState } from 'react';
 import foodsDB from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
   const [foods, setFoods] = useState(foodsDB);
+  const [search, setSearch] = useState('')
 
   function handleOnFoodAdded(foodAdded) {
     setFoods([foodAdded, ...foods])
   }
 
+  const handleTextRecived = (inputText) => {
+    setSearch(inputText);
+  }
+
   return (
     <div>
+      <Search onSearchTextAdded={handleTextRecived}/>
       <AddFoodForm onFoodAdded={handleOnFoodAdded}/>
       <div className="App d-flex flex-wrap">
-        {foods.map((food) => (
+        {foods.filter((food) => food.name.toLowerCase().includes(search.toLowerCase())).map((food) => (
           <FoodBox key={food.name} food={food} />
         ))}
       </div>

5a30a0095cdc1e2f7685f2e5a05e2b2a43392b00
michael07200 <mickmhtong@gmail.com>
2023-04-18 20:42:53 +0200

    build: Iteration 5

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import jsonData from './foods.json';
 
 import AddFoodForm from './components/AddFoodForm';
 import FoodBox from './components/FoodBox';
+import SearchBar from './components/SearchBar';
 
 const ProductsPage = () => {
   const [foods, setFoods] = useState(jsonData);
+  const [search, setSearch] = useState('');
+
+  const filteredFoods = foods.filter((food) => {
+    return food.name.toLowerCase().includes(search.toLowerCase());
+  });
 
   return (
     <div>
       <h1>IronStore</h1>
+      <SearchBar setSearch={setSearch} />
       <AddFoodForm foods={foods} setFoods={setFoods} />
-      <FoodBox foods={foods} />
+      <FoodBox foods={filteredFoods} />
     </div>
   );
 };

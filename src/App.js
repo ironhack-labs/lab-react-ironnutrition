73dec12fe43e5f6587305bc0994f0198736454e3
michael07200 <mickmhtong@gmail.com>
2023-04-18 19:25:49 +0200

    build: Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Food list</h1>
+      {foods.map((food) => (
+        <div>
+          <p>{food.name}</p>
+          <img src={food.image} width={100} />
+        </div>
+      ))}
+      )
     </div>
   );
 }
-
 export default App;

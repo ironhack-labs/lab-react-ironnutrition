2b47ec018ab9d0cf9665e8a2266b6a4876dada3f
ross-u <cirkovic.uros.srb@gmail.com>
2023-04-27 02:33:28 +0200

    feat: update style-guide files

--- style-guide/FoodBox.example.jsx
+++ style-guide/FoodBox.example.jsx
@@ -2,23 +2,19 @@
 // In this file you can find a reference example of the structure
 // and content that the component should render.
 // Remember to import Ant Design components before using them.
-import { Card, Col, Button } from 'antd';
+import { Card, Col, Button } from "antd";
 
-// Iteration 2
-function FoodBox(props) {
+function FoodBox() {
   return (
     <Col>
-      <Card
-        title={'FOOD_NAME_HERE'}
-        style={{ width: 230, height: 300, margin: 10 }}
-      >
-        <img src={'FOOD_IMAGE_HERE'} height={60} alt="food" />
-        <p>Calories: FOOD_CALORIES_HERE</p>
-        <p>Servings: FOOD_SERVINGS_HERE</p>
+      <Card title={"FOOD_NAME"} style={{ width: 230, height: 300, margin: 10 }}>
+        <img src={"FOOD_IMAGE"} height={60} alt="food" />
+        <p>Calories: FOOD_CALORIES</p>
+        <p>Servings FOOD_SERVINGS</p>
         <p>
-          <b>Total Calories: FOOD_CALORIES * FOOD_SERVINGS </b> kcal
+          <b>Total Calories: FOOD_CALORIES * FOOD_SERVINGS</b> kcal
         </p>
-        <Button type="primary"> Delete </Button>
+        <Button type="primary" onClick={() => {}}> Delete </Button>
       </Card>
     </Col>
   );

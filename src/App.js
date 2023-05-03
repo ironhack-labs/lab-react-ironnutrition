1265e295dbdcc7ce95a4141c192e28c61e36c41d
HeimLi <127536715+HeimLi@users.noreply.github.com>
2023-05-03 17:08:49 +0200

    3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Row, Divider, Button } from 'antd';
+import foods from "./foods.json";
+import FoodBox from "./components/FoodBox";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Display Add Food component here */}
+
+      {/* <Button> Hide Form / Add New Food </Button> */}
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {foods.map(food => 
+          <FoodBox key={food._id} food={food}/>
+          )}
+      </Row>
     </div>
   );
 }

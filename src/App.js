030c2e509e8d702a8cf2aa66021e10b7fde55d1d
beckjosh <Joshbeck17@gmail.com>
2023-05-03 17:55:56 +0200

    "Pull Request #1"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+// import { Card, Row, Col, Divider, Input, Button } from "antd";
+import { useState } from "react";
+import foods from "./foods.json";
+import FoodBox from './components/Foodbox';
+
+const foodsList = [...foods]
 
 function App() {
+  //Save the imported foods from the JSON file in a state variable
+  const [foods, setFoods] = useState(foodsList);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="foods">
+      <div>
+        <span></span>
+        {foods.map((food) => {
+          return <div key={foods._id}>
+            <FoodBox food />
+          </div>
+        })}
+      </div>
     </div>
   );
 }

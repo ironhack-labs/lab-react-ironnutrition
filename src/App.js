289286c0174f9f2d852853d4e12cb39e124cdb34
Lilly Liefers <lillyliefers@me.com>
2023-05-04 09:28:44 +0200

    Startedlab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+import foods from "./foods.json";
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+  const [foodList, setFoodList] = useState (foods);
+
+  return <div className="App">
+    {foodList.map((food) => (
+        <div key={food.id}>
+          <p>{food.name}</p>
+          <img src={food.image} alt={food.name} width={100} />
+        </div>
+     ))}
+
+  </div>;
+
 }
-
-export default App;
+ export default App;

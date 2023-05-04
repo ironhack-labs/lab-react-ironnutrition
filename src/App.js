d768137e6d2d8addd217c10ddfed67b17eb3aff2
bojana-petroska <bojana.petroska@yahoo.com>
2023-05-04 14:44:26 +0200

    refactored

--- src/App.js
+++ src/App.js
@@ -2,51 +2,81 @@ import './App.css';
 import foodsFromJSON from "./foods.json";
 import React, { useState } from 'react'
 import FoodBox from './components/FoodBox';
-import { Row, Divider, Button } from 'antd';
+import { Col, Row, Divider, Button, Layout } from 'antd';
 import AddFoodForm from './components/AddFoodForm';
 import OnSearch from './components/Search';
+import { findByLabelText } from '@testing-library/react';
+import EmptyList from './components/EmptyList';
+const { Content } = Layout;
 
 function App() {
 
   const [foods, setFoods] = useState(foodsFromJSON)
+  const foodsBackUp = foodsFromJSON
 
+  
   const handleAddFood = (newFood) => {
     setFoods([...foods, newFood]);
   };
-
+  
   const filterFoodList = (str) => {
     let filteredFoods;
     if (str === '') {
-      filteredFoods = foods;
+      filteredFoods = foodsBackUp;
     } else {
-      filteredFoods = foods.filter((food) => {
+      filteredFoods = foodsBackUp.filter((food) => {
         return food.name.toLowerCase().includes(str.toLowerCase());
       });
     }
     setFoods(filteredFoods);
   };
-
-  const deleteFood = (e) => {
-    const newFoods = [...foods];
-    newFoods.splice(e, 1);
+  
+  const deleteFood = (foodToDelete) => {
+    const newFoods = foods.filter(food => {
+      return food !== foodToDelete
+    });
     setFoods(newFoods);
+  };
+   
+  const contentStyle = {
+    display: 'flex',
+    wrap: 'wrap',
+    justifyContent: 'space-around',
+    textAlign: 'center',
+    //minHeight: 120,
+    lineHeight: '120px',
+    //color: '#fff',
+    backgroundColor: '#108ee9',
+    maxWidth: '80%',
+    margin: '0 150px'
   };
   
 
   return (
     <div className="App">
         <Row>
-          <AddFoodForm addFood={handleAddFood} />
+          <Col span={14} offset={5}>
+            <AddFoodForm addFood={handleAddFood} />
+          </Col>
         </Row>
         <Row>
-          <OnSearch onSearch={filterFoodList}/>
+          <Col span={14} offset={5}>
+            <OnSearch onSearch={filterFoodList}/>
+          </Col>
         </Row>
       <Divider>Food List</Divider>
-        <Row>
-        {foods.map((food) => (
-          <FoodBox key={food.name} food={food} deleteFood={() => deleteFood()} />
-        ))}
-        </Row>
+        <Layout>       
+          <Content style={contentStyle}>
+          {/* {!deleteFood.length && <EmptyList />} */}
+            <Row style={{ width: '80%', justifyContent: 'space-around' }}>
+              {/* <Col span={14} offset={5}> */}
+                {foods.map((food) => (
+              <FoodBox key={food.name} food={food} deleteFood={() => deleteFood(food)} />
+                ))}
+              {/* </Col> */}
+            </Row>
+          </Content>
+        </Layout>
 
       <Button> Hide Form / Add New Food </Button>
 
@@ -60,5 +90,4 @@ function App() {
 }
 
 export default App
-
 
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -3,11 +3,12 @@ import { Card, Col, Button } from 'antd';
 // Iteration 2
 function FoodBox(props) {
     const { food, deleteFood } = props
+    const { foodToDelete } = props
     return (
     <Col>
       <Card
         title={food.name}
-        style={{ width: 230, height: 300, margin: 10 }}
+        style={{ width: 230, margin: 10 }}
       >
         <img src={food.image} height={60} alt="food" />
         <p>Calories: {food.calories}</p>
@@ -15,7 +16,7 @@ function FoodBox(props) {
         <p>
           <b>Total Calories: {food.calories} </b> kcal
         </p>
-        <Button type="primary" onClick={deleteFood}> Delete </Button>
+        <Button type="primary" onClick={() => deleteFood(food)}> Delete </Button>
       </Card>
     </Col>
   );

5da122b698cc408af39c7624ab69fbbd9149745b
Jogurgithub412 <bitterspine@hotmail.com>
2023-05-25 01:03:59 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import foodsData from './foods.json';
+import { Row, Divider } from 'antd';
+import { useState } from 'react';
+
+import FoodBox from './Components/FoodBox';
+import AddFoodForm from './Components/AddFoodForm';
+import Search from './Components/Search';
 
 function App() {
+  const [foods, setFoods] = useState(foodsData);
+  const [filteredFoods, setFilteredFoods] = useState(foodsData);
+
+  const addNewFood = (newFood) => {
+    const updatedFoods = [...foods, newFood];
+    setFoods(updatedFoods);
+  };
+
+  const handleSearch = (filteredFoods) => {
+    setFilteredFoods(filteredFoods);
+  };
+  const handleDelete = (foodToDelete) => {
+    const updatedFoods = foods.filter((food) => food !== foodToDelete);
+    setFoods(updatedFoods);
+    setFilteredFoods(updatedFoods);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddFoodForm addNewFood={addNewFood} />
+      <Search foods={foods} onSearch={handleSearch} />
+      <Divider>Food List</Divider>
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {filteredFoods.map((food) => (
+          <FoodBox key={food.name} food={food} onDelete={handleDelete} />
+        ))}
+      </Row>
     </div>
   );
 }

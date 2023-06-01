1d954bd67a3e5c36ee32ea10b0650cbb31b7c261
Yvo de Rooij <110634893+YvodeRooij@users.noreply.github.com>
2023-06-01 21:43:14 +0200

    finished five iterations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import AddFoodForm from './components/AddFoodForm';
+import FoodBox from './components/FoodBox';
+import SearchBar from './components/SearchBar';
+import foods from './foods.json';
+import { useState } from 'react';
 
 function App() {
+  const [food, setFood] = useState(foods);
+  const [search, setSearch] = useState('');
+
+  const filteredFood = food.filter((foodItem) => {
+    return foodItem.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Iron Food</h1>
+      <AddFoodForm allFood={food} setFood={setFood} />
+      <SearchBar searchWord={search} setSearch={setSearch} />
+      {filteredFood.map((foodItem, index) => {
+        return (
+          <div>
+            <FoodBox food={foodItem} key={index} />
+          </div>
+        );
+      })}
     </div>
   );
 }

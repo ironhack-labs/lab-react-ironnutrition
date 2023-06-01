87eb972799a619f13ff22611af709ea0c83e7bfe
MaaX59 <Maax.strandberg@gmail.com>
2023-06-01 20:59:10 +0200

    displaying food cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Card, Row, Col, Divider, Input, Button } from "antd";
+import foods from "./foods.json";
+import { useState } from 'react';
+import FoodBox from './components/FoodBox';
 
 function App() {
+  const  [food, setFood]  = useState(foods);
+  console.log({food});
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Food List</h2>
+      
+      {food.map((food) =>
+        <FoodBox
+      food={food} />
+        )}
+
+
+
+      
     </div>
   );
 }

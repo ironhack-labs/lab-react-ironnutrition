1ec28abace38a4242784389f6d3ac51c65630f18
Lena Imdahl <mail@lenaimdahl.de>
2023-06-02 08:44:31 +0200

    added iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import { Row } from 'antd';
-import foods from './foods.json';
+import { useState } from 'react';
+import { Row, Divider } from 'antd';
+import foodsJSON from './foods.json';
 import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
+  const [foods, setFoods] = useState(foodsJSON);
   return (
     <div>
+      <AddFoodForm foods={foods} setFoods={setFoods} />
+      <Divider>Food List</Divider>
       <h1>Food List</h1>
       <Row style={{ width: '100%', justifyContent: 'center' }}>
         {foods.map((food) => {
---
+++ src/components/AddFoodForm.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import { Input, Divider } from 'antd';
+
+function AddFoodForm({ foods, setFoods }) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState('');
+  const [servings, setServings] = useState(true);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newFood = {
+      name,
+      image,
+      calories,
+      servings,
+    };
+    setFoods([newFood, ...foods]);
+    setName('');
+    setImage('');
+    setCalories('');
+    setServings('');
+  }
+
+  return (
+    <div>
+      <h4>Add new food</h4>
+      <form onSubmit={handleSubmit}>
+        <Divider>Add Food Entry</Divider>
+        <label>
+          <h4>Name</h4>
+          <Input
+            type="text"
+            value={name}
+            name="name"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+        </label>
+        <label>
+          <h4>Image URL</h4>
+          <Input
+            type="text"
+            name="image"
+            value={image}
+            onChange={(event) => {
+              setImage(event.target.value);
+            }}
+          />
+        </label>
+        <label>
+          <h4>Calories</h4>
+          <Input
+            type="text"
+            name="calories"
+            value={calories}
+            onChange={(event) => {
+              setCalories(event.target.value);
+            }}
+          />
+        </label>
+        <label>
+          <h4>Servings</h4>
+          <Input
+            type="text"
+            name="servings"
+            checked={servings}
+            onChange={(event) => {
+              setServings(event.target.value);
+            }}
+          />
+        </label>
+        <button type="submit">Add new Food</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddFoodForm;

5e0a6b82783f31d9b8adc02694e4fad577554715
Lena Imdahl <mail@lenaimdahl.de>
2023-06-02 09:00:24 +0200

    added iteration 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,29 @@ import './App.css';
 import { useState } from 'react';
 import { Row, Divider } from 'antd';
 import foodsJSON from './foods.json';
+import SearchBar from './components/SearchBar';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
 
 function App() {
   const [foods, setFoods] = useState(foodsJSON);
+  const [search, setSearch] = useState('');
+
+  const filteredFoods = foods.filter((food) => {
+    const searchCheck = food.name.toLowerCase().includes(search.toLowerCase());
+
+    return searchCheck;
+  });
+  //  setFoods(filteredFoods);
+
   return (
     <div>
+      <SearchBar search={search} setSearch={setSearch} />
       <AddFoodForm foods={foods} setFoods={setFoods} />
       <Divider>Food List</Divider>
       <h1>Food List</h1>
       <Row style={{ width: '100%', justifyContent: 'center' }}>
-        {foods.map((food) => {
+        {filteredFoods.map((food) => {
           return (
             <FoodBox
               food={{

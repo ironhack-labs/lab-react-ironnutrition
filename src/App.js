1724221466e6bc4337a957eeabf6386d29658e1a
olgam <olgamartyniuk88@gmail.com>
2023-06-02 14:55:04 +0200

    working on searchbar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import { useState } from 'react';
 import './App.css';
 import AddFoodForm from './components/AddFoodForm';
 import FoodBox from './components/FoodBox';
-import foodsJson from "./foods.json";
+import SearchBar from './components/SearchBar';
+import foodsJson from './foods.json';
 
 function App() {
   const [foods, setFoods] = useState(foodsJson);
+  const [search, setSearch] = useState('');
 
   return (
     <div className="App">
       <h1>Food List</h1>
-      <AddFoodForm allFoods={foods} setFoods={setFoods}/>
-      <div className='food-box-map'>
-        {foods.map((foodItem) => 
-          <FoodBox singleFood={foodItem} />
-        )}
+      <AddFoodForm allFoods={foods} setFoods={setFoods} />
+      <SearchBar search={search} setSearch={setSearch} />
+      <div className="food-box-map">
+        {foods
+          .filter((oneFoodItem) => {
+            if (
+              oneFoodItem.name
+                .toLowerCase()
+                .includes(search.toLocaleLowerCase())
+            ) {
+              return true;
+            }
+          })
+          .map((foodItem) => {
+            return <FoodBox singleFood={foodItem} />;
+          })}
       </div>
-     
     </div>
   );
 }

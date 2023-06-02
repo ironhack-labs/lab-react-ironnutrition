34f1ba1010130be0cb18155ed78aafc1adfdc5c6
Natassa <112552395+natko22@users.noreply.github.com>
2023-06-02 13:12:14 +0200

    done with iteration 2 3 and 4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { useState } from 'react';
 import './App.css';
 import foods from './foods.json';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import { Divider } from 'antd';
 
 function App() {
   const [foodList, setFoodList] = useState(foods);
+
   return (
     <div>
-      {foodList.map((food) => {
-        return (
-          <div>
-            <p>{food.name}</p>
-            <img src={food.image} alt={food.name} width={100} />
-          </div>
-        );
-      })}
+      <AddFoodForm foodList={foodList} setFoodList={setFoodList} />
+      <Divider>Food List </Divider>
+      <div className="foodList">
+        {foodList.map((food) => {
+          return <FoodBox key={food.name} food={food} />;
+        })}
+        ;
+      </div>
     </div>
   );
 }

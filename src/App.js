0e1da36ac246a1c6c68af2acfe80d71dd7fcd409
Natassa <112552395+natko22@users.noreply.github.com>
2023-06-02 14:53:04 +0200

    done with iteration 5

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
 import { Divider } from 'antd';
+import Search from './components/Search';
 
 function App() {
   const [foodList, setFoodList] = useState(foods);
+  const [input, setInput] = useState('');
+  const filteredFoods = foodList.filter((food) => {
+    return food.name.toLowerCase().includes(input.toLowerCase());
+  });
 
   return (
     <div>
       <AddFoodForm foodList={foodList} setFoodList={setFoodList} />
+      <Search setFoodList={setFoodList} input={input} setInput={setInput} />
       <Divider>Food List </Divider>
       <div className="foodList">
-        {foodList.map((food) => {
+        {filteredFoods.map((food) => {
           return <FoodBox key={food.name} food={food} />;
         })}
-        ;
       </div>
     </div>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Divider, Input } from 'antd';
 
-function Search() {
-  return <div>Search</div>;
+function Search({ input, setInput }) {
+  const handleSearch = (event) => {
+    setInput(event.target.value);
+  };
+  return (
+    <>
+      <Divider>Search</Divider>
+
+      <label>Search</label>
+      <Input value={input} type="text" onChange={handleSearch} />
+    </>
+  );
 }
 
 export default Search;

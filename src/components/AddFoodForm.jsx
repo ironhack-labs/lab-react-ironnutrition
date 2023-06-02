4adf897505605e7f424c4604749d60aa49fc04b9
olgam <olgamartyniuk88@gmail.com>
2023-06-02 13:55:13 +0200

    form styling done

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -24,12 +24,12 @@ function AddFoodForm({ allFoods , setFoods }) {
     return (
       <>
           <div className="food-form">
-            <h4>Add a new food item</h4>
+            <h3>Add a new food item</h3>
             <form onSubmit={handleAddFood}>
-                <input type="text" value={name} onChange={(event) => {setName(event.target.value)}} />
-                <input type="text" value={image} onChange={(event) => {setImage(event.target.value)}} />
-                <input type="text" value={calories} onChange={(event) => {setCalories(event.target.value)}} />
-                <input type="text" value={servings} onChange={(event) => {setServings(event.target.value)}} />
+                <input type="text" value={name} placeholder="name" onChange={(event) => {setName(event.target.value)}} />
+                <input type="text" value={image} placeholder="image url" onChange={(event) => {setImage(event.target.value)}} />
+                <input type="text" value={calories} placeholder="calories" onChange={(event) => {setCalories(event.target.value)}} />
+                <input type="text" value={servings}  placeholder="servings" onChange={(event) => {setServings(event.target.value)}} />
                 <button type="submit">ADD ITEM</button>
             </form>
         

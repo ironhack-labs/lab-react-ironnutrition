c8ac9d5339e175cc569c4159e3cfa15e8594a794
raqueldfrutos <raqueldfrutos@gmail.com>
2023-06-04 00:39:40 +0200

    It 4 & 5

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import foodsDataJSON from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
   const [foods, setFoods] = useState(foodsDataJSON);
-  console.log(foods)
+  const [foodsCopy] = useState(foodsDataJSON)
+  
+  const AddNewFood = (newFood) =>{
+    const updatedList = [...foods, newFood]
+    setFoods(updatedList)
+  }
+
+  const showFilteredFoods = (search) => {
+    const filteredList = foodsCopy.filter(food => {
+      return food.name.toLowerCase().includes(search.toLowerCase())
+    })
+    setFoods(filteredList);
+  }
+
 
   return (
     <div className="App">
       <Divider>Food List</Divider>
-      <AddFoodForm />
+      <AddFoodForm AddNewFood={AddNewFood}/>
+
+      <Divider>Search for a food</Divider>
+      <Search showFilteredFoods={showFilteredFoods}/> 
 
       <Divider>Food List</Divider>
       <Row style={{ width: '100%', justifyContent: 'center' }}>
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,25 +1,45 @@
-import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import { Input } from 'antd';
+import { useState } from 'react';
 
 
-function AddFoodForm () {
+function AddFoodForm (props) {
+
+    const {AddNewFood} = props;
+
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [calories, setCalories] = useState(0);
+    const [servings, setServings] = useState(0);
+
+    const handleNameInput = (event) => { setName(event.target.value)}
+    const handleImageInput = (event) => { setImage(event.target.value)}
+    const handleCaloriesInput = (event) => { setCalories(event.target.value)}
+    const handleServingsInput = (event) => { setServings(event.target.value)}
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newFood = {name, image, calories, servings}
+        AddNewFood(newFood)
+        console.log('NEW FOOD CREATED', newFood)
+    }
+
+    
     return (
         <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="name">Name</label>
-            <Input  type="text" />
+            <Input  type="text" value={name} onChange={handleNameInput} />
 
             <label htmlFor="image">Image</label>
-            <Input  type="text" />
+            <Input  type="text" value={image} onChange={handleImageInput}/>
 
             <label htmlFor="calories">Calories</label>
-            <Input  type="number" />
+            <Input  type="number" value={calories} onChange={handleCaloriesInput}/>
 
             <label htmlFor="servings">Servings</label>
-            <Input  type="number" />
+            <Input  type="number" value={servings} onChange={handleServingsInput} />
 
-            <button> Create </button>
-
-
+            <button type="submit"> Create </button>
         </form>
 
         </div>

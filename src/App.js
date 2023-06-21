b7dfe6b87b220c69deca9f6d1dacc36c8838c298
Jani Ojala <jani@stptrade.com>
2023-06-21 21:41:10 +0200

    done with rendering foodbox components and implemented add food form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
+  const [foodsToDisplay, setFoodsToDisplay] = useState(foods);
+
+  const createFood = (newFood) => {
+    // new list = an array with the new movie + all the movies we had before
+    const newList = [newFood, ...foodsToDisplay];
+
+    // update state
+    setFoodsToDisplay(newList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Divider>Food List</Divider>
+      <AddFoodForm callbackToCreate={createFood} />
+
+      {foodsToDisplay.map((foodObj, index) => {
+        return (
+          <header className="App-header">
+            <div>
+              {/* key = {index}; */}
+              <FoodBox foodDetails={foodObj} />
+            </div>
+          </header>
+        );
+      })}
     </div>
   );
 }

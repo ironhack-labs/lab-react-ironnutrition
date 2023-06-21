3b3f9dc735a30ecc409d0af927769269d5960dab
“Gokce <gokceberndt@gmail.com>
2023-06-21 21:27:44 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import { useState } from 'react';
+import FoodBox from './components/FoodBox';
 import logo from './logo.svg';
 import foods from './foods.json';
 import './App.css';
-
-
+import { Row, Divider, Button } from 'antd';
 
 function App() {
-
   const [foodsToDisplay, setFoodsToDisplay] = useState(foods);
 
-  return (
-    
-    <div className="App">
-      <h1>Food List</h1>
-      <hr/>
-      {foodsToDisplay.map((foodObj, index) => {
-        return (
-          <div>
-            
-            <p>{foodObj.name}</p>
-            <img src={foodObj.image} alt={foodObj.name} width={100} />
-          </div>
-        );
-      })}
-    </div>
-  );
+//   return (
+//     <div className="App">
+//             <Divider>Food List</Divider>
+//       {foodsToDisplay.map((foodObj, index) => {
+//         return (
+//           <div>
+      
+//             <Row style={{ width: '100%', justifyContent: 'center' }}>
+//               <FoodBox key={index} foodDetails={foodObj} />
+//             </Row>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+return (
+  <div className="App">
+    <Divider>Food List</Divider>
+    <Row gutter={[16, 16]} justify="center">
+      {foodsToDisplay.map((foodObj, index) => (
+        <FoodBox key={index} foodDetails={foodObj} />
+      ))}
+    </Row>
+  </div>
+);
 }
 
 export default App;
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,11 +1,27 @@
+import { Card, Col, Button } from 'antd';
 
-
-
-
-function FoodBox (props){
-
-    
-
-    return
-
+function FoodBox(props) {
+  return (
+    <Col>
+      <Card
+        title={props.foodDetails.name}
+        style={{ width: 230, height: 320, margin: 10 }}
+      >
+        <img src={props.foodDetails.image} height={60} alt="food" />
+        <p>Calories: {props.foodDetails.calories}</p>
+        <p>
+          Servings: <strong>{props.foodDetails.servings}</strong>
+        </p>
+        <p>
+          <strong>
+            Total Calories: {props.foodDetails.calories * props.foodDetails.servings}
+          </strong>{' '}
+          kcal{' '}
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
+  );
 }
+
+export default FoodBox;

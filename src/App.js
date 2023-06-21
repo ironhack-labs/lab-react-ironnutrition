4225495a082481bab24b6fb1feef4066da981f68
JCouderc87 <jeremy.couderc@yahoo.fr>
2023-06-21 18:49:51 +0200

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import foods from './foods.json';
 import { useState } from 'react';
 import FoodBox from './component/FoodBox';
+import AddFoodForm from './component/AddFoodForm';
+import Search from './component/Search';
+import Search from 'antd/es/transfer/search';
 
 
 function App() {
 
-  const [foodToDisplay, setFoodToDisplay] = useState(foods);
+  const [foodToDisplay, setFoodToDisplay] = useState(foods.slice(0, 5));
+
+  const createFood = (newFood) => {
+    const newList = [newFood, ...foodToDisplay];
+
+    // update state
+    setFoodToDisplay(newList);
+  }
 
   return (
     <div className="App">
+      <Search />
+      <AddFoodForm callbackToCreate={createFood}/>
+
       {foodToDisplay.map((food) => {
         return (
         <FoodBox oneFood={food}/>

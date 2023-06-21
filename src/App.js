b2384c595767deceafefc57d046d63ac5662b4bf
gtildis <gretil_15@hotmail.com>
2023-06-21 17:52:52 +0200

    Iteration 1 | Render a Simple List

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import { useState } from 'react';
 import foods from './foods.json';
+import './App.css';
+//import { Card, Row, Col, Divider, Input, Button } from "antd"
+import { Card, Divider } from 'antd';
 
 function App() {
-  return <div className="App"></div>;
+  const [foodsToDisplay, setfoodsToDisplay] = useState(foods);
+
+  return (
+    <div className="App">
+      <Divider>Food List</Divider>
+      {foodsToDisplay.map((foodie) => {
+        return (
+          <Card>
+            <p> {foodie.name} </p>
+            <img src={foodie.image} alt={foodie.title} width={80} />
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;

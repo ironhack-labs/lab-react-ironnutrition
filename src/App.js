8a819a460377f76489e3931e289299d938448ede
Philippe Miossec <pmiossec@gmail.com>
2023-06-21 17:30:12 +0200

    Iteration 3 | Render a List of FoodBox Components

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ function App() {
   return (
     <div className="App">
     <h1>Food List</h1>
-    {foods.map(f => <div key={f.name}>
-  <p> {f.name} </p>
-  <img src={f.image} width={200} alt={f.name} />
-</div>)}
 
-<FoodBox food={ {
-  name: "Orange",
-  calories: 85,
-  image: "https://i.imgur.com/abKGOcv.jpg",
-  servings: 1
-}} />
+    <div className='food-list'>
+      {foods.map(f => <FoodBox key={f.name} food={f}/>)}
+
+      <FoodBox food={ {
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      }} />
+    </div>
     </div>
   );
 }

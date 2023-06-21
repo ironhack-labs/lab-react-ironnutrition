3d8e841b928b5dd1272cf651c9ac351cb4c0215d
hymced <hymced@gmail.com>
2023-06-21 16:44:47 +0200

    add example ant

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@
 
 import './App.css';
 import foods from "./foods.json";
-import { Card, Row, Col, Divider, Input, Button } from "antd";
 
 function App () {
   return <div className="App"></div>;

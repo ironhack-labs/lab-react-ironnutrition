c314254552a67eb6b22bbc06e944fb564d13c94a
gtildis <gretil_15@hotmail.com>
2023-06-21 19:44:57 +0200

    Iteration 5 | Implement search bar

--- src/App.js
+++ src/App.js
@@ -5,21 +5,30 @@ import './App.css';
 import { Divider, Row } from 'antd';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
   const [foodsToDisplay, setFoodsToDisplay] = useState(foods);
+  const [search, setSearch] = useState('');
 
   const addFoodie = (newFoodie) => {
     const newList = [newFoodie, ...foodsToDisplay];
     setFoodsToDisplay(newList);
   };
+
+  const filteredFoods = foodsToDisplay.filter((food) => {
+    return food.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <div className="App">
       <AddFoodForm callbackAdd={addFoodie} />
 
+      <Search search={search} setSearch={setSearch} />
+
       <Divider>Food List</Divider>
       <Row style={{ width: '100%', justifyContent: 'center' }}>
-        {foodsToDisplay.map((foodie, index) => {
+        {filteredFoods.map((foodie, index) => {
           return <FoodBox key={index} foodie={foodie} />;
         })}
       </Row>

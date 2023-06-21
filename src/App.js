220a4e9960174d4e06f363416389fe3aef9d8b60
Lucas Silva <gaviao.lucas@gmail.com>
2023-06-21 22:25:21 +0100

    done, stopped at search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, Row, Col, Divider, Input, Button } from "antd";
+import foods from "./foods.json";
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App () {
+
+  const [foodToDisplay, setFoodList] = useState(foods);
+
+  const createFood = (newFood) => {
+  
+    console.log(newFood)
+    const newFoodToDisplay = [newFood, ...foodToDisplay];
+
+    setFoodList(newFoodToDisplay)
+  }
+  
+  const deleteFood = (foodName) => {
+    const newList = foodToDisplay.filter((foodObj)=> {
+      return foodObj.name !== foodName;
+    })
+    setFoodList(newList);
+}
+// const filteredFood = (foodToDisplay.filter((foodObj) => {
+//   return(
+//     foodObj.toLowerCase().includes(query.toLowerCase())
+//   )
+// }))
+
+
+return (
+  <div className="App">
+
+    <AddFoodForm callbackToCreate={createFood} />
+
+    <Button> Hide Form / Add New Food </Button>
+
+    {/* <Search /> */}
+
+    <Divider>Food List</Divider>
+
+    <Row style={{ width: '100%', justifyContent: 'center' }}>
+      {foodToDisplay.map((foodObj, index) => {
+          return (
+            <FoodBox 
+          key={index}
+          food={foodObj}
+          callbackToDelete={deleteFood}
+          />
+          );
+        })}
+      </Row>
+
     </div>
   );
 }
-
 export default App;

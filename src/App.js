4842d824a470045db5e9bd392fe53c2154acd43c
Emily Stevenson <e.stevenson6@hotmail.co.uk>
2023-06-21 21:03:00 +0100

    iteration 8 complete - by Emily

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { Row, Divider, Button } from 'antd';
 import { useState } from 'react';
 import foods from './foods.json';
-import FoodBox from './components/FoodBox'
+import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
 import Search from './components/Search';
 
 function App(props) {
   const [foodsToDisplay, setFoodsToDisplay] = useState(foods);
-  const [query, setQuery] = useState("")
+  const [query, setQuery] = useState('');
+  const [message, setMessage] = useState('')
 
-  const filteredFoods = foodsToDisplay.filter(foods => {
-   return foods.name.toLowerCase().includes(query.toLowerCase())
-  })
+  const filteredFoods = foodsToDisplay.filter((foods) => {
+    return foods.name.toLowerCase().includes(query.toLowerCase());
+  });
 
   const addFood = (newFood) => {
     const newList = [...foodsToDisplay, newFood];
@@ -21,30 +22,30 @@ function App(props) {
   const deleteFood = (foodName) => {
     const newList = foodsToDisplay.filter((element) => {
       return element.name !== foodName;
-    })
-    setFoodsToDisplay(newList);
-  }
-
-  
+    });
+    if (newList.length > 0){
+    setFoodsToDisplay(newList);}
+    else {
+      setFoodsToDisplay(newList);
+      setMessage("Fuck Yeah!")}
+  };
 
   return (
     <div className="App">
       <Button> Hide Form / Add New Food </Button>
 
-      <AddFoodForm foodsToDisplay={foodsToDisplay} addFood={addFood}/>
+      <AddFoodForm foodsToDisplay={foodsToDisplay} addFood={addFood} />
 
-      <Search query={query} setQuery={setQuery}/>
+      <Search query={query} setQuery={setQuery} />
 
       <Divider>Food List</Divider>
 
-
       <Row style={{ width: '100%', justifyContent: 'center' }}>
-      {filteredFoods
-      .map(function (foodObj) {
-        return (
-          <FoodBox foodObj={foodObj} deleteFood={deleteFood}/>
-        );
-      })}
+        {filteredFoods.map(function (foodObj) {
+          return <FoodBox foodObj={foodObj} deleteFood={deleteFood} />
+          ;
+        })}
+        {message}
       </Row>
     </div>
   );

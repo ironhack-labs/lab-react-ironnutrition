dafa73ca7d6c31659859830f2d1b5b00768d4c3c
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-07-27 18:31:06 -0600

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import './App.css';
+import dishes from './foods.json'
+import { Row, Divider, Button } from 'antd'
 
 function App() {
+  const [ dishesData, setDichesData ] = useState(dishes);
+
   return (
     <div className="App">
+      <Divider>
+        <h2>Food List</h2>
+      </Divider>
+      {dishesData.map((food) => {
+        return(
+          <Row>
+            <p>{food.name}</p>
+            <img src={food.image} width={200}/>
+          </Row>
+        )
+      })}
     </div>
   );
 }

0c278538a5d6c6b2e9832637cbecb02d82d583cf
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-07-29 12:34:44 -0600

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
+// Style Guide:
+// In this file you can find a reference example of the structure
+// and content that the component should render.
+// Remember to import Ant Design components before using them.
+import { Row, Divider, Button, Col } from 'antd';
+import FoodBox from './components/FoodBox'
+import foodData from './foods.json'
 import { useState } from 'react';
-import './App.css';
-import foods from './foods.json'
-import { Row, Divider, Button } from 'antd'
-import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
-  const [ foodsData, setFoodsData ] = useState(foods);
+  const [foods, setFoods ] = useState(foodData);
+  
+  const addFood = (newFood) => {
+    setFoods(prevFoods => [ newFood, ...prevFoods])
+  }
 
   return (
     <div className="App">
-      <Divider>
-        <h2>Food List</h2>
-      </Divider>
-          <Row>
-            {foodsData.map((food, i) => {
-              return <FoodBox key={food.name + 1} food={food} id={i}/>
-            })}
-          </Row>
+      {/* Display Add Food component here */}
+      <Row justify={'center'}>
+        <Col span={6}>
+        <AddFoodForm addFood={addFood}/>
+        <Button> Hide Form / Add New Food </Button>
+        </Col>
+      </Row>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {/* Render the list of Food Box components here */}
+        {foods.map((food, i) => <FoodBox key={i} {...food}/>)}
+
+      </Row>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,22 +1,29 @@
-import { Button, Card, Col } from "antd"
+// Style Guide:
+// In this file you can find a reference example of the structure
+// and content that the component should render.
+// Remember to import Ant Design components before using them.
+import { Card, Col, Button } from 'antd';
 
-const FoodBox = (props) => {
-    return (
-        <Col>
-        <Card
-          title={props.food.name}
-          style={{ width: 230, height: 300, margin: 10 }}
-        >
-          <img src={props.food.image} height={60} alt="food" />
-          <p>Calories: {props.food.calories}</p>
-          <p>Servings: {props.food.servings}</p>
-          <p>
-            <b>Total Calories: {props.food.calories*props.food.servings} </b> kcal
-          </p>
-          <Button type="primary"> Delete </Button>
-        </Card>
-      </Col>
-    )
+// Iteration 2
+function FoodBox(props) {
+  const {name, image, calories, servings} = props;
+
+  return (
+    <Col>
+      <Card
+        title={name}
+        style={{ width: 230, height: 300, margin: 10 }}
+      >
+        <img src={image} height={60} alt="food" />
+        <p>Calories: {calories}</p>
+        <p>Servings: {servings}</p>
+        <p>
+          <b>Total Calories: {calories*servings} </b> kcal
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
+  );
 }
 
 export default FoodBox;

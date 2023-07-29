e1e38514b00b3b3e3436f8aecba55a51c16ce811
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-07-29 14:02:23 +0100

    Iteratio 4

--- src/App.js
+++ src/App.js
@@ -4,46 +4,19 @@ import { useState } from 'react'
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
 
-
-
-
 function App() {
   const [items, setItems] = useState(foods)
-  const [name, setName] = useState("");
-  const [image, setImage] = useState("");
-  const [calories, setCalories] = useState("");
-  const [servings, setServings] = useState("");
 
+  function addFood(food) {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const newFood = {
-        name,
-        image,
-        calories,
-        servings
-    }
-   
-    const updatedFoodList = [...items]
-     updatedFoodList.push(newFood)
-    setItems(updatedFoodList)
-
-    console.log(updatedFoodList)
-
-    setName("")
-    setImage("")
-    setCalories("")
-    setServings("")
+   return setItems([food, ...items])
   }
-
- 
-
+  
   return (
      <div className="App">
-      <AddFoodForm />
+      <AddFoodForm addFood={addFood} />
       {items.map(item => {
-        return <FoodBox food={ item } handleSubmit={handleSubmit}/>
+        return <FoodBox food={ item } />
       })}
     </div>
   );
--- src/components/AddFoodForm.js
+++ src/components/AddFoodForm.js
@@ -1,25 +1,48 @@
 import { Divider, Input } from 'antd';
+import { useState } from 'react';
 
 
 function AddFoodForm(props) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [calories, setCalories] = useState("");
+  const [servings, setServings] = useState("");
 
-  const { handleSubmit } = props
+  const { addFood } = props;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newFood = {
+        name,
+        image,
+        calories,
+        servings
+    }
+   
+    addFood(newFood);
+
+    setName("")
+    setImage("")
+    setCalories("")
+    setServings("")
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <Divider>Add Food Entry</Divider>
 
       <label>Name</label>
-      <Input value={props.name} type="text" onChange={(e) => {setName(e.target.value)}} />
+      <Input value={name} type="text" onChange={(e) => {setName(e.target.value)}} />
 
       <label>Image</label>
-      <Input value={props.image} type="text" onChange={(e) => {setImage(e.target.value)}} /> 
+      <Input value={image} type="text" onChange={(e) => {setImage(e.target.value)}} /> 
 
       <label>Calories</label>
-      <Input value={props.calories} type="text" onChange={(e) => {setCalories(e.target.value)}} />
+      <Input value={calories} type="text" onChange={(e) => {setCalories(e.target.value)}} />
 
       <label>Servings</label>
-      <Input value={props.servings} type="text" onChange={(e) => {{setServings(e.target.value)}}} />
+      <Input value={servings} type="text" onChange={(e) => {setServings(e.target.value)}} />
 
       <button type="submit">Create</button>
     </form>

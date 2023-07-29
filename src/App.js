d1d9f771bd1872466f99a0fca57213f22379a6f4
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-07-29 14:12:53 +0100

    Iteration 5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import foods from "./foods.json"
 import { useState } from 'react'
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
   const [items, setItems] = useState(foods)
@@ -11,9 +12,25 @@ function App() {
 
    return setItems([food, ...items])
   }
+
+  function handleSearch(e) {
+
+    const foodsCopy = [...foods]
+
+    const filterBySearch = foodsCopy.filter(food => {
+      if (food.name.toLowerCase().includes(e)) {
+         return food; 
+        }
+    });
+
+    setItems(filterBySearch)
+
+  }
+  
   
   return (
      <div className="App">
+      <Search handleSearch={handleSearch}/>
       <AddFoodForm addFood={addFood} />
       {items.map(item => {
         return <FoodBox food={ item } />

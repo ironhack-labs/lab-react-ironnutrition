c2d0665ee2a9dc9ecbe7b4a39aacbf93a671ee79
Aldo Gonzalez <aldogonjuarez@.com>
2023-07-30 02:14:11 -0600

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { useState } from 'react';
 import foodsData from './foods.json';
 import FoodBox from './components/FoodBox';
-import { Row, Divider, Button } from 'antd';
+import { Row, Divider, Button, Col } from 'antd';
 import './App.css';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
 
   const [foods, setFoods ] = useState(foodsData);
+
+  const addFood = (newFood) => {
+    setFoods(prevFoods => [newFood, ...prevFoods])
+  }
   return (
     <div className="App">
       {/* Display Add Food component here */}
+      <Row justify={'center'}>
+        <Col span={6}>
+        <AddFoodForm addFood={addFood}/>
+
+        </Col>
+        </Row>
 
       <Button> Hide Form / Add New Food </Button>
 

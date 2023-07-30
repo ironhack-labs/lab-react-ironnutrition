7d76ea28ccde71b58c30f4ec7fb6a72be5f00e4e
Aldo Gonzalez <aldogonjuarez@.com>
2023-07-30 01:45:29 -0600

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import { useState } from 'react'
+import foodsData from './foods.json';
 import './App.css';
 
 function App() {
+  const [foods, setFoods] = useState(foodsData);
   return (
     <div className="App">
+      {
+        foods.map((food,i) => (
+          <div key={i}>
+          <p> {food.name}</p>
+          <img src={food.image} width={80} alt={food.image} />
+          </div>
+        ))
+      }
+        
       
     </div>
   );

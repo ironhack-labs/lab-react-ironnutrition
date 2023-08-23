b3ec5c4ee497affbfb707d5d405551d4f2112301
Agnieszka Morawska <agnesmora.art@gmail.com>
2023-08-23 18:08:22 +0100

    completed

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,20 @@
 // Your code here
+
+
+function FoodBox(props) {
+  const {food, clickToDelete} = props;
+  return (
+    <div>
+      <p>{food.name}</p>
+      <img src={food.image} />
+      <p>{food.calories}</p>
+      <p>{food.servings}</p>
+      <p>
+        <b>Total Calories: {food.servings * food.calories} </b> kcal
+      </p>
+      <button onClick={()=> clickToDelete(food.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default FoodBox;

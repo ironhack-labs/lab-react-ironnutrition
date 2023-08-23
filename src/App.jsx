33b9fe7d9910601fa8e149da7d3de3e5de4dafb6
Miguel Leite <mikleite@gmail.com>
2023-08-23 16:31:41 +0100

    lab solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import AddFood from "./components/AddFoodForm";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const deleteFood = (foodId) => {
+    const filteredFoods = foods.filter((food) => {
+      return food.id !== foodId;
+    })
+    setFoods(filteredFoods)
+  }
+
+  const addNewFood = (newFood) => {
+    const updatedFoods = [... foods, newFood];
+    setFoods(updatedFoods);
+  }
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+    <h1>Foods List</h1>
+    <div className="FoodsList">
+      {foods.map((food) =>{
+        return <FoodBox key={food.id} food={food} clickToDelete={deleteFood}/>
+      })}
+    </div>
+    <AddFood addNewFood={addNewFood} />
     </div>
   );
 }

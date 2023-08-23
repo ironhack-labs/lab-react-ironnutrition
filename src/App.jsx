dad3c9f045bc445fe39881451e5d1229f1ac63e2
John Doe <johndoe@example.com>
2023-08-23 17:32:16 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import "./App.css";
+import { useState } from "react";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import AddFood from "./components/AddFoodForm";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const deleteFood= (foodId)=>{
+    const filteredFoods = foods.filter((food)=>{
+      return food.id !== foodId;
+    })
+    setFoods(filteredFoods)
+  }
+
+  const addNewFood= (newFood)=> {
+    const updatedFoods= [... foods, newFood];
+    setFoods(updatedFoods)
+  }
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <h2> Add Food Entry</h2>
+      <AddFood addNewFood= {addNewFood}/>
+      <h1>Foods list</h1>
+      <div className="FoodsList">
+        {foods.map((food)=> {
+          return <FoodBox key={food.id} food={food} clickToDelete={deleteFood}/>
+        })}
+      </div>
     </div>
   );
 }

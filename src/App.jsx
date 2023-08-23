21712d0c3f9c386f67be18cbabbb858779868530
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-08-23 18:28:03 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBoox from "./components/FoodBox";
+import AddNewFood from "./components/AddFoodForm";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const deleteFoods = (foodsId) => {
+    const filteredFood = foods.filter((food) => {
+      return food.id !== foodsId;
+    });
+    setFoods(filteredFood);
+  }
+
+  const addNewFood = (newFood) => {
+        const updatedFoods = [...foods, newFood];
+
+        setFoods(updatedFoods);
+    }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      {foodsJson.map((onlyfood) =>{
+        return(<FoodBoox key={onlyfood.id} onlyFood={onlyfood} deleteBtn={deleteFoods} />)
+      })}
+      <AddNewFood addNewFood={addNewFood}/>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,18 @@
-// Your code here
+function FoodBoox(props){
+    const {onlyFood, deleteBtn} = props;
+
+    return(
+        <div>
+            <p>{onlyFood.name}</p>
+            <img src={onlyFood.image} />
+            <p>Calories: {onlyFood.calories}</p>
+            <p>Servings {onlyFood.servings}</p>
+            <p>
+                <b>Total Calories: {onlyFood.servings} * {onlyFood.calories} </b> kcal
+            </p>
+            <button onClick={() => {deleteBtn(onlyFood.id)}}>Delete</button>
+        </div>
+    );
+}
+
+export default FoodBoox;

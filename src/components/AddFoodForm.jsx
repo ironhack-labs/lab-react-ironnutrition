04e912579a81de7f4fee9f87682fd6f3d6be7174
jsguillotte <julia.guillotte@gmail.com>
2023-08-23 11:28:00 -0500

    solved lab

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,71 @@
 // Your code here
+import React, { useState } from 'react';
+
+function AddFoodForm({ onAddFood }) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState('');
+  const [servings, setServings] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newFood = {
+      name,
+      image,
+      calories: Number(calories),
+      servings: Number(servings),
+    };
+
+    onAddFood(newFood);
+
+    setName('');
+    setImage('');
+    setCalories('');
+    setServings('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Image:</label>
+        <input
+          type="text"
+          name="image"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Calories:</label>
+        <input
+          type="number"
+          name="calories"
+          value={calories}
+          onChange={(e) => setCalories(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Servings:</label>
+        <input
+          type="number"
+          name="servings"
+          value={servings}
+          onChange={(e) => setServings(e.target.value)}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default AddFoodForm;

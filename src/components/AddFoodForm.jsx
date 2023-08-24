0e0b82afc1f5d8b66232f921e41373da38460a76
Mehdi Nader <mehdinader1079@gmail.com>
2023-08-24 10:21:15 +0100

    almoste done

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,75 @@
-// Your code here
+import React, { useState } from "react";
+
+function AddFoodForm({ onAddFood }) {
+  const [newFood, setNewFood] = useState({
+    name: "",
+    image: "",
+    calories: 0,
+    servings: 0,
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewFood({
+      ...newFood,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    onAddFood(newFood);
+   
+    setNewFood({
+      name: "",
+      image: "",
+      calories: 0,
+      servings: 0,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={newFood.name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>Image:</label>
+        <input
+          type="text"
+          name="image"
+          value={newFood.image}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>Calories:</label>
+        <input
+          type="number"
+          name="calories"
+          value={newFood.calories}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>Servings:</label>
+        <input
+          type="number"
+          name="servings"
+          value={newFood.servings}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default AddFoodForm;

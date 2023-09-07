1c6f0174d0adc235a0b1b5952bc729f01bb8cd53
Nicolas Stevens <stvns.ncls@gmail.com>
2023-09-07 21:24:11 +0200

    100% lab with bonuses

---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import { Input } from 'antd';
+import React, { useState } from 'react';
+
+function Search({ searchFood }) {
+  const [search, setSearch] = useState('');
+
+  return (
+    <div>
+      <label>Search</label>
+      <Input
+        value={search}
+        type="text"
+        onChange={(e) => {
+          setSearch(e.target.value);
+          searchFood(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
+export default Search;

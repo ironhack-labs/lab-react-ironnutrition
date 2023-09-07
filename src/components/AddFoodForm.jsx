b64d2cd6315bd48e3fddbf58e3869a0f0625aab2
Maria Garcia <mariagarciamontes06@outlook.com>
2023-09-07 22:01:50 +0200

    Iteration 1-6

---
+++ src/components/AddFoodForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { Divider, Image, Input } from 'antd';
+
+function AddFoodForm(props) {
+    const [name,setName] = useState('')
+    const [image,setImage] = useState('')
+    const [calories,setCalories] = useState('')
+    const [servings,setServings] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const newFood = {name,image,calories,servings}
+        props.addNewFood(newFood)
+
+        setName('')
+        setImage('')
+        setCalories('')
+        setServings('')
+    }
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <Divider>Add Food Entry</Divider>
+  
+        <label>Name</label>
+        <Input value={name} type="text" onChange={(e) => {setName(e.target.value)}} />
+  
+        <label>Image</label>
+        <Input value={image} type="text" onChange={(e) => {setImage(e.target.value)}} />
+  
+        <label>Calories</label>
+        <Input value={calories} type="number" onChange={(e) => {setCalories(e.target.value)}} />
+  
+        <label>Servings</label>
+        <Input value={servings} type="number" onChange={(e) => {setServings(e.target.value)}} />
+  
+        <button type="submit">Create</button>
+      </form>
+    );
+  }
+
+export default AddFoodForm
---
+++ src/components/FoodBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Card, Col, Button } from 'antd';
+
+function FoodBox(props) {
+    const {foods} = props
+  return (
+    <Col>
+      <Card
+        title={foods.name}
+        style={{ width: 230, height: 300, margin: 10 }}
+      >
+        <img src={foods.image} height={60} alt="food" />
+        <p>Calories: {foods.calories}</p>
+        <p>Servings: {foods.servings}</p>
+        <p>
+          <b>Total Calories: {foods.calories * foods.servings}</b> kcal
+        </p>
+        <Button type="primary" onClick={(e)=>{props.deleteFood(foods.name)}}> Delete </Button>
+      </Card>
+    </Col>
+  );
+}
+
+export default FoodBox;

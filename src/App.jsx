1a0e47925cfcc362ce237c9aa5e9456ee1d9083d
Gabriela García <ggarciaborregon@gmail.com>
2023-09-08 12:55:12 +0200

    iteration 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
 import "./App.css";
-import foodsJson from "./foods.json";
-import { useState } from "react";
-import FoodBox from '../src/components/FoodBox';
-import FoodForm from '../src/components/AddFoodForm';
+import FoodList from './components/FoodList'
 
 function App() {
-
-  const [foods, setFoods] = useState(foodsJson);
-
-  // Delete
-
-  const onDeleteFood = (id) => {
-    const newFoods = foods.filter(food => id !== food.id )
-
-    setFoods(newFoods)
-  }
-
-  // Create
-
-  const onCreateFood = (food) => {
-    setFoods([
-      food,
-      ...foods
-    ])
-  }
-
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
-      <div className="form">
-        <FoodForm onCreateFood={onCreateFood}/>
-      </div>
-      {foods.map((food) => (
-        <FoodBox key={food.id} food={food} onDeleteFood={() => {onDeleteFood(food.id)}}/>
-      ))}
+      <FoodList/>
     </div>
   );
 }

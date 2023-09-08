e280675bbd72c11cf0b17c460864b42fe0cd071c
Gabriela García <ggarciaborregon@gmail.com>
2023-09-08 13:20:50 +0200

    iterations 8 & 9

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -5,6 +5,8 @@ import FoodForm from './AddFoodForm';
 
 const FoodList = () => {
     const [foods, setFoods] = useState(foodsJson);
+    const [search, setSearch] = useState('');
+    const [filteredFoods, setFilteredFoods] = useState([]);
 
     // Delete
 
@@ -23,17 +25,50 @@ const FoodList = () => {
         ])
     }
 
+    // Search
+    const onChangeSearch = (event) => {
+        const searchTerm = event.target.value;
+        setSearch(searchTerm);
+
+        const filtered = foods.filter((food) =>
+            food.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+
+        setFilteredFoods(filtered)
+    }
+
+
     return (
         <div className="App">
             <h1>LAB | React IronNutrition</h1>
             <div className="form">
                 <FoodForm onCreateFood={onCreateFood} />
             </div>
-            {foods.map((food) => (
-                <FoodBox key={food.id} food={food} onDeleteFood={() => { onDeleteFood(food.id) }} />
-            ))}
+            <hr />
+            <div className="mb-3">
+                <label htmlFor="search" className="form-label">Search</label>
+                <input
+                    className="form-control"
+                    id="search"
+                    name="search"
+                    placeholder="Type a text to start filtering"
+                    value={search}
+                    onChange={onChangeSearch}
+                    autoComplete="off"
+                />
+            </div>
+            {search === '' ? (
+                foods.length === 0 ? <p>Oops! There is no more content to show.</p> : (
+                    foods.map((food) => (
+                        <FoodBox key={food.id} food={food} onDeleteFood={() => { onDeleteFood(food.id) }} />
+                    ))
+                ) 
+            ) : (filteredFoods.map((food) => (
+                    <FoodBox key={food.id} food={food} onDeleteFood={() => { onDeleteFood(food.id) }} />
+                ))
+            )}
         </div>
     )
 }
 
-export default FoodList
+export default FoodList;

b4b4d8367ef48f39934a893020d59bcac24b4927
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-13 18:39:28 +0200

    Main iterations Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import { useState } from "react";
 import "./App.css";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
+
+  const [foodToDisplay, setfoodToDisplay] = useState(foodsJson);
+
+  const deleteFood = (foodid) => {
+    const newList = foodToDisplay.filter((element) => {
+        return foodid !== element.id
+    })
+    setfoodToDisplay(newList)
+}
+
+const addNewFood = (newFood) => {
+  const newList = [newFood, ...foodToDisplay];
+  setfoodToDisplay(newList);
+}
+
   return (
+     
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <AddFoodForm callBackToAddFood={addNewFood}/>
+      <FoodBox foodDetails={foodToDisplay} callBackToDelete={deleteFood}/>
     </div>
   );
 }

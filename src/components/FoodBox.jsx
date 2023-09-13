24204a426d8347ce1bf219a98835dcc5c2c39f6a
Johanna Geßner <johanna-gessner@web.de>
2023-09-13 18:31:10 +0200

    done

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,24 @@
-// Your code here
+function FoodBox(props) {
+
+
+    const caloriesTotal = props.food.servings * props.food.calories
+
+    return (
+        <div>
+            <p>{props.food.name}</p>
+
+            <img src={props.food.image} alt={props.food.name} />
+
+            <p>Calories: {props.food.calories}</p>
+            <p>Servings: {props.food.servings}</p>
+
+            <p>
+                <b>Total Calories: {caloriesTotal} </b> kcal
+            </p>
+
+            <button onClick={() => props.callbackToDelete(props.food.id)}>Delete</button>
+        </div>
+    )
+}
+
+export default FoodBox;

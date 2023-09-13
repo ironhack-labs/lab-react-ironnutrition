a15ba9ce8a84369ac238e69aaadd16bac26471fc
HannaInIT <ann10062016@gmail.com>
2023-09-13 20:13:02 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import "./App.css";
+import AddFoodForm from "./components/AddFoodForm";
+import foods from "./foods.json";
+import FoodBox from "./components/FoodBox";
 
 function App() {
+
+  const [originalFoods, setOriginalFoods] = useState(foods);
+
+  const deleteFood = (foodName) => {
+    const newList = originalFoods.filter((element) => {
+      return element.name !== foodName;
+    });
+    setOriginalFoods(newList);
+  }
+
+  const addFood = (newFood) => {
+    const newList = [newFood, ...originalFoods];
+    setOriginalFoods(newList);
+  }
+
   return (
-    <div className="App">
-      <h1>LAB | React IronNutrition</h1>
-    </div>
+    <>
+      <AddFoodForm callbackToAdd={addFood} />
+
+      <header>
+        <h1>Food list</h1>
+      </header>
+      
+      {originalFoods.map((foodObj, index) => (
+        <div key={foodObj.name}>
+          <FoodBox food={foodObj} callbackToDelete={deleteFood} />
+        </div>
+      ))}
+    </>
+
   );
 }
 
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,98 @@
-// Your code here
+import { useState } from "react";
+
+
+function AddFoodForm(props) {
+
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [calories, setCalories] = useState("");
+    const [servings, setServings] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const foodDetails = {
+            name: name,
+            image: image,
+            calories: calories,
+            servings: servings
+        }
+
+        // invoke function in the parent component
+        props.callbackToAdd(foodDetails);
+
+        // clear form
+        setName("");
+        setImage("");
+        setCalories("");
+        setServings("");
+    }
+
+
+    return (
+        <section>
+            <header>
+                <h2>Add food entry</h2>
+            </header>
+            <form onSubmit={handleSubmit}>
+
+                <label>
+                   Name:
+                    <input
+                        type="text"
+                        name="name"
+                        required={true}
+                        placeholder="enter the name"
+                        // value={name}
+                        onChange={(e) => { setName(e.target.value) }}
+                    />
+                </label>
+
+
+                <label>
+                   Image:
+                    <input
+                        type="file"
+                        name="image"
+                        required={true}
+                        placeholder="add the image"
+                        // value={name}
+                        // onChange={(e) => { setTitle(e.target.value) }}
+                    />
+                </label>
+
+                <label>
+                Calories:
+                    <input
+                        type="number"
+                        name="calories"
+                        required={true}
+                        placeholder="enter calories"
+                        min={1}
+                        // value={name}
+                        onChange={(e) => { setCalories(e.target.value) }}
+                    />
+                </label>
+
+                <label>
+                Servings:
+                    <input
+                        type="number"
+                        name="servingss"
+                        required={true}
+                        placeholder="enter servings"
+                        min={1}
+                        // value={name}
+                        onChange={(e) => { setServings(e.target.value) }}
+                    />
+                </label>
+
+                <button>Create</button>
+            </form>
+        </section>
+    )
+}
+
+export default AddFoodForm;
+
+

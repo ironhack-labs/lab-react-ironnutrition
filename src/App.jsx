562f957ccd7ce9a95d346fe89d1c4adfb7a6ce76
Roberto Kyuchukov <robertokyuchukov@Robertos-iMac.local>
2023-09-13 18:15:18 +0300

    first 4 iterations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import {useState} from 'react';
+import FoodBox from "./components/FoodBox";
+
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const deleteFood = () => {
+    const newList = foods.filter((elm) => {
+      return elm.name !== food.name;
+    })
+    setFoods([...newList]);
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+
+      {foods.map((element) => {
+       return(
+        <FoodBox key={element.id} element={{
+        name: element.name,
+        calories: element.calories,
+        image: element.image,
+        servings: element.servings
+      }} callBackFunction={() => {deleteFood(element.name)}}/>
+      ) 
+      })}
+
     </div>
   );
 }

f536f61c14ef8ddb3880e54004555b018f62c2ad
Cyclonej <cyclonejmoto@gmail.com>
2023-09-14 01:03:43 +0200

    done

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,22 @@
-// Your code here
+import "./style.css"
+
+function FoodBox(props){
+
+    return (
+        <div className="Pictures">
+            <p>{props.food.name}</p>
+
+            <img src={props.food.image} width= "400px" height="300px"/>
+
+            <p>Calories: {props.food.calories}</p>
+            <p>Servings: {props.food.servings}</p>
+
+            <p>
+                <b>Total Calories: {props.food.servings * props.food.calories} </b> kcal
+            </p>
+
+            <button onClick={() => props.callbackToDelete(props.food.id)}>Delete</button>
+        </div>
+    )
+}
+export default FoodBox

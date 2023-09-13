ce6adf5693e4e5b5e67aede153db345d1573d93f
Anna Kazulak <anna.kazulak@gmail.com>
2023-09-13 20:36:15 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import FoodList from "./components/FoodList";
+import { Divider, Row } from "antd";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <Divider>Food List</Divider>
+      <Row style={{ width: "100%", justifyContent: "center" }}>
+        {/* Render the list of Food Box components here */}
+        <FoodList />
+      </Row>
     </div>
   );
 }

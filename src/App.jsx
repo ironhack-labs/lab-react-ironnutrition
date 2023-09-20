026ce5b1f5e6e8f686ad5b02ea5aeefb51633208
Alice <johndoe@example.com>
2023-09-20 16:11:58 +0200

    half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import { useState } from "react";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson); //original
+  const [foodsArr, setFoodsArr] = useState(foods); // filtered ARRAY
+
+  // If you were to use only foodsArr and modify it directly for filtering or deleting,
+  // you would lose the reference to the original data,
+  // making it more challenging to reset or manage the data effectively.
+
+  const deleteFood = (id) => {
+    const filteredFoods = foods.filter((food) => {
+      return food.id !== id;
+    });
+
+    const filteredFoodArr = foodsArr.filter((food) => {
+      return food.id !== id;
+    });
+
+    setFoodsArr(filteredFoodArr);
+    setFoods(filteredFoods);
+  };
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+
+      {foodsArr.map((food) => {
+        return (
+          <>
+            <FoodBox
+              key={food.id}
+              food={food} // const{food}=props (in FoodBox jsx)
+              deleteFood={deleteFood}
+
+              //IF you WANT...
+              // name={food.name}
+              // calories={food.calories}
+              // servings={food.servings}
+              //then, you need destructure the props in FoodBox jsx like this:
+              // const { name, calories, servings } = props;
+            />
+          </>
+        );
+      })}
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,39 @@
-// Your code here
+function FoodBox(props) {
+  console.log("FoodBox Props:", props);
+  const { food, deleteFood } = props;
+
+  return (
+    <div className="AddFood">
+      <h4>Add Food</h4>
+      {/* <form onSubmit={handleSubmit}> */}
+      <label>{food.name}</label>
+      {/* <input type="text" name="name" value={props.name} onChange={handleTitleInput} /> */}
+      <img src={food.image}/>
+
+      <label>Calories:{food.calories} </label>
+      {/* <input
+        type="number"
+        name="calories"
+        value={props.calories}
+        onChange={handleDirectorInput}
+      /> */}
+
+      <label>Servings:{food.servings}</label>
+      {/* <input
+        type="number"
+        name="servings"
+        value={props.calories}
+        onChange={handleOscarsInput}
+      /> */}
+
+      <p>
+        <b>Total Calories: {food.calories * food.servings} </b> kcal
+      </p>
+
+      <button type="primary" onClick={()=> deleteFood(food.id)}>Delete 🗑️</button>
+      {/* </form> */}
+    </div>
+  );
+}
+
+export default FoodBox;

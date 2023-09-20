2e43c96f78542745a27b692c904852116b14ee83
You-Rim Kim <yourimykim@gmail.com>
2023-09-20 16:45:13 +0200

    lab almost done

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,23 @@
-// Your code here
+function FoodBox({food, onDelete}) {
+  const handleDelete = () => {
+    onDelete(food.id);
+  };
+
+
+    return (
+        
+        <div className="FoodBox">
+        <p>{food.name}</p>
+        <img src={food.image} alt={food.name} />
+        <p>Calories: {food.calories}</p>
+        <p>Servings: {food.servings}</p>
+        <p>
+          <b>Total Calories: {food.servings * food.calories} kcal</b>
+        </p>
+        <button onClick={handleDelete}>Delete</button>
+
+      </div>
+    );
+  }
+
+export default FoodBox;

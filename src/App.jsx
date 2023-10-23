8ca03aafa89f2cf81cd8cd1c81b813b818c3483a
Driztnafein <127021511+Driztnafein@users.noreply.github.com>
2023-10-23 20:14:14 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import React, { useState } from "react"
 import "./App.css";
+import foodsJson from "./foods.json"
+import FoodBox from "./components/FoodBox"
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const foodBoxes = foods.map((foodItem) => {
+    return <FoodBox key={foodItem.name} food={foodItem} /> 
+  })
+
   return (
-    <div className="App">
+     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      {foodBoxes}
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,17 @@
 // Your code here
+function FoodBox({ food }) {
+    return (
+      <div>
+        <p>{food.name}</p>
+        <img src={food.image} alt={food.name} />
+        <p>Calories: {food.calories}</p>
+        <p>Servings: {food.servings}</p>
+        <p>
+          <b>Total Calories: {food.servings * food.calories} </b> kcal
+        </p>
+        <button>Delete</button>
+      </div>
+    );
+  }
+
+export default FoodBox;

6d36962db61c18521c23d30433645e8bedf6e783
Carlos A Martin Aparicio <carlosktm08@gmail.com>
2023-10-28 13:24:48 +0200

    feat:Lab Iron Nutrition Carlos M

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,28 @@
-// Your code here
+import React from 'react'
+
+const FoodBox = ({ food, onDelete }) => {
+    const { name, calories, image, servings, id } = food;
+    const totalCalories = servings * calories;
+
+    const handleDelete = () => {
+        onDelete(id);
+    };
+
+
+    return (
+        <div className="food-box">
+            <p>{name}</p>
+
+            <img src={image} alt={name} />
+
+            <p>Calories: {calories}</p>
+            <p>Servings: {servings}</p>
+            <p>
+                <b>Total Calories: {totalCalories} kcal</b> kcal
+            </p>
+
+            <button onClick={handleDelete}>Delete</button>
+        </div>)
+}
+
+export default FoodBox

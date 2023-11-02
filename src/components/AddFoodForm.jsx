ebaa62ebefd84cf28d07ddb92aac4918ce0e292b
Marcelo Melo <melooliveira31@gmail.com>
2023-11-02 17:45:39 +0000

    done

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,89 @@
-// Your code here
+import React, { useState } from "react";
+import { Divider, Input, Button } from "antd";
+
+function AddFoodForm(props) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [calories, setCalories] = useState("");
+  const [servings, setServings] = useState("");
+
+  const handleNameInput = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleImageInput = (e) => {
+    setImage(e.target.value);
+  };
+
+  const handleCaloriesInput = (e) => {
+    setCalories(e.target.value);
+  };
+
+  const handleServingsInput = (e) => {
+    setServings(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newFood = {
+      name,
+      image,
+      calories,
+      servings,
+    };
+
+    props.addNewFood(newFood);
+
+    setName("");
+    setImage("");
+    setCalories("");
+    setServings("");
+  };
+
+  return (
+    <div>
+      <Divider>Add Food Entry</Divider>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:{" "}
+          <Input
+            type="text"
+            name="name"
+            onChange={handleNameInput}
+            value={name}
+          />
+        </label>
+        <label>
+          Image:{" "}
+          <Input
+            type="text"
+            name="image"
+            onChange={handleImageInput}
+            value={image}
+          />
+        </label>
+        <label>
+          Calories:{" "}
+          <Input
+            type="number"
+            name="calories"
+            onChange={handleCaloriesInput}
+            value={calories}
+          />
+        </label>
+        <label>
+          Servings:{" "}
+          <Input
+            type="number"
+            name="servings"
+            onChange={handleServingsInput}
+            value={servings}
+          />
+        </label>
+        <Button type="submit">Create</Button>
+      </form>
+    </div>
+  );
+}
+
+export default AddFoodForm;
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,26 @@
-// Your code here
+import { Card, Col, Button } from "antd";
+
+function FoodBox(props) {
+  return (
+    <Col>
+      <Card
+        title={props.food.name}
+        style={{ width: 230, height: 300, margin: 10 }}
+      >
+        <img src={props.food.image} height={60} alt="food" />
+        <p>Calories: {props.food.calories}</p>
+        <p>Servings {props.food.servings}</p>
+        <p>
+          <b>Total Calories: {props.food.calories * props.food.servings}</b>{" "}
+          kcal
+        </p>
+        <Button type="primary" onClick={() => props.delete(props.food.id)}>
+          {" "}
+          Delete{" "}
+        </Button>
+      </Card>
+    </Col>
+  );
+}
+
+export default FoodBox;

0fcc3f69151561c929490b5aeacd6f215ca8fd5e
Daniel Rodrigues <danielbrodrigues25@gmail.com>
2023-11-02 17:07:47 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import "./App.css";
-
+import foodsJson from "./foods.json"
+import FoodBox from "./components/FoodBox";
+import { useState } from 'react';
+import AddFoodForm from "./components/AddFoodForm";
 function App() {
+
+  const [foods, setFoods] = useState(foodsJson);
+
+  const deleteFood = (id) => {
+    setFoods(currentFoods => currentFoods.filter(food => food.id !== id));
+  };
+
+  const addNewFood = (newFood) => {
+    setFoods([...foods, newFood]);
+};
+  
+
+  
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <AddFoodForm addNewFood={addNewFood} />
+      {foods.map((food, index)=>{
+          return(<FoodBox key={food.id} food ={food} deleteFood={deleteFood}/>)
+        })}
+      
     </div>
   );
 }

e1a0f8070e0303a2dca06ecc9ab3500b6348135c
Andre Gomes <andrefilipe.valeriogomes@gmail.com>
2023-11-02 22:47:52 +0000

    more or less done

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,22 @@
-// Your code here
+import React from "react";
+
+function FoodBox({ food }) {
+  const { name, calories, image, servings } = food;
+  const totalCalories = calories * servings;
+
+  return (
+    <div className="food-box">
+      <p>{name}</p>
+      <img src={image} alt={name} />
+      <p>Calories: {calories} kcal</p>
+      <p>Servings: {servings}</p>
+      <p>
+        <b>Total Calories: {totalCalories} kcal</b>
+      </p>
+      <button>Delete</button>
+
+    </div>
+  );
+}
+
+export default FoodBox;

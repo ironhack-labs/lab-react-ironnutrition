c4574890363436eaf8d3f2de931972344cf69714
Guilherme Silva <guilhermemas2004@gmail.com>
2023-11-03 08:53:00 +0000

    done

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,24 @@
 // Your code here
+import React from 'react'
+import { Card, Col, Button } from "antd";
+
+function FoodBox(props) {
+
+    const totalCalories = props.food.calories * props.food.servings
+  return (
+    <div>
+        <Col>
+          <Card title={props.food.name} style={{ width: 230, height: 300, margin: 10 }}>
+            <img src={props.food.image} height={60} alt="food" />
+            <p>Calories: {props.food.calories}</p>
+            <p>Servings {props.food.servings}</p>
+            <p>
+              <b>Total Calories: {totalCalories}</b> kcal
+            </p>
+            <Button type="primary" onClick={() => props.deleteFood(props.food.id)}> Delete </Button>
+          </Card>
+        </Col>
+    </div>
+  )
+}
+export default FoodBox;

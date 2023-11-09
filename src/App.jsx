52d479eddfe5d45960f8cd31648be84fa2b16ba9
“elisejonkers” <elisejonkers18@hotmail.com>
2023-11-09 19:08:21 +0100

    display items and add delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 import "./App.css";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+  const [name, setName] = useState("")
+  const [image, setImage] = useState("")
+  const [calories, setCalories] = useState("")
+  const [servings, setServings] = useState("")
+  
+
+  const deleteItem = (foodID) => {
+    const newFoodsList = foods.filter((foodDetails) => {
+      return foodDetails.id !== foodID;
+    });
+    setFoods(newFoodsList);
+  };
+
+  const addItem = (event) => {
+    event.preventDefault()
+    const newFoodItem = {
+      name: name,
+      image: image,
+      calories: calories,
+      servings: servings,
+    }
+
+    const newFoodArray = [newFoodItem, ...foods]
+  }
+
+
   return (
-    <div className="App">
-      <h1>LAB | React IronNutrition</h1>
-    </div>
+    <>
+      <AddFoodForm callbackToAdd={addItem}/>
+      <FoodBox foodArray={foods} callbackToDelete={deleteItem} />
+
+    </>
   );
 }
 
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,43 @@
-// Your code here
+function AddFoodForm(props) {
+  return (
+    <>
+    <h1>Add Food Entry</h1>
+    <form
+      onSubmit={function () {
+        props.callbackToAdd();
+      }}
+    >
+      <label>
+        Name:
+        <input 
+        type="text" 
+        name="name"
+        required={true} />
+      </label>
+      <label>
+        Image:
+        <input 
+        type="text" 
+        name="image" />
+      </label>
+      <label>
+        Calories:
+        <input 
+        type="number" 
+        name="calories"
+        required={true} />
+      </label>
+      <label>
+        Servings:
+        <input 
+        type="number" 
+        name="servings"
+        required={true} />
+      </label>
+      <button type="submit">Create</button>
+    </form>
+    </>
+  );
+}
+
+export default AddFoodForm;

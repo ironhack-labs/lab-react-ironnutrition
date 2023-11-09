039b4741fb2f3470be0f2c9b7d542f455dd9df06
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-09 17:04:19 +0000

    Working on Iteration 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
 import "./App.css";
 import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 import { useState } from "react";
 import foodsJson from "./foods.json";
 
 function App() {
     const [foods, setFoods] = useState(foodsJson);
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [calories, setCalories] = useState("");
+    const [servings, setServings] = useState("");
 
     const deleteFood = (foodElmId) => {
         const newArray = foods.filter((elm) => elm.id !== foodElmId);
         setFoods(newArray);
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newFood = {
+            name: name,
+            image: image,
+            calories: calories,
+            servings: servings,
+        };
+
+        const newList = [newFood, ...foods];
+        setFoods(newList);
+
+        setName("");
+        setImage("");
+        setCalories("");
+        setServings("");
+    };
+
     return (
         <div className="App">
             <h1>LAB | React IronNutrition</h1>
+            <AddFoodForm callbackToDelete={deleteFood} />
             {foods.map((foodObj, index) => {
-                return (
-                  <FoodBox 
-                    key={index}
-                    food={foodObj} 
-                    callbackToDelete={deleteFood} 
-                />
-                );
+                return <FoodBox key={index} food={foodObj} callbackToSubmit={handleSubmit} />;
             })}
         </div>
     );
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,48 @@
-// Your code here
+function addFoodForm(props) {
+    return (
+        <form
+            onSubmit={() => {
+                props.callbackToSubmit;
+            }}
+        >
+            <label>
+                Name
+                <input
+                    name="name"
+                    type="text"
+                    required={true}
+                    //value={props....}
+                ></input>
+            </label>
+            <label>
+                Image
+                <input
+                    name="image"
+                    type="text"
+                    //value={props....}
+                ></input>
+            </label>
+            <label>
+                Calories
+                <input
+                    name="calories"
+                    type="number"
+                    required={true}
+                    //value={props....}
+                ></input>
+            </label>
+            <label>
+                Servings
+                <input
+                    name="servings"
+                    type="number"
+                    required={true}
+                    //value={props....}
+                ></input>
+            </label>
+            <button>Create</button>
+        </form>
+    );
+}
+
+export default addFoodForm;

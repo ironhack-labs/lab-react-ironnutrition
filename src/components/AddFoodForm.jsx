736c9a202d3ca69314024663b80e6b5296fd57ff
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-09 20:02:32 +0100

    done with Mert!

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,93 @@
 // Your code here
+import { useState } from 'react';
+
+function AddFoodForm(props) {
+
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [calories, setCalories] = useState("");
+    const [servings, setServings] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newFoodsItem = {
+            name: name,
+            image: image,
+            calories: calories,
+            servings: servings
+        }
+
+        props.callbackToAddFoodsItem(newFoodsItem);
+
+        setName("");
+        setImage("");
+        setCalories("");
+        setServings("");
+    }
+
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+                
+                <hr />
+                <label>
+                    <p>Name:</p>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="enter food name"
+                        required={true}
+                        value={name}
+                        onChange={(e) => {setName(e.target.value) }}
+                    />
+                </label>
+
+                <label>
+                    <p>Image:</p>
+                    <input
+                        type="text"
+                        name="image"
+                        placeholder="enter image URL"
+                        required={true}
+                        value={image}
+                        onChange={(e) => {setImage(e.target.value) }}
+                    />
+                </label>
+
+                <label>
+                    <p>Calories:</p>
+                    <input
+                        type="number"
+                        name="calories"
+                        placeholder="enter calorie count"
+                        required={true}
+                        value={calories}
+                        onChange={(e) => {setCalories(e.target.value) }}
+                    />
+                </label>
+
+                <label>
+                    <p>Servings:</p>
+                    <input
+                        type="text"
+                        name="servings"
+                        placeholder="enter number of servings"
+                        required={true}
+                        value={servings}
+                        onChange={(e) => {setServings(e.target.value) }}
+                    />
+                </label>
+
+                <br />
+                <button className="submit-button">Submit Item</button>
+                <br />
+
+            </form>
+        </section>
+
+    )
+
+}
+
+export default AddFoodForm;
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,26 @@
 // Your code here
+function FoodBox(props) {
+
+    return (
+
+        props.foodsArr.map((foodsItem, i) => {
+
+            return (
+                <div>
+                    <section className="food-item-box">
+                        <p className="food-title">{foodsItem.name}</p>
+                        <img src={foodsItem.image} />
+                        <p>Calories: {foodsItem.calories}</p>
+                        <p>Servings {foodsItem.servings}</p>
+                        <p><b>Total Calories: {foodsItem.calories * foodsItem.servings}</b> kcal</p>
+                        <button onClick={function() {props.callbackToDelete(foodsItem.name);
+                        }}>Delete</button>
+                    </section>
+                </div>
+
+            )
+        })
+    )
+}
+
+export default FoodBox;

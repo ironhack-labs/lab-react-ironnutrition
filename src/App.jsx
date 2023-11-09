5ec59014aef86fa55d9307f647fcee9db2cfdfbb
Fran Martinez <pmg.webdev@gmail.com>
2023-11-09 17:46:52 +0100

    iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import FoodBox from "./components/FoodBox";
 import foodsJson from "./foods.json"
 import { useState } from "react";
 
@@ -6,9 +7,15 @@ import { useState } from "react";
 function App() {
 const [foodList, setFoodList]= useState(foodsJson)
 
+const copyFoodList = [...foodList]
+console.log(copyFoodList)
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+
+      {/* <FoodBox food={foodList} /> */}
+      <FoodBox food={foodList} />
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,27 @@
-// Your code here
+function FoodBox(props){
+    return (
+        <div>
+           { console.log(props.food[0].name)}
+        <p>{props.food[0].name} </p>
+
+        <img 
+            src={props.food[0].image} 
+            alt="food image"
+            width="200px"
+            height="200px"
+            
+        />
+
+        <p>Calories: {props.food[0].calories} </p>
+        <p>Servings: {props.food[0].servings} </p>
+
+        <p>
+            <b>Total calories: {props.food[0].servings * props.food[0].calories} </b>
+        </p>
+
+        <button>Delete</button>
+    </div>
+    );
+}
+
+export default FoodBox;

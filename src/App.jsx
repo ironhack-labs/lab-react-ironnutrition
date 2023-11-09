656e48a479b6e269451cb22bf6c9d626704ad424
Yassine Medah <yassinemedah1994@gmail.com>
2023-11-09 19:51:06 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
+  const[foods, setFoods] = useState(foodsJson)
+  function deleteFood(foodId){
+    const newFood = [...foods].filter(food=>foodId != food.id)
+    setFoods(newFood)
+    }
+    function addFood(food){
+      setFoods([...foods, food])
+    }
+  
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <AddFoodForm addFoodFunction = {addFood}  />
+     {foods.map(food=>{
+      return (<div key = {food.id}><FoodBox food = {food} deleteFoodFunction = {deleteFood} /></div>) 
+     })}
+
+
     </div>
   );
 }
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,33 @@
-// Your code here
+import { useState } from "react"
+
+function AddFoodForm(){
+    const [name, setName] = useState("")
+    const [image, setImage] = useState("")
+    const [calories, setCalories] = useState(0)
+    const [servings, setServings] = useState(0)
+    const handleNameInput = event => setName(event.target.value);
+    const handleImageInput = event => setImage(event.target.value);
+    const handleCaloriesInput = event => setCalories(event.target.value);
+    const handleServingsInput = event => setServings(event.target.value);
+    function handleSubmit(event){
+        event.preventDefault()
+        const newFood = {"name" : name, "image" : image, "calories" : calories, "servings" : servings}
+        props.addFoodFunction(newFood)
+        }
+    return(
+       <div>
+        <form>
+            <label >Name:</label>
+            <input type="text" name="name" value={name} onChange={handleNameInput}/>
+            <label >Image:</label>
+            <input type="text" name="image" value={image} onChange={handleImageInput}/>
+            <label >Calories:</label>
+            <input type="number" name="calories" value={calories} onChange={handleCaloriesInput}/>
+            <label >Servings:</label>
+            <input type="number" name="servings" value={servings} onChange={handleServingsInput}/>
+        <button onClick={()=>handleSubmit} htmlType="submit">Create</button>
+        </form>
+       </div> 
+    )
+}
+export default AddFoodForm

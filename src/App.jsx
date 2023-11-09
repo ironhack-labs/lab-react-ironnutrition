153bd8c0f8f491d89be27c19fa0a1864a58ca425
Menatalla Gomaa <minafathi98@gmail.com>
2023-11-09 21:51:37 +0100

    submitting the lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import React, { useState } from "react";
 import "./App.css";
+import FoodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+
+
 
 function App() {
+
+  const [foodsState, setFoodsState] = useState('');
+
+
+
+  const deleteFood = (foodToDelete) => {
+    const updatedFoods = foodsState.filter((food) => food !== foodToDelete);
+    setFoodsState(updatedFoods);
+  };
+
+
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <h1>Food List</h1>
+
+      {FoodsJson.map((food) => (
+        <FoodBox key={food.name} food={food} deleteFood={deleteFood}/>
+      ))}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import foodsJson from "./foods.json";
+
+const Root =document.getElementById("root");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

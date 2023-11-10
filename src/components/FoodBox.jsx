dc4c6d926f8ec2c26c8256dcea9ea6b80a3f6209
Huseyin Hepyalniz <huseyinhepyalniz@Huseyins-Air.home>
2023-11-10 08:45:07 +0300

    done

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,20 @@
 // Your code here
+
+export default function FoodBox({ food, handleDelete }) {
+    return (
+      <div>
+        <p>{food.name}</p>
+  
+        <img src={food.image} />
+  
+        <p>Calories: {food.calories}</p>
+        <p>Servings {food.servings}</p>
+  
+        <p>
+          <b>Total Calories: {food.servings * food.calories} </b> kcal
+        </p>
+  
+        <button onClick={() => handleDelete(food.id)}>Delete</button>
+      </div>
+    );
+  }

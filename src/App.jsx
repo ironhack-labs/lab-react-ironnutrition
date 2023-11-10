b0bbc2d453bda0a28ae706882917b0c060a3a532
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-10 08:34:40 +0100

    Iteraion 1-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import { useState } from "react";
+import FoodBox from "./components/FoodBox";
+import foodsJson from "./foods.json";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const handleDelete = (foodId) => {
+    const newArr = foods.filter((foodId) => {
+      return foods.id !== foodId;
+    });
+    setFoods(newArr);
+  };
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+
+      {foodsJson.map((food, index) => {
+        return <FoodBox food={food} key={index} handleDelete={handleDelete} />;
+      })}
     </div>
   );
 }

7e86bbe7a735f15ffed4c5da64bf4545727b1a98
Nqbral <sim.chevalier@gmail.com>
2023-11-18 12:20:30 +0100

    done

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,65 @@
-// Your code here
+import { useState } from "react";
+
+function AddFoodForm(props) {
+  const [foodForm, setFoodForm] = useState({
+    name: "",
+    image: "",
+    calories: 0,
+    servings: 0,
+  });
+
+  const handleChange = (event) => {
+    const key = event.target.name;
+    const value = event.target.value;
+    setFoodForm({ ...foodForm, [key]: value });
+  };
+
+  return (
+    <div>
+      <h1>Add Food Entry</h1>
+      <form onSubmit={(event) => props.handleSubmit(event, foodForm)}>
+        <div>
+          <label htmlFor="name-input">Name</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            id="name-input"
+            placeholder="Name"
+            name="name"
+          />
+        </div>
+        <div>
+          <label htmlFor="image-input">Image</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            id="image-input"
+            placeholder="https://www.example.com/image.png"
+            name="image"
+          />
+        </div>
+        <div>
+          <label htmlFor="calories-input">Calories</label>
+          <input
+            type="number"
+            onChange={handleChange}
+            id="calories-input"
+            name="calories"
+          />
+        </div>
+        <div>
+          <label htmlFor="servings-input">Servings</label>
+          <input
+            type="number"
+            onChange={handleChange}
+            id="servings-input"
+            name="servings"
+          />
+        </div>
+        <button>Create</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddFoodForm;
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,24 @@
-// Your code here
+function FoodBox(props) {
+  return (
+    <div style={{ border: "1px solid grey" }}>
+      <p>{props.food.name}</p>
+      <img style={{ maxHeight: "100px" }} src={props.food.image} />
+      <p>Calories: {props.food.calories}</p>
+      <p>Servings: {props.food.servings}</p>
+
+      <p>
+        <b>Total Calories: {props.food.servings * props.food.calories}</b> kcal
+      </p>
+
+      <button
+        onClick={() => {
+          props.handleDelete(props.food.id);
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default FoodBox;

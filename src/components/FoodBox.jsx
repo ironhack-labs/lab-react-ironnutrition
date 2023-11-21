52dc4d96398fb70fe4231963cbf5643769b1745f
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-11-21 18:14:17 -0500

    complete IronNutrition

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,25 @@
-// Your code here
+const FoodBox = ({ food, onDelete }) => {
+  const { id,name, calories, image, servings } = food;
+  const totalCalories = servings * calories;
+const handleDelete = () => {
+  onDelete(id);
+};
+  return (
+    <div>
+      <p>{name}</p>
+
+      <img src={image} alt={name} style={{width: "300px"}} />
+
+      <p>Calories: {calories}</p>
+      <p>Servings: {servings}</p>
+
+      <p>
+        <b>Total Calories: {totalCalories} </b> kcal
+      </p>
+
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+};
+
+export default FoodBox;

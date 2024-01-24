471a4cdcff046212d569cc999ab812dbe803ec7f
Sergio Esteban <Sergioss772022@gmail.com>
2024-01-24 12:49:41 +0100

    [MAD-WD-PT-092023] Sergio Esteban

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+import foodsJson from './foods.json';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const handleDelete = (foodId) => {
+    const updateFoods = foods.filter((food) => food.id !== foodId);
+    setFoods(updateFoods);
+  };
+
+  const handleAddFood = (newFood) => {
+    setFoods([...foods, { ...newFood, id: foods.length +1 }]);
+  }
+
   return (
-    <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+    <div className="app-container">
+      <h1>Food List</h1>
+      <AddFoodForm onAddFood={handleAddFood} />
+      <div className="food-list-container">
+        {foods.map((food, index) => (
+          <FoodBox key={index} food={food} onDelete={handleDelete} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,24 @@
-// Your code here
+
+const FoodBox = ( {food, onDelete} ) => {
+    const { name, calories, image, servings } = food;
+
+
+    const handleDelete = () => {
+        onDelete(food.id)
+    };
+
+    return (
+        <div className="food-box">
+        <p className="food-name">{name}</p>
+        <img className="food-image" src={image} alt={name} />
+        <p className="food-info">Calories: {calories}</p>
+        <p className="food-info">Servings: {servings}</p>
+        <p className="food-info">
+          <b>Total Calories: {servings * calories} kcal</b>
+        </p>
+        <button className="delete-button" onClick={handleDelete}>Delete</button>
+      </div>
+    )
+}
+
+export default FoodBox;

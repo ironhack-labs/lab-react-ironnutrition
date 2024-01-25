fa8eb5f8cd868bfe5a63a4a36a89777780bde6c7
jorge Centeno <eljorgeceneno@gmail.com>
2024-01-25 21:38:00 +0100

    3 itineratoins

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { useState } from "react";
 import "./App.css";
-import foodsjson from './foods.json'
+import foodsjson from "./foods.json";
 import FoodBox from "./components/FoodBox";
 
 function App() {
-  const [foods,setFoods] = useState(foodsjson)
+  const [foods, setFoods] = useState(foodsjson);
   return (
     <div className="App">
-      <FoodBox food={ {
-  name: "Orange",
-  calories: 85,
-  image: "https://i.imgur.com/abKGOcv.jpg",
-  servings: 2
-} } 
-/>
       <h1>LAB | React IronNutrition</h1>
+      {foods.map((eachFood)=>{
+        return ( <FoodBox key={eachFood.id} food={eachFood}></FoodBox>)
+      })}
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -3,7 +3,7 @@ function FoodBox(props) {
     <div>
       <p>{props.food.name}</p>
 
-      <img src={props.food.image} alt="" />
+      <img src={props.food.image} alt="" height={"100"}/>
 
       <p>Calories: {props.food.calories}</p>
       <p>Servings {props.food.servings}</p>

0c8ed91948333cbe09d2315b8359327aceefbba4
Irene <amoreperdesi@gmail.com>
2024-01-25 22:18:15 +0100

    Updated code 1 and 2 iterations

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
 
   return (
     <div className="App">
-      <FoodBox food={{name: "Orange",calories: 85,image: "https://i.imgur.com/abKGOcv.jpg",servings: 1}}/>{" "}
+      <FoodBox
+        food={{
+          name: "Orange",
+          calories: 85,
+          image: "https://i.imgur.com/abKGOcv.jpg",
+          servings: 1,
+        }}
+      />{" "}
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,24 +1,25 @@
 // Your code here
 
 function FoodBox(props) {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <p>{props.food.name}</p>
-  
-          <img src= {props.food.image}/>
-  
-          <p>Calories: {props.food.calories}</p>
-          <p>Servings SERVINGS</p>
-  
-          <p>
-            <b>Total Calories: {props.food.servings * props.food.calories} </b> kcal
-          </p>
-  
-          <button>Delete</button>
-        </div>
+        <p>{props.food.name}</p>
+
+        <img src={props.food.image} />
+
+        <p>Calories: {props.food.calories}</p>
+        <p>Servings {props.food.servings}</p>
+
+        <p>
+          <b>Total Calories: {props.food.servings * props.food.calories} </b>{" "}
+          kcal
+        </p>
+
+        <button>Delete</button>
       </div>
-    );
-  }
-  
-  export default FoodBox;
+    </div>
+  );
+}
+
+export default FoodBox;

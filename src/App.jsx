f9f6ac21b46036aa49475c946af25defd4b551d1
moreira8125 <your_github_email>
2024-01-25 20:54:50 +0000

    mandatory iterations done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import "./App.css";
+import foodsJson from "./foods.json";
+import { useState } from "react";
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
+  const [food, setFood] = useState(foodsJson);
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <AddFoodForm food={food} setFood={setFood}></AddFoodForm>
+      <FoodBox food={food} setFood={setFood}></FoodBox>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,43 @@
+/* eslint-disable no-unused-vars */
 // Your code here
+import React from "react";
+
+function FoodBox(props) {
+  console.log(props.food);
+
+  function deleteFood(id) {
+    const filteredFood = props.food.filter((element) => {
+      return element.id !== id;
+    });
+    props.setFood(filteredFood);
+  }
+
+  return (
+    <div>
+      {props.food.map((element) => {
+        const caloriesPerServing = element.calories * element.servings;
+        return (
+          <div key={element.id}>
+            <p>{element.name}</p>
+            <img src={element.image} alt="" style={{ height: "200px" }} />
+            <p>Calories: {element.calories}</p>
+            <p>Servings: {element.servings}</p>
+            <p>
+              <b>Total Calories: {caloriesPerServing} kcal</b>
+            </p>
+            <button
+              onClick={() => {
+                deleteFood(element.id);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
+      <p></p>
+    </div>
+  );
+}
+
+export default FoodBox;

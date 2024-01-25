7a341c579e536b5af190f9d7e0cca5fb5796ab83
Irene <amoreperdesi@gmail.com>
2024-01-25 22:05:02 +0100

    1 and 2 iterations

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,24 @@
 // Your code here
+
+function FoodBox(props) {
+    return (
+      <div>
+        <div>
+          <p>{props.food.name}</p>
+  
+          <img src= {props.food.image}/>
+  
+          <p>Calories: {props.food.calories}</p>
+          <p>Servings SERVINGS</p>
+  
+          <p>
+            <b>Total Calories: {props.food.servings * props.food.calories} </b> kcal
+          </p>
+  
+          <button>Delete</button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default FoodBox;

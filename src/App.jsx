be7d0513e68ec63d6accbe1b8366b0b75d51b9d1
jorge Centeno <eljorgeceneno@gmail.com>
2024-01-26 23:08:18 +0100

    until itineration 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import foodsjson from "./foods.json";
 import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
   const [foods, setFoods] = useState(foodsjson);
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <AddFoodForm foods={foods} setFoods={setFoods}></AddFoodForm>
       {foods.map((eachFood)=>{
         return ( <FoodBox key={eachFood.id} food={eachFood} foods={foods} setFoods={setFoods}></FoodBox>)
+      
       })}
     </div>
   );
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,23 +1,42 @@
+import { useState } from "react"
 function AddFoodForm(props) {
-    
+    const [name,setName] = useState('')
+    const [image,setImage] = useState('')
+    const [calories,setCalories] = useState(0)
+    const [servings,setServings] = useState(0)
+
+    function handleSubmit(e){
+
+    e.preventDefault() 
+
+    const newFood ={name:name,image:image,calories:calories,servings:servings}
+
+    props.setFoods([newFood,...props.foods])
+
+    setName('')
+    setImage('')
+    setCalories(0)
+    setServings(0)
+}
     return (
-<form>
+<form onSubmit={handleSubmit}>
     <label>
         Name
-        <input type="text" name="name"></input>
+        <input type="text" onChange={(e)=>{setName(e.target.value)}} value={name}></input>
     </label>
     <label>
      Image
-     <input type="text" name="image"></input>
+     <input type="text" onChange={(e)=>{setImage(e.target.value)}} value={image}></input>
     </label>
     <label>
     Calories
-    <input type="Number" name="Calories"></input>
+    <input type="Number" onChange={(e)=>{setCalories(e.target.value)}} value={calories}></input>
     </label>
     <label>
     Servings
-    <input type="Number" name="Servings"></input>
+    <input type="Number" onChange={(e)=>{setServings(e.target.value)}} value={servings}></input>
     </label>
+    <button type="submit">Create</button>
 </form>
     );
   }

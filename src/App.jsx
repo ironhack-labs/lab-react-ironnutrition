a6d8f531174bf3a7fd61a0109b58914e5878b5b1
jltorres29 <jltorres29@hotmail.com>
2024-01-28 23:02:59 +0100

    iteracion 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import React, { useState, useEffect } from "react";
+import foodsData from './foods.json';
+import FoodBox from "./components/FoodBox";
+
 import "./App.css";
 
 function App() {
+const [foods, setFoods] = useState([]);
+
+useEffect(() => {
+
+  setFoods(foodsData);
+}, []);
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <h2 className="food-list">Food List</h2>
+      
+        <div className="row">
+
+        <FoodBox food={ {
+            name: "Orange",
+            calories: 85,
+            image: "https://i.imgur.com/abKGOcv.jpg",
+            servings: 1
+          } } />
+       
+        </div>
     </div>
+    
   );
 }
 
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,18 @@
 // Your code here
+
+const FoodBox = ({food = []}) => {
+    return (
+        <div key={food.id} className="card-body">
+            <h4 className="card-title">{food.name}</h4>
+            <img src={food.image} className="card-img-top" alt="image"/>
+            <p className="calories">Calories: {food.calories}</p>
+            <p className="servings">Servings: {food.servings}</p>
+            <p>
+              <b>Total Calories: {food.calories}</b> kcal {/*faltaria hacer la multiplicacion de porciones*calorias*/}
+            </p>
+            <button>Delete</button>
+        </div>
+    )
+}
+
+export default FoodBox

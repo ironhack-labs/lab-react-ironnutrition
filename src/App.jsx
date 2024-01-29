ba48368256c8704c7be8ccc77436fac8f06667b5
jltorres29 <jltorres29@hotmail.com>
2024-01-29 18:10:26 +0100

    iteracion 3 terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import React, { useState, useEffect } from "react";
-import foodsData from './foods.json';
+import React, { useState } from "react";
+import foodsData from "./foods.json";
 import FoodBox from "./components/FoodBox";
 
 import "./App.css";
 
 function App() {
-const [foods, setFoods] = useState([]);
-
-useEffect(() => {
-
-  setFoods(foodsData);
-}, []);
+  const [foods, setFoods] = useState(foodsData);
 
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
       <h2 className="food-list">Food List</h2>
-      
-        <div className="row">
 
-        <FoodBox food={ {
-            name: "Orange",
-            calories: 85,
-            image: "https://i.imgur.com/abKGOcv.jpg",
-            servings: 1
-          } } />
-       
-        </div>
+      <div className="row">
+        {foods.map((food, index) => (
+          <FoodBox key={index} food={food} />
+        ))}
+      </div>
     </div>
-    
   );
 }
 
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,14 +1,16 @@
 // Your code here
 
-const FoodBox = ({food = []}) => {
+const FoodBox = ({food}) => {
+    const {name, image, calories, servings} = food;
+    
     return (
-        <div key={food.id} className="card-body">
-            <h4 className="card-title">{food.name}</h4>
-            <img src={food.image} className="card-img-top" alt="image"/>
-            <p className="calories">Calories: {food.calories}</p>
-            <p className="servings">Servings: {food.servings}</p>
+        <div className="card-body">
+            <h4 className="card-title">{name}</h4>
+            <img src={image} className="card-img-top" alt="image"/>
+            <p className="calories">Calories: {calories}</p>
+            <p className="servings">Servings: {servings}</p>
             <p>
-              <b>Total Calories: {food.calories}</b> kcal {/*faltaria hacer la multiplicacion de porciones*calorias*/}
+              <b>Total Calories: {calories * servings}</b> kcal {/*faltaria hacer la multiplicacion de porciones*calorias*/}
             </p>
             <button>Delete</button>
         </div>

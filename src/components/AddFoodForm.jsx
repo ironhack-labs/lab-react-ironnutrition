71d90c9473dabb777dd469c4fbcf315df18cdc32
Marianna Di Vito <maridivi89@gmail.com>
2024-02-08 15:05:07 +0000

    done

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,87 @@
-// Your code here
+import { useState } from "react";
+
+export default function AddFoodForm({ onAddFood }) {
+  const [newFood, setNewFood] = useState({
+    name: "",
+    image: "",
+    calories: 0,
+    servings: 0,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewFood((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddFood({
+      name: newFood.name,
+      image: newFood.image,
+      calories: newFood.calories,
+      servings: newFood.servings,
+    });
+    setNewFood({
+      name: "",
+      image: "",
+      calories: 0,
+      servings: 0,
+    });
+  };
+
+  return (
+    <div>
+      <h2>Add Food Entry</h2>
+      <form onSubmit={handleSubmit} className="Form">
+        <label>
+          Name
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={newFood.name}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Image
+          <input
+            name="image"
+            type="url"
+            placeholder="https://example.com"
+            value={newFood.image}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Calories
+          <input
+            name="calories"
+            type="number"
+            placeholder="0"
+            value={newFood.calories}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Servings
+          <input
+            name="servings"
+            type="number"
+            placeholder="0"
+            value={newFood.servings}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+}
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,17 @@
-// Your code here
+export default function FoodBox({ food, deleteFood }) {
+  return (
+    <div className="FoodBox">
+      <p>{food.name}</p>
+      <img src={food.image} />
+
+      <p>Calories: {food.calories}</p>
+      <p>Servings: {food.servings}</p>
+
+      <p>
+        <b>Total Calories: {food.servings * food.calories} </b> kcal
+      </p>
+
+      <button onClick={deleteFood}>Delete</button>
+    </div>
+  );
+}

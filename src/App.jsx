8a2d01207977701f22e471c2c16e6231bcc87cac
HisokaHX <luca.tobiello92@gmail.comgit config --global user.email luca.tobiello92@gmail.com>
2024-02-09 19:46:23 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import foodJson from './foods.json';
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
+  const [foods, setFoods] = useState(foodJson);
+
+  const handleOnDelete = (id) => {
+    const deleteFoods = foods.filter(food => food.id !== id)
+    setFoods(deleteFoods)
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+
+      <AddFoodForm addFood={(newFood) => setFoods([...foods, newFood])} />
+
+      {foods.map((food, index) => {
+        return <FoodBox key={index} food={food} onDelete={handleOnDelete} />
+      })}
+
     </div>
   );
 }
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,72 @@
-// Your code here
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+
+const AddFoodForm = ({ addFood }) => {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newFood = {
+      name,
+      image,
+      calories,
+      servings,
+    };
+    addFood(newFood);
+    setName('');
+    setImage('');
+    setCalories(0);
+    setServings(0);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="image">Image</label>
+        <input
+          type="text"
+          id="image"
+          name="image"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="calories">Calories</label>
+        <input
+          type="number"
+          id="calories"
+          name="calories"
+          value={calories}
+          onChange={(e) => setCalories(Number(e.target.value))}
+        />
+      </div>
+      <div>
+        <label htmlFor="servings">Servings</label>
+        <input
+          type="number"
+          id="servings"
+          name="servings"
+          value={servings}
+          onChange={(e) => setServings(Number(e.target.value))}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  );
+};
+
+export default AddFoodForm;

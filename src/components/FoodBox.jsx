6f417461de50777cb7c37e97a4e9368d9204b8f7
Vitor193 <vitormiguel135@gmail.com>
2024-02-26 14:38:53 +0000

    done

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,18 @@
 // Your code here
+
+const FoodBox = ({ food }) => (
+
+    <div className="food-box">
+
+        <div>
+            <h1>{food.name}</h1>
+            <img src={food.image} alt={food.name} />
+            <p>Calories: {food.calories}</p>
+            <p>Servings: {food.servings}</p>
+            <p>Total calories: {food.servings * food.calories}</p>
+            <button>Delete</button>
+        </div>
+    </div>
+)
+
+export default FoodBox

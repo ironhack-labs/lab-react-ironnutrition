a91145651d5ee14e7b419a95cabb51e12c83c0a4
Idir FOUQUET <fouquetidir@gmail.com>
2024-03-07 21:13:10 +0100

    Iteration 1 & 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import FoodBox from "./components/FoodBox";
+import foodsJson from "./foods.json";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+    <FoodBox food={{
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+}}/>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,20 @@
-// Your code here
+function FoodBox({food}) {
+  return (
+    <div>
+  <p>{food.name}</p>
+
+  <img src={food.image} />
+
+  <p>Calories: {food.calories}</p>
+  <p>Servings {food.servings}</p>
+
+  <p>
+    <b>Total Calories: {food.servings * food.calories} </b> kcal
+  </p>
+
+  <button>Delete</button>
+</div>
+  )
+}
+
+export default FoodBox

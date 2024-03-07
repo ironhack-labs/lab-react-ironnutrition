57be840214fa637388730c2e4a24f2b04c2f6028
guzegui <guzmanegui@gmail.com>
2024-03-07 15:40:11 +0100

    Done Iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import "./App.css";
-import foodsJson from "./foods.json";
 import { useState } from "react";
-
-const [foods, setFoods] = useState(JSON.parse(foodsJson));
+import foodsJson from "./foods.json";
+import "./App.css";
+import FoodBox from "./components/FoodBox";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  let orangeIndex = foods.findIndex((element) => element.name === "Orange");
+
+  console.log(orangeIndex)
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <FoodBox food={foods[14]} />
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,23 @@
-// Your code here
+function FoodBox(props) {
+
+const {name, image, calories, servings} = props.food;
+
+  return (
+    <div className="FoodBox">
+      <p>{name}</p>
+
+      <img src={image} />
+
+      <p>Calories: {calories} </p>
+      <p>Servings {servings}</p>
+
+      <p>
+        <b>Total Calories: {servings * calories} </b> kcal
+      </p>
+
+      <button>Delete</button>
+    </div>
+  );
+}
+
+export default FoodBox;

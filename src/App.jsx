d7b1915ce6243f2c384b18d5535f83663d29ca5b
Idir FOUQUET <fouquetidir@gmail.com>
2024-03-08 18:28:13 +0100

    Iteration 6 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from "react";
 import "./App.css";
-import FoodBox from "./components/FoodBox";
-import AddFoodForm from "./components/AddFoodForm";
-import foodsJson from "./foods.json";
+import FoodList from "./components/FoodList";
+
 
 function App() {
 
-  const addNewFood = (foodItem) => {
-    setFoodList([...foodList, foodItem])
-  }
-
-  const deleteFoodItem = (id) => {
-    const newArray = [...foodList] 
-    newArray.forEach((food) => {
-      if (food.id == id) {
-        const foodIndex = newArray.indexOf(food)
-        newArray.splice(foodIndex, 1)
-      }
-    } )
-    setFoodList(newArray)
-  }
-
-  const [foodList, setFoodList] = useState(foodsJson)
   return (
     
     <div className="App">
-    <AddFoodForm addNewFood={addNewFood}/>
-    {foodList.map((food) => (<FoodBox deleteFoodItem={deleteFoodItem} key={food.id} food={food}/>)
-    )}
+      <FoodList/>
     </div>
   );
 }
---
+++ src/components/FoodList.jsx
@@ -0,0 +1,35 @@
+import FoodBox from "./FoodBox";
+import { useState } from "react";
+import AddFoodForm from "./AddFoodForm";
+import foodsJson from "../foods.json";
+
+function FoodList() {
+
+    const addNewFood = (foodItem) => {
+        setFoodList([...foodList, foodItem])
+      }
+    
+      const deleteFoodItem = (id) => {
+        const newArray = [...foodList] 
+        newArray.forEach((food) => {
+          if (food.id == id) {
+            const foodIndex = newArray.indexOf(food)
+            newArray.splice(foodIndex, 1)
+          }
+        } )
+        setFoodList(newArray)
+      }
+    
+      const [foodList, setFoodList] = useState(foodsJson)
+
+  return (
+    <div>
+      <AddFoodForm addNewFood={addNewFood} />
+      {foodList.map((food) => (
+        <FoodBox deleteFoodItem={deleteFoodItem} key={food.id} food={food} />
+      ))}
+    </div>
+  );
+}
+
+export default FoodList;

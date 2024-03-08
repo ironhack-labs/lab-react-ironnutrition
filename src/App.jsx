a4e8cb13c2e575fb74bb939d189efab8178a5be1
Nawelchafi <chafinawel@gmail.com>
2024-03-09 00:51:51 +0100

    done deleting objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./App.css";
-
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
 function App() {
+  const [food, setFood] = useState(foodsJson)
+  function deleteFood(id) {
+
+    const newFoodList = food.filter((item) => { return item.id !== id })
+    return setFood(newFoodList)
+  }
+
+  const foodlist = food.map((item) => <FoodBox key={item.id} food={item} handleDelete={deleteFood} />)
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <div>{foodlist}</div>
     </div>
   );
 }
